ee80f1e10291739f1bfa4f42e089693822a5a603
Jonathan Ellis <jbellis@apache.org>
2009-12-12 17:50:13 +0000

    merge r889937 from trunk
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cassandra/branches/cassandra-0.5@889954 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -145,7 +145,7 @@ public class StorageProxy implements StorageProxyMBean
                     Message hintedMessage = rm.makeRowMutationMessage();
                     hintedMessage.addHeader(RowMutation.HINT, target.getAddress());
                     if (logger.isDebugEnabled())
-                        logger.debug("insert writing key " + rm.key() + " to " + unhintedMessage.getMessageId() + "@" + hintedTarget + " for " + target);
+                        logger.debug("insert writing key " + rm.key() + " to " + hintedMessage.getMessageId() + "@" + hintedTarget + " for " + target);
                     MessagingService.instance().sendOneWay(hintedMessage, hintedTarget);
                 }
             }
