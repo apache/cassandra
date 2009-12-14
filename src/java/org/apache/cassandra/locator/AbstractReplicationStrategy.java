82d843bad887b2858c94f559d66a55bce37fc3a9
Jonathan Ellis <jbellis@apache.org>
2009-12-14 22:29:26 +0000

    fix add that should have been addAll.  patch by Jaakko Laine; reviewed by jbellis for CASSANDRA-603
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cassandra/trunk@890507 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
+++ src/java/org/apache/cassandra/locator/AbstractReplicationStrategy.java
@@ -91,11 +91,11 @@ public abstract class AbstractReplicationStrategy
 
         List<InetAddress> endpoints = new ArrayList<InetAddress>(naturalEndpoints);
 
-        for (Map.Entry<Range, InetAddress> entry : tokenMetadata_.getPendingRanges().entrySet())
+        for (Map.Entry<Range, Collection<InetAddress>> entry : tokenMetadata_.getPendingRanges().entrySet())
         {
             if (entry.getKey().contains(token))
             {
-                endpoints.add(entry.getValue());
+                endpoints.addAll(entry.getValue());
             }
         }
 
