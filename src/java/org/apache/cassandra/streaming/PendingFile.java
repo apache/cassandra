449e33cdf1c43e75f33b1f2da429a2a26edc8694
Jonathan Ellis <jbellis@apache.org>
2010-01-31 04:02:07 +0000

    add missing PendingFile.java
    git-svn-id: https://svn.apache.org/repos/asf/incubator/cassandra/trunk@904973 13f79535-47bb-0310-9956-ffa450edef68

---
+++ src/java/org/apache/cassandra/streaming/PendingFile.java
@@ -0,0 +1,90 @@
+package org.apache.cassandra.streaming;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+import org.apache.cassandra.io.ICompactSerializer;
+
+class PendingFile
+{
+    private static ICompactSerializer<PendingFile> serializer_;
+
+    static
+    {
+        serializer_ = new InitiatedFileSerializer();
+    }
+
+    public static ICompactSerializer<PendingFile> serializer()
+    {
+        return serializer_;
+    }
+
+    private String targetFile_;
+    private long expectedBytes_;
+    private String table_;
+
+    public PendingFile(String targetFile, long expectedBytes, String table)
+    {
+        targetFile_ = targetFile;
+        expectedBytes_ = expectedBytes;
+        table_ = table;
+    }
+
+    public String getTable()
+    {
+        return table_;
+    }
+
+    public String getTargetFile()
+    {
+        return targetFile_;
+    }
+
+    public void setTargetFile(String file)
+    {
+        targetFile_ = file;
+    }
+
+    public long getExpectedBytes()
+    {
+        return expectedBytes_;
+    }
+
+    public boolean equals(Object o)
+    {
+        if ( !(o instanceof PendingFile) )
+            return false;
+
+        PendingFile rhs = (PendingFile)o;
+        return targetFile_.equals(rhs.targetFile_);
+    }
+
+    public int hashCode()
+    {
+        return toString().hashCode();
+    }
+
+    public String toString()
+    {
+        return targetFile_ + ":" + expectedBytes_;
+    }
+
+    private static class InitiatedFileSerializer implements ICompactSerializer<PendingFile>
+    {
+        public void serialize(PendingFile sc, DataOutputStream dos) throws IOException
+        {
+            dos.writeUTF(sc.targetFile_);
+            dos.writeLong(sc.expectedBytes_);
+            dos.writeUTF(sc.table_);
+        }
+
+        public PendingFile deserialize(DataInputStream dis) throws IOException
+        {
+            String targetFile = dis.readUTF();
+            long expectedBytes = dis.readLong();
+            String table = dis.readUTF();
+            return new PendingFile(targetFile, expectedBytes, table);
+        }
+    }
+}
