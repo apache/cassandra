ca5a747b8e88310fd2a6f52afbb71a49d1ebd99f
Jonathan Ellis <jbellis@apache.org>
2010-03-16 23:16:55 +0000

    warn on gossip from wrong cluster.  patch by Brandon Williams; reviewed by jbellis for CASSANDRA-769
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.6@924043 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -891,6 +891,10 @@ public class Gossiper implements IFailureDetectionEventListener, IEndPointStateC
             {
                 Gossiper.instance.join(from);
             }
+            else
+            {
+                logger_.warn("ClusterName mismatch from " + from + " " + joinMessage.clusterId_  + "!=" + DatabaseDescriptor.getClusterName());
+            }
         }
     }
 
@@ -912,7 +916,10 @@ public class Gossiper implements IFailureDetectionEventListener, IEndPointStateC
                 GossipDigestSynMessage gDigestMessage = GossipDigestSynMessage.serializer().deserialize(dis);
                 /* If the message is from a different cluster throw it away. */
                 if ( !gDigestMessage.clusterId_.equals(DatabaseDescriptor.getClusterName()) )
+                {
+                    logger_.warn("ClusterName mismatch from " + from + " " + gDigestMessage.clusterId_  + "!=" + DatabaseDescriptor.getClusterName());
                     return;
+                }
 
                 List<GossipDigest> gDigestList = gDigestMessage.getGossipDigests();
                 /* Notify the Failure Detector */
