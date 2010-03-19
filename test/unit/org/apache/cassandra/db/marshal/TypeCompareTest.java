53347c91870718987dac8aca02997eefe3327a08
Jonathan Ellis <jbellis@apache.org>
2010-03-19 20:26:45 +0000

    use lexical comparison if time part of uuid is the same.  patch by Sylvain Lebresne and jbellis for CASSANDRA-907
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.6@925407 13f79535-47bb-0310-9956-ffa450edef68

--- test/unit/org/apache/cassandra/db/marshal/TypeCompareTest.java
+++ test/unit/org/apache/cassandra/db/marshal/TypeCompareTest.java
@@ -22,9 +22,10 @@ package org.apache.cassandra.db.marshal;
 
 
 import java.io.UnsupportedEncodingException;
+import java.nio.ByteBuffer;
+import java.util.UUID;
 
 import org.apache.commons.lang.ArrayUtils;
-
 import org.junit.Test;
 
 public class TypeCompareTest
@@ -65,5 +66,22 @@ public class TypeCompareTest
         assert comparator.compare("z".getBytes("UTF-8"), "a".getBytes("UTF-8")) > 0;
         assert comparator.compare("z".getBytes("UTF-8"), "z".getBytes("UTF-8")) == 0;
         assert comparator.compare("a".getBytes("UTF-8"), "z".getBytes("UTF-8")) < 0;
+    }
+
+    @Test
+    public void testTimeUUID()
+    {
+        // two different UUIDs w/ the same timestamp
+        UUID uuid1 = UUID.fromString("1077e700-c7f2-11de-86d5-f5bcc793a028");
+        byte[] bytes1 = new byte[16];
+        ByteBuffer bb1 = ByteBuffer.wrap(bytes1);
+        bb1.putLong(uuid1.getMostSignificantBits());  bb1.putLong(uuid1.getLeastSignificantBits());
+
+        UUID uuid2 = UUID.fromString("1077e700-c7f2-11de-982e-6fad363d5f29");
+        byte[] bytes2 = new byte[16];
+        ByteBuffer bb2 = ByteBuffer.wrap(bytes2);
+        bb2.putLong(uuid2.getMostSignificantBits());  bb2.putLong(uuid2.getLeastSignificantBits());
+
+        assert new TimeUUIDType().compare(bytes1, bytes2) != 0;
     }
 }
