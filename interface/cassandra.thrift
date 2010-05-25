dea8015cb79e512a0252ae5d2eadd5ff1b8880be
Jonathan Ellis <jbellis@apache.org>
2010-05-25 01:09:16 +0000

    belatedly document that CL.ALL is supported for reads
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.6@947886 13f79535-47bb-0310-9956-ffa450edef68

--- interface/cassandra.thrift
+++ interface/cassandra.thrift
@@ -151,7 +151,7 @@ exception AuthorizationException {
  *              calls will have correct data even if the initial read gets an older value. (This is called 'read repair'.)
  *      QUORUM  Will query all storage nodes and return the record with the most recent timestamp once it has at least a
  *              majority of replicas reported. Again, the remaining replicas will be checked in the background.
- *      ALL     Not yet supported, but we plan to eventually.
+ *      ALL     Queries all storage nodes and returns the record with the most recent timestamp.
 */
 enum ConsistencyLevel {
     ZERO = 0,
