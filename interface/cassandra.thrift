00cf0489054c4a95cd197c52e45b01f85829b520
Jonathan Ellis <jbellis@apache.org>
2010-05-25 01:17:27 +0000

    document DatacenterShardStrategy.  patch by jbellis
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@947889 13f79535-47bb-0310-9956-ffa450edef68

--- interface/cassandra.thrift
+++ interface/cassandra.thrift
@@ -137,22 +137,23 @@ exception AuthorizationException {
  * allowing availability in the face of node failures up to half of <ReplicationFactor>. Of course if latency is more
  * important than consistency then you can use lower values for either or both.
  *
- * Write:
- *      ZERO    Ensure nothing. A write happens asynchronously in background
- *      ANY     Ensure that the write has been written once somewhere, including possibly being hinted in a non-target node.
- *      ONE     Ensure that the write has been written to at least 1 node's commit log and memory table before responding to the client.
- *      QUORUM  Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes before responding to the client.
- *      ALL     Ensure that the write is written to <code>&lt;ReplicationFactor&gt;</code> nodes before responding to the client.
+ * Write consistency levels make the following guarantees before reporting success to the client:
+ *   ZERO         Ensure nothing. A write happens asynchronously in background
+ *   ANY          Ensure that the write has been written once somewhere, including possibly being hinted in a non-target node.
+ *   ONE          Ensure that the write has been written to at least 1 node's commit log and memory table
+ *   QUORUM       Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes
+ *   DCQUORUM     Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes, within the local datacenter (requires DatacenterShardStrategy)
+ *   DCQUORUMSYNC Ensure that the write has been written to <ReplicationFactor> / 2 + 1 nodes in each datacenter (requires DatacenterShardStrategy)
+ *   ALL          Ensure that the write is written to <code>&lt;ReplicationFactor&gt;</code> nodes before responding to the client.
  *
  * Read:
- *      ZERO    Not supported, because it doesn't make sense.
- *      ANY     Not supported. You probably want ONE instead.
- *      ONE     Will return the record returned by the first node to respond. A consistency check is always done in a
- *              background thread to fix any consistency issues when ConsistencyLevel.ONE is used. This means subsequent
- *              calls will have correct data even if the initial read gets an older value. (This is called 'read repair'.)
- *      QUORUM  Will query all storage nodes and return the record with the most recent timestamp once it has at least a
- *              majority of replicas reported. Again, the remaining replicas will be checked in the background.
- *      ALL     Queries all storage nodes and returns the record with the most recent timestamp.
+ *   ZERO         Not supported, because it doesn't make sense.
+ *   ANY          Not supported. You probably want ONE instead.
+ *   ONE          Will return the record returned by the first node to respond. A consistency check is always done in a background thread to fix any consistency issues when ConsistencyLevel.ONE is used. This means subsequent calls will have correct data even if the initial read gets an older value. (This is called 'read repair'.)
+ *   QUORUM       Will query all storage nodes and return the record with the most recent timestamp once it has at least a majority of replicas reported. Again, the remaining replicas will be checked in the background.
+ *   DCQUORUM     Returns the record with the most recent timestamp once a majority of replicas within the local datacenter have replied.
+ *   DCQUORUMSYNC Returns the record with the most recent timestamp once a majority of replicas within each datacenter have replied.
+ *   ALL          Queries all storage nodes and returns the record with the most recent timestamp.
 */
 enum ConsistencyLevel {
     ZERO = 0,
