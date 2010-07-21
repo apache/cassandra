c2d748c061f58ad5fa0e022ae6116fd92a1a93f3
Gary Dusbabek <gdusbabek@apache.org>
2010-07-21 15:13:59 +0000

    split OutputBuffer from DataOutputBuffer. patch by stuhood, reviewed by gdusbabek. CASSANDRA-1186
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@966274 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/io/util/OutputBuffer.java
+++ src/java/org/apache/cassandra/io/util/OutputBuffer.java
@@ -23,56 +23,53 @@ import java.io.DataInput;
 import java.io.DataOutputStream;
 import java.io.IOException;
 
-
 /**
- * An implementation of the DataOutputStream interface. This class is completely thread
- * unsafe.
+ * Extends ByteArrayOutputStream to minimize copies.
  */
-public final class DataOutputBuffer extends DataOutputStream
+public final class OutputBuffer extends ByteArrayOutputStream
 {
-    public DataOutputBuffer()
+    public OutputBuffer()
     {
         this(128);
     }
-    
-    public DataOutputBuffer(int size)
+
+    public OutputBuffer(int size)
     {
-        super(new OutputBuffer(size));
-    }
-    
-    private OutputBuffer buffer()
-    {
-        return (OutputBuffer)out;
+        super(size);
     }
 
+    public byte[] getData()
+    {
+        return buf;
+    }
+    
+    public int getLength()
+    {
+        return count;
+    }
+    
+    public void write(DataInput in, int len) throws IOException
+    {
+        int newcount = count + len;
+        if (newcount > buf.length)
+        {
+            byte newbuf[] = new byte[Math.max(buf.length << 1, newcount)];
+            System.arraycopy(buf, 0, newbuf, 0, count);
+            buf = newbuf;
+        }
+        in.readFully(buf, count, len);
+        count = newcount;
+    }
+    
     /**
      * @return The valid contents of the buffer, possibly by copying: only safe for one-time-use buffers.
      */
     public byte[] asByteArray()
     {
-        return buffer().asByteArray();
-    }
-    
-    /**
-     * Returns the current contents of the buffer. Data is only valid to
-     * {@link #getLength()}.
-     */
-    public byte[] getData()
-    {
-        return buffer().getData();
-    }
-    
-    /** Returns the length of the valid data currently in the buffer. */
-    public int getLength()
-    {
-        return buffer().getLength();
-    }
-    
-    /** Resets the buffer to empty. */
-    public DataOutputBuffer reset()
-    {
-        this.written = 0;
-        buffer().reset();
-        return this;
+        if (count == buf.length)
+            // no-copy
+            return buf;
+        // copy
+        return this.toByteArray();
     }
 }
