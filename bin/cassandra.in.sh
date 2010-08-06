6f2a92bdaa4ca1cd32c47c2ccb9def0b42cb9479
Eric Evans <eevans@apache.org>
2010-08-06 01:25:07 +0000

    move JVM_OPTS from cassandra.in.sh to the conf/ dir
    Patch by Nick Bailey; review by eevans for CASSANDRA-1347
    
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@982845 13f79535-47bb-0310-9956-ffa450edef68

--- bin/cassandra.in.sh
+++ bin/cassandra.in.sh
@@ -14,16 +14,19 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-
-cassandra_home=`dirname $0`/..
+if [ "x$CASSANDRA_HOME" = "x" ]; then
+    CASSANDRA_HOME=`dirname $0`/..
+fi
 
 # The directory where Cassandra's configs live (required)
-CASSANDRA_CONF=$cassandra_home/conf
+if [ "x$CASSANDRA_CONF" = "x" ]; then
+    CASSANDRA_CONF=$CASSANDRA_HOME/conf
+fi
 
 # This can be the path to a jar file, or a directory containing the 
 # compiled classes. NOTE: This isn't needed by the startup script,
 # it's just used here in constructing the classpath.
-cassandra_bin=$cassandra_home/build/classes
+cassandra_bin=$CASSANDRA_HOME/build/classes
 #cassandra_bin=$cassandra_home/build/cassandra.jar
 
 # JAVA_HOME can optionally be set here
@@ -32,25 +35,6 @@ cassandra_bin=$cassandra_home/build/classes
 # The java classpath (required)
 CLASSPATH=$CASSANDRA_CONF:$cassandra_bin
 
-for jar in $cassandra_home/lib/*.jar; do
+for jar in $CASSANDRA_HOME/lib/*.jar; do
     CLASSPATH=$CLASSPATH:$jar
 done
-
-# Arguments to pass to the JVM
-JVM_OPTS=" \
-        -ea \
-        \
-        -Xms256M \
-        -Xmx1G \
-        -Xss128k \
-        \
-        -XX:+UseParNewGC \
-        -XX:+UseConcMarkSweepGC \
-        -XX:+CMSParallelRemarkEnabled \
-        -XX:SurvivorRatio=8 \
-        -XX:MaxTenuringThreshold=1 \
-        -XX:+HeapDumpOnOutOfMemoryError \
-        \
-        -Dcom.sun.management.jmxremote.port=8080 \
-        -Dcom.sun.management.jmxremote.ssl=false \
-        -Dcom.sun.management.jmxremote.authenticate=false"
