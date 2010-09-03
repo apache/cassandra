95f017207de0925cff6f714202f19d8ece8854d6
Jonathan Ellis <jbellis@apache.org>
2010-09-03 15:41:47 +0000

    merge from 0.6
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@992333 13f79535-47bb-0310-9956-ffa450edef68

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -57,7 +57,8 @@ JVM_OPTS="$JVM_OPTS -XX:+UseConcMarkSweepGC"
 JVM_OPTS="$JVM_OPTS -XX:+CMSParallelRemarkEnabled" 
 JVM_OPTS="$JVM_OPTS -XX:SurvivorRatio=8" 
 JVM_OPTS="$JVM_OPTS -XX:MaxTenuringThreshold=1"
-JVM_OPTS="$JVM_OPTS -XX:CMSInitiatingOccupancyFraction=80"
+JVM_OPTS="$JVM_OPTS -XX:CMSInitiatingOccupancyFraction=75"
+JVM_OPTS="$JVM_OPTS -XX:UseCMSInitiatingOccupancyOnly"
 
 # jmx: metrics and administration interface
 # 
