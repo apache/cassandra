a61c338d9db83492ff6dba2c7009cce6cbd5cd7a
Jonathan Ellis <jbellis@apache.org>
2010-10-02 05:06:52 +0000

    merge from 0.6
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@1003743 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/db/commitlog/CommitLog.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLog.java
@@ -422,12 +422,6 @@ public class CommitLog
             logger.debug("discard completed log segments for " + context + ", column family " + id + ".");
 
         /*
-         * log replay assumes that we only have to look at entries past the last
-         * flush position, so verify that this flush happens after the last. See CASSANDRA-936
-        */
-        assert context.position >= context.getSegment().getHeader().getPosition(id)
-               : "discard at " + context + " is not after last flush at " + context.getSegment().getHeader().getPosition(id);
-        /*
          * Loop through all the commit log files in the history. Now process
          * all files that are older than the one in the context. For each of
          * these files the header needs to modified by resetting the dirty
