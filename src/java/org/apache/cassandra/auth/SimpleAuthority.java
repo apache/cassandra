67198f00bc294c7450933477d16f26aa808e3a6e
Eric Evans <eevans@apache.org>
2010-10-06 15:19:07 +0000

    refactor SimpleAuthority for CF resources
    Patch by eevans; reviewed by Stu Hood for CASSANDRA-1554
    
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@1005079 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/auth/SimpleAuthority.java
+++ src/java/org/apache/cassandra/auth/SimpleAuthority.java
@@ -33,52 +33,110 @@ public class SimpleAuthority implements IAuthority
     public final static String ACCESS_FILENAME_PROPERTY = "access.properties";
     // magical property for WRITE permissions to the keyspaces list
     public final static String KEYSPACES_WRITE_PROPERTY = "<modify-keyspaces>";
+    private Properties accessProperties = null;
 
     @Override
     public EnumSet<Permission> authorize(AuthenticatedUser user, List<Object> resource)
     {
         if (resource.size() < 2 || !Resources.ROOT.equals(resource.get(0)) || !Resources.KEYSPACES.equals(resource.get(1)))
-            // we only know how to handle keyspace authorization
             return Permission.NONE;
-
-        String keyspace;
-        EnumSet<Permission> authorized;
-        if (resource.size() < 3)
+        
+        String keyspace, columnFamily = null;
+        EnumSet<Permission> authorized = Permission.NONE;
+        
+        // /cassandra/keyspaces
+        if (resource.size() == 2)
         {
-            // authorize the user for the keyspace list using the 'magical' keyspace,
-            // but give them read access by default
             keyspace = KEYSPACES_WRITE_PROPERTY;
             authorized = EnumSet.of(Permission.READ);
         }
+        // /cassandra/keyspaces/<keyspace name>
+        else if (resource.size() == 3)
+        {
+            keyspace = (String)resource.get(2);
+        }
+        // /cassandra/keyspaces/<keyspace name>/<cf name>
+        else if (resource.size() == 4)
+        {
+            keyspace = (String)resource.get(2);
+            columnFamily = (String)resource.get(3);
+        }
         else
         {
-            // otherwise, authorize them for the actual keyspace
-            keyspace = (String)resource.get(2);
-            authorized = Permission.NONE;
+            // We don't currently descend any lower in the hierarchy.
+            throw new UnsupportedOperationException();
         }
-
-        String afilename = System.getProperty(ACCESS_FILENAME_PROPERTY);
+        
+        String accessFilename = System.getProperty(ACCESS_FILENAME_PROPERTY);
         try
         {
-            FileInputStream in = new FileInputStream(afilename);
-            Properties props = new Properties();
-            props.load(in);
-            in.close();
-
-            // structure:
-            // given keyspace X, users A B and C can be authorized like this (separate their names with spaces):
-            // X = A B C
+            // TODO: auto-reload when the file has been updated
+            if (accessProperties == null)   // Don't hit the disk on every invocation
+            {
+                FileInputStream in = new FileInputStream(accessFilename);
+                accessProperties = new Properties();
+                accessProperties.load(in);
+                in.close();
+            }
             
-            if (null == props.getProperty(keyspace))
-                // no one is authorized
-                return authorized;
-            for (String allow : props.getProperty(keyspace).split(","))
-                if (allow.equals(user.username))
-                    authorized = Permission.ALL;
+            // Special case access to the keyspace list
+            if (keyspace == KEYSPACES_WRITE_PROPERTY)
+            {
+                String kspAdmins = accessProperties.getProperty(KEYSPACES_WRITE_PROPERTY);
+                for (String admin : kspAdmins.split(","))
+                    if (admin.equals(user.username))
+                        return Permission.ALL;
+            }
+            
+            boolean canRead = false, canWrite = false;
+            String readers = null, writers = null;
+            
+            if (columnFamily == null)
+            {    
+                readers = accessProperties.getProperty(keyspace + ".<ro>");
+                writers = accessProperties.getProperty(keyspace + ".<rw>");
+            }
+            else
+            {
+                readers = accessProperties.getProperty(keyspace + "." + columnFamily + ".<ro>");
+                writers = accessProperties.getProperty(keyspace + "." + columnFamily + ".<rw>");
+            }
+            
+            if (readers != null)
+            {
+                for (String reader : readers.split(","))
+                {
+                    if (reader.equals(user.username))
+                    {
+                        canRead = true;
+                        break;
+                    }
+                }
+            }
+            
+            if (writers != null)
+            {
+                for (String writer : writers.split(","))
+                {
+                    if (writer.equals(user.username))
+                    {
+                        canWrite = true;
+                        break;
+                    }
+                }
+            }
+            
+            if (canWrite)
+                authorized = Permission.ALL;
+            else if (canRead)
+                authorized = EnumSet.of(Permission.READ);
+                
         }
         catch (IOException e)
         {
-            throw new RuntimeException(String.format("Authorization table file '%s' could not be opened: %s", afilename, e.getMessage()));
+            throw new RuntimeException(String.format("Authorization table file '%s' could not be opened: %s",
+                                                     accessFilename,
+                                                     e.getMessage()));
         }
 
         return authorized;
