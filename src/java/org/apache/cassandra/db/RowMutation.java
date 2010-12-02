f0104cdce5ec423ea4b47f0b3bbd8a7b8c8440a6
Jonathan Ellis <jbellis@apache.org>
2010-12-02 17:42:15 +0000

    revert last
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1041489 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/db/RowMutation.java
+++ src/java/org/apache/cassandra/db/RowMutation.java
@@ -47,7 +47,6 @@ public class RowMutation
 {
     private static ICompactSerializer<RowMutation> serializer_;
     public static final String HINT = "HINT";
-    public static final String FORWARD_HEADER = "FORWARD";
 
     static
     {
