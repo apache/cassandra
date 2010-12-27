f100754ae8b1217d03719efc9c1441f6fc086fed
Gary Dusbabek <gdusbabek@apache.org>
2010-12-27 23:25:27 +0000

    set DEFAULT_REPLICATE_ON_WRITE to false. patch by gdusbabek, reviewed by eevans. CASSANDRA-1903
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@1053223 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/config/CFMetaData.java
+++ src/java/org/apache/cassandra/config/CFMetaData.java
@@ -50,7 +50,7 @@ public final class CFMetaData
     public final static double DEFAULT_ROW_CACHE_SIZE = 0.0;
     public final static double DEFAULT_KEY_CACHE_SIZE = 200000;
     public final static double DEFAULT_READ_REPAIR_CHANCE = 1.0;
-    public final static boolean DEFAULT_REPLICATE_ON_WRITE = true;
+    public final static boolean DEFAULT_REPLICATE_ON_WRITE = false;
     public final static int DEFAULT_ROW_CACHE_SAVE_PERIOD_IN_SECONDS = 0;
     public final static int DEFAULT_KEY_CACHE_SAVE_PERIOD_IN_SECONDS = 3600;
     public final static int DEFAULT_GC_GRACE_SECONDS = 864000;
