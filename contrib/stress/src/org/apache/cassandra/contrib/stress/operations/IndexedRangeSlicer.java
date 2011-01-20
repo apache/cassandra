2a3fee59ae9b44711d720de9503bace14d360cf7
Jonathan Ellis <jbellis@apache.org>
2011-01-20 20:32:25 +0000

    rename tests -> operations
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1061497 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/stress/src/org/apache/cassandra/contrib/stress/operations/IndexedRangeSlicer.java
+++ contrib/stress/src/org/apache/cassandra/contrib/stress/operations/IndexedRangeSlicer.java
@@ -15,7 +15,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.apache.cassandra.contrib.stress.tests;
+package org.apache.cassandra.contrib.stress.operations;
 
 import org.apache.cassandra.contrib.stress.util.OperationThread;
 import org.apache.cassandra.thrift.*;
