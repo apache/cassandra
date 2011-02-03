b2978d2fc1b5c2ab0d845ebb99e503f64f9fe5e8
Jonathan Ellis <jbellis@apache.org>
2011-02-03 17:42:07 +0000

    log exceptions on scheduled executors patch by jbellis; reviewed by stuhood for CASSANDRA-2061
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1066890 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/concurrent/DebuggableThreadPoolExecutor.java
+++ src/java/org/apache/cassandra/concurrent/DebuggableThreadPoolExecutor.java
@@ -75,10 +75,15 @@ public class DebuggableThreadPoolExecutor extends ThreadPoolExecutor
         }
     }
 
+    @Override
     public void afterExecute(Runnable r, Throwable t)
     {
         super.afterExecute(r,t);
+        logTaskException(r, t);
+    }
 
+    static void logTaskException(Runnable r, Throwable t)
+    {
         // exceptions wrapped by FutureTask
         if (r instanceof FutureTask)
         {
@@ -92,7 +97,9 @@ public class DebuggableThreadPoolExecutor extends ThreadPoolExecutor
             }
             catch (ExecutionException e)
             {
-                if (Thread.getDefaultUncaughtExceptionHandler() != null)
+                if (Thread.getDefaultUncaughtExceptionHandler() == null)
+                    logger.error("Error in executor task", e.getCause());
+                else
                     Thread.getDefaultUncaughtExceptionHandler().uncaughtException(Thread.currentThread(), e.getCause());
             }
         }
@@ -100,7 +107,10 @@ public class DebuggableThreadPoolExecutor extends ThreadPoolExecutor
         // exceptions for non-FutureTask runnables [i.e., added via execute() instead of submit()]
         if (t != null)
         {
-            logger.error("Error in ThreadPoolExecutor", t);
+            if (Thread.getDefaultUncaughtExceptionHandler() == null)
+                logger.error("Error in executor task", t);
+            else
+                Thread.getDefaultUncaughtExceptionHandler().uncaughtException(Thread.currentThread(), t);
         }
     }
 }
