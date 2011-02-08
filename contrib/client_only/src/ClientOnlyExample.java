8d907f7e1c0fcb3e8bb2f377529fb3f7d3778f57
Brandon Williams <brandonwilliams@apache.org>
2011-02-08 17:35:06 +0000

    Add a build and run script to client_only example. Patch by Jeremy Hanna, reviewed by eevans for CASSANDRA-2119.
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1068488 13f79535-47bb-0310-9956-ffa450edef68

--- contrib/client_only/src/ClientOnlyExample.java
+++ contrib/client_only/src/ClientOnlyExample.java
@@ -17,21 +17,32 @@
  */
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 import org.apache.cassandra.db.*;
 import org.apache.cassandra.db.filter.QueryPath;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.service.StorageService;
-import org.apache.cassandra.thrift.ColumnPath;
-import org.apache.cassandra.thrift.ConsistencyLevel;
+import org.apache.cassandra.thrift.*;
 import org.apache.cassandra.utils.ByteBufferUtil;
+
+import org.apache.thrift.TException;
+import org.apache.thrift.protocol.TBinaryProtocol;
+import org.apache.thrift.protocol.TProtocol;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TSocket;
+import org.apache.thrift.transport.TTransport;
+import org.apache.thrift.transport.TTransportException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ClientOnlyExample
 {
+    private static final Logger logger = LoggerFactory.getLogger(ClientOnlyExample.class);
+
+    private static final String KEYSPACE = "Keyspace1";
+    private static final String COLUMN_FAMILY = "Standard1";
 
     private static void testWriting() throws Exception
     {
@@ -49,8 +60,8 @@ public class ClientOnlyExample
         // do some writing.
         for (int i = 0; i < 100; i++)
         {
-            RowMutation change = new RowMutation("Keyspace1", ByteBuffer.wrap(("key" + i).getBytes()));
-            ColumnPath cp = new ColumnPath("Standard1").setColumn(("colb").getBytes());
+            RowMutation change = new RowMutation(KEYSPACE, ByteBuffer.wrap(("key" + i).getBytes()));
+            ColumnPath cp = new ColumnPath(COLUMN_FAMILY).setColumn(("colb").getBytes());
             change.add(new QueryPath(cp), ByteBuffer.wrap(("value" + i).getBytes()), 0);
 
             // don't call change.apply().  The reason is that is makes a static call into Table, which will perform
@@ -85,8 +96,8 @@ public class ClientOnlyExample
         for (int i = 0; i < 100; i++)
         {
             List<ReadCommand> commands = new ArrayList<ReadCommand>();
-            SliceByNamesReadCommand readCommand = new SliceByNamesReadCommand("Keyspace1", ByteBuffer.wrap(("key" + i).getBytes()),
-                                                                              new QueryPath("Standard1", null, null), cols);
+            SliceByNamesReadCommand readCommand = new SliceByNamesReadCommand(KEYSPACE, ByteBuffer.wrap(("key" + i).getBytes()),
+                                                                              new QueryPath(COLUMN_FAMILY, null, null), cols);
             readCommand.setDigestQuery(false);
             commands.add(readCommand);
             List<Row> rows = StorageProxy.read(commands, ConsistencyLevel.ONE);
@@ -113,16 +124,16 @@ public class ClientOnlyExample
     /**
      * First, bring one or more nodes up. Then run ClientOnlyExample with these VM arguments:
      *
-     -Xmx1G
-     -Dstorage-config=/Users/gary.dusbabek/cass-configs/trunk/conf3-client
-
-     Pass "write" or "read" into the program to exercise the various methods.
-
-     Caveats:
-
-     1.  Much of cassandra is not reentrant.  That is, you can't spin a client up, down, then back up in the same jvm.
-     2.  Because of the above, you still need to force-quit the process. StorageService.stopClient() doesn't (can't)
-         spin everything down.
+     * -Xmx1G
+     * -Dcassandra.config=/Users/gary/cassandra/conf/cassandra.yaml (optional, will first look for cassandra.yaml on classpath)
+     *
+     * Pass "write" or "read" into the program to exercise the various methods.
+     *
+     * Caveats:
+     *
+     * 1.  Much of cassandra is not reentrant.  That is, you can't spin a client up, down, then back up in the same jvm.
+     * 2.  Because of the above, you still need to force-quit the process. StorageService.stopClient() doesn't (can't)
+     *     spin everything down.
      */
     public static void main(String args[]) throws Exception
     {
@@ -134,7 +145,50 @@ public class ClientOnlyExample
         }
         else if ("write".equalsIgnoreCase(args[0]))
         {
+            setupKeyspace(createConnection());
             testWriting();
         }
+    }
+    
+    /**
+    * This method will fail if the keyspace already exists
+    */
+    private static void setupKeyspace(Cassandra.Iface client) throws TException, InvalidRequestException
+    {
+        List<CfDef> cfDefList = new ArrayList<CfDef>();
+        CfDef columnFamily = new CfDef(KEYSPACE, COLUMN_FAMILY);
+        cfDefList.add(columnFamily);
+
+        client.system_add_keyspace(new KsDef(KEYSPACE, "org.apache.cassandra.locator.SimpleStrategy", 1, cfDefList));
+        int magnitude = client.describe_ring(KEYSPACE).size();
+        try
+        {
+            Thread.sleep(1000 * magnitude);
+        }
+        catch (InterruptedException e)
+        {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Cassandra.Iface createConnection() throws TTransportException
+    {
+        if (System.getProperty("cassandra.host") == null || System.getProperty("cassandra.port") == null)
+        {
+           logger.warn("cassandra.host or cassandra.port is not defined, using default");
+        }
+        return createConnection( System.getProperty("cassandra.host","localhost"),
+                                 Integer.valueOf(System.getProperty("cassandra.port","9160")),
+                                 Boolean.valueOf(System.getProperty("cassandra.framed", "true")) );
+    }
+
+    private static Cassandra.Client createConnection(String host, Integer port, boolean framed) throws TTransportException
+    {
+        TSocket socket = new TSocket(host, port);
+        TTransport trans = framed ? new TFramedTransport(socket) : socket;
+        trans.open();
+        TProtocol protocol = new TBinaryProtocol(trans);
+
+        return new Cassandra.Client(protocol);
     }
 }
