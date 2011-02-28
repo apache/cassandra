96d0a86fd983d840dee7c88b998f6ae80c847b89
Brandon Williams <brandonwilliams@apache.org>
2011-02-28 21:32:08 +0000

    CFRW no longer loses mutations. Patch by Eldon Stegall, reviewed by Stu Hood and brandonwilliams for CASSANDRA-2255
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1075543 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordWriter.java
+++ src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordWriter.java
@@ -325,7 +325,7 @@ implements org.apache.hadoop.mapred.RecordWriter<ByteBuffer,List<org.apache.cass
                 }
 
                 Map<ByteBuffer, Map<String, List<Mutation>>> batch = new HashMap<ByteBuffer, Map<String, List<Mutation>>>();
-                while (batch.size() < batchThreshold)
+                while (batch.size() < batchThreshold || mutation != null)
                 {
                     Map<String, List<Mutation>> subBatch = batch.get(mutation.left);
                     if (subBatch == null)
@@ -335,6 +335,7 @@ implements org.apache.hadoop.mapred.RecordWriter<ByteBuffer,List<org.apache.cass
                     }
                     
                     subBatch.get(columnFamily).add(mutation.right);
+                    mutation = null;
                     if ((mutation = queue.poll()) == null)
                         break;
                 }
