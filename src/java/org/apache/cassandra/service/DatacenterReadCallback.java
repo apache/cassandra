0251a8f0f66055960c2dc5502b6496313bf074f3
Jonathan Ellis <jbellis@apache.org>
2011-04-28 13:36:07 +0000

    trigger read repair correctly forLOCAL_QUORUM reads patch by jbellis; reviewed by slebresne for CASSANDRA-2556
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1097455 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/service/DatacenterReadCallback.java
+++ src/java/org/apache/cassandra/service/DatacenterReadCallback.java
@@ -48,31 +48,17 @@ public class DatacenterReadCallback<T> extends ReadCallback<T>
     }
 
     @Override
-    public void response(Message message)
+    protected boolean waitingFor(Message message)
     {
-        resolver.preprocess(message);
-
-        int n = localdc.equals(snitch.getDatacenter(message.getFrom()))
-              ? received.incrementAndGet()
-              : received.get();
-
-        if (n == blockfor && resolver.isDataPresent())
-        {
-            condition.signal();
-            maybeResolveForRepair();
-        }
+        return localdc.equals(snitch.getDatacenter(message.getFrom()));
     }
-    
-    @Override
-    public void response(ReadResponse result)
-    {
-        ((RowDigestResolver) resolver).injectPreProcessed(result);
 
-        if (received.incrementAndGet() == blockfor && resolver.isDataPresent())
-        {
-            condition.signal();
-            maybeResolveForRepair();
-        }
+    @Override
+    protected boolean waitingFor(ReadResponse response)
+    {
+        // cheat and leverage our knowledge that a local read is the only way the ReadResponse
+        // version of this method gets called
+        return true;
     }
     
     @Override
