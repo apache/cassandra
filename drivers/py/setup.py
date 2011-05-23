d00575bc85acc3c7cb4f0a783bd0e0d7673eef1a
Eric Evans <eevans@apache.org>
2011-05-23 23:12:16 +0000

    bump cql driver version
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.8.0@1126807 13f79535-47bb-0310-9956-ffa450edef68

--- drivers/py/setup.py
+++ drivers/py/setup.py
@@ -20,7 +20,7 @@ from os.path import abspath, join, dirname
 
 setup(
     name="cql",
-    version="1.0.2",
+    version="1.0.3",
     description="Cassandra Query Language driver",
     long_description=open(abspath(join(dirname(__file__), 'README'))).read(),
     url="http://cassandra.apache.org",
