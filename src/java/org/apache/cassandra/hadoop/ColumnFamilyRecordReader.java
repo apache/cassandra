37edeb575392329531d76abc47852d4c3cfa493a
T Jake Luciani <jake@apache.org>
2011-06-24 15:49:46 +0000

    merge from 0.8
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@1139361 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordReader.java
+++ src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordReader.java
@@ -1,4 +1,5 @@
 package org.apache.cassandra.hadoop;
+
 /*
  * 
  * Licensed to the Apache Software Foundation (ASF) under one
@@ -20,7 +21,6 @@ package org.apache.cassandra.hadoop;
  * 
  */
 
-
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
@@ -38,6 +38,7 @@ import org.apache.cassandra.dht.IPartitioner;
 import org.apache.cassandra.thrift.*;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.cassandra.utils.Pair;
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.mapreduce.InputSplit;
 import org.apache.hadoop.mapreduce.RecordReader;
@@ -45,22 +46,35 @@ import org.apache.hadoop.mapreduce.TaskAttemptContext;
 import org.apache.thrift.TException;
 import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSocket;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap<ByteBuffer, IColumn>>
 {
-    private ColumnFamilySplit split;
-    private RowIterator iter;
+    private static final Logger                              logger = LoggerFactory
+                                                                            .getLogger(ColumnFamilyRecordReader.class);
+    private ColumnFamilySplit                                split;
+    private RowIterator                                      iter;
     private Pair<ByteBuffer, SortedMap<ByteBuffer, IColumn>> currentRow;
-    private SlicePredicate predicate;
-    private int totalRowCount; // total number of rows to fetch
-    private int batchRowCount; // fetch this many per batch
-    private String cfName;
-    private String keyspace;
-    private TSocket socket;
-    private Cassandra.Client client;
-    private ConsistencyLevel consistencyLevel;
+    private SlicePredicate                                   predicate;
+    private int                                              totalRowCount;                                            // total
+                                                                                                                        // number
+                                                                                                                        // of
+                                                                                                                        // rows
+                                                                                                                        // to
+                                                                                                                        // fetch
+    private int                                              batchRowCount;                                            // fetch
+                                                                                                                        // this
+                                                                                                                        // many
+                                                                                                                        // per
+                                                                                                                        // batch
+    private String                                           cfName;
+    private String                                           keyspace;
+    private TSocket                                          socket;
+    private Cassandra.Client                                 client;
+    private ConsistencyLevel                                 consistencyLevel;
 
-    public void close() 
+    public void close()
     {
         if (socket != null && socket.isOpen())
         {
@@ -69,7 +83,7 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
             client = null;
         }
     }
-    
+
     public ByteBuffer getCurrentKey()
     {
         return currentRow.left;
@@ -79,13 +93,14 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
     {
         return currentRow.right;
     }
-    
+
     public float getProgress()
     {
-        // the progress is likely to be reported slightly off the actual but close enough
-        return ((float)iter.rowsRead()) / totalRowCount;
+        // the progress is likely to be reported slightly off the actual but
+        // close enough
+        return ((float) iter.rowsRead()) / totalRowCount;
     }
-    
+
     public void initialize(InputSplit split, TaskAttemptContext context) throws IOException
     {
         this.split = (ColumnFamilySplit) split;
@@ -95,10 +110,9 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
         batchRowCount = ConfigHelper.getRangeBatchSize(conf);
         cfName = ConfigHelper.getInputColumnFamily(conf);
         consistencyLevel = ConsistencyLevel.valueOf(ConfigHelper.getReadConsistencyLevel(conf));
-        
-        
+
         keyspace = ConfigHelper.getInputKeyspace(conf);
-        
+
         try
         {
             // only need to connect once
@@ -106,12 +120,31 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
                 return;
 
             // create connection using thrift
-            String location = getLocation();
-            socket = new TSocket(location, ConfigHelper.getRpcPort(conf));
-            TBinaryProtocol binaryProtocol = new TBinaryProtocol(new TFramedTransport(socket));
-            client = new Cassandra.Client(binaryProtocol);
-            socket.open();
-
+            List<String> locationsAttempted = new ArrayList<String>();
+            for (Iterator<String> it = getLocations(conf); it.hasNext();)
+            {
+                String location = it.next();
+                try
+                {
+                    socket = new TSocket(location, ConfigHelper.getRpcPort(conf));
+                    TBinaryProtocol binaryProtocol = new TBinaryProtocol(new TFramedTransport(socket));
+                    client = new Cassandra.Client(binaryProtocol);
+                    socket.open();
+                    break;
+                }
+                catch (TException e)
+                {
+                    logger.info("failed to connect to " + location + ':' + ConfigHelper.getRpcPort(conf), e);
+                    locationsAttempted.add(location);
+                    client = null;
+                }
+            }
+            if (null == client)
+            {
+                throw new RuntimeException("For the split " + split + " there were no locations "
+                        + (ConfigHelper.getInputSplitUseOnlySameDCReplica(conf) ? "(from same DC) " : "") + "alive: "
+                        + StringUtils.join(locationsAttempted, ", "));
+            }
             // log in
             client.set_keyspace(keyspace);
             if (ConfigHelper.getInputKeyspaceUserName(conf) != null)
@@ -130,7 +163,7 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
 
         iter = new RowIterator();
     }
-    
+
     public boolean nextKeyValue() throws IOException
     {
         if (!iter.hasNext())
@@ -140,46 +173,52 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
     }
 
     // we don't use endpointsnitch since we are trying to support hadoop nodes that are
-    // not necessarily on Cassandra machines, too.  This should be adequate for single-DC clusters, at least.
-    private String getLocation()
+    // not necessarily on Cassandra machines, too. This should be adequate for
+    // single-DC clusters, at least.
+    private Iterator<String> getLocations(final Configuration conf) throws IOException
     {
-        InetAddress[] localAddresses;
         try
         {
-            localAddresses = InetAddress.getAllByName(InetAddress.getLocalHost().getHostAddress());
+            for (InetAddress address : InetAddress.getAllByName(InetAddress.getLocalHost().getHostAddress()))
+            {
+                for (final String location : split.getLocations())
+                {
+                    InetAddress locationAddress = getInetAddressByName(location);
+                    if (address.equals(locationAddress))
+                    {
+                        // add fall back replicas from same DC via the following
+                        // Iterator
+                        return new SplitEndpointIterator(location, conf);
+                    }
+                }
+            }
         }
         catch (UnknownHostException e)
         {
             throw new AssertionError(e);
         }
-        for (InetAddress address : localAddresses)
+        
+        return Arrays.asList(split.getLocations()).iterator();
+    }
+
+    private static InetAddress getInetAddressByName(String name)
+    {
+        try
         {
-            for (String location : split.getLocations())
-            {
-                InetAddress locationAddress = null;
-                try
-                {
-                    locationAddress = InetAddress.getByName(location);
-                }
-                catch (UnknownHostException e)
-                {
-                    throw new AssertionError(e);
-                }
-                if (address.equals(locationAddress))
-                {
-                    return location;
-                }
-            }
+            return InetAddress.getByName(name);
         }
-        return split.getLocations()[0];
+        catch (UnknownHostException e)
+        {
+            throw new AssertionError(e);
+        }
     }
 
     private class RowIterator extends AbstractIterator<Pair<ByteBuffer, SortedMap<ByteBuffer, IColumn>>>
     {
-        private List<KeySlice> rows;
-        private String startToken;
-        private int totalRead = 0;
-        private int i = 0;
+        private List<KeySlice>     rows;
+        private String             startToken;
+        private int                totalRead = 0;
+        private int                i         = 0;
         private final AbstractType comparator;
         private final AbstractType subComparator;
         private final IPartitioner partitioner;
@@ -218,43 +257,39 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
 
         private void maybeInit()
         {
-            // check if we need another batch 
+            // check if we need another batch
             if (rows != null && i >= rows.size())
                 rows = null;
-            
+
             if (rows != null)
                 return;
 
             if (startToken == null)
             {
                 startToken = split.getStartToken();
-            } 
+            }
             else if (startToken.equals(split.getEndToken()))
             {
                 rows = null;
                 return;
             }
-            
-            KeyRange keyRange = new KeyRange(batchRowCount)
-                                .setStart_token(startToken)
-                                .setEnd_token(split.getEndToken());
+
+            KeyRange keyRange = new KeyRange(batchRowCount).setStart_token(startToken)
+                    .setEnd_token(split.getEndToken());
             try
             {
-                rows = client.get_range_slices(new ColumnParent(cfName),
-                                               predicate,
-                                               keyRange,
-                                               consistencyLevel);
-                  
+                rows = client.get_range_slices(new ColumnParent(cfName), predicate, keyRange, consistencyLevel);
+
                 // nothing new? reached the end
                 if (rows.isEmpty())
                 {
                     rows = null;
                     return;
                 }
-                               
+
                 // reset to iterate through this new batch
                 i = 0;
-                
+
                 // prepare for the next slice to be read
                 KeySlice lastRow = rows.get(rows.size() - 1);
                 ByteBuffer rowkey = lastRow.key;
@@ -279,7 +314,7 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
             maybeInit();
             if (rows == null)
                 return endOfData();
-            
+
             totalRead++;
             KeySlice ks = rows.get(i++);
             SortedMap<ByteBuffer, IColumn> map = new TreeMap<ByteBuffer, IColumn>(comparator);
@@ -300,7 +335,8 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
 
         private IColumn unthriftifySuper(SuperColumn super_column)
         {
-            org.apache.cassandra.db.SuperColumn sc = new org.apache.cassandra.db.SuperColumn(super_column.name, subComparator);
+            org.apache.cassandra.db.SuperColumn sc = new org.apache.cassandra.db.SuperColumn(super_column.name,
+                    subComparator);
             for (Column column : super_column.columns)
             {
                 sc.addColumn(unthriftifySimple(column));
@@ -311,6 +347,109 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
         private IColumn unthriftifySimple(Column column)
         {
             return new org.apache.cassandra.db.Column(column.name, column.value, column.timestamp);
+        }
+    }
+
+    private class SplitEndpointIterator extends AbstractIterator<String>
+    {
+        private final boolean       restrictToSameDC;
+        private final String        location;
+        private final Configuration conf;
+        private Cassandra.Client    client;
+        private List<String>        endpoints;
+        private int                 endpointsIdx = -1;
+
+        SplitEndpointIterator(final String location, final Configuration conf)
+        {
+            this.location = location;
+            this.conf = conf;
+            restrictToSameDC = ConfigHelper.getInputSplitUseOnlySameDCReplica(conf);
+        }
+
+        protected String computeNext()
+        {
+            if (-1 == endpointsIdx)
+            {
+                // location is the preference. always return it first.
+                endpointsIdx = 0;
+                return location;
+            }
+            else
+            {
+                if (null == endpoints)
+                {
+                    try
+                    {
+                        for (String nextLocation : split.getLocations())
+                        {
+                            try
+                            {
+                                endpoints = sortEndpointsByProximity(nextLocation, Arrays.asList(split.getLocations()),
+                                        restrictToSameDC);
+                                if (location.equals(endpoints.get(0)))
+                                {
+                                    ++endpointsIdx;
+                                }
+                                break;
+                            }
+                            catch (TException e)
+                            {
+                                logger.info(
+                                        "failed to sortEndpointsByProximity(" + location + ", ["
+                                                + StringUtils.join(split.getLocations(), ',') + "], "
+                                                + restrictToSameDC + ")", e);
+                            }
+                            catch (IOException e)
+                            {
+                                logger.info(
+                                        "failed to sortEndpointsByProximity(" + location + ", ["
+                                                + StringUtils.join(split.getLocations(), ',') + "], "
+                                                + restrictToSameDC + ")", e);
+                            }
+                        }
+                    }
+                    catch (InvalidRequestException e)
+                    {
+                        throw new AssertionError(e);
+                    }
+                    if (null == endpoints)
+                    {
+                        throw new AssertionError("failed to find any fallback replica endpoints from "
+                                + StringUtils.join(split.getLocations(), ','));
+                    }
+                }
+                if (endpoints.size() > endpointsIdx)
+                {
+                    return endpoints.get(endpointsIdx++);
+                }
+            }
+            return endOfData();
+        }
+
+        private List<String> sortEndpointsByProximity(String connectTo, List<String> endpoints, boolean restrictToSameDC)
+                throws InvalidRequestException, TException, IOException
+        {
+            try
+            {
+                // try first our configured initialAddress
+                return getClient(ConfigHelper.getInitialAddress(conf)).sort_endpoints_by_proximity(location, endpoints,
+                        restrictToSameDC);
+            }
+            catch (IOException ex)
+            {
+                // connect through the endpoint. if it fails it's no good
+                // anyway.
+                return getClient(connectTo).sort_endpoints_by_proximity(location, endpoints, restrictToSameDC);
+            }
+        }
+
+        private Cassandra.Client getClient(String host) throws IOException
+        {
+            if (null == client)
+            {
+                client = ColumnFamilyInputFormat.createConnection(host, ConfigHelper.getRpcPort(conf), true);
+            }
+            return client;
         }
     }
 }
