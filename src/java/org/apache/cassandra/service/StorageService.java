b6ea29d251fad924ac18a5bc6195352ceefb776f
Brandon Williams <brandonwilliams@apache.org>
2011-08-08 23:41:44 +0000

    Fix stringification for local address in getRangeToRpcaddressMap
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.8@1155157 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -618,7 +618,7 @@ public class StorageService implements IEndpointStateChangeSubscriber, StorageSe
             for (InetAddress endpoint: entry.getValue())
             {
                 if (endpoint.equals(FBUtilities.getLocalAddress()))
-                    rpcaddrs.add(DatabaseDescriptor.getRpcAddress().toString());
+                    rpcaddrs.add(DatabaseDescriptor.getRpcAddress().getHostAddress());
                 else
                     rpcaddrs.add(Gossiper.instance.getEndpointStateForEndpoint(endpoint).getApplicationState(ApplicationState.RPC_ADDRESS).value);
             }
