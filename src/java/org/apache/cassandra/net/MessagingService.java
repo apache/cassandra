8c89038a38d5b856c39b11fc8265a3abd0362217
Sylvain Lebresne <slebresne@apache.org>
2011-09-01 13:21:18 +0000

    merge from 0.8
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/trunk@1164070 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -154,7 +154,10 @@ public final class MessagingService implements MessagingServiceMBean
                 String ip = expiredCallbackInfo.target.getHostAddress();
                 AtomicLong c = timeoutsPerHost.get(ip);
                 if (c == null)
-                    c = timeoutsPerHost.put(ip, new AtomicLong());
+                {
+                    c = new AtomicLong();
+                    timeoutsPerHost.put(ip, c);
+                }
                 c.incrementAndGet();
                 // we only create AtomicLong instances here, so that the write
                 // access to the hashmap happens single-threadedly.
