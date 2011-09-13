71ebcafb49033dc895db971ddf7b702bed55a8cd
Jonathan Ellis <jbellis@apache.org>
2011-09-13 19:24:15 +0000

    Fix divide by zero error in GCInspector patch by jbellis; reviewed by slebresne for CASSANDRA-3164
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-0.7@1170308 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/service/GCInspector.java
+++ src/java/org/apache/cassandra/service/GCInspector.java
@@ -106,7 +106,7 @@ public class GCInspector
             
             if (previousCount == null)
                 previousCount = 0L;           
-            if (count == previousCount)
+            if (count.equals(previousCount))
                 continue;
             
             gccounts.put(gc.getName(), count);
