e3812748ecb0b02436ebf82b7169c2c1c75b6627
Jonathan Ellis <jbellis@apache.org>
2011-10-07 03:48:59 +0000

    default hsha thrift server to cpu core count in rpc pool patch by jbellis; reviewed by brandonwilliams for CASSANDRA-3329
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-1.0.0@1179931 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -365,7 +365,15 @@ public class DatabaseDescriptor
 
             if (!CassandraDaemon.rpc_server_types.contains(conf.rpc_server_type.toLowerCase()))
                 throw new ConfigurationException("Unknown rpc_server_type: " + conf.rpc_server_type);
-            
+            if (conf.rpc_min_threads == null)
+                conf.rpc_min_threads = conf.rpc_server_type.toLowerCase().equals("hsha")
+                                     ? Runtime.getRuntime().availableProcessors()
+                                     : 16;
+            if (conf.rpc_max_threads == null)
+                conf.rpc_max_threads = conf.rpc_server_type.toLowerCase().equals("hsha")
+                                     ? Runtime.getRuntime().availableProcessors()
+                                     : Integer.MAX_VALUE;
+
             /* data file and commit log directories. they get created later, when they're needed. */
             if (conf.commitlog_directory != null && conf.data_file_directories != null && conf.saved_caches_directory != null)
             {
