80b9483a5c341a2ff21d5ef1571738e0412fb638
Brandon Williams <brandonwilliams@apache.org>
2011-10-18 17:15:55 +0000

    add comment to explain 3375
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-1.0@1185760 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/db/HintedHandOffManager.java
+++ src/java/org/apache/cassandra/db/HintedHandOffManager.java
@@ -236,6 +236,7 @@ public class HintedHandOffManager implements HintedHandOffManagerMBean
             // sleep a random amount to stagger handoff delivery from different replicas.
             // (if we had to wait, then gossiper randomness took care of that for us already.)
             if (waited == 0) {
+                // use a 'rounded' sleep interval because of a strange bug with windows: CASSANDRA-3375
                 int sleep = FBUtilities.threadLocalRandom().nextInt(2000) * 30;
                 logger_.debug("Sleeping {}ms to stagger hint delivery", sleep);
                 Thread.sleep(sleep);
