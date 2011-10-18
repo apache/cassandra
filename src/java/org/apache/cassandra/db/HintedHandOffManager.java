6b5e7a1564498f7646f4fd410a34d3f0a81eb977
Brandon Williams <brandonwilliams@apache.org>
2011-10-18 17:04:56 +0000

    HHOM only sleeps for rounded intervals to prevent problems on windows. Patch by Flavio Baronti, reviewed by brandonwilliams for CASSANDRA-3375
    git-svn-id: https://svn.apache.org/repos/asf/cassandra/branches/cassandra-1.0@1185757 13f79535-47bb-0310-9956-ffa450edef68

--- src/java/org/apache/cassandra/db/HintedHandOffManager.java
+++ src/java/org/apache/cassandra/db/HintedHandOffManager.java
@@ -236,7 +236,7 @@ public class HintedHandOffManager implements HintedHandOffManagerMBean
             // sleep a random amount to stagger handoff delivery from different replicas.
             // (if we had to wait, then gossiper randomness took care of that for us already.)
             if (waited == 0) {
-                int sleep = FBUtilities.threadLocalRandom().nextInt(60000);
+                int sleep = FBUtilities.threadLocalRandom().nextInt(2000) * 30;
                 logger_.debug("Sleeping {}ms to stagger hint delivery", sleep);
                 Thread.sleep(sleep);
             }
