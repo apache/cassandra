7c374547793c30f62366b2a28a2ca2c1d914f146
Pavel Yaskevich <povel.y@gmail.com>
2012-01-24 22:28:41 +0200

    fixes NPE exception in ClientState.hasKeyspaceSchemaAccess(...) method introduced by CASSANDRA-3759

--- src/java/org/apache/cassandra/service/ClientState.java
+++ src/java/org/apache/cassandra/service/ClientState.java
@@ -124,7 +124,7 @@ public class ClientState
         validateLogin();
         
         // hardcode disallowing messing with system keyspace
-        if (keyspace.equalsIgnoreCase(Table.SYSTEM_TABLE) && perm == Permission.WRITE)
+        if (keyspace != null && keyspace.equalsIgnoreCase(Table.SYSTEM_TABLE) && perm == Permission.WRITE)
             throw new InvalidRequestException("system keyspace is not user-modifiable");
 
         resourceClear();
