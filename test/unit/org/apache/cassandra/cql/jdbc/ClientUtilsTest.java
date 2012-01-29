0d1991eec4781afc1f6673949d306cfe02ab8fa9
Eric Evans <eevans@apache.org>
2012-01-29 11:04:01 -0600

    clientutil: use decompose() for tests
    Patch by eevans for CASSANDRA-3665

--- test/unit/org/apache/cassandra/cql/jdbc/ClientUtilsTest.java
+++ test/unit/org/apache/cassandra/cql/jdbc/ClientUtilsTest.java
@@ -5,10 +5,9 @@ import java.math.BigInteger;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
-import java.util.Date;
+import java.sql.Date;
 import java.util.UUID;
 
-import org.apache.cassandra.utils.ByteBufferUtil;
 import org.apache.cassandra.utils.UUIDGen;
 import org.junit.Test;
 
@@ -18,23 +17,23 @@ public class ClientUtilsTest
     @Test
     public void test() throws UnknownHostException
     {
-        JdbcAscii.instance.compose(wr("string"));
-        JdbcBoolean.instance.compose(wr("false"));
-        JdbcBytes.instance.compose(wr("string"));
-        JdbcDate.instance.compose(ByteBufferUtil.bytes((new Date(System.currentTimeMillis())).getTime()));
-        JdbcDecimal.instance.compose(decomposeBigDecimal(new BigDecimal(1)));
-        JdbcDouble.instance.compose(ByteBufferUtil.bytes(1.0d));
-        JdbcFloat.instance.compose(ByteBufferUtil.bytes(1.0f));
-        JdbcInt32.instance.compose(ByteBufferUtil.bytes(1));
-        JdbcInteger.instance.compose(ByteBuffer.wrap((new BigInteger("1")).toByteArray()));
-        JdbcLong.instance.compose(ByteBufferUtil.bytes(1L));
-        JdbcUTF8.instance.compose(wr("string"));
+        JdbcAscii.instance.compose(JdbcAscii.instance.decompose("string"));
+        JdbcBoolean.instance.compose(JdbcBoolean.instance.decompose(true));
+        JdbcBytes.instance.compose(JdbcBytes.instance.decompose(ByteBuffer.wrap("string".getBytes())));
+        JdbcDate.instance.compose(JdbcDate.instance.decompose(new Date(System.currentTimeMillis())));
+        JdbcDecimal.instance.compose(JdbcDecimal.instance.decompose(new BigDecimal(1)));
+        JdbcDouble.instance.compose(JdbcDouble.instance.decompose(new Double(1.0d)));
+        JdbcFloat.instance.compose(JdbcFloat.instance.decompose(new Float(1.0f)));
+        JdbcInt32.instance.compose(JdbcInt32.instance.decompose(1));
+        JdbcInteger.instance.compose(JdbcInteger.instance.decompose(new BigInteger("1")));
+        JdbcLong.instance.compose(JdbcLong.instance.decompose(1L));
+        JdbcUTF8.instance.compose(JdbcUTF8.instance.decompose("string"));
 
         // UUIDGen
         UUID uuid = UUIDGen.makeType1UUIDFromHost(InetAddress.getLocalHost());
-        JdbcUUID.instance.compose(ByteBuffer.wrap(UUIDGen.decompose(uuid)));
-        JdbcTimeUUID.instance.compose(ByteBuffer.wrap(UUIDGen.decompose(uuid)));
-        JdbcLexicalUUID.instance.compose(ByteBuffer.wrap(UUIDGen.decompose(uuid)));
+        JdbcTimeUUID.instance.compose(JdbcTimeUUID.instance.decompose(uuid));
+        JdbcUUID.instance.compose(JdbcUUID.instance.decompose(uuid));
+        JdbcLexicalUUID.instance.compose(JdbcLexicalUUID.instance.decompose(uuid));
 
         // Raise a MarshalException
         try
@@ -45,27 +44,5 @@ public class ClientUtilsTest
         {
             // Success
         }
-    }
-
-    /* Copypasta from DecimalType */
-    private static ByteBuffer decomposeBigDecimal(BigDecimal value)
-    {
-        if (value == null) return ByteBufferUtil.EMPTY_BYTE_BUFFER;
-
-        BigInteger bi = value.unscaledValue();
-        Integer scale = value.scale();
-        byte[] bibytes = bi.toByteArray();
-        byte[] sbytes = ByteBufferUtil.bytes(scale).array();
-        byte[] bytes = new byte[bi.toByteArray().length+4];
-
-        for (int i = 0 ; i < 4 ; i++) bytes[i] = sbytes[i];
-        for (int i = 4 ; i < bibytes.length+4 ; i++) bytes[i] = bibytes[i-4];
-
-        return ByteBuffer.wrap(bytes);
-    }
-
-    private static ByteBuffer wr(String value)
-    {
-        return ByteBuffer.wrap(value.getBytes());
     }
 }
