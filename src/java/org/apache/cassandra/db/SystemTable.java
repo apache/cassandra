984194d0c62e922d4600046456df7aa3a85ca4f1
Brandon Williams <brandonwilliams@apache.org>
2012-02-15 09:06:52 -0600

    Correct InetAddress equality comparisons. Patch by brandonwilliams reviewed by jbellis for CASSANDRA-3913

--- src/java/org/apache/cassandra/db/SystemTable.java
+++ src/java/org/apache/cassandra/db/SystemTable.java
@@ -138,7 +138,7 @@ public class SystemTable
      */
     public static synchronized void updateToken(InetAddress ep, Token token)
     {
-        if (ep == FBUtilities.getBroadcastAddress())
+        if (ep.equals(FBUtilities.getBroadcastAddress()))
         {
             removeToken(token);
             return;
--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -1095,7 +1095,7 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
      */
     public void addSavedEndpoint(InetAddress ep)
     {
-        if (ep == FBUtilities.getBroadcastAddress())
+        if (ep.equals(FBUtilities.getBroadcastAddress()))
         {
             logger.debug("Attempt to add self as saved endpoint");
             return;
