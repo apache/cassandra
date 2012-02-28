40617c8be6ea85287f1c75d07c87e8315aa1ed4c
Jonathan Ellis <jbellis@apache.org>
2012-02-28 10:37:32 -0600

    Revert "update hasSameReplication to use the List passed to it"

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2668,11 +2668,10 @@ public class StorageService implements IEndpointStateChangeSubscriber, StorageSe
     {
         if (list.isEmpty())
             return false;
-        
         for (int i = 0; i < list.size() -1; i++)
         {
-            KSMetaData ksm1 = Schema.instance.getKSMetaData(list.get(i));
-            KSMetaData ksm2 = Schema.instance.getKSMetaData(list.get(i + 1));
+            KSMetaData ksm1 = Schema.instance.getKSMetaData(Schema.instance.getNonSystemTables().get(i));
+            KSMetaData ksm2 = Schema.instance.getKSMetaData(Schema.instance.getNonSystemTables().get(i + 1));
             if (!ksm1.strategyClass.equals(ksm2.strategyClass) ||
                     !Iterators.elementsEqual(ksm1.strategyOptions.entrySet().iterator(),
                                              ksm2.strategyOptions.entrySet().iterator()))
