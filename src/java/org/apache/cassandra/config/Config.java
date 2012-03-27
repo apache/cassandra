e12840e2182dfd12cb70df9a5aca4111a3a13217
Pavel Yaskevich <xedin@apache.org>
2012-03-27 23:02:43 +0300

    make autoconfiguration happen only if key_cache_size_in_mb value was left blank in conf/cassandra.yaml (CASSANDRA-4087)

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -128,7 +128,7 @@ public class Config
     public boolean trickle_fsync = false;
     public int trickle_fsync_interval_in_kb = 10240;
 
-    public String key_cache_size_in_mb = "auto";
+    public Integer key_cache_size_in_mb = null;
     public int key_cache_save_period = 14400;
     public int key_cache_keys_to_save = Integer.MAX_VALUE;
 
