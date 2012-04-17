7b38d6a4dff57b666d9a882029e3dbfddd92a412
Sylvain Lebresne <sylvain@datastax.com>
2012-04-17 09:40:18 +0200

    Fix underscode after merge

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -1985,7 +1985,7 @@ public class StorageService implements IEndpointStateChangeSubscriber, StorageSe
         Token parsedBeginToken = getPartitioner().getTokenFactory().fromString(beginToken);
         Token parsedEndToken = getPartitioner().getTokenFactory().fromString(endToken);
 
-        logger_.info("starting user-requested repair of range ({}, {}] for keyspace {} and column families {}",
+        logger.info("starting user-requested repair of range ({}, {}] for keyspace {} and column families {}",
                      new Object[] {parsedBeginToken, parsedEndToken, tableName, columnFamilies});
         AntiEntropyService.RepairFuture future = forceTableRepair(new Range<Token>(parsedBeginToken, parsedEndToken), tableName, isSequential, columnFamilies);
         if (future == null)
@@ -1996,7 +1996,7 @@ public class StorageService implements IEndpointStateChangeSubscriber, StorageSe
         }
         catch (Exception e)
         {
-            logger_.error("Repair session " + future.session.getName() + " failed.", e);
+            logger.error("Repair session " + future.session.getName() + " failed.", e);
         }
     }
 
