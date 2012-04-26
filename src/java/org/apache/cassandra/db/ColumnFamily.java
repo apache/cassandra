eb9f96146efb2611ce75739ecf5e2c2607650d7b
Sylvain Lebresne <sylvain@datastax.com>
2012-04-26 16:32:06 +0200

    Fix bug with super columns where row cache is not updated
    patch by slebresne; reviewed by jbellis for CASSANDRA-4190

--- src/java/org/apache/cassandra/db/ColumnFamily.java
+++ src/java/org/apache/cassandra/db/ColumnFamily.java
@@ -141,7 +141,7 @@ public class ColumnFamily extends AbstractColumnContainer
     }
 
     /**
-     * Same as addAll() but do a cloneMeShallow of SuperColumn if necessary to
+     * Same as addAll() but do a cloneMe of SuperColumn if necessary to
      * avoid keeping references to the structure (see #3957).
      */
     public void addAllWithSCCopy(ColumnFamily cf, Allocator allocator)
@@ -150,7 +150,7 @@ public class ColumnFamily extends AbstractColumnContainer
         {
             for (IColumn c : cf)
             {
-                columns.addColumn(((SuperColumn)c).cloneMeShallow(), allocator);
+                columns.addColumn(((SuperColumn)c).cloneMe(), allocator);
             }
             delete(cf);
         }
