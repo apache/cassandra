7007a8510247f88a27d54abb9e67f11ea337a38e
Brandon Williams <brandonwilliams@apache.org>
2012-05-18 15:20:19 -0500

    Fix minor typos in nodetool help

--- src/java/org/apache/cassandra/tools/NodeCmd.java
+++ src/java/org/apache/cassandra/tools/NodeCmd.java
@@ -136,9 +136,9 @@ public class NodeCmd
         StringBuilder header = new StringBuilder();
         header.append("\nAvailable commands:\n");
         // No args
-        addCmdHelp(header, "ring", "Print informations on the token ring");
+        addCmdHelp(header, "ring", "Print information about the token ring");
         addCmdHelp(header, "join", "Join the ring");
-        addCmdHelp(header, "info", "Print node informations (uptime, load, ...)");
+        addCmdHelp(header, "info", "Print node information (uptime, load, ...)");
         addCmdHelp(header, "cfstats", "Print statistics on column families");
         addCmdHelp(header, "ids", "Print list of unique host IDs");
         addCmdHelp(header, "version", "Print cassandra version");
