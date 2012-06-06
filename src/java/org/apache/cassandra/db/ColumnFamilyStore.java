383a608e7ff73befaaf34ad7cc0aab4cc26d1316
Yuki Morishita <yukim@apache.org>
2012-06-06 13:54:54 -0500

    only load key cache when caching is ALL/KEYS_ONLY, fix by yukim, reviewed by slebresne for CASSANDRA-4313

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -224,7 +224,7 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
         data = new DataTracker(this);
         Directories.SSTableLister sstables = directories.sstableLister().skipCompacted(true).skipTemporary(true);
         data.addInitialSSTables(SSTableReader.batchOpen(sstables.list().entrySet(), data, metadata, this.partitioner));
-        if (caching != Caching.NONE || caching != Caching.ROWS_ONLY)
+        if (caching == Caching.ALL || caching == Caching.KEYS_ONLY)
             CacheService.instance.keyCache.loadSaved(this);
 
         // compaction strategy should be created after the CFS has been prepared
