1280c6c7dd7103955a728f8f274f26ab6f85b528
Jonathan Ellis <jbellis@apache.org>
2012-06-14 13:51:45 -0500

    inline

--- src/java/org/apache/cassandra/db/compaction/LeveledCompactionTask.java
+++ src/java/org/apache/cassandra/db/compaction/LeveledCompactionTask.java
@@ -47,8 +47,7 @@ public class LeveledCompactionTask extends CompactionTask
     {
         try
         {
-            int n = super.execute(collector);
-            return n;
+            return super.execute(collector);
         }
         finally
         {
