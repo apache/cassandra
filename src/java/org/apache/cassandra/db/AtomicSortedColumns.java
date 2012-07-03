ShoOgino/cassandraFile

implement addAllWithSizeDelta for ThreadSafeSortedColumns (used in Memtable for supercolumns); see #4399

src/java/org/apache/cassandra/db/AtomicSortedColumns.java
@@ -342,41 +342,30 @@ public class AtomicSortedColumns implements ISortedColumns
         long addColumn(IColumn column, Allocator allocator)
         {
             ByteBuffer name = column.name();
-            IColumn oldColumn;
-            long sizeDelta = 0;
             while (true)
             {
-                oldColumn = map.putIfAbsent(name, column);
+                IColumn oldColumn = map.putIfAbsent(name, column);
                 if (oldColumn == null)
-                {
-                    sizeDelta += column.serializedSize();
-                    break;
-                }
+                    return column.serializedSize();
 
                 if (oldColumn instanceof SuperColumn)
                 {
                     assert column instanceof SuperColumn;
                     long previousSize = oldColumn.serializedSize();
                     ((SuperColumn) oldColumn).putColumn((SuperColumn)column, allocator);
-                    sizeDelta += oldColumn.serializedSize() - previousSize;
-                    break;  // Delegated to SuperColumn
+                    return oldColumn.serializedSize() - previousSize;
                 }
                 else
                 {
                     // calculate reconciled col from old (existing) col and new col
                     IColumn reconciledColumn = column.reconcile(oldColumn, allocator);
                     if (map.replace(name, oldColumn, reconciledColumn))
-                    {
-                        sizeDelta += reconciledColumn.serializedSize() - oldColumn.serializedSize();
-                        break;
-                    }
+                        return reconciledColumn.serializedSize() - oldColumn.serializedSize();
 
                     // We failed to replace column due to a concurrent update or a concurrent removal. Keep trying.
                     // (Currently, concurrent removal should not happen (only updates), but let us support that anyway.)
                 }
             }
-
-            return sizeDelta;
         }
 
         void retainAll(ISortedColumns columns)

src/java/org/apache/cassandra/db/ThreadSafeSortedColumns.java
@@ -87,22 +87,31 @@ public class ThreadSafeSortedColumns extends AbstractThreadUnsafeSortedColumns i
     */
     public void addColumn(IColumn column, Allocator allocator)
     {
+        addColumnInternal(column, allocator);
+    }
+
+    private long addColumnInternal(IColumn column, Allocator allocator)
+    {
         ByteBuffer name = column.name();
-        IColumn oldColumn;
-        while ((oldColumn = map.putIfAbsent(name, column)) != null)
+        while (true)
         {
+            IColumn oldColumn = map.putIfAbsent(name, column);
+            if (oldColumn == null)
+                return column.serializedSize();
+
             if (oldColumn instanceof SuperColumn)
             {
                 assert column instanceof SuperColumn;
+                long previousSize = oldColumn.serializedSize();
                 ((SuperColumn) oldColumn).putColumn((SuperColumn)column, allocator);
-                break;  // Delegated to SuperColumn
+                return oldColumn.serializedSize() - previousSize;
             }
             else
             {
                 // calculate reconciled col from old (existing) col and new col
                 IColumn reconciledColumn = column.reconcile(oldColumn, allocator);
                 if (map.replace(name, oldColumn, reconciledColumn))
-                    break;
+                    return reconciledColumn.serializedSize() - oldColumn.serializedSize();
 
                 // We failed to replace column due to a concurrent update or a concurrent removal. Keep trying.
                 // (Currently, concurrent removal should not happen (only updates), but let us support that anyway.)
@@ -113,10 +122,19 @@ public class ThreadSafeSortedColumns extends AbstractThreadUnsafeSortedColumns i
     /**
      * We need to go through each column in the column container and resolve it before adding
      */
-    protected void addAllColumns(ISortedColumns cm, Allocator allocator, Function<IColumn, IColumn> transformation)
+    public void addAll(ISortedColumns cm, Allocator allocator, Function<IColumn, IColumn> transformation)
     {
+        addAllWithSizeDelta(cm, allocator, transformation);
+    }
+
+    @Override
+    public long addAllWithSizeDelta(ISortedColumns cm, Allocator allocator, Function<IColumn, IColumn> transformation)
+    {
+        delete(cm.getDeletionInfo());
+        long sizeDelta = 0;
         for (IColumn column : cm.getSortedColumns())
-            addColumn(transformation.apply(column), allocator);
+            sizeDelta += addColumnInternal(transformation.apply(column), allocator);
+        return sizeDelta;
     }
 
     public boolean replace(IColumn oldColumn, IColumn newColumn)

apache/cassandra

src/java/org/apache/cassandra/db/AtomicSortedColumns.java
@@ -338,41 +338,30 @@ public class AtomicSortedColumns implements ISortedColumns
         long addColumn(IColumn column, Allocator allocator)
         {
             ByteBuffer name = column.name();
-            IColumn oldColumn;
-            long sizeDelta = 0;
             while (true)
             {
-                oldColumn = map.putIfAbsent(name, column);
+                IColumn oldColumn = map.putIfAbsent(name, column);
                 if (oldColumn == null)
-                {
-                    sizeDelta += column.dataSize();
-                    break;
-                }
+                    return column.dataSize();
 
                 if (oldColumn instanceof SuperColumn)
                 {
                     assert column instanceof SuperColumn;
                     long previousSize = oldColumn.dataSize();
                     ((SuperColumn) oldColumn).putColumn((SuperColumn)column, allocator);
-                    sizeDelta += oldColumn.dataSize() - previousSize;
-                    break;  // Delegated to SuperColumn
+                    return oldColumn.dataSize() - previousSize;
                 }
                 else
                 {
                     // calculate reconciled col from old (existing) col and new col
                     IColumn reconciledColumn = column.reconcile(oldColumn, allocator);
                     if (map.replace(name, oldColumn, reconciledColumn))
-                    {
-                        sizeDelta += reconciledColumn.dataSize() - oldColumn.dataSize();
-                        break;
-                    }
+                        return reconciledColumn.dataSize() - oldColumn.dataSize();
 
                     // We failed to replace column due to a concurrent update or a concurrent removal. Keep trying.
                     // (Currently, concurrent removal should not happen (only updates), but let us support that anyway.)
                 }
             }
-
-            return sizeDelta;
         }
 
         void retainAll(ISortedColumns columns)

src/java/org/apache/cassandra/db/ThreadSafeSortedColumns.java
@@ -88,22 +88,31 @@ public class ThreadSafeSortedColumns extends AbstractThreadUnsafeSortedColumns i
     */
     public void addColumn(IColumn column, Allocator allocator)
     {
+        addColumnInternal(column, allocator);
+    }
+
+    private long addColumnInternal(IColumn column, Allocator allocator)
+    {
         ByteBuffer name = column.name();
-        IColumn oldColumn;
-        while ((oldColumn = map.putIfAbsent(name, column)) != null)
+        while (true)
         {
+            IColumn oldColumn = map.putIfAbsent(name, column);
+            if (oldColumn == null)
+                return column.dataSize();
+
             if (oldColumn instanceof SuperColumn)
             {
                 assert column instanceof SuperColumn;
+                long previousSize = oldColumn.dataSize();
                 ((SuperColumn) oldColumn).putColumn((SuperColumn)column, allocator);
-                break;  // Delegated to SuperColumn
+                return oldColumn.dataSize() - previousSize;
             }
             else
             {
                 // calculate reconciled col from old (existing) col and new col
                 IColumn reconciledColumn = column.reconcile(oldColumn, allocator);
                 if (map.replace(name, oldColumn, reconciledColumn))
-                    break;
+                    return reconciledColumn.dataSize() - oldColumn.dataSize();
 
                 // We failed to replace column due to a concurrent update or a concurrent removal. Keep trying.
                 // (Currently, concurrent removal should not happen (only updates), but let us support that anyway.)
@@ -114,10 +123,19 @@ public class ThreadSafeSortedColumns extends AbstractThreadUnsafeSortedColumns i
     /**
      * We need to go through each column in the column container and resolve it before adding
      */
-    protected void addAllColumns(ISortedColumns cm, Allocator allocator, Function<IColumn, IColumn> transformation)
+    public void addAll(ISortedColumns cm, Allocator allocator, Function<IColumn, IColumn> transformation)
     {
+        addAllWithSizeDelta(cm, allocator, transformation);
+    }
+
+    @Override
+    public long addAllWithSizeDelta(ISortedColumns cm, Allocator allocator, Function<IColumn, IColumn> transformation)
+    {
+        delete(cm.getDeletionInfo());
+        long sizeDelta = 0;
         for (IColumn column : cm.getSortedColumns())
-            addColumn(transformation.apply(column), allocator);
+            sizeDelta += addColumnInternal(transformation.apply(column), allocator);
+        return sizeDelta;
     }
 
     public boolean replace(IColumn oldColumn, IColumn newColumn)