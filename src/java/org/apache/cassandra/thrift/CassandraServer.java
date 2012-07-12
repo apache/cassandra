13f8eee99d0acb7063eab5dab7a8980d1b71a3b9
Jonathan Ellis <jbellis@apache.org>
2012-07-11 23:29:37 -0500

    CS.schedule throws UE instead of TOE patch by jbellis; reviewed by slebresne for CASSANDRA-4414

--- src/java/org/apache/cassandra/thrift/CassandraServer.java
+++ src/java/org/apache/cassandra/thrift/CassandraServer.java
@@ -893,9 +893,16 @@ public class CassandraServer implements Cassandra.Iface
     /**
      * Schedule the current thread for access to the required services
      */
-    private void schedule(long timeoutMS) throws TimeoutException
+    private void schedule(long timeoutMS) throws UnavailableException
     {
-        requestScheduler.queue(Thread.currentThread(), state().getSchedulingValue(), timeoutMS);
+        try
+        {
+            requestScheduler.queue(Thread.currentThread(), state().getSchedulingValue(), timeoutMS);
+        }
+        catch (TimeoutException e)
+        {
+            throw new UnavailableException();
+        }
     }
 
     /**
