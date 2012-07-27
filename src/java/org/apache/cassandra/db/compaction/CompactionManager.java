7ff8e3c384df32eaca361cbb77632d04cc64060b
Sylvain Lebresne <sylvain@datastax.com>
2012-07-27 10:06:33 +0200

    Don't purge columns during upgradesstables
    patch by slebresne; reviewed by jbellis for CASSANDRA-4462

--- src/java/org/apache/cassandra/db/compaction/CompactionManager.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionManager.java
@@ -67,6 +67,9 @@ public class CompactionManager implements CompactionManagerMBean
     private static final Logger logger = LoggerFactory.getLogger(CompactionManager.class);
     public static final CompactionManager instance;
 
+    public static final int NO_GC = Integer.MIN_VALUE;
+    public static final int GC_ALL = Integer.MAX_VALUE;
+
     /**
      * compactionLock has two purposes:
      * - Compaction acquires its readLock so that multiple compactions can happen simultaneously,
@@ -238,7 +241,7 @@ public class CompactionManager implements CompactionManagerMBean
                 {
                     // SSTables are marked by the caller
                     // NOTE: it is important that the task create one and only one sstable, even for Leveled compaction (see LeveledManifest.replace())
-                    CompactionTask task = new CompactionTask(cfs, Collections.singletonList(sstable), Integer.MAX_VALUE);
+                    CompactionTask task = new CompactionTask(cfs, Collections.singletonList(sstable), NO_GC);
                     task.isUserDefined(true);
                     task.setCompactionType(OperationType.UPGRADE_SSTABLES);
                     task.execute(executor);
@@ -974,7 +977,7 @@ public class CompactionManager implements CompactionManagerMBean
     static int getDefaultGcBefore(ColumnFamilyStore cfs)
     {
         return cfs.isIndex()
-               ? Integer.MAX_VALUE
+               ? GC_ALL
                : (int) (System.currentTimeMillis() / 1000) - cfs.metadata.getGcGraceSeconds();
     }
 
