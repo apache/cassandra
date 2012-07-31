f775e624281c87c483bb36d271e21d55f0d1ca00
Jonathan Ellis <jbellis@apache.org>
2012-07-31 10:44:13 -0500

    formatting

--- src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordReader.java
+++ src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordReader.java
@@ -359,6 +359,11 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
                     return;
                 }
 
+                // prepare for the next slice to be read
+                KeySlice lastRow = rows.get(rows.size() - 1);
+                ByteBuffer rowkey = lastRow.key;
+                startToken = partitioner.getTokenFactory().toString(partitioner.getToken(rowkey));
+
                 // remove ghosts when fetching all columns
                 if (isEmptyPredicate)
                 {
@@ -368,7 +373,7 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
                         KeySlice ks = it.next();
                         if (ks.getColumnsSize() == 0)
                         {
-                           it.remove();
+                            it.remove();
                         }
                     }
 
