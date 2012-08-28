501d7134f587c333034ccb2607502c23c61d3efd
Jonathan Ellis <jbellis@apache.org>
2012-08-28 15:53:03 -0500

    r/m unused SSTII.echoData

--- src/java/org/apache/cassandra/io/sstable/SSTableIdentityIterator.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableIdentityIterator.java
@@ -227,17 +227,6 @@ public class SSTableIdentityIterator implements Comparable<SSTableIdentityIterat
         }
     }
 
-    public void echoData(DataOutput out) throws IOException
-    {
-        if (!(input instanceof RandomAccessReader))
-            throw new UnsupportedOperationException();
-
-        ((RandomAccessReader) input).seek(dataStart);
-        inputWithTracker.reset(0);
-        while (inputWithTracker.getBytesRead() < dataSize)
-            out.write(inputWithTracker.readByte());
-    }
-
     public ColumnFamily getColumnFamilyWithColumns() throws IOException
     {
         assert inputWithTracker.getBytesRead() == headerSize();
