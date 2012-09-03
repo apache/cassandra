84b25f0b097ec2480e8805bb2c4f8d7e93ebe737
Dave Brosius <dbrosius@apache.org>
2012-09-03 12:18:12 -0400

    fix build

--- src/java/org/apache/cassandra/utils/Pair.java
+++ src/java/org/apache/cassandra/utils/Pair.java
@@ -24,7 +24,7 @@ public class Pair<T1, T2>
     public final T1 left;
     public final T2 right;
 
-    private Pair(T1 left, T2 right)
+    public Pair(T1 left, T2 right)
     {
         this.left = left;
         this.right = right;
