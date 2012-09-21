ef8620f8839f7f5278dbfb5b817c3656396966a4
Jonathan Ellis <jbellis@apache.org>
2012-09-21 11:32:23 -0500

    switch out Arrays.asList.toString for Arrays.toString patch by yukim; reviewed by jbellis for CASSANDRA-4697

--- src/java/org/apache/cassandra/db/compaction/LeveledManifest.java
+++ src/java/org/apache/cassandra/db/compaction/LeveledManifest.java
@@ -607,7 +607,7 @@ public class LeveledManifest
         }
 
         logger.debug("Estimating {} compactions to do for {}.{}",
-                     new Object[] {Arrays.asList(estimated), cfs.table.name, cfs.columnFamily});
+                     new Object[] {Arrays.toString(estimated), cfs.table.name, cfs.columnFamily});
         return Ints.checkedCast(tasks);
     }
 
