e752de96f7e3ae676b5dba0564e1321d6661a0cc
Jonathan Ellis <jbellis@apache.org>
2012-09-26 10:03:57 -0500

    fix listEndpointsPendingHints to generate String representation of the tokens for which we have hints

--- src/java/org/apache/cassandra/db/HintedHandOffManager.java
+++ src/java/org/apache/cassandra/db/HintedHandOffManager.java
@@ -446,32 +446,31 @@ public class HintedHandOffManager implements HintedHandOffManagerMBean
 
     public List<String> listEndpointsPendingHints()
     {
-        List<Row> rows = getHintsSlice(1);
+        Token.TokenFactory tokenFactory = StorageService.getPartitioner().getTokenFactory();
 
         // Extract the keys as strings to be reported.
         LinkedList<String> result = new LinkedList<String>();
-        for (Row r : rows)
+        for (Row row : getHintsSlice(1))
         {
-            if (r.cf != null) //ignore removed rows
-                result.addFirst(new String(r.key.key.array()));
+            if (row.cf != null) //ignore removed rows
+                result.addFirst(tokenFactory.toString(row.key.token));
         }
         return result;
     }
 
     public Map<String, Integer> countPendingHints()
     {
-        List<Row> rows = getHintsSlice(Integer.MAX_VALUE);
+        Token.TokenFactory tokenFactory = StorageService.getPartitioner().getTokenFactory();
 
         Map<String, Integer> result = new HashMap<String, Integer>();
-        Token.TokenFactory tokenFactory = StorageService.getPartitioner().getTokenFactory();
-        for (Row r : rows)
+        for (Row row : getHintsSlice(Integer.MAX_VALUE))
         {
-            if (r.cf == null) // ignore removed rows
+            if (row.cf == null) // ignore removed rows
                 continue;
 
-            int count = r.cf.getColumnCount();
+            int count = row.cf.getColumnCount();
             if (count > 0)
-                result.put(tokenFactory.toString(r.key.token), count);
+                result.put(tokenFactory.toString(row.key.token), count);
         }
         return result;
     }
