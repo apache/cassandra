2a91a48181b269684d491d961a0c513bf81baf25
Sylvain Lebresne <sylvain@datastax.com>
2012-10-05 10:24:37 +0200

    Fix comparison against IndexInterval in SSTR.getPosition()

--- src/java/org/apache/cassandra/io/sstable/SSTableReader.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableReader.java
@@ -758,15 +758,18 @@ public class SSTableReader extends SSTable
 
         // scan the on-disk index, starting at the nearest sampled position.
         // The check against IndexInterval is to be exit the loop in the EQ case when the key looked for is not present
-        // (bloom filter false positive).
+        // (bloom filter false positive). But note that for non-EQ cases, we might need to check the first key of the
+        // next index position because the searched key can be greater the last key of the index interval checked if it
+        // is lesser than the first key of next interval (and in that case we must return the position of the first key
+        // of the next interval).
         int i = 0;
         Iterator<FileDataInput> segments = ifile.iterator(sampledPosition, INDEX_FILE_BUFFER_BYTES);
-        while (segments.hasNext() && i < DatabaseDescriptor.getIndexInterval())
+        while (segments.hasNext() && i <= DatabaseDescriptor.getIndexInterval())
         {
             FileDataInput in = segments.next();
             try
             {
-                while (!in.isEOF() && i < DatabaseDescriptor.getIndexInterval())
+                while (!in.isEOF() && i <= DatabaseDescriptor.getIndexInterval())
                 {
                     i++;
 
