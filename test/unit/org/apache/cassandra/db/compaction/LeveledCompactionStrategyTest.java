9d58b71583b4de7228a1cc106f02ecdad8778efd
Yuki Morishita <yukim@apache.org>
2012-10-11 09:51:00 -0500

    move long LCS test to long-test

--- test/unit/org/apache/cassandra/db/compaction/LeveledCompactionStrategyTest.java
+++ test/unit/org/apache/cassandra/db/compaction/LeveledCompactionStrategyTest.java
@@ -18,11 +18,6 @@
 package org.apache.cassandra.db.compaction;
 
 import java.nio.ByteBuffer;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.concurrent.*;
 
 import org.junit.Test;
 
@@ -35,9 +30,6 @@ import org.apache.cassandra.db.Table;
 import org.apache.cassandra.db.filter.QueryPath;
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
-import org.apache.cassandra.io.sstable.SSTable;
-import org.apache.cassandra.io.sstable.SSTableReader;
-import org.apache.cassandra.io.sstable.SSTableScanner;
 import org.apache.cassandra.service.AntiEntropyService;
 import org.apache.cassandra.utils.ByteBufferUtil;
 import org.apache.cassandra.utils.FBUtilities;
@@ -90,93 +82,5 @@ public class LeveledCompactionStrategyTest extends SchemaLoader
         AntiEntropyService.TreeRequest req = new AntiEntropyService.TreeRequest("1", FBUtilities.getLocalAddress(), range, p);
         AntiEntropyService.Validator validator = new AntiEntropyService.Validator(req);
         CompactionManager.instance.submitValidation(store, validator).get();
-    }
-
-    @Test
-    public void testParallelLeveledCompaction() throws Exception
-    {
-        String ksname = "Keyspace1";
-        String cfname = "StandardLeveled";
-        Table table = Table.open(ksname);
-        ColumnFamilyStore store = table.getColumnFamilyStore(cfname);
-        store.disableAutoCompaction();
-
-        LeveledCompactionStrategy lcs = (LeveledCompactionStrategy)store.getCompactionStrategy();
-
-        ByteBuffer value = ByteBuffer.wrap(new byte[100 * 1024]); // 100 KB value, make it easy to have multiple files
-
-        // Enough data to have a level 1 and 2
-        int rows = 128;
-        int columns = 10;
-
-        // Adds enough data to trigger multiple sstable per level
-        for (int r = 0; r < rows; r++)
-        {
-            DecoratedKey key = Util.dk(String.valueOf(r));
-            RowMutation rm = new RowMutation(ksname, key.key);
-            for (int c = 0; c < columns; c++)
-            {
-                rm.add(new QueryPath(cfname, null, ByteBufferUtil.bytes("column" + c)), value, 0);
-            }
-            rm.apply();
-            store.forceBlockingFlush();
-        }
-
-        // Execute LCS in parallel
-        ExecutorService executor = new ThreadPoolExecutor(4, 4,
-                                                          Long.MAX_VALUE, TimeUnit.SECONDS,
-                                                          new LinkedBlockingDeque<Runnable>());
-        List<Runnable> tasks = new ArrayList<Runnable>();
-        while (true)
-        {
-            while (true)
-            {
-                final AbstractCompactionTask t = lcs.getMaximalTask(Integer.MIN_VALUE);
-                if (t == null)
-                    break;
-                tasks.add(new Runnable()
-                {
-                    public void run()
-                    {
-                        try
-                        {
-                            t.execute(null);
-                        }
-                        finally
-                        {
-                            t.unmarkSSTables();
-                        }
-                    }
-                });
-            }
-            if (tasks.isEmpty())
-                break;
-
-            List<Future<?>> futures = new ArrayList<Future<?>>(tasks.size());
-            for (Runnable r : tasks)
-                futures.add(executor.submit(r));
-            FBUtilities.waitOnFutures(futures);
-
-            tasks.clear();
-        }
-
-        // Assert all SSTables are lined up correctly.
-        LeveledManifest manifest = lcs.manifest;
-        int levels = manifest.getLevelCount();
-        for (int level = 0; level < levels; level++)
-        {
-            List<SSTableReader> sstables = manifest.getLevel(level);
-            // score check
-            assert (double) SSTable.getTotalBytes(sstables) / manifest.maxBytesForLevel(level) < 1.00;
-            // overlap check for levels greater than 0
-            if (level > 0)
-            {
-               for (SSTableReader sstable : sstables)
-               {
-                   Set<SSTableReader> overlaps = LeveledManifest.overlapping(sstable, sstables);
-                   assert overlaps.size() == 1 && overlaps.contains(sstable);
-               }
-            }
-        }
     }
 }
