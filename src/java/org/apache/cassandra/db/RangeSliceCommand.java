fe5e4aef2e463acc157b41ef4a7f9fab9a61050e
Dave Brosius <dbrosius@apache.org>
2012-10-14 20:31:02 -0400

    remove unused ctors, oops missed first commit with one

--- src/java/org/apache/cassandra/db/RangeSliceCommand.java
+++ src/java/org/apache/cassandra/db/RangeSliceCommand.java
@@ -96,11 +96,6 @@ public class RangeSliceCommand implements IReadCommand
         this(keyspace, column_parent.getColumn_family(), column_parent.super_column, predicate, range, row_filter, maxResults, false, false);
     }
 
-    public RangeSliceCommand(String keyspace, ColumnParent column_parent, IFilter predicate, AbstractBounds<RowPosition> range, List<IndexExpression> row_filter, int maxResults, boolean maxIsColumns, boolean isPaging)
-    {
-        this(keyspace, column_parent.getColumn_family(), column_parent.super_column, predicate, range, row_filter, maxResults, maxIsColumns, isPaging);
-    }
-
     public RangeSliceCommand(String keyspace, String column_family, ByteBuffer super_column, IFilter predicate, AbstractBounds<RowPosition> range, List<IndexExpression> row_filter, int maxResults)
     {
         this(keyspace, column_family, super_column, predicate, range, row_filter, maxResults, false, false);
