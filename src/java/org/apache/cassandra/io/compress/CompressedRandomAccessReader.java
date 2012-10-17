ffbcb4b11b40c52253b02098ca0225b9cf426cd2
Pavel Yaskevich <pyaskevich@twitter.com>
2012-10-17 11:06:28 -0700

    add a close() method to CRAR to prevent leaking file descriptors patch by Pavel Yaskevich; reviewed by Jonathan Ellis for CASSANDRA-4820

--- src/java/org/apache/cassandra/io/compress/CompressedRandomAccessReader.java
+++ src/java/org/apache/cassandra/io/compress/CompressedRandomAccessReader.java
@@ -125,6 +125,13 @@ public class CompressedRandomAccessReader extends RandomAccessReader
     }
 
     @Override
+    public void close() throws IOException
+    {
+        super.close();
+        source.close();
+    }
+
+    @Override
     public String toString()
     {
         return String.format("%s - chunk length %d, data length %d.", getPath(), metadata.chunkLength(), metadata.dataLength);
