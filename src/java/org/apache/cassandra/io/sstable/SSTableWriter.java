a4fda1fac86301d1b82268bd2239a9838f9f30c6
Jonathan Ellis <jbellis@apache.org>
2012-10-19 17:40:25 -0500

    fix indexing empty column values patch by jbellis for CASSANDRA-4832
    (orig: 72dcc298d335721c053444249c157e9a6431ebea)

--- src/java/org/apache/cassandra/io/sstable/SSTableWriter.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableWriter.java
@@ -129,8 +129,7 @@ public class SSTableWriter extends SSTable
      */
     private long beforeAppend(DecoratedKey<?> decoratedKey) throws IOException
     {
-        assert decoratedKey != null : "Keys must not be null";
-        assert decoratedKey.key.remaining() > 0 : "Keys must not be empty";
+        assert decoratedKey != null : "Keys must not be null"; // empty keys ARE allowed b/c of indexed column values
         if (lastWrittenKey != null && lastWrittenKey.compareTo(decoratedKey) >= 0)
             throw new RuntimeException("Last written key " + lastWrittenKey + " >= current key " + decoratedKey + " writing into " + getFilename());
         return (lastWrittenKey == null) ? 0 : dataFile.getFilePointer();
