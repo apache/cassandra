38bfc6dca06bd0192167ae5e9bd51d593542f03e
Jonathan Ellis <jbellis@apache.org>
2012-10-27 11:18:31 -0700

    avoid flushing everyone on truncate; save truncation position in system table instead patch by jbellis; reviewed by yukim for CASSANDRA-4906

--- src/java/org/apache/cassandra/cql3/UntypedResultSet.java
+++ src/java/org/apache/cassandra/cql3/UntypedResultSet.java
@@ -133,7 +133,14 @@ public class UntypedResultSet implements Iterable<UntypedResultSet.Row>
 
         public <T> Set<T> getSet(String column, AbstractType<T> type)
         {
-            return SetType.getInstance(type).compose(data.get(column));
+            ByteBuffer raw = data.get(column);
+            return raw == null ? null : SetType.getInstance(type).compose(raw);
+        }
+
+        public <K, V> Map<K, V> getMap(String column, AbstractType<K> keyType, AbstractType<V> valueType)
+        {
+            ByteBuffer raw = data.get(column);
+            return raw == null ? null : MapType.getInstance(keyType, valueType).compose(raw);
         }
 
         @Override
