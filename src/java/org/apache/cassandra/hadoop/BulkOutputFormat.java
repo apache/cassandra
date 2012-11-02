78d6f64f33c592890051c690ddf5d26b7b2af027
Jonathan Ellis <jbellis@apache.org>
2012-11-01 22:21:50 -0500

    BOF still requires CF to be preconfigured

--- src/java/org/apache/cassandra/hadoop/BulkOutputFormat.java
+++ src/java/org/apache/cassandra/hadoop/BulkOutputFormat.java
@@ -37,8 +37,10 @@ public class BulkOutputFormat extends OutputFormat<ByteBuffer,List<Mutation>>
 
     private void checkOutputSpecs(Configuration conf)
     {
-        if (ConfigHelper.getOutputKeyspace(conf) == null)
-            throw new UnsupportedOperationException("You must set the keyspace with setOutputKeyspace()");
+        if (ConfigHelper.getOutputKeyspace(conf) == null || ConfigHelper.getOutputColumnFamily(conf) == null)
+        {
+            throw new UnsupportedOperationException("you must set the keyspace and columnfamily with setColumnFamily()");
+        }
     }
 
     @Override
