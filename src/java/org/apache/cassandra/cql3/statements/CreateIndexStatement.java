617a4ab6fd1ee76820a95de5fe32a5b283e07c06
Sylvain Lebresne <sylvain@datastax.com>
2012-11-05 17:17:16 +0100

    Bug when composite index is created in a table having collections
    patch by slebresne; reviewed by jbellis for CASSANDRA-4909

--- src/java/org/apache/cassandra/cql3/statements/CreateIndexStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/CreateIndexStatement.java
@@ -76,11 +76,14 @@ public class CreateIndexStatement extends SchemaAlteringStatement
                 if (logger.isDebugEnabled())
                     logger.debug("Updating column {} definition for index {}", columnName, indexName);
 
+                if (cd.getValidator().isCollection())
+                    throw new InvalidRequestException("Indexes on collections are no yet supported");
+
                 if (cfDef.isComposite)
                 {
                     CompositeType composite = (CompositeType)cfm.comparator;
                     Map<String, String> opts = new HashMap<String, String>();
-                    opts.put(CompositesIndex.PREFIX_SIZE_OPTION, String.valueOf(composite.types.size() - 1));
+                    opts.put(CompositesIndex.PREFIX_SIZE_OPTION, String.valueOf(composite.types.size() - (cfDef.hasCollections ? 2 : 1)));
                     cd.setIndexType(IndexType.COMPOSITES, opts);
                 }
                 else
