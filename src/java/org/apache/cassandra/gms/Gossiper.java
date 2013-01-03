c2812f38a4eea8124786e3753c4e45a7e07a9fd1
Dave Brosius <dbrosius@apache.org>
2013-01-02 23:52:38 -0500

    no need for boxing histrionics

--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -805,7 +805,7 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
             subscriber.onJoin(ep, epState);
     }
 
-    private Boolean isDeadState(EndpointState epState)
+    private boolean isDeadState(EndpointState epState)
     {
         if (epState.getApplicationState(ApplicationState.STATUS) == null)
             return false;
