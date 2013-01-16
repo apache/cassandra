551da4cfa5448d076edc529596c73f9180a60c6d
Brandon Williams <brandonwilliams@apache.org>
2013-01-16 14:44:38 -0600

    Trim dc/rack names in [G]PFS Patch by Alexey Zotov, reviewed by brandonwilliams for CASSANDRA-5165

--- src/java/org/apache/cassandra/locator/GossipingPropertyFileSnitch.java
+++ src/java/org/apache/cassandra/locator/GossipingPropertyFileSnitch.java
@@ -48,6 +48,8 @@ public class GossipingPropertyFileSnitch extends AbstractNetworkTopologySnitch
         myRack = SnitchProperties.get("rack", null);
         if (myDC == null || myRack == null)
             throw new ConfigurationException("DC or rack not found in snitch properties");
+        myDC = myDC.trim();
+        myRack = myRack.trim();
         try
         {
             psnitch = new PropertyFileSnitch();
