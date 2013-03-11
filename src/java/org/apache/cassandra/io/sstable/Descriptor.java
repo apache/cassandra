ba06e4128b4cc66edb3e8e3ea02ce5cb5eae211c
Jonathan Ellis <jbellis@apache.org>
2013-03-11 18:13:57 -0500

    add tmp flag to Descriptor hashcode patch by Boris Yen; reviewed by jbellis for CASSANDRA-4021

--- src/java/org/apache/cassandra/io/sstable/Descriptor.java
+++ src/java/org/apache/cassandra/io/sstable/Descriptor.java
@@ -100,7 +100,7 @@ public class Descriptor
         this.cfname = cfname;
         this.generation = generation;
         temporary = temp;
-        hashCode = Objects.hashCode(directory, generation, ksname, cfname);
+        hashCode = Objects.hashCode(directory, generation, ksname, cfname, temp);
 
         hasStringsInBloomFilter = version.compareTo("c") < 0;
         hasIntRowSize = version.compareTo("d") < 0;
@@ -266,7 +266,7 @@ public class Descriptor
         if (!(o instanceof Descriptor))
             return false;
         Descriptor that = (Descriptor)o;
-        return that.directory.equals(this.directory) && that.generation == this.generation && that.ksname.equals(this.ksname) && that.cfname.equals(this.cfname);
+        return that.directory.equals(this.directory) && that.generation == this.generation && that.ksname.equals(this.ksname) && that.cfname.equals(this.cfname) && that.temporary == this.temporary;
     }
 
     @Override
