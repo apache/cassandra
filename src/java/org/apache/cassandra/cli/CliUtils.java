0e03790059e390fb58f45b8dc5e44e76f5e913d3
Aleksey Yeschenko <aleksey@apache.org>
2013-03-24 19:41:29 +0300

    Follow-up to CASSANDRA-5052

--- src/java/org/apache/cassandra/cli/CliUtils.java
+++ src/java/org/apache/cassandra/cli/CliUtils.java
@@ -53,7 +53,7 @@ public class CliUtils
 
     public static String maybeEscapeName(String name)
     {
-        return name.charAt(0) == '_' ? "\'" + name + "\'" : name;
+        return Character.isLetter(name.charAt(0)) ? name : "\'" + name + "\'";
     }
 
     /**
