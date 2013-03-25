b4d26bb4e417ef1d0ac02955eac041489fa5b508
Sylvain Lebresne <sylvain@datastax.com>
2013-03-25 15:25:08 +0100

    Fix bad default for min/max timestamp in SSTableMetadata
    patch by slebresne; reviewed by jasobrown for CASSANDRA-5372

--- src/java/org/apache/cassandra/io/sstable/SSTableMetadata.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableMetadata.java
@@ -62,7 +62,7 @@ public class SSTableMetadata
         this(defaultRowSizeHistogram(),
              defaultColumnCountHistogram(),
              ReplayPosition.NONE,
-             Long.MIN_VALUE,
+             Long.MAX_VALUE,
              Double.MIN_VALUE,
              null,
              Collections.<Integer>emptySet());
@@ -223,7 +223,7 @@ public class SSTableMetadata
                 // make sure we don't omit replaying something that we should.  see CASSANDRA-4782
                 replayPosition = ReplayPosition.NONE;
             }
-            long maxTimestamp = desc.containsTimestamp() ? dis.readLong() : Long.MIN_VALUE;
+            long maxTimestamp = desc.containsTimestamp() ? dis.readLong() : Long.MAX_VALUE;
             if (!desc.tracksMaxTimestamp) // see javadoc to Descriptor.containsTimestamp
                 maxTimestamp = Long.MAX_VALUE;
             double compressionRatio = desc.hasCompressionRatio
