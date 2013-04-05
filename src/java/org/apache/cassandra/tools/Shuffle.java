26bcbeea1a2a7af74aa830290b03ecdb56ed16f3
Jonathan Ellis <jbellis@apache.org>
2013-04-05 18:43:49 -0500

    fix inable -> disable edit

--- src/java/org/apache/cassandra/tools/Shuffle.java
+++ src/java/org/apache/cassandra/tools/Shuffle.java
@@ -596,7 +596,7 @@ public class Shuffle extends AbstractJmxClient
         sb.append(" ls               List pending relocations").append(String.format("%n"));
         sb.append(" clear            Clear pending relocations").append(String.format("%n"));
         sb.append(" en[able]         Enable shuffling").append(String.format("%n"));
-        sb.append(" in[able]        Disable shuffling").append(String.format("%n%n"));
+        sb.append(" dis[able]        Disable shuffling").append(String.format("%n%n"));
 
         printHelp("shuffle [options] <sub-command>", sb.toString());
     }
