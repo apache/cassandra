87b350ff777983cdd2f4136c5b649c8dfb11b685
Jonathan Ellis <jbellis@apache.org>
2013-04-09 14:09:26 -0500

    Include fatal errors in trace events patch by jbellis; reviewed by aleksey for CASSANDRA-5447

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -41,6 +41,7 @@ import org.apache.cassandra.db.compaction.CompactionManager;
 import org.apache.cassandra.io.FSError;
 import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.thrift.ThriftServer;
+import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.CLibrary;
 import org.apache.cassandra.utils.Mx4jTool;
 
@@ -172,6 +173,7 @@ public class CassandraDaemon
             {
                 exceptions.incrementAndGet();
                 logger.error("Exception in thread " + t, e);
+                Tracing.trace("Exception in thread " + t, e);
                 for (Throwable e2 = e; e2 != null; e2 = e2.getCause())
                 {
                     // some code, like FileChannel.map, will wrap an OutOfMemoryError in another exception
