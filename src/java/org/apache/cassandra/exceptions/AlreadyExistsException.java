27e8f87ff14296c7381d1e7767e37f75072c916c
Jonathan Ellis <jbellis@apache.org>
2013-05-28 20:32:49 -0500

    fix parameter order in error message for #5596

--- src/java/org/apache/cassandra/exceptions/AlreadyExistsException.java
+++ src/java/org/apache/cassandra/exceptions/AlreadyExistsException.java
@@ -31,7 +31,7 @@ public class AlreadyExistsException extends ConfigurationException
 
     public AlreadyExistsException(String ksName, String cfName)
     {
-        this(ksName, cfName, String.format("Cannot add already existing column family \"%s\" to keyspace \"%s\"", ksName, cfName));
+        this(ksName, cfName, String.format("Cannot add already existing column family \"%s\" to keyspace \"%s\"", cfName, ksName));
     }
 
     public AlreadyExistsException(String ksName)
