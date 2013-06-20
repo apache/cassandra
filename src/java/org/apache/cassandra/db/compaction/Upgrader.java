3814af8087c8b5541bea563344afcc344f5efa2a
Brandon Williams <brandonwilliams@apache.org>
2013-06-20 13:15:54 -0500

    Add standalone sstableupgrade utility. Patch by Nick Bailey, reviewed by brandonwilliams for CASSANDRA-5524

---
+++ src/java/org/apache/cassandra/db/compaction/Upgrader.java
@@ -0,0 +1,167 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.cassandra.db.compaction;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.IOError;
+import java.util.*;
+
+import com.google.common.base.Throwables;
+
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.RowIndexEntry;
+import org.apache.cassandra.db.compaction.AbstractCompactedRow;
+import org.apache.cassandra.db.compaction.AbstractCompactionStrategy;
+import org.apache.cassandra.db.compaction.AbstractCompactionIterable;
+import org.apache.cassandra.db.compaction.CompactionIterable;
+import org.apache.cassandra.db.compaction.CompactionController;
+import org.apache.cassandra.db.compaction.CompactionManager;
+import org.apache.cassandra.db.compaction.CompactionTask;
+import org.apache.cassandra.db.compaction.OperationType;
+import org.apache.cassandra.io.sstable.*;
+import org.apache.cassandra.io.util.RandomAccessReader;
+import org.apache.cassandra.utils.CloseableIterator;
+import org.apache.cassandra.utils.OutputHandler;
+
+public class Upgrader
+{
+    private final ColumnFamilyStore cfs;
+    private final SSTableReader sstable;
+    private final Collection<SSTableReader> toUpgrade;
+    private final File directory;
+
+    private final OperationType compactionType = OperationType.UPGRADE_SSTABLES;
+    private final CompactionController controller;
+    private final AbstractCompactionStrategy strategy;
+    private final long estimatedRows;
+
+    private final int gcBefore = CompactionManager.NO_GC;
+
+    private final OutputHandler outputHandler;
+
+    public Upgrader(ColumnFamilyStore cfs, SSTableReader sstable, OutputHandler outputHandler)
+    {
+        this.cfs = cfs;
+        this.sstable = sstable;
+        this.toUpgrade = Collections.singletonList(sstable);
+        this.outputHandler = outputHandler;
+
+        this.directory = new File(sstable.getFilename()).getParentFile();
+
+        this.controller = new UpgradeController(cfs);
+
+        this.strategy = cfs.getCompactionStrategy();
+        long estimatedTotalKeys = Math.max(DatabaseDescriptor.getIndexInterval(), SSTableReader.getApproximateKeyCount(toUpgrade));
+        long estimatedSSTables = Math.max(1, SSTable.getTotalBytes(this.toUpgrade) / strategy.getMaxSSTableSize());
+        this.estimatedRows = (long) Math.ceil((double) estimatedTotalKeys / estimatedSSTables);
+    }
+
+    private SSTableWriter createCompactionWriter()
+    {
+        SSTableMetadata.Collector sstableMetadataCollector = SSTableMetadata.createCollector();
+
+        // Get the max timestamp of the precompacted sstables
+        // and adds generation of live ancestors
+        for (SSTableReader sstable : toUpgrade)
+        {
+            sstableMetadataCollector.addAncestor(sstable.descriptor.generation);
+            for (Integer i : sstable.getAncestors())
+            {
+                if (new File(sstable.descriptor.withGeneration(i).filenameFor(Component.DATA)).exists())
+                    sstableMetadataCollector.addAncestor(i);
+            }
+        }
+
+        return new SSTableWriter(cfs.getTempSSTablePath(directory), estimatedRows, cfs.metadata, cfs.partitioner, sstableMetadataCollector);
+    }
+
+    public void upgrade()
+    {
+        outputHandler.output("Upgrading " + sstable);
+
+
+        AbstractCompactionIterable ci = new CompactionIterable(compactionType, strategy.getScanners(this.toUpgrade), controller);
+
+        CloseableIterator<AbstractCompactedRow> iter = ci.iterator();
+
+        Collection<SSTableReader> sstables = new ArrayList<SSTableReader>();
+        Collection<SSTableWriter> writers = new ArrayList<SSTableWriter>();
+
+        try
+        {
+            SSTableWriter writer = createCompactionWriter();
+            writers.add(writer);
+            while (iter.hasNext())
+            {
+                AbstractCompactedRow row = iter.next();
+
+                RowIndexEntry indexEntry = writer.append(row);
+            }
+
+            long maxAge = CompactionTask.getMaxDataAge(this.toUpgrade);
+            for (SSTableWriter completedWriter : writers)
+                sstables.add(completedWriter.closeAndOpenReader(maxAge));
+
+            outputHandler.output("Upgrade of " + sstable + " complete.");
+
+        }
+        catch (Throwable t)
+        {
+            for (SSTableWriter writer : writers)
+                writer.abort();
+            // also remove already completed SSTables
+            for (SSTableReader sstable : sstables)
+            {
+                sstable.markCompacted();
+                sstable.releaseReference();
+            }
+            throw Throwables.propagate(t);
+        }
+        finally
+        {
+            controller.close();
+
+            try
+            {
+                iter.close();
+            }
+            catch (IOException e)
+            {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    private static class UpgradeController extends CompactionController
+    {
+        public UpgradeController(ColumnFamilyStore cfs)
+        {
+            super(cfs, Integer.MAX_VALUE);
+        }
+
+        @Override
+        public boolean shouldPurge(DecoratedKey key, long delTimestamp)
+        {
+            return false;
+        }
+    }
+}
+
---
+++ src/java/org/apache/cassandra/tools/StandaloneUpgrader.java
@@ -0,0 +1,223 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cassandra.tools;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.*;
+
+import com.google.common.base.Throwables;
+
+import org.apache.commons.cli.*;
+
+import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.config.Schema;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.Directories;
+import org.apache.cassandra.db.Table;
+import org.apache.cassandra.db.compaction.Upgrader;
+import org.apache.cassandra.io.sstable.*;
+import org.apache.cassandra.service.CassandraDaemon;
+import org.apache.cassandra.utils.OutputHandler;
+
+import static org.apache.cassandra.tools.BulkLoader.CmdLineOptions;
+
+public class StandaloneUpgrader
+{
+    static
+    {
+        CassandraDaemon.initLog4j();
+    }
+
+    private static final String TOOL_NAME = "sstableupgrade";
+    private static final String DEBUG_OPTION  = "debug";
+    private static final String HELP_OPTION  = "help";
+
+    public static void main(String args[]) throws IOException
+    {
+        Options options = Options.parseArgs(args);
+        try
+        {
+            // load keyspace descriptions.
+            DatabaseDescriptor.loadSchemas();
+
+            if (Schema.instance.getCFMetaData(options.keyspace, options.cf) == null)
+                throw new IllegalArgumentException(String.format("Unknown keyspace/columnFamily %s.%s",
+                                                                 options.keyspace,
+                                                                 options.cf));
+
+            Table table = Table.openWithoutSSTables(options.keyspace);
+            ColumnFamilyStore cfs = table.getColumnFamilyStore(options.cf);
+
+            OutputHandler handler = new OutputHandler.SystemOutput(false, options.debug);
+            Directories.SSTableLister lister = cfs.directories.sstableLister();
+            if (options.snapshot != null)
+                lister.onlyBackups(true).snapshots(options.snapshot);
+            else
+                lister.includeBackups(false);
+
+            Collection<SSTableReader> readers = new ArrayList<SSTableReader>();
+
+            // Upgrade sstables
+            for (Map.Entry<Descriptor, Set<Component>> entry : lister.list().entrySet())
+            {
+                Set<Component> components = entry.getValue();
+                if (!components.contains(Component.DATA) || !components.contains(Component.PRIMARY_INDEX))
+                    continue;
+
+                try
+                {
+                    SSTableReader sstable = SSTableReader.openNoValidation(entry.getKey(), components, cfs.metadata);
+                    if (sstable.descriptor.version.equals(Descriptor.Version.CURRENT))
+                        continue;
+                    readers.add(sstable);
+                }
+                catch (Exception e)
+                {
+                    System.err.println(String.format("Error Loading %s: %s", entry.getKey(), e.getMessage()));
+                    if (options.debug)
+                        e.printStackTrace(System.err);
+
+                    continue;
+                }
+            }
+
+            int numSSTables = readers.size();
+            handler.output("Found " + numSSTables + " sstables that need upgrading.");
+
+            for (SSTableReader sstable : readers)
+            {
+                try
+                {
+                    Upgrader upgrader = new Upgrader(cfs, sstable, handler);
+                    upgrader.upgrade();
+
+                    sstable.markCompacted();
+                    sstable.releaseReference();
+                }
+                catch (Exception e)
+                {
+                    System.err.println(String.format("Error upgrading %s: %s", sstable, e.getMessage()));
+                    if (options.debug)
+                        e.printStackTrace(System.err);
+                }
+            }
+
+            SSTableDeletingTask.waitForDeletions();
+            System.exit(0);
+        }
+        catch (Exception e)
+        {
+            System.err.println(e.getMessage());
+            if (options.debug)
+                e.printStackTrace(System.err);
+            System.exit(1);
+        }
+    }
+
+    private static class Options
+    {
+        public final String keyspace;
+        public final String cf;
+        public final String snapshot;
+
+        public boolean debug;
+
+        private Options(String keyspace, String cf, String snapshot)
+        {
+            this.keyspace = keyspace;
+            this.cf = cf;
+            this.snapshot = snapshot;
+        }
+
+        public static Options parseArgs(String cmdArgs[])
+        {
+            CommandLineParser parser = new GnuParser();
+            CmdLineOptions options = getCmdLineOptions();
+            try
+            {
+                CommandLine cmd = parser.parse(options, cmdArgs, false);
+
+                if (cmd.hasOption(HELP_OPTION))
+                {
+                    printUsage(options);
+                    System.exit(0);
+                }
+
+                String[] args = cmd.getArgs();
+                if (args.length >= 4 || args.length < 2)
+                {
+                    String msg = args.length < 2 ? "Missing arguments" : "Too many arguments";
+                    errorMsg(msg, options);
+                    System.exit(1);
+                }
+
+                String keyspace = args[0];
+                String cf = args[1];
+                String snapshot = null;
+                if (args.length == 3)
+                    snapshot = args[2];
+
+                Options opts = new Options(keyspace, cf, snapshot);
+
+                opts.debug = cmd.hasOption(DEBUG_OPTION);
+
+                return opts;
+            }
+            catch (ParseException e)
+            {
+                errorMsg(e.getMessage(), options);
+                return null;
+            }
+        }
+
+        private static void errorMsg(String msg, CmdLineOptions options)
+        {
+            System.err.println(msg);
+            printUsage(options);
+            System.exit(1);
+        }
+
+        private static CmdLineOptions getCmdLineOptions()
+        {
+            CmdLineOptions options = new CmdLineOptions();
+            options.addOption(null, DEBUG_OPTION,          "display stack traces");
+            options.addOption("h",  HELP_OPTION,           "display this help message");
+            return options;
+        }
+
+        public static void printUsage(CmdLineOptions options)
+        {
+            String usage = String.format("%s [options] <keyspace> <cf> [snapshot]", TOOL_NAME);
+            StringBuilder header = new StringBuilder();
+            header.append("--\n");
+            header.append("Upgrade the sstables in the given cf (or snapshot) to the current version of Cassandra." );
+            header.append("This operation will rewrite the sstables in the specified cf to match the " );
+            header.append("currently installed version of Cassandra.\n");
+            header.append("The snapshot option will only upgrade the specified snapshot. Upgrading " );
+            header.append("snapshots is required before attempting to restore a snapshot taken in a " );
+            header.append("major version older than the major version Cassandra is currently running. " );
+            header.append("This will replace the files in the given snapshot as well as break any " );
+            header.append("hard links to live sstables." );
+            header.append("\n--\n");
+            header.append("Options are:");
+            new HelpFormatter().printHelp(usage, header.toString(), options, "");
+        }
+    }
+}
+
