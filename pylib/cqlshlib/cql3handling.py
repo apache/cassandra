f79a2d3a6f982c25ecf79010a6e1d9b538462600
Aleksey Yeschenko <aleksey@apache.org>
2013-07-15 18:35:33 +0300

    cqlsh: handle CUSTOM 2i in DESCRIBE output
    patch by Aleksey Yeschenko; reviewed by Jonathan Ellis for
    CASSANDRA-5760

--- pylib/cqlshlib/cql3handling.py
+++ pylib/cqlshlib/cql3handling.py
@@ -1395,6 +1395,8 @@ CqlRuleSet.append_rules(syntax_rules)
 
 class CqlColumnDef:
     index_name = None
+    index_type = None
+    index_options = {}
 
     def __init__(self, name, cqltype):
         self.name = name
@@ -1409,6 +1411,9 @@ class CqlColumnDef:
             colname = layout[u'column']
         c = cls(colname, lookup_casstype(layout[u'validator']))
         c.index_name = layout[u'index_name']
+        c.index_type = layout[u'index_type']
+        if c.index_type == 'CUSTOM':
+            c.index_options = json.loads(layout[u'index_options'])
         return c
 
     def __str__(self):
