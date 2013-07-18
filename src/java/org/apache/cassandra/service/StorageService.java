373c0336e3e902d2332765ce712546a9e1bef837
Brandon Williams <brandonwilliams@apache.org>
2013-07-18 18:46:12 -0500

    fix build

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -944,7 +944,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         }
         Tracing.instance();
         if (!Gossiper.instance.seenAnySeed())
-            throw new IllegalStateException("Unable to contact any seeds!")
+            throw new IllegalStateException("Unable to contact any seeds!");
         setMode(Mode.JOINING, "Starting to bootstrap...", true);
         new BootStrapper(FBUtilities.getBroadcastAddress(), tokens, tokenMetadata).bootstrap(); // handles token update
         logger.info("Bootstrap completed! for the tokens {}", tokens);
