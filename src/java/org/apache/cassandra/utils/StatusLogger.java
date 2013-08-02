ec673ac99708ba6c0d62a50f6c870d846a7435ba
Jonathan Ellis <jbellis@apache.org>
2013-08-02 12:16:17 -0500

    add "all time blocked" to StatusLogger output patch by Tyler Hobbs; reviewed by jbellis for CASSANDRA-5825

--- src/java/org/apache/cassandra/utils/StatusLogger.java
+++ src/java/org/apache/cassandra/utils/StatusLogger.java
@@ -50,7 +50,7 @@ public class StatusLogger
         MBeanServer server = ManagementFactory.getPlatformMBeanServer();
 
         // everything from o.a.c.concurrent
-        logger.info(String.format("%-25s%10s%10s%10s", "Pool Name", "Active", "Pending", "Blocked"));
+        logger.info(String.format("%-25s%10s%10s%15s%10s%18s", "Pool Name", "Active", "Pending", "Completed", "Blocked", "All Time Blocked"));
         Set<ObjectName> request, internal;
         try
         {
@@ -65,8 +65,13 @@ public class StatusLogger
         {
             String poolName = objectName.getKeyProperty("type");
             JMXEnabledThreadPoolExecutorMBean threadPoolProxy = JMX.newMBeanProxy(server, objectName, JMXEnabledThreadPoolExecutorMBean.class);
-            logger.info(String.format("%-25s%10s%10s%10s",
-                                      poolName, threadPoolProxy.getActiveCount(), threadPoolProxy.getPendingTasks(), threadPoolProxy.getCurrentlyBlockedTasks()));
+            logger.info(String.format("%-25s%10s%10s%15s%10s%18s",
+                                      poolName,
+                                      threadPoolProxy.getActiveCount(),
+                                      threadPoolProxy.getPendingTasks(),
+                                      threadPoolProxy.getCompletedTasks(),
+                                      threadPoolProxy.getCurrentlyBlockedTasks(),
+                                      threadPoolProxy.getTotalBlockedTasks()));
         }
         // one offs
         CompactionManager cm = CompactionManager.instance;
