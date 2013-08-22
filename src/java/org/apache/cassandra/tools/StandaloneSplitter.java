3f5322f02af3ef7e93706cd80ca65c5b127478e0
Marcus Eriksson <marcuse@spotify.com>
2013-08-22 15:39:16 +0200

    fix build - log4j is gone

--- src/java/org/apache/cassandra/tools/StandaloneSplitter.java
+++ src/java/org/apache/cassandra/tools/StandaloneSplitter.java
@@ -32,7 +32,6 @@ import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.compaction.LeveledManifest;
 import org.apache.cassandra.db.compaction.SSTableSplitter;
 import org.apache.cassandra.io.sstable.*;
-import org.apache.cassandra.service.CassandraDaemon;
 import org.apache.cassandra.utils.Pair;
 
 import static org.apache.cassandra.tools.BulkLoader.CmdLineOptions;
@@ -40,11 +39,6 @@ import static org.apache.cassandra.tools.BulkLoader.CmdLineOptions;
 public class StandaloneSplitter
 {
     public static final int DEFAULT_SSTABLE_SIZE = 50;
-
-    static
-    {
-        CassandraDaemon.initLog4j();
-    }
 
     private static final String TOOL_NAME = "sstablessplit";
     private static final String VERBOSE_OPTION = "verbose";
