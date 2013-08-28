a35f1c288f7c7fd82aca6bb7aaef8ec3f6bf857d
Jonathan Ellis <jbellis@apache.org>
2013-08-28 18:26:40 -0500

    Fix CqlRecordWriter with composite keys patch by Craig McMillian; reviewed by jbellis for CASSANDRA-5949

--- src/java/org/apache/cassandra/hadoop/cql3/CqlRecordWriter.java
+++ src/java/org/apache/cassandra/hadoop/cql3/CqlRecordWriter.java
@@ -307,7 +307,7 @@ final class CqlRecordWriter extends AbstractColumnFamilyRecordWriter<Map<String,
         {
             ByteBuffer[] keys = new ByteBuffer[partitionKeyColumns.length];
             for (int i = 0; i< keys.length; i++)
-                keys[i] = keyColumns.get(partitionKeyColumns[i]);
+                keys[i] = keyColumns.get(partitionKeyColumns[i]).duplicate();
 
             partitionKey = ((CompositeType) keyValidator).build(keys);
         }
