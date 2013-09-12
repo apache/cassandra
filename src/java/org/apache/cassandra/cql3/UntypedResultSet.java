394b35edf3e6fa4d83386622b438a8ca684385ad
Jonathan Ellis <jbellis@apache.org>
2013-09-12 13:40:40 +0200

    enhance UntypedResultSet patch by Ben Coverston; reviewed by jbellis for CASSANDRA-6000

--- src/java/org/apache/cassandra/cql3/UntypedResultSet.java
+++ src/java/org/apache/cassandra/cql3/UntypedResultSet.java
@@ -77,10 +77,12 @@ public class UntypedResultSet implements Iterable<UntypedResultSet.Row>
 
     public static class Row
     {
-        Map<String, ByteBuffer> data = new HashMap<String, ByteBuffer>();
+        final Map<String, ByteBuffer> data = new HashMap<String, ByteBuffer>();
+        final List<ColumnSpecification> columns;
 
         public Row(List<ColumnSpecification> names, List<ByteBuffer> columns)
         {
+            this.columns = names;
             for (int i = 0; i < names.size(); i++)
                 data.put(names.get(i).toString(), columns.get(i));
         }
@@ -142,10 +144,21 @@ public class UntypedResultSet implements Iterable<UntypedResultSet.Row>
             return raw == null ? null : SetType.getInstance(type).compose(raw);
         }
 
+        public <T> List<T> getList(String column, AbstractType<T> type)
+        {
+            ByteBuffer raw = data.get(column);
+            return raw == null ? null : ListType.getInstance(type).compose(raw);
+        }
+
         public <K, V> Map<K, V> getMap(String column, AbstractType<K> keyType, AbstractType<V> valueType)
         {
             ByteBuffer raw = data.get(column);
             return raw == null ? null : MapType.getInstance(keyType, valueType).compose(raw);
+        }
+
+        public List<ColumnSpecification> getColumns()
+        {
+            return columns;
         }
 
         @Override
