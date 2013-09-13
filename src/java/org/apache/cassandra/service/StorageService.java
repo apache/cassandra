08c07f5f323a30926fdb760a01b7ef993693153c
Brandon Williams <brandonwilliams@apache.org>
2013-09-13 12:17:37 -0500

    Fix NPE in nodetool on booting instances. Patch by Mikhail Stepura, reviewed by brandonwilliams for CASSANDRA-5968

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -323,7 +323,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public boolean isRPCServerRunning()
     {
-        if (daemon == null)
+        if ((daemon == null) || (daemon.thriftServer == null))
         {
             return false;
         }
@@ -358,7 +358,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public boolean isNativeTransportRunning()
     {
-        if (daemon == null)
+        if ((daemon == null) || (daemon.nativeServer == null))
         {
             return false;
         }
