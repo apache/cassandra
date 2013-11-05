dc71d2200339f9cf781346afc4b95b60dd3cd88f
Brandon Williams <brandonwilliams@apache.org>
2013-11-05 15:33:49 -0600

    fix build

--- src/java/org/apache/cassandra/hadoop/AbstractColumnFamilyInputFormat.java
+++ src/java/org/apache/cassandra/hadoop/AbstractColumnFamilyInputFormat.java
@@ -95,9 +95,12 @@ public abstract class AbstractColumnFamilyInputFormat<K, Y> extends InputFormat<
     {
         logger.debug("Creating authenticated client for CF input format");
         TTransport transport;
-        try {
-            transport = ConfigHelper.getClientTransportFactory(conf).openTransport(location, port, conf);
-        } catch (Exception e) {
+        try
+        {
+            transport = ConfigHelper.getClientTransportFactory(conf).openTransport(location, port);
+        }
+        catch (Exception e)
+        {
             throw new TTransportException("Failed to open a transport to " + location + ":" + port + ".", e);
         }
         TProtocol binaryProtocol = new TBinaryProtocol(transport, true, true);
