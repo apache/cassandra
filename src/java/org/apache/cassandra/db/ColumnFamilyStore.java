8a51a72ff0c014ae41bb36a51268ddf91cc71b85
Aleksey Yeschenko <aleksey@apache.org>
2013-12-28 20:56:50 +0300

    shutdown() the compaction strategy in CFS.invalidate()

--- src/java/org/apache/cassandra/db/ColumnFamilyStore.java
+++ src/java/org/apache/cassandra/db/ColumnFamilyStore.java
@@ -289,26 +289,27 @@ public class ColumnFamilyStore implements ColumnFamilyStoreMBean
     /** call when dropping or renaming a CF. Performs mbean housekeeping and invalidates CFS to other operations */
     public void invalidate()
     {
+        valid = false;
+
         try
         {
-            valid = false;
             unregisterMBean();
-
-            SystemTable.removeTruncationRecord(metadata.cfId);
-            data.unreferenceSSTables();
-            indexManager.invalidate();
-
-            for (RowCacheKey key : CacheService.instance.rowCache.getKeySet())
-            {
-                if (key.cfId == metadata.cfId)
-                    invalidateCachedRow(key);
-            }
         }
         catch (Exception e)
         {
             // this shouldn't block anything.
             logger.warn("Failed unregistering mbean: " + mbeanName, e);
         }
+
+        compactionStrategy.shutdown();
+
+        SystemTable.removeTruncationRecord(metadata.cfId);
+        data.unreferenceSSTables();
+        indexManager.invalidate();
+
+        for (RowCacheKey key : CacheService.instance.rowCache.getKeySet())
+            if (key.cfId == metadata.cfId)
+                invalidateCachedRow(key);
     }
 
     /**
