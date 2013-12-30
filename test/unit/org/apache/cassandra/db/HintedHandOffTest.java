23bc52386bd13fddf01037ac7e9cfb234f52ec43
Brandon Williams <brandonwilliams@apache.org>
2013-12-30 08:30:43 -0600

    fix test build

--- test/unit/org/apache/cassandra/db/HintedHandOffTest.java
+++ test/unit/org/apache/cassandra/db/HintedHandOffTest.java
@@ -102,8 +102,8 @@ public class HintedHandOffTest extends SchemaLoader
         hintStore.clearUnsafe();
 
         // insert 1 hint
-        RowMutation rm = new RowMutation(KEYSPACE4, ByteBufferUtil.bytes(1));
-        rm.add(STANDARD1_CF, ByteBufferUtil.bytes(String.valueOf(COLUMN1)), ByteBufferUtil.EMPTY_BYTE_BUFFER, System.currentTimeMillis());
+        Mutation rm = new Mutation(KEYSPACE4, ByteBufferUtil.bytes(1));
+        rm.add(STANDARD1_CF, Util.cellname(COLUMN1), ByteBufferUtil.EMPTY_BYTE_BUFFER, System.currentTimeMillis());
 
         HintedHandOffManager.instance.hintFor(rm, HintedHandOffManager.calculateHintTTL(rm), UUID.randomUUID()).apply();
 
