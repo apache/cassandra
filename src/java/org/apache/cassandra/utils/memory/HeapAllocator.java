6643dbdb1e1aef61c016b3f0c7d01c2c82ad91c0
Dave Brosius <dbrosius@mebigfatguy.com>
2014-02-02 00:26:38 -0500

    don't import internal sun classes

--- src/java/org/apache/cassandra/utils/memory/HeapAllocator.java
+++ src/java/org/apache/cassandra/utils/memory/HeapAllocator.java
@@ -19,8 +19,6 @@ package org.apache.cassandra.utils.memory;
 
 import java.nio.ByteBuffer;
 
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 public final class HeapAllocator extends AbstractAllocator
 {
     public static final HeapAllocator instance = new HeapAllocator();
