723fcd1936ad792b8e85490312cbe3edc9d4099e
Brandon Williams <brandonwilliams@apache.org>
2014-02-04 11:00:04 -0600

    fix NPE when stopping nonexistent native server, again.

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -346,7 +346,8 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         {
             throw new IllegalStateException("No configured daemon");
         }
-        daemon.nativeServer.stop();
+        if (daemon.nativeServer != null)
+            daemon.nativeServer.stop();
     }
 
     public boolean isNativeTransportRunning()
