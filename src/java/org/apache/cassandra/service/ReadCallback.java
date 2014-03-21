e682c0370e68af75779bf2b0cd1622fce6e03eab
Jonathan Ellis <jbellis@apache.org>
2014-03-21 16:55:36 -0500

    inline

--- src/java/org/apache/cassandra/service/ReadCallback.java
+++ src/java/org/apache/cassandra/service/ReadCallback.java
@@ -114,7 +114,10 @@ public class ReadCallback<TMessage, TResolved> implements IAsyncCallback<TMessag
         if (n >= blockfor && resolver.isDataPresent())
         {
             condition.signalAll();
-            maybeResolveForRepair(n);
+            // kick off a background digest comparison if this is a result that (may have) arrived after
+            // the original resolve that get() kicks off as soon as the condition is signaled
+            if (blockfor < endpoints.size() && n == endpoints.size())
+                StageManager.getStage(Stage.READ_REPAIR).execute(new AsyncRepairRunner());
         }
     }
 
@@ -144,19 +147,6 @@ public class ReadCallback<TMessage, TResolved> implements IAsyncCallback<TMessag
                                                        MessagingService.Verb.INTERNAL_RESPONSE,
                                                        MessagingService.current_version);
         response(message);
-    }
-
-    /**
-     * Check digests in the background on the Repair stage if we've received replies
-     * to all the requests we sent.
-     */
-    protected void maybeResolveForRepair(int n)
-    {
-        if (blockfor < endpoints.size() && n == endpoints.size())
-        {
-            assert resolver.isDataPresent();
-            StageManager.getStage(Stage.READ_REPAIR).execute(new AsyncRepairRunner());
-        }
     }
 
     public void assureSufficientLiveNodes() throws UnavailableException
