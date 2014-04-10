apache/cassandra

commit db9bc6929657fac40cf25af94bf919f1b213655a

use parameterized logging

src/java/org/apache/cassandra/repair/Differencer.java
@@ -71,7 +71,7 @@ public class Differencer implements Runnable
         }
 
         // non-0 difference: perform streaming repair
-        logger.info(String.format(format, "have " + differences.size() + " range(s) out of sync"));
+        logger.info(format, "have {} range(s) out of sync", differences.size());
         performStreamingRepair();
     }
 

src/java/org/apache/cassandra/client/RingCache.java
@@ -96,7 +96,7 @@ public class RingCache
             }
             catch (TException e)
             {
-                logger.debug("Error contacting seed list" + ConfigHelper.getOutputInitialAddress(conf) + " " + e.getMessage());
+                logger.debug("Error contacting seed list {} {}", ConfigHelper.getOutputInitialAddress(conf), e.getMessage());
             }
         }
 

src/java/org/apache/cassandra/db/commitlog/CommitLogReplayer.java
@@ -239,8 +239,7 @@ public class CommitLogReplayer
                 }
 
                 if (logger.isDebugEnabled())
-                    logger.debug(String.format("replaying mutation for %s.%s: %s", rm.getKeyspaceName(), ByteBufferUtil.bytesToHex(rm.key()), "{" + StringUtils.join(rm.getColumnFamilies().iterator(), ", ")
-                            + "}"));
+                    logger.debug("replaying mutation for {}.{}: {}", rm.getKeyspaceName(), ByteBufferUtil.bytesToHex(rm.key()), "{" + StringUtils.join(rm.getColumnFamilies().iterator(), ", ") + "}");
 
                 final long entryLocation = reader.getFilePointer();
                 final RowMutation frm = rm;

src/java/org/apache/cassandra/db/compaction/Scrubber.java
@@ -120,7 +120,7 @@ public class Scrubber implements Closeable
                 if (scrubInfo.isStopRequested())
                     throw new CompactionInterruptedException(scrubInfo.getCompactionInfo());
                 long rowStart = dataFile.getFilePointer();
-                outputHandler.debug("Reading row at " + rowStart);
+                outputHandler.debug("Reading row at {}" + rowStart);
 
                 DecoratedKey key = null;
                 long dataSize = -1;

src/java/org/apache/cassandra/hadoop/AbstractColumnFamilyInputFormat.java
@@ -123,7 +123,7 @@ public abstract class AbstractColumnFamilyInputFormat<K, Y> extends InputFormat<
         keyspace = ConfigHelper.getInputKeyspace(context.getConfiguration());
         cfName = ConfigHelper.getInputColumnFamily(context.getConfiguration());
         partitioner = ConfigHelper.getInputPartitioner(context.getConfiguration());
-        logger.debug("partitioner is " + partitioner);
+        logger.debug("partitioner is {}", partitioner);
 
         // cannonical ranges, split into pieces, fetching the splits in parallel
         ExecutorService executor = new ThreadPoolExecutor(0, 128, 60L, TimeUnit.SECONDS, new LinkedBlockingQueue<Runnable>());
@@ -252,7 +252,7 @@ public abstract class AbstractColumnFamilyInputFormat<K, Y> extends InputFormat<
                                     subSplit.getRow_count(),
                                     endpoints);
 
-                    logger.debug("adding " + split);
+                    logger.debug("adding {}", split);
                     splits.add(split);
                 }
             }
@@ -292,7 +292,7 @@ public abstract class AbstractColumnFamilyInputFormat<K, Y> extends InputFormat<
             }
             catch (IOException e)
             {
-                logger.debug("failed connect to endpoint " + host, e);
+                logger.debug("failed connect to endpoint {}", host, e);
             }
             catch (InvalidRequestException e)
             {

src/java/org/apache/cassandra/hadoop/BulkRecordWriter.java
@@ -247,7 +247,7 @@ implements org.apache.hadoop.mapred.RecordWriter<ByteBuffer,List<Mutation>>
                 if (loader.getFailedHosts().size() > maxFailures)
                     throw new IOException("Too many hosts failed: " + loader.getFailedHosts());
                 else
-                    logger.warn("Some hosts failed: " + loader.getFailedHosts());
+                    logger.warn("Some hosts failed: {}", loader.getFailedHosts());
             }
         }
     }

src/java/org/apache/cassandra/hadoop/ColumnFamilyRecordReader.java
@@ -174,7 +174,7 @@ public class ColumnFamilyRecordReader extends RecordReader<ByteBuffer, SortedMap
     {
         if (!iter.hasNext())
         {
-            logger.debug("Finished scanning " + iter.rowsRead() + " rows (estimate was: " + totalRowCount + ")");
+            logger.debug("Finished scanning {} rows (estimate was: {})", iter.rowsRead(), totalRowCount);
             return false;
         }
 

src/java/org/apache/cassandra/hadoop/cql3/CqlRecordWriter.java
@@ -334,7 +334,7 @@ final class CqlRecordWriter extends AbstractColumnFamilyRecordWriter<Map<String,
         
         Column rawPartitionKeys = result.rows.get(0).columns.get(1);
         String keyString = ByteBufferUtil.string(ByteBuffer.wrap(rawPartitionKeys.getValue()));
-        logger.debug("partition keys: " + keyString);
+        logger.debug("partition keys: {}", keyString);
 
         List<String> keys = FBUtilities.fromJsonList(keyString);
         partitionKeyColumns = new String[keys.size()];
@@ -348,7 +348,7 @@ final class CqlRecordWriter extends AbstractColumnFamilyRecordWriter<Map<String,
         Column rawClusterColumns = result.rows.get(0).columns.get(2);
         String clusterColumnString = ByteBufferUtil.string(ByteBuffer.wrap(rawClusterColumns.getValue()));
 
-        logger.debug("cluster columns: " + clusterColumnString);
+        logger.debug("cluster columns: {}", clusterColumnString);
         clusterColumns = FBUtilities.fromJsonList(clusterColumnString);
     }
 

commit 66a6990aa076bbcdebee952f47c95ccdad735dbc

Fix incorrect logging output
Change in db9bc6929657fac40cf25af94bf919f1b213655a broke logging not to
output number of ranges out of sync.

src/java/org/apache/cassandra/repair/Differencer.java
@@ -71,7 +71,7 @@ public class Differencer implements Runnable
         }
 
         // non-0 difference: perform streaming repair
-        logger.info(format, "have {} range(s) out of sync", differences.size());
+        logger.info(String.format(format, "have " + differences.size() + " range(s) out of sync"));
         performStreamingRepair();
     }
 