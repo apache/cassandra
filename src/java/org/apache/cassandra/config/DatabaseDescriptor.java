b3a225ef101d8ac2de183279843f94a54a38c907
Jonathan Ellis <jbellis@apache.org>
2014-04-23 07:25:35 -0500

    Increase default CL space to 8GB patch by Benedict Elliott Smith; tested by Ryan McGuire and reviewed by jbellis for CASSANDRA-7031

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -187,7 +187,7 @@ public class DatabaseDescriptor
         }
 
         if (conf.commitlog_total_space_in_mb == null)
-            conf.commitlog_total_space_in_mb = hasLargeAddressSpace() ? 1024 : 32;
+            conf.commitlog_total_space_in_mb = hasLargeAddressSpace() ? 8192 : 32;
 
         /* evaluate the DiskAccessMode Config directive, which also affects indexAccessMode selection */
         if (conf.disk_access_mode == Config.DiskAccessMode.auto)
