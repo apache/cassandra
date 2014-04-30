1b51bec1455265de050fa85f1a2f90c26d28f591
Dave Brosius <dbrosius@mebigfatguy.com>
2014-04-29 21:07:09 -0400

    fix compile

--- test/unit/org/apache/cassandra/db/ArrayBackedSortedColumnsTest.java
+++ test/unit/org/apache/cassandra/db/ArrayBackedSortedColumnsTest.java
@@ -223,7 +223,7 @@ public class ArrayBackedSortedColumnsTest extends SchemaLoader
         int[] values = new int[]{ 1, 2, 3, 5, 9, 15, 21, 22 };
 
         for (int i = 0; i < values.length; ++i)
-            map.addColumn(new Cell(type.makeCellName(values[i])));
+            map.addColumn(new BufferCell(type.makeCellName(values[i])));
 
         SearchIterator<CellName, Cell> iter = map.searchIterator();
         for (int i = 0 ; i < values.length ; i++)
