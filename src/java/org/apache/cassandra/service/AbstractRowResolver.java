74e96b460ae779e5823178108001871d4a10be7a
Jonathan Ellis <jbellis@apache.org>
2014-04-30 11:46:51 -0500

    change NBHS in ARR to CLQ patch by bes; reviewed by jbellis for CASSANDRA-7116

--- src/java/org/apache/cassandra/service/AbstractRowResolver.java
+++ src/java/org/apache/cassandra/service/AbstractRowResolver.java
@@ -18,9 +18,9 @@
 package org.apache.cassandra.service;
 
 import java.nio.ByteBuffer;
-import java.util.Set;
+import java.util.Collection;
+import java.util.concurrent.ConcurrentLinkedQueue;
 
-import org.cliffc.high_scale_lib.NonBlockingHashSet;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -34,7 +34,8 @@ public abstract class AbstractRowResolver implements IResponseResolver<ReadRespo
     protected static final Logger logger = LoggerFactory.getLogger(AbstractRowResolver.class);
 
     protected final String keyspaceName;
-    protected final Set<MessageIn<ReadResponse>> replies = new NonBlockingHashSet<MessageIn<ReadResponse>>();
+    // CLQ gives us thread-safety without the overhead of guaranteeing uniqueness like a Set would
+    protected final Collection<MessageIn<ReadResponse>> replies = new ConcurrentLinkedQueue<>();
     protected final DecoratedKey key;
 
     public AbstractRowResolver(ByteBuffer key, String keyspaceName)
