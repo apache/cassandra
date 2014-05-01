233761e53988301222dfaa590dc7fd8ee396c9b4
Sylvain Lebresne <sylvain@datastax.com>
2014-05-01 11:37:36 +0200

    Fix regression from CASSANDRA-6855

--- src/java/org/apache/cassandra/cql3/QueryOptions.java
+++ src/java/org/apache/cassandra/cql3/QueryOptions.java
@@ -58,7 +58,7 @@ public abstract class QueryOptions
 
     public static QueryOptions forInternalCalls(ConsistencyLevel consistency, List<ByteBuffer> values)
     {
-        return new DefaultQueryOptions(consistency, values, false, SpecificOptions.DEFAULT, 0);
+        return new DefaultQueryOptions(consistency, values, false, SpecificOptions.DEFAULT, 3);
     }
 
     public static QueryOptions fromPreV3Batch(ConsistencyLevel consistency)
@@ -123,8 +123,6 @@ public abstract class QueryOptions
 
         private final SpecificOptions options;
 
-        // The protocol version of incoming queries. This is set during deserializaion and will be 0
-        // if the QueryOptions does not come from a user message (or come from thrift).
         private final transient int protocolVersion;
 
         DefaultQueryOptions(ConsistencyLevel consistency, List<ByteBuffer> values, boolean skipMetadata, SpecificOptions options, int protocolVersion)
