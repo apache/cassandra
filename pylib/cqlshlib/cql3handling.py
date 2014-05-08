a626c6dcbcfa4692167e57e3217d46642e26ff53
Mikhail Stepura <mishail@apache.org>
2014-05-08 13:00:03 -0700

    CQLSH: adjusted compaction_strategy_options with the latest docs
    patch by Mikhail Stepura; reviewed by Brandon Williams for CASSANDRA-7185

--- pylib/cqlshlib/cql3handling.py
+++ pylib/cqlshlib/cql3handling.py
@@ -79,7 +79,7 @@ class Cql3ParsingRuleSet(CqlParsingRuleSet):
         # (CQL3 option name, schema_columnfamilies column name (or None if same),
         #  list of known map keys)
         ('compaction', 'compaction_strategy_options',
-            ('class', 'min_threshold', 'max_threshold')),
+            ('class', 'max_threshold', 'tombstone_compaction_interval', 'tombstone_threshold', 'enabled')),
         ('compression', 'compression_parameters',
             ('sstable_compression', 'chunk_length_kb', 'crc_check_chance')),
     )
@@ -473,6 +473,10 @@ def cf_prop_val_mapkey_completer(ctxt, cass):
         csc = csc.split('.')[-1]
         if csc == 'SizeTieredCompactionStrategy':
             opts.add('min_sstable_size')
+            opts.add('min_threshold')
+            opts.add('bucket_high')
+            opts.add('bucket_low')
+            opts.add('cold_reads_to_omit')
         elif csc == 'LeveledCompactionStrategy':
             opts.add('sstable_size_in_mb')
         return map(escape_value, opts)
