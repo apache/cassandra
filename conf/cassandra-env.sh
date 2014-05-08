16fd1a4a89958595ca2ae44fdac2eb7aa1ad6be2
Dave Brosius <dbrosius@mebigfatguy.com>
2014-05-08 00:32:29 -0400

    fix c* launch issues on Russian os's due to output of linux 'free' cmd
    patch by dbrosius reviewed by bwilliams for cassandra-6162

--- conf/cassandra-env.sh
+++ conf/cassandra-env.sh
@@ -18,7 +18,7 @@ calculate_heap_sizes()
 {
     case "`uname`" in
         Linux)
-            system_memory_in_mb=`free -m | awk '/Mem:/ {print $2}'`
+            system_memory_in_mb=`free -m | awk '/:/ {print $2;exit}'`
             system_cpu_cores=`egrep -c 'processor([[:space:]]+):.*' /proc/cpuinfo`
         ;;
         FreeBSD)
