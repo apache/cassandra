d92a79f5b3287ee26e45b666b2c4c5feec823f74
Tyler Hobbs <tyler@datastax.com>
2014-05-28 16:59:06 -0500

    Ninja: remove redundant check for null

--- src/java/org/apache/cassandra/cql3/Tuples.java
+++ src/java/org/apache/cassandra/cql3/Tuples.java
@@ -308,9 +308,6 @@ public class Tuples
         public Value bind(List<ByteBuffer> values) throws InvalidRequestException
         {
             ByteBuffer value = values.get(bindIndex);
-            if (value == null)
-                return null;
-
             return value == null ? null : Value.fromSerialized(value, (TupleType)receiver.type);
         }
     }
