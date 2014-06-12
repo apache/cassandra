7065b1bdf119e07f2e64735f60ab3e3d036c00de
Brandon Williams <brandonwilliams@apache.org>
2014-06-12 18:42:21 -0500

    Track metrics at a keyspace level
    Patch by brandonwilliams, reviewed by yukim for CASSANDRA-6539

--- src/java/org/apache/cassandra/db/Table.java
+++ src/java/org/apache/cassandra/db/Table.java
@@ -42,6 +42,7 @@ import org.apache.cassandra.io.sstable.SSTableReader;
 import org.apache.cassandra.locator.AbstractReplicationStrategy;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.tracing.Tracing;
+import org.apache.cassandra.metrics.KeyspaceMetrics;
 
 /**
  * It represents a Keyspace.
@@ -74,6 +75,7 @@ public class Table
     private final ConcurrentMap<UUID, ColumnFamilyStore> columnFamilyStores = new ConcurrentHashMap<UUID, ColumnFamilyStore>();
     private final Object[] indexLocks;
     private volatile AbstractReplicationStrategy replicationStrategy;
+    public final KeyspaceMetrics metric;
     public static final Function<String,Table> tableTransformer = new Function<String, Table>()
     {
         public Table apply(String tableName)
@@ -132,6 +134,7 @@ public class Table
             {
                 for (ColumnFamilyStore cfs : t.getColumnFamilyStores())
                     t.unloadCf(cfs);
+                t.metric.release();
             }
             return t;
         }
@@ -271,6 +274,7 @@ public class Table
             logger.debug("Initializing {}.{}", name, cfm.cfName);
             initCf(cfm.cfId, cfm.cfName, loadSSTables);
         }
+        this.metric = new KeyspaceMetrics(this);
     }
 
     public void createReplicationStrategy(KSMetaData ksm)
