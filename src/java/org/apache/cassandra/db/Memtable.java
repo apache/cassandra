93f54fc44461c995ff30d42f284c78d1a9a02723
Jonathan Ellis <jbellis@apache.org>
2014-06-20 15:31:52 -0500

    revert #7275

--- src/java/org/apache/cassandra/db/Memtable.java
+++ src/java/org/apache/cassandra/db/Memtable.java
@@ -380,17 +380,11 @@ public class Memtable
 
         protected void runWith(File sstableDirectory) throws Exception
         {
-            try
-            {
-                assert sstableDirectory != null : "Flush task is not bound to any disk";
+            assert sstableDirectory != null : "Flush task is not bound to any disk";
 
-                SSTableReader sstable = writeSortedContents(context, sstableDirectory);
-                cfs.replaceFlushed(Memtable.this, sstable);
-            }
-            finally
-            {
-                latch.countDown();
-            }
+            SSTableReader sstable = writeSortedContents(context, sstableDirectory);
+            cfs.replaceFlushed(Memtable.this, sstable);
+            latch.countDown();
         }
 
         protected Directories getDirectories()
