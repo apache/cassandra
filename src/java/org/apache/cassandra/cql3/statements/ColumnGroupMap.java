a9c71e4a2049070247535f5870253148619b14e8
Sylvain Lebresne <sylvain@datastax.com>
2014-06-30 15:56:56 +0200

    Fix Assertion errors when using empty clustering with static columns
    patch by slebresne; reviewed by iamaleksey for CASSANDRA-7455

--- src/java/org/apache/cassandra/cql3/statements/ColumnGroupMap.java
+++ src/java/org/apache/cassandra/cql3/statements/ColumnGroupMap.java
@@ -136,13 +136,13 @@ public class ColumnGroupMap
 
             if (currentGroup == null)
             {
-                currentGroup = new ColumnGroupMap(current, composite.isStaticName(c.name()));
+                currentGroup = new ColumnGroupMap(current, CompositeType.isStaticName(c.name()));
                 currentGroup.add(current, idx, c);
                 previous = current;
                 return;
             }
 
-            if (!isSameGroup(current))
+            if ((currentGroup.isStatic && !CompositeType.isStaticName(c.name())) || !isSameGroup(current))
             {
                 groups.add(currentGroup);
                 // Note that we know that only the first group built can be static
