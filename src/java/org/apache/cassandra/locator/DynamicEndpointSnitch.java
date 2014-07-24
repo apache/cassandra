67ce78fcc70422b7d831f6fc4a6e5512ca4d5ffa
Tyler Hobbs <tyler@datastax.com>
2014-07-24 17:38:26 -0500

    Always merge ranges owned by a single node in DES
    Patch by Ala' Alkhaldi; review by Tyler Hobbs for CASSANDRA-6930

--- src/java/org/apache/cassandra/locator/DynamicEndpointSnitch.java
+++ src/java/org/apache/cassandra/locator/DynamicEndpointSnitch.java
@@ -318,6 +318,10 @@ public class DynamicEndpointSnitch extends AbstractEndpointSnitch implements ILa
         if (!subsnitch.isWorthMergingForRangeQuery(merged, l1, l2))
             return false;
 
+        // skip checking scores in the single-node case
+        if (l1.size() == 1 && l2.size() == 1 && l1.get(0).equals(l2.get(0)))
+            return true;
+
         // Make sure we return the subsnitch decision (i.e true if we're here) if we lack too much scores
         double maxMerged = maxScore(merged);
         double maxL1 = maxScore(l1);
