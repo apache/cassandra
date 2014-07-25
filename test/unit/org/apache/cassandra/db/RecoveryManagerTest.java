60e56d4bebdd7889df88274540d58159b17a3364
Brandon Williams <brandonwilliams@apache.org>
2014-07-25 10:58:37 -0500

    fix test for 2.0

--- test/unit/org/apache/cassandra/db/RecoveryManagerTest.java
+++ test/unit/org/apache/cassandra/db/RecoveryManagerTest.java
@@ -137,7 +137,7 @@ public class RecoveryManagerTest extends SchemaLoader
         Date date = CommitLogArchiver.format.parse("2112:12:12 12:12:12");
         long timeMS = date.getTime();
 
-        Keyspace keyspace1 = Keyspace.open(KEYSPACE1);
+        Keyspace keyspace1 = Keyspace.open("Keyspace1");
         DecoratedKey dk = Util.dk("dkey");
 
         // Col 0 and 9 are the only ones to be recovered
@@ -149,9 +149,9 @@ public class RecoveryManagerTest extends SchemaLoader
             else
                 ts = TimeUnit.MILLISECONDS.toMicros(timeMS + (i * 1000));
 
-            ColumnFamily cf = ArrayBackedSortedColumns.factory.create(KEYSPACE1, "Standard1");
+            ColumnFamily cf = ArrayBackedSortedColumns.factory.create("Keyspace1", "Standard1");
             cf.addColumn(column("name-" + i, "value", ts));
-            Mutation rm = new Mutation(KEYSPACE1, dk.getKey(), cf);
+            RowMutation rm = new RowMutation("Keyspace1", dk.key, cf);
             rm.apply();
         }
 
