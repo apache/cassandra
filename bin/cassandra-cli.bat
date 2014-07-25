3e489dad7d56b0a6a4084cd3f17a86ec41cb9fd5
Jonathan Ellis <jbellis@apache.org>
2014-07-25 17:23:11 -0500

    Clean up Windows batch scripts patch by Josh McKenzie; reviewed by Ala' Alkhaldi for CASSANDRA-7619

--- bin/cassandra-cli.bat
+++ bin/cassandra-cli.bat
@@ -14,30 +14,15 @@
 @REM  See the License for the specific language governing permissions and
 @REM  limitations under the License.
 
-
 @echo off
 if "%OS%" == "Windows_NT" setlocal
+
+pushd "%~dp0"
+call cassandra.in.bat
 
 if NOT DEFINED CASSANDRA_HOME set CASSANDRA_HOME=%~dp0..
 if NOT DEFINED JAVA_HOME goto :err
 
-REM Ensure that any user defined CLASSPATH variables are not used on startup
-set CLASSPATH=
-
-REM For each jar in the CASSANDRA_HOME lib directory call append to build the CLASSPATH variable.
-for %%i in ("%CASSANDRA_HOME%\lib\*.jar") do call :append "%%i"
-goto okClasspath
-
-:append
-set CLASSPATH=%CLASSPATH%;%1
-goto :eof
-
-:okClasspath
-REM Include the build\classes\main directory so it works in development
-set CASSANDRA_CLASSPATH=%CLASSPATH%;"%CASSANDRA_HOME%\build\classes\main";"%CASSANDRA_HOME%\build\classes\thrift"
-goto runCli
-
-:runCli
 echo Starting Cassandra Client
 "%JAVA_HOME%\bin\java" -cp %CASSANDRA_CLASSPATH% org.apache.cassandra.cli.CliMain %*
 goto finally
