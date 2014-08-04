4a9634a31ea7bbdf3b6c0173a94e6ab3911bc989
Jonathan Ellis <jbellis@apache.org>
2014-08-04 12:02:19 -0500

    Support connecting to ipv6 jmx with nodetool patch by Robert Stupp; reviewed by jbellis for CASSANDRA-7669

--- src/java/org/apache/cassandra/tools/NodeProbe.java
+++ src/java/org/apache/cassandra/tools/NodeProbe.java
@@ -60,7 +60,7 @@ import org.apache.cassandra.utils.SimpleCondition;
  */
 public class NodeProbe
 {
-    private static final String fmtUrl = "service:jmx:rmi:///jndi/rmi://%s:%d/jmxrmi";
+    private static final String fmtUrl = "service:jmx:rmi:///jndi/rmi://[%s]:%d/jmxrmi";
     private static final String ssObjName = "org.apache.cassandra.db:type=StorageService";
     private static final int defaultPort = 7199;
     final String host;
