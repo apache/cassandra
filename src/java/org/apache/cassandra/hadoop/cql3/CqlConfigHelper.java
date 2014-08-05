f7598815059bad05b6c6a4af53c8f5f5c8800a04
Brandon Williams <brandonwilliams@apache.org>
2014-08-05 14:05:21 -0500

    fix build, remove dead method again

--- src/java/org/apache/cassandra/hadoop/cql3/CqlConfigHelper.java
+++ src/java/org/apache/cassandra/hadoop/cql3/CqlConfigHelper.java
@@ -519,11 +519,6 @@ public class CqlConfigHelper
             }
 
             @Override
-            public void onSuspected(Host host)
-            {
-            }
-
-            @Override
             public HostDistance distance(Host host)
             {
                 if (host.getAddress().getHostName().equals(stickHost))
