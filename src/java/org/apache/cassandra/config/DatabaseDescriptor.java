a21e655ebd4f7d63926f00ce6b2a86390c35dfb9
Jonathan Ellis <jbellis@apache.org>
2014-08-06 12:41:46 -0500

    Add cassandra.auto_bootstrap system property patch by Joe Hohertz; reviewed by jbellis for CASSANDRA-7650

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1138,7 +1138,7 @@ public class DatabaseDescriptor
 
     public static boolean isAutoBootstrap()
     {
-        return conf.auto_bootstrap;
+        return Boolean.parseBoolean(System.getProperty("cassandra.auto_bootstrap", conf.auto_bootstrap.toString()));
     }
 
     public static void setHintedHandoffEnabled(boolean hintedHandoffEnabled)
