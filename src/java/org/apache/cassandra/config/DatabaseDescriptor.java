e0c4c6e60b682630ecc1285d4def1a652d9203dc
Sylvain Lebresne <sylvain@datastax.com>
2014-08-07 16:06:39 +0200

    CASSANDRA-7635, rebase for 2.1

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1354,6 +1354,11 @@ public class DatabaseDescriptor
         return conf.batchlog_replay_throttle_in_kb;
     }
 
+    public static void setHintedHandoffThrottleInKB(Integer throttleInKB)
+    {
+        conf.hinted_handoff_throttle_in_kb = throttleInKB;
+    }
+
     public static int getMaxHintsThread()
     {
         return conf.max_hints_delivery_threads;
