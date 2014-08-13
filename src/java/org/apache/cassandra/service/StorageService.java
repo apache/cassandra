3fe702825e882398052fa26b230861aac47a0671
Jonathan Ellis <jbellis@apache.org>
2014-08-13 10:22:00 -0500

    Remove duplicates from StorageService.getJoiningNodes patch by jbellis; reviewed by ayeschenko for CASSANDRA-7478

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -2115,7 +2115,7 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public List<String> getJoiningNodes()
     {
-        return stringify(tokenMetadata.getBootstrapTokens().values());
+        return stringify(tokenMetadata.getBootstrapTokens().valueSet());
     }
 
     public List<String> getLiveNodes()
