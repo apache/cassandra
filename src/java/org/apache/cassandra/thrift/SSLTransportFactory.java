df046dfe092ea5a08ec0e29a00d30423284d1f86
Jason Brown <jasobrown@apple.com>
2014-10-06 14:44:51 -0700

    SSL does not work in cassandra-cli.
    patch by jasobrown, reviewed by beobal for CASSANDRA-7899

--- src/java/org/apache/cassandra/thrift/SSLTransportFactory.java
+++ src/java/org/apache/cassandra/thrift/SSLTransportFactory.java
@@ -18,10 +18,9 @@
 package org.apache.cassandra.thrift;
 
 import com.google.common.collect.Sets;
-import org.apache.cassandra.cli.transport.FramedTransportFactory;
+import org.apache.thrift.transport.TFramedTransport;
 import org.apache.thrift.transport.TSSLTransportFactory;
 import org.apache.thrift.transport.TTransport;
-import org.apache.thrift.transport.TTransportException;
 
 import java.util.Map;
 import java.util.Set;
@@ -58,7 +57,8 @@ public class SSLTransportFactory implements ITransportFactory
         if (null != keystore)
             params.setKeyStore(keystore, keystorePassword);
         TTransport trans = TSSLTransportFactory.getClientSocket(host, port, SOCKET_TIMEOUT, params);
-        return new FramedTransportFactory().getTransport(trans);
+        int frameSize = 15 * 1024 * 1024; // 15 MiB
+        return new TFramedTransport(trans, frameSize);
     }
 
     @Override
