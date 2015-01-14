92f753957325413e2aba67f36b4305fb0316b501
Sylvain Lebresne <sylvain@datastax.com>
2015-01-14 14:11:41 +0100

    Minor clarification/simplification in IndexedSliceReader

--- src/java/org/apache/cassandra/db/columniterator/IndexedSliceReader.java
+++ src/java/org/apache/cassandra/db/columniterator/IndexedSliceReader.java
@@ -333,7 +333,6 @@ class IndexedSliceReader extends AbstractIterator<OnDiskAtom> implements OnDiskA
             // can be mistakenly added this way.
             if (reversed && !prefetched.isEmpty())
             {
-                boolean gotSome = false;
                 // Avoids some comparison when we know it's not useful
                 boolean inSlice = false;
 
@@ -349,7 +348,6 @@ class IndexedSliceReader extends AbstractIterator<OnDiskAtom> implements OnDiskA
                         if (prefetchedCol instanceof RangeTombstone)
                         {
                             blockColumns.addLast(prefetched.poll());
-                            gotSome = true;
                             continue;
                         }
 
@@ -367,7 +365,6 @@ class IndexedSliceReader extends AbstractIterator<OnDiskAtom> implements OnDiskA
                     else if (inSlice || isColumnBeforeSliceFinish(prefetchedCol))
                     {
                         blockColumns.addLast(prefetched.poll());
-                        gotSome = true;
                         inSlice = true;
                     }
                     // if col is after slice, ignore
@@ -376,7 +373,7 @@ class IndexedSliceReader extends AbstractIterator<OnDiskAtom> implements OnDiskA
                         prefetched.poll();
                     }
                 }
-                if (gotSome)
+                if (!blockColumns.isEmpty())
                     return true;
             }
             try
