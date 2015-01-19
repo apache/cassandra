d314c07a1fd621362d7b6bb80d7f551943018d44
Dave Brosius <dbrosius@mebigfatguy.com>
2015-01-19 09:35:21 -0500

    use constants

--- src/java/org/apache/cassandra/serializers/CollectionSerializer.java
+++ src/java/org/apache/cassandra/serializers/CollectionSerializer.java
@@ -94,7 +94,7 @@ public abstract class CollectionSerializer<T> implements TypeSerializer<T>
 
     protected static void writeValue(ByteBuffer output, ByteBuffer value, int version)
     {
-        if (version >= 3)
+        if (version >= Server.VERSION_3)
         {
             if (value == null)
             {
