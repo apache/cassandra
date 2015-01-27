6068daea866bffd9fcc20f81219a21631dbf9bf0
Dave Brosius <dbrosius@mebigfatguy.com>
2015-01-27 02:28:04 -0500

    using commons-lang3, not commons-lang

--- test/unit/org/apache/cassandra/cql3/CreateIndexStatementTest.java
+++ test/unit/org/apache/cassandra/cql3/CreateIndexStatementTest.java
@@ -19,7 +19,7 @@ package org.apache.cassandra.cql3;
 
 import java.util.Locale;
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import org.junit.Test;
 
