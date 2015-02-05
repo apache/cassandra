b15113411a6efa725d3f420a7e0f6bc796aa9780
Benedict Elliott Smith <benedict@apache.org>
2015-02-05 14:32:54 +0000

    remove extraneous Range.normalize() in SSTableScanner

--- src/java/org/apache/cassandra/io/sstable/SSTableScanner.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableScanner.java
@@ -59,7 +59,7 @@ public class SSTableScanner implements ISSTableScanner
     public static ISSTableScanner getScanner(SSTableReader sstable, Collection<Range<Token>> tokenRanges, RateLimiter limiter)
     {
         // We want to avoid allocating a SSTableScanner if the range don't overlap the sstable (#5249)
-        List<Pair<Long, Long>> positions = sstable.getPositionsForRanges(Range.normalize(tokenRanges));
+        List<Pair<Long, Long>> positions = sstable.getPositionsForRanges(tokenRanges);
         if (positions.isEmpty())
             return new EmptySSTableScanner(sstable.getFilename());
 
