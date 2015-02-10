ad91d41628e6e2ff89d552fc3630682cd1c29f3f
Tyler Hobbs <tylerhobbs@apache.org>
2015-02-10 13:20:51 -0600

    nija: fix cqlsh completion for full() collection indexes

--- pylib/cqlshlib/cql3handling.py
+++ pylib/cqlshlib/cql3handling.py
@@ -1001,7 +1001,7 @@ syntax_rules += r'''
                                cf=<columnFamilyName> "(" (
                                    col=<cident> |
                                    "keys(" col=<cident> ")" |
-                                   "fullCollection(" col=<cident> ")"
+                                   "full(" col=<cident> ")"
                                ) ")"
                                ( "USING" <stringLiteral> ( "WITH" "OPTIONS" "=" <mapLiteral> )? )?
                          ;
