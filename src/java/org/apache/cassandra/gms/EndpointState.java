763130bdbde2f4cec2e8973bcd5203caf51cc89f
T Jake Luciani <jake@apache.org>
2015-03-19 13:39:23 -0400

    Followup commit for 7816
    patch by Stephania ; reviewed by tjake for CASSANDRA-7816

--- src/java/org/apache/cassandra/gms/EndpointState.java
+++ src/java/org/apache/cassandra/gms/EndpointState.java
@@ -47,14 +47,12 @@ public class EndpointState
     /* fields below do not get serialized */
     private volatile long updateTimestamp;
     private volatile boolean isAlive;
-    private volatile boolean hasPendingEcho;
 
     EndpointState(HeartBeatState initialHbState)
     {
         hbState = initialHbState;
         updateTimestamp = System.nanoTime();
         isAlive = true;
-        hasPendingEcho = false;
     }
 
     HeartBeatState getHeartBeatState()
@@ -114,16 +112,6 @@ public class EndpointState
     void markDead()
     {
         isAlive = false;
-    }
-
-    public boolean hasPendingEcho()
-    {
-        return hasPendingEcho;
-    }
-
-    public void markPendingEcho(boolean val)
-    {
-        hasPendingEcho = val;
     }
 
     public String toString()
