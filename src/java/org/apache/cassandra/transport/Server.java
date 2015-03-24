469764412706af185d8b2bece5c6d985d67c3ef7
Tyler Hobbs <tyler@datastax.com>
2015-03-24 13:54:34 -0500

    ninja: Update QueryOptions.DEFAULT to match latest protocol version

--- src/java/org/apache/cassandra/transport/Server.java
+++ src/java/org/apache/cassandra/transport/Server.java
@@ -66,6 +66,7 @@ public class Server implements CassandraDaemon.Server
     private static final Logger logger = LoggerFactory.getLogger(Server.class);
     private static final boolean enableEpoll = Boolean.valueOf(System.getProperty("cassandra.native.epoll.enabled", "true"));
 
+    public static final int VERSION_1 = 1;
     public static final int VERSION_2 = 2;
     public static final int VERSION_3 = 3;
     public static final int VERSION_4 = 4;
