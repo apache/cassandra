afd6cf2cbbb549c4672a2a6d987745bfaecbc0d7
Dave Brosius <dbrosius@mebigfatguy.com>
2015-04-13 00:04:22 -0400

    better error message

--- src/java/org/apache/cassandra/auth/CassandraRoleManager.java
+++ src/java/org/apache/cassandra/auth/CassandraRoleManager.java
@@ -110,7 +110,7 @@ public class CassandraRoleManager implements IRoleManager
          int rounds = Integer.getInteger(GENSALT_LOG2_ROUNDS_PROPERTY, 10);
          if (rounds < 4 || rounds > 31)
          throw new ConfigurationException(String.format("Bad value for system property -D%s." +
-                                                        "Please use a value 4 and 31",
+                                                        "Please use a value between 4 and 31 inclusively",
                                                         GENSALT_LOG2_ROUNDS_PROPERTY));
          return rounds;
     }
