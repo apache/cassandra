3ef932af75ea0fb49b8c681229553ead003be78f
Ariel Weisberg <ariel.weisberg@datastax.com>
2015-05-08 08:15:48 -0500

    Fix SSTableRewriterTest when run with compression.
    Patch by aweisberg; reviewed by jmckenzie for CASSANDRA-9277

--- test/unit/org/apache/cassandra/io/sstable/SSTableRewriterTest.java
+++ test/unit/org/apache/cassandra/io/sstable/SSTableRewriterTest.java
@@ -218,12 +218,12 @@ public class SSTableRewriterTest extends SchemaLoader
         SSTableWriter writer = getWriter(cfs, dir);
         try
         {
-            for (int i = 0; i < 1000; i++)
+            for (int i = 0; i < 10000; i++)
                 writer.append(StorageService.getPartitioner().decorateKey(random(i, 10)), cf);
             SSTableReader s = writer.openEarly(1000);
             assert s != null;
             assertFileCounts(dir.list(), 2, 2);
-            for (int i = 1000; i < 2000; i++)
+            for (int i = 10000; i < 20000; i++)
                 writer.append(StorageService.getPartitioner().decorateKey(random(i, 10)), cf);
             SSTableReader s2 = writer.openEarly(1000);
             assertTrue(s.last.compareTo(s2.last) < 0);
