37d81b9dcb08dd8c16a560591f46481b5d316841
Sam Tunnicliffe <sam@beobal.com>
2015-05-12 11:49:03 +0100

    Improve javadoc around trigger execution
    Patch by Sam Tunnicliffe; reviewed by brandonwilliams for CASSANDRA-9334

--- src/java/org/apache/cassandra/triggers/ITrigger.java
+++ src/java/org/apache/cassandra/triggers/ITrigger.java
@@ -36,7 +36,7 @@ import org.apache.cassandra.db.RowMutation;
  * 2) ITrigger implementation can be instantiated multiple times during the server life time.
  *      (Depends on the number of times trigger folder is updated.)<br>
  * 3) ITrigger implementation should be state-less (avoid dependency on instance variables).<br>
- * 
+ *
  * <br><b>The API is still beta and can change.</b>
  */
 public interface ITrigger
@@ -46,7 +46,7 @@ public interface ITrigger
      *
      * @param key - Row Key for the update.
      * @param update - Update received for the CF
-     * @return modifications to be applied, null if no action to be performed.
+     * @return additional modifications to be applied along with the supplied update
      */
     public Collection<RowMutation> augment(ByteBuffer key, ColumnFamily update);
 }
