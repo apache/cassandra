5a571c6291678d7f92c120d1f9b366374e51c07d
Dave Brosius <dbrosius@mebigfatguy.com>
2015-05-26 00:42:58 -0400

    fix wrong variable in warning message

--- src/java/org/apache/cassandra/db/compaction/Scrubber.java
+++ src/java/org/apache/cassandra/db/compaction/Scrubber.java
@@ -199,7 +199,7 @@ public class Scrubber implements Closeable
                         throw new IOError(new IOException("Impossible row size (greater than file length): " + dataSize));
 
                     if (dataStart != dataStartFromIndex)
-                        outputHandler.warn(String.format("Data file row position %d differs from index file row position %d", dataStart, dataSizeFromIndex));
+                        outputHandler.warn(String.format("Data file row position %d differs from index file row position %d", dataStart, dataStartFromIndex));
 
                     if (dataSize != dataSizeFromIndex)
                         outputHandler.warn(String.format("Data file row size %d differs from index file row size %d", dataSize, dataSizeFromIndex));
