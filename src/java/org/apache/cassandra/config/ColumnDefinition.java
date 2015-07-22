172a9754d62559984b325babd6902ae78c9f6891
Sylvain Lebresne <sylvain@datastax.com>
2015-07-22 09:46:23 +0200

    Fix MarshalException when upgrading super column families
    patch by slebresne; reviewed by iamaleksey for CASSANDRA-9582

--- src/java/org/apache/cassandra/config/ColumnDefinition.java
+++ src/java/org/apache/cassandra/config/ColumnDefinition.java
@@ -386,10 +386,11 @@ public class ColumnDefinition extends ColumnSpecification
                       : Kind.REGULAR;
 
             Integer componentIndex = null;
-            if (row.has(COMPONENT_INDEX))
-                componentIndex = row.getInt(COMPONENT_INDEX);
-            else if (kind == Kind.CLUSTERING_COLUMN && isSuper)
+            if (kind == Kind.REGULAR && isSuper)
                 componentIndex = 1; // A ColumnDefinition for super columns applies to the column component
+                                    // (we don't trust the COMPONENT_INDEX if we have one due to #9582)
+            else if (row.has(COMPONENT_INDEX))
+                componentIndex = row.getInt(COMPONENT_INDEX);
 
             // Note: we save the column name as string, but we should not assume that it is an UTF8 name, we
             // we need to use the comparator fromString method
