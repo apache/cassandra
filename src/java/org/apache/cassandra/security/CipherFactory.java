f54580d0715a9189ba1658ad036e7d19cecdc3c8
Jason Brown <jasedbrown@gmail.com>
2015-08-06 05:14:15 -0700

    ninja-fix an assert in CipherFactory, and added tests for it

--- src/java/org/apache/cassandra/security/CipherFactory.java
+++ src/java/org/apache/cassandra/security/CipherFactory.java
@@ -109,7 +109,7 @@ public class CipherFactory
 
     public Cipher getDecryptor(String transformation, String keyAlias, byte[] iv) throws IOException
     {
-        assert iv != null || iv.length > 0 : "trying to decrypt, but the initialization vector is empty";
+        assert iv != null && iv.length > 0 : "trying to decrypt, but the initialization vector is empty";
         return buildCipher(transformation, keyAlias, iv, Cipher.DECRYPT_MODE);
     }
 
