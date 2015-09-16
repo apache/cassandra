588dc06eb8b5a7678128e9f0c0421fc391b14bce
Yuki Morishita <yukim@apache.org>
2015-09-16 08:36:50 -0500

    Fix NPE in nodetool compactionhistory
    patch by Pierre N. and yukim; reviewed by yukim for CASSANDRA-9758

--- src/java/org/apache/cassandra/utils/FBUtilities.java
+++ src/java/org/apache/cassandra/utils/FBUtilities.java
@@ -28,13 +28,15 @@ import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.text.NumberFormat;
 import java.util.*;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.zip.Checksum;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 
 import com.google.common.base.Joiner;
 import com.google.common.collect.AbstractIterator;
@@ -472,8 +474,18 @@ public class FBUtilities
         return s;
     }
 
-    public static String toString(Map<?,?> map)
+    /**
+     * Make straing out of the given {@code Map}.
+     *
+     * @param map Map to make string.
+     * @return String representation of all entries in the map,
+     *         where key and value pair is concatenated with ':'.
+     */
+    @Nonnull
+    public static String toString(@Nullable Map<?, ?> map)
     {
+        if (map == null)
+            return "";
         Joiner.MapJoiner joiner = Joiner.on(", ").withKeyValueSeparator(":");
         return joiner.join(map);
     }
