cc2478d4ce5d0cabb150489ebec5cda1442bc174
Carl Yeksigian <carl@apache.org>
2015-10-06 11:05:04 -0400

    Allow EACH_QUORUM for reads
    patch by Carl Yeksigian; revieiwed by Ariel Weisberg for CASSANDRA-9602

--- src/java/org/apache/cassandra/exceptions/UnavailableException.java
+++ src/java/org/apache/cassandra/exceptions/UnavailableException.java
@@ -30,6 +30,11 @@ public class UnavailableException extends RequestExecutionException
         this("Cannot achieve consistency level " + consistency, consistency, required, alive);
     }
 
+    public UnavailableException(ConsistencyLevel consistency, String dc, int required, int alive)
+    {
+        this("Cannot achieve consistency level " + consistency + " in DC " + dc, consistency, required, alive);
+    }
+
     public UnavailableException(String msg, ConsistencyLevel consistency, int required, int alive)
     {
         super(ExceptionCode.UNAVAILABLE, msg);
