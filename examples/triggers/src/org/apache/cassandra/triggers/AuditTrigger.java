fb463c79e0cb63114a2f220182e7c7ed3e0f4205
Mike Adamson <madamson@datastax.com>
2015-10-20 11:18:37 +0100

    Updated trigger example
    patch by Mike Adamson; reviewed by Aleksey Yeschenko for CASSANDRA-10257

--- examples/triggers/src/org/apache/cassandra/triggers/AuditTrigger.java
+++ examples/triggers/src/org/apache/cassandra/triggers/AuditTrigger.java
@@ -18,49 +18,42 @@
 package org.apache.cassandra.triggers;
 
 import java.io.InputStream;
-import java.nio.ByteBuffer;
-import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
+import java.util.Collections;
 import java.util.Properties;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import org.apache.cassandra.db.Cell;
-import org.apache.cassandra.db.ColumnFamily;
+import org.apache.cassandra.config.Schema;
 import org.apache.cassandra.db.Mutation;
+import org.apache.cassandra.db.RowUpdateBuilder;
+import org.apache.cassandra.db.partitions.Partition;
 import org.apache.cassandra.io.util.FileUtils;
+import org.apache.cassandra.utils.FBUtilities;
+import org.apache.cassandra.utils.UUIDGen;
 
-public class InvertedIndex implements ITrigger
+public class AuditTrigger implements ITrigger
 {
-    private static final Logger logger = LoggerFactory.getLogger(InvertedIndex.class);
     private Properties properties = loadProperties();
 
-    public Collection<Mutation> augment(ByteBuffer key, ColumnFamily update)
+    public Collection<Mutation> augment(Partition update)
     {
-        List<Mutation> mutations = new ArrayList<>(update.getColumnCount());
+        String auditKeyspace = properties.getProperty("keyspace");
+        String auditTable = properties.getProperty("table");
 
-        String indexKeySpace = properties.getProperty("keyspace");
-        String indexColumnFamily = properties.getProperty("table");
-        for (Cell cell : update)
-        {
-            // Skip the row marker and other empty values, since they lead to an empty key.
-            if (cell.value().remaining() > 0)
-            {
-                Mutation mutation = new Mutation(indexKeySpace, cell.value());
-                mutation.add(indexColumnFamily, cell.name(), key, System.currentTimeMillis());
-                mutations.add(mutation);
-            }
-        }
+        RowUpdateBuilder audit = new RowUpdateBuilder(Schema.instance.getCFMetaData(auditKeyspace, auditTable),
+                                                      FBUtilities.timestampMicros(),
+                                                      UUIDGen.getTimeUUID());
 
-        return mutations;
+        audit.add("keyspace_name", update.metadata().ksName);
+        audit.add("table_name", update.metadata().cfName);
+        audit.add("primary_key", update.metadata().getKeyValidator().getString(update.partitionKey().getKey()));
+
+        return Collections.singletonList(audit.build());
     }
 
     private static Properties loadProperties()
     {
         Properties properties = new Properties();
-        InputStream stream = InvertedIndex.class.getClassLoader().getResourceAsStream("InvertedIndex.properties");
+        InputStream stream = AuditTrigger.class.getClassLoader().getResourceAsStream("AuditTrigger.properties");
         try
         {
             properties.load(stream);
@@ -73,7 +66,6 @@ public class InvertedIndex implements ITrigger
         {
             FileUtils.closeQuietly(stream);
         }
-        logger.info("loaded property file, InvertedIndex.properties");
         return properties;
     }
 }
