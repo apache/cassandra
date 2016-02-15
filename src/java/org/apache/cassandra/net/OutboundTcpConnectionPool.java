f3b5ff8422adbf2f896656b06f26b2749a24a41b
Benjamin Lerer <b.lerer@gmail.com>
2016-02-15 10:01:02 +0100

    Fix potential resource leak warnings
    patch by Benjamin Lerer; reviewed by Sylvain Lebresne for CASSANDRA-11083

--- src/java/org/apache/cassandra/net/OutboundTcpConnectionPool.java
+++ src/java/org/apache/cassandra/net/OutboundTcpConnectionPool.java
@@ -122,6 +122,8 @@ public class OutboundTcpConnectionPool
         return newSocket(endPoint());
     }
 
+    // Closing the socket will close the underlying channel.
+    @SuppressWarnings("resource")
     public static Socket newSocket(InetAddress endpoint) throws IOException
     {
         // zero means 'bind on any available port.'
