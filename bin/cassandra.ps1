757e314cefd53d5f0d6a459be6f7e511a1a6fea0
Yuki Morishita <yukim@apache.org>
2016-02-17 10:26:08 -0600

    Use logback setting for 'cassandra -v' command
    patch by yukim; reviewed by Paulo Motta for CASSANDRA-10767

--- bin/cassandra.ps1
+++ bin/cassandra.ps1
@@ -184,7 +184,7 @@ Function PrintVersion()
     $pinfo = New-Object System.Diagnostics.ProcessStartInfo
     $pinfo.FileName = "$env:JAVA_BIN"
     $pinfo.UseShellExecute = $false
-    $pinfo.Arguments = "-cp $env:CLASSPATH org.apache.cassandra.tools.GetVersion"
+    $pinfo.Arguments = "-Dlogback.configurationFile=logback-tools.xml -cp $env:CLASSPATH org.apache.cassandra.tools.GetVersion"
     $p = New-Object System.Diagnostics.Process
     $p.StartInfo = $pinfo
     $p.Start() | Out-Null
