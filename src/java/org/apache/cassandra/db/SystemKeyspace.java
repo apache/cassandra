360541f16a158f560d7cce1fcc264e246a80d10f
Joel Knighton <joel.knighton@datastax.com>
2016-05-24 12:07:41 -0500

    Persist local metadata in SystemKeyspace earlier in initialization sequence
    Patch by Joel Knighton; reviewed by Sam Tunnicliffe for CASSANDRA-11742

--- src/java/org/apache/cassandra/db/SystemKeyspace.java
+++ src/java/org/apache/cassandra/db/SystemKeyspace.java
@@ -294,11 +294,10 @@ public final class SystemKeyspace
 
     public static void finishStartup()
     {
-        persistLocalMetadata();
         LegacySchemaTables.saveSystemKeyspaceSchema();
     }
 
-    private static void persistLocalMetadata()
+    public static void persistLocalMetadata()
     {
         String req = "INSERT INTO system.%s (" +
                      "key," +
