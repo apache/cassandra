c253f0806779cc09fa03fa638d09d1da1ffba70b
Pavel Yaskevich <xedin@apache.org>
2016-06-23 22:10:53 -0700

    Move skip_stop_words filter before stemming
    patch by doanduyhai; reviewed by xedin for CASSANDRA-12078

--- src/java/org/apache/cassandra/index/sasi/analyzer/StandardAnalyzer.java
+++ src/java/org/apache/cassandra/index/sasi/analyzer/StandardAnalyzer.java
@@ -128,10 +128,10 @@ public class StandardAnalyzer extends AbstractAnalyzer
             builder = builder.add("to_lower", new BasicResultFilters.LowerCase());
         if (!options.isCaseSensitive() && options.shouldUpperCaseTerms())
             builder = builder.add("to_upper", new BasicResultFilters.UpperCase());
-        if (options.shouldStemTerms())
-            builder = builder.add("term_stemming", new StemmingFilters.DefaultStemmingFilter(options.getLocale()));
         if (options.shouldIgnoreStopTerms())
             builder = builder.add("skip_stop_words", new StopWordFilters.DefaultStopWordFilter(options.getLocale()));
+        if (options.shouldStemTerms())
+            builder = builder.add("term_stemming", new StemmingFilters.DefaultStemmingFilter(options.getLocale()));
         return builder.build();
     }
 
