6c16d2745b403d0192c780886b875cc629c0227c
Dave Brosius <dbrosius@mebigfatguy.com>
2016-07-04 17:23:46 -0400

    c* uses commons-lang3, not commons-lang

--- src/java/org/apache/cassandra/db/commitlog/CommitLogReader.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLogReader.java
@@ -23,7 +23,7 @@ import java.util.concurrent.atomic.AtomicInteger;
 import java.util.zip.CRC32;
 
 import com.google.common.annotations.VisibleForTesting;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
