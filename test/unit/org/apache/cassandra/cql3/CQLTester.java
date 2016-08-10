5694a2ac3f227bdd22f1532059b566820deaf23e
Marcus Eriksson <marcuse@apache.org>
2016-08-10 08:34:28 +0200

    Debug logging for unit tests
    Patch by marcuse; reviewed by Michael Shuler for CASSANDRA-12425

--- test/unit/org/apache/cassandra/cql3/CQLTester.java
+++ test/unit/org/apache/cassandra/cql3/CQLTester.java
@@ -707,8 +707,8 @@ public abstract class CQLTester
         UntypedResultSet rs;
         if (usePrepared)
         {
-            if (logger.isDebugEnabled())
-                logger.debug("Executing: {} with values {}", query, formatAllValues(values));
+            if (logger.isTraceEnabled())
+                logger.trace("Executing: {} with values {}", query, formatAllValues(values));
             if (reusePrepared)
             {
                 rs = QueryProcessor.executeInternal(query, transformValues(values));
@@ -728,14 +728,14 @@ public abstract class CQLTester
         else
         {
             query = replaceValues(query, values);
-            if (logger.isDebugEnabled())
-                logger.debug("Executing: {}", query);
+            if (logger.isTraceEnabled())
+                logger.trace("Executing: {}", query);
             rs = QueryProcessor.executeOnceInternal(query);
         }
         if (rs != null)
         {
-            if (logger.isDebugEnabled())
-                logger.debug("Got {} rows", rs.size());
+            if (logger.isTraceEnabled())
+                logger.trace("Got {} rows", rs.size());
         }
         return rs;
     }
