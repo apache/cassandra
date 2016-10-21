f51ee61f9dfef4288381f2133f6859c6a57a0693
Nate McCall <zznate.m@gmail.com>
2016-10-21 15:34:40 +1300

    ninja fix to remove double param in logging msg from merge

--- src/java/org/apache/cassandra/io/sstable/format/big/BigTableWriter.java
+++ src/java/org/apache/cassandra/io/sstable/format/big/BigTableWriter.java
@@ -208,7 +208,7 @@ public class BigTableWriter extends SSTableWriter
         if (rowSize > DatabaseDescriptor.getCompactionLargePartitionWarningThreshold())
         {
             String keyString = metadata.getKeyValidator().getString(key.getKey());
-            logger.warn("Writing large partition {}/{}:{} ({}) to sstable {}", metadata.ksName, metadata.cfName, keyString, rowSize, FBUtilities.prettyPrintMemory(rowSize), getFilename());
+            logger.warn("Writing large partition {}/{}:{} ({}) to sstable {}", metadata.ksName, metadata.cfName, keyString, FBUtilities.prettyPrintMemory(rowSize), getFilename());
         }
     }
 
