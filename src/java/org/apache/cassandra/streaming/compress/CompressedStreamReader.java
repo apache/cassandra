e1dda71a4fc43310a0e41dddb4b428fe6ef3b9c6
Dave Brosius <dbrosius@mebigfatguy.com>
2017-02-11 17:47:18 -0500

    match up logging markers with parameters

--- src/java/org/apache/cassandra/streaming/compress/CompressedStreamReader.java
+++ src/java/org/apache/cassandra/streaming/compress/CompressedStreamReader.java
@@ -72,7 +72,7 @@ public class CompressedStreamReader extends StreamReader
             throw new IOException("CF " + tableId + " was dropped during streaming");
         }
 
-        logger.debug("[Stream #{}] Start receiving file #{} from {}, repairedAt = {}, size = {}, ks = '{}', table = '{}'.",
+        logger.debug("[Stream #{}] Start receiving file #{} from {}, repairedAt = {}, size = {}, ks = '{}', pendingRepair = '{}', table = '{}'.",
                      session.planId(), fileSeqNum, session.peer, repairedAt, totalSize, cfs.keyspace.getName(), session.getPendingRepair(),
                      cfs.getTableName());
 
