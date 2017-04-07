64d8a1d9fdacf3e7396cdf2f5c61171c1c9bccf2
Andrés de la Peña <a.penya.garcia@gmail.com>
2017-04-07 11:13:39 +0200

    Fix 2ndary index queries on partition keys for tables with static columns
    patch by Andrés de la Peña; reviewed by Benjamin Lerer for CASSANDRA-13147

--- src/java/org/apache/cassandra/cql3/statements/SelectStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/SelectStatement.java
@@ -1368,11 +1368,11 @@ public class SelectStatement implements CQLStatement
 
         CQL3Row.RowIterator iter = cfm.comparator.CQL3RowBuilder(cfm, now).group(cells);
 
-        // If there is static columns but there is no non-static row, then provided the select was a full
-        // partition selection (i.e. not a 2ndary index search and there was no condition on clustering columns)
-        // then we want to include the static columns in the result set (and we're done).
+        // If there is static columns but there is no non-static row,
+        // and the select was a full partition selection (i.e. there was no condition on clustering or regular columns),
+        // we want to include the static columns in the result set (and we're done).
         CQL3Row staticRow = iter.getStaticRow();
-        if (staticRow != null && !iter.hasNext() && !usesSecondaryIndexing && hasNoClusteringColumnsRestriction())
+        if (staticRow != null && !iter.hasNext() && hasNoClusteringColumnsRestriction() && hasNoRegularColumnsRestriction())
         {
             result.newRow();
             for (ColumnDefinition def : selection.getColumns())
@@ -1449,6 +1449,16 @@ public class SelectStatement implements CQLStatement
         for (int i = 0; i < columnRestrictions.length; i++)
             if (columnRestrictions[i] != null)
                 return false;
+        return true;
+    }
+
+    private boolean hasNoRegularColumnsRestriction()
+    {
+        for (ColumnDefinition def : restrictedColumns.keySet())
+        {
+            if (def.kind == ColumnDefinition.Kind.REGULAR)
+                return false;
+        }
         return true;
     }
 
