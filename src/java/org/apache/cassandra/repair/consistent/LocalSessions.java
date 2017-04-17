2381be31404403285948a3097c9ed55e7f901135
Blake Eggleston <bdeggleston@gmail.com>
2017-04-17 15:28:28 -0700

    Start compaction when incremental repair finishes
    Patch by Blake Eggleston; Reviewed by Marcus Eriksson for CASSANDRA-13454

--- src/java/org/apache/cassandra/repair/consistent/LocalSessions.java
+++ src/java/org/apache/cassandra/repair/consistent/LocalSessions.java
@@ -50,6 +50,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.compaction.CompactionManager;
+import org.apache.cassandra.schema.Schema;
 import org.apache.cassandra.schema.SchemaConstants;
 import org.apache.cassandra.cql3.QueryProcessor;
 import org.apache.cassandra.cql3.UntypedResultSet;
@@ -463,9 +466,15 @@ public class LocalSessions
                                         "Invalid state transition %s -> %s",
                                         session.getState(), state);
             logger.debug("Setting LocalSession state from {} -> {} for {}", session.getState(), state, session.sessionID);
+            boolean wasCompleted = session.isCompleted();
             session.setState(state);
             session.setLastUpdate();
             save(session);
+
+            if (session.isCompleted() && !wasCompleted)
+            {
+                sessionCompleted(session);
+            }
         }
     }
 
@@ -590,6 +599,19 @@ public class LocalSessions
         {
             logger.error("error setting session to FINALIZE_PROMISED", e);
             failSession(sessionID);
+        }
+    }
+
+    @VisibleForTesting
+    protected void sessionCompleted(LocalSession session)
+    {
+        for (TableId tid: session.tableIds)
+        {
+            ColumnFamilyStore cfs = Schema.instance.getColumnFamilyStoreInstance(tid);
+            if (cfs != null)
+            {
+                CompactionManager.instance.submitBackground(cfs);
+            }
         }
     }
 
