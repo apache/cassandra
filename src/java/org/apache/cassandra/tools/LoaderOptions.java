c22ee2bd451d030e99cfb65be839bbc735a5352f
jaydeepkumar1984 <chovatia.jaydeep@gmail.com>
2017-09-18 17:07:56 -0700

    Add sstableloader option to accept target keyspace name
    patch by Jaydeepkumar Chovatia; reviewed by Jay Zhuang for CASSANDRA-13884

--- src/java/org/apache/cassandra/tools/LoaderOptions.java
+++ src/java/org/apache/cassandra/tools/LoaderOptions.java
@@ -38,6 +38,7 @@ import org.apache.cassandra.tools.BulkLoader.CmdLineOptions;
 import com.datastax.driver.core.AuthProvider;
 import com.datastax.driver.core.PlainTextAuthProvider;
 import org.apache.commons.cli.*;
+import org.apache.commons.lang3.StringUtils;
 
 public class LoaderOptions
 {
@@ -59,6 +60,7 @@ public class LoaderOptions
     public static final String INTER_DC_THROTTLE_MBITS = "inter-dc-throttle";
     public static final String TOOL_NAME = "sstableloader";
     public static final String ALLOW_SERVER_PORT_DISCOVERY_OPTION = "server-port-discovery";
+    public static final String TARGET_KEYSPACE = "target-keyspace";
 
     /* client encryption options */
     public static final String SSL_TRUSTSTORE = "truststore";
@@ -88,6 +90,7 @@ public class LoaderOptions
     public final Set<InetSocketAddress> hosts;
     public final Set<InetAddressAndPort> ignores;
     public final boolean allowServerPortDiscovery;
+    public final String targetKeyspace;
 
     LoaderOptions(Builder builder)
     {
@@ -109,6 +112,7 @@ public class LoaderOptions
         hosts = builder.hosts;
         allowServerPortDiscovery = builder.allowServerPortDiscovery;
         ignores = builder.ignores;
+        targetKeyspace = builder.targetKeyspace;
     }
 
     static class Builder
@@ -134,6 +138,7 @@ public class LoaderOptions
         Set<InetSocketAddress> hosts = new HashSet<>();
         Set<InetAddressAndPort> ignores = new HashSet<>();
         boolean allowServerPortDiscovery;
+        String targetKeyspace;
 
         Builder()
         {
@@ -509,6 +514,14 @@ public class LoaderOptions
                     clientEncOptions.cipher_suites = cmd.getOptionValue(SSL_CIPHER_SUITES).split(",");
                 }
 
+                if (cmd.hasOption(TARGET_KEYSPACE))
+                {
+                    targetKeyspace = cmd.getOptionValue(TARGET_KEYSPACE);
+                    if (StringUtils.isBlank(targetKeyspace))
+                    {
+                        errorMsg("Empty keyspace is not supported.", options);
+                    }
+                }
                 return this;
             }
             catch (ParseException | ConfigurationException | MalformedURLException e)
@@ -615,6 +628,7 @@ public class LoaderOptions
         options.addOption("ciphers", SSL_CIPHER_SUITES, "CIPHER-SUITES", "Client SSL: comma-separated list of encryption suites to use");
         options.addOption("f", CONFIG_PATH, "path to config file", "cassandra.yaml file path for streaming throughput and client/server SSL.");
         options.addOption("spd", ALLOW_SERVER_PORT_DISCOVERY_OPTION, "allow server port discovery", "Use ports published by server to decide how to connect. With SSL requires StartTLS to be used.");
+        options.addOption("k", TARGET_KEYSPACE, "target keyspace name", "target keyspace name");
         return options;
     }
 
