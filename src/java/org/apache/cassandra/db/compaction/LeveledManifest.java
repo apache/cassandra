48562536f17a6e88aaf18d46e5ffa0a54c6b5be6
vusal-ahmadoglu <vusal.ahmadoglu@gmail.com>
2017-09-20 21:09:45 +0200

    Moving getSTCSInL0CompactionCandidate out of loop to prevent useless multiple retrievals
    Patch by Vusal Ahmadoglu; Reviewed by Jirsa for CASSANDRA-12961

--- src/java/org/apache/cassandra/db/compaction/LeveledManifest.java
+++ src/java/org/apache/cassandra/db/compaction/LeveledManifest.java
@@ -345,6 +345,11 @@ public class LeveledManifest
         // This isn't a magic wand -- if you are consistently writing too fast for LCS to keep
         // up, you're still screwed.  But if instead you have intermittent bursts of activity,
         // it can help a lot.
+
+        // Let's check that L0 is far enough behind to warrant STCS.
+        // If it is, it will be used before proceeding any of higher level
+        CompactionCandidate l0Compaction = getSTCSInL0CompactionCandidate();
+
         for (int i = generations.length - 1; i > 0; i--)
         {
             List<SSTableReader> sstables = getLevel(i);
@@ -359,7 +364,6 @@ public class LeveledManifest
             if (score > 1.001)
             {
                 // before proceeding with a higher level, let's see if L0 is far enough behind to warrant STCS
-                CompactionCandidate l0Compaction = getSTCSInL0CompactionCandidate();
                 if (l0Compaction != null)
                     return l0Compaction;
 
@@ -389,7 +393,7 @@ public class LeveledManifest
             // Since we don't have any other compactions to do, see if there is a STCS compaction to perform in L0; if
             // there is a long running compaction, we want to make sure that we continue to keep the number of SSTables
             // small in L0.
-            return getSTCSInL0CompactionCandidate();
+            return l0Compaction;
         }
         return new CompactionCandidate(candidates, getNextLevel(candidates), maxSSTableSizeInBytes);
     }
