7a424bc2a79dce98817054057dd3a479b202f09e
Jay Zhuang <jay.zhuang@yahoo.com>
2018-01-14 21:43:30 -0800

    Update Intellij JUnit configuration
    And other ninja build fixes.
    
    patch by Jay Zhuang; reviewed by Stefan Podkowinski for CASSANDRA-14169

--- test/unit/org/apache/cassandra/io/sstable/LegacySSTableTest.java
+++ test/unit/org/apache/cassandra/io/sstable/LegacySSTableTest.java
@@ -94,7 +94,7 @@ public class LegacySSTableTest
     public static void defineSchema() throws ConfigurationException
     {
         String scp = System.getProperty(LEGACY_SSTABLE_PROP);
-        Assert.assertNotNull("System property " + LEGACY_SSTABLE_ROOT + " not set", scp);
+        Assert.assertNotNull("System property " + LEGACY_SSTABLE_PROP + " not set", scp);
 
         LEGACY_SSTABLE_ROOT = new File(scp).getAbsoluteFile();
         Assert.assertTrue("System property " + LEGACY_SSTABLE_ROOT + " does not specify a directory", LEGACY_SSTABLE_ROOT.isDirectory());
