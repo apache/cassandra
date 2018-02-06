f2fc2e96738505118d9ad161ec77d66a2369fe44
Jason Brown <jasedbrown@gmail.com>
2018-02-06 13:38:46 -0800

    Remove GossipDigestSynVerbHandler#doSort()
    patch by jasobrown; reviewed by Joel Knighton for CASSANDRA-14174

--- src/java/org/apache/cassandra/gms/GossipDigestSynVerbHandler.java
+++ src/java/org/apache/cassandra/gms/GossipDigestSynVerbHandler.java
@@ -19,8 +19,6 @@ package org.apache.cassandra.gms;
 
 import java.util.*;
 
-import com.google.common.collect.Maps;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -99,8 +97,6 @@ public class GossipDigestSynVerbHandler implements IVerbHandler<GossipDigestSyn>
             logger.trace("Gossip syn digests are : {}", sb);
         }
 
-        doSort(gDigestList);
-
         List<GossipDigest> deltaGossipDigestList = new ArrayList<GossipDigest>();
         Map<InetAddressAndPort, EndpointState> deltaEpStateMap = new HashMap<InetAddressAndPort, EndpointState>();
         Gossiper.instance.examineGossiper(gDigestList, deltaGossipDigestList, deltaEpStateMap);
@@ -111,48 +107,5 @@ public class GossipDigestSynVerbHandler implements IVerbHandler<GossipDigestSyn>
         if (logger.isTraceEnabled())
             logger.trace("Sending a GossipDigestAckMessage to {}", from);
         MessagingService.instance().sendOneWay(gDigestAckMessage, from);
-    }
-
-    /*
-     * First construct a map whose key is the endpoint in the GossipDigest and the value is the
-     * GossipDigest itself. Then build a list of version differences i.e difference between the
-     * version in the GossipDigest and the version in the local state for a given InetAddressAndPort.
-     * Sort this list. Now loop through the sorted list and retrieve the GossipDigest corresponding
-     * to the endpoint from the map that was initially constructed.
-    */
-    private void doSort(List<GossipDigest> gDigestList)
-    {
-        /* Construct a map of endpoint to GossipDigest. */
-        Map<InetAddressAndPort, GossipDigest> epToDigestMap = Maps.newHashMapWithExpectedSize(gDigestList.size());
-        for (GossipDigest gDigest : gDigestList)
-        {
-            epToDigestMap.put(gDigest.getEndpoint(), gDigest);
-        }
-
-        /*
-         * These digests have their maxVersion set to the difference of the version
-         * of the local EndpointState and the version found in the GossipDigest.
-        */
-        List<GossipDigest> diffDigests = new ArrayList<GossipDigest>(gDigestList.size());
-        for (GossipDigest gDigest : gDigestList)
-        {
-            InetAddressAndPort ep = gDigest.getEndpoint();
-            EndpointState epState = Gossiper.instance.getEndpointStateForEndpoint(ep);
-            int version = (epState != null) ? Gossiper.instance.getMaxEndpointStateVersion(epState) : 0;
-            int diffVersion = Math.abs(version - gDigest.getMaxVersion());
-            diffDigests.add(new GossipDigest(ep, gDigest.getGeneration(), diffVersion));
-        }
-
-        gDigestList.clear();
-        Collections.sort(diffDigests);
-        int size = diffDigests.size();
-        /*
-         * Report the digests in descending order. This takes care of the endpoints
-         * that are far behind w.r.t this local endpoint
-        */
-        for (int i = size - 1; i >= 0; --i)
-        {
-            gDigestList.add(epToDigestMap.get(diffDigests.get(i).getEndpoint()));
-        }
     }
 }
