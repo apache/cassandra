d250cfd21823ceeaa59812671a120dc8aa9e95d1
Lerh Chuan Low <lerh@instaclustr.com>
2018-02-09 11:14:12 -0800

    Re-examine commented out Compactions and AntiCompactionsTest
    Patch by Lerh Chuan Low; Reviewed by Paulo Motta for CASSANDRA-13698

--- src/java/org/apache/cassandra/db/SystemKeyspace.java
+++ src/java/org/apache/cassandra/db/SystemKeyspace.java
@@ -40,8 +40,6 @@ import org.slf4j.LoggerFactory;
 
 import com.google.common.util.concurrent.Futures;
 
-import org.apache.cassandra.concurrent.Stage;
-import org.apache.cassandra.concurrent.StageManager;
 import org.apache.cassandra.config.CFMetaData;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.cql3.QueryProcessor;
