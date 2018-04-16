8a5e1cbe293ee7c83efba0d0101ada0a80cfaf00
Dinesh A. Joshi <dinesh.joshi@apple.com>
2018-04-16 10:18:22 -0700

    Fix junit failure for SSTableReaderTest#testOpeningSSTable
    Patch by Dinesh Joshi; Reviewed by Chris Lohfink for CASSANDRA-14387

--- test/unit/org/apache/cassandra/io/sstable/SSTableReaderTest.java
+++ test/unit/org/apache/cassandra/io/sstable/SSTableReaderTest.java
@@ -381,7 +381,7 @@ public class SSTableReaderTest
         long bloomModified = Files.getLastModifiedTime(bloomPath).toMillis();
         long summaryModified = Files.getLastModifiedTime(summaryPath).toMillis();
 
-        Thread.sleep(TimeUnit.MILLISECONDS.toMillis(10)); // sleep to ensure modified time will be different
+        Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1000)); // sleep to ensure modified time will be different
 
         // Offline tests
         // check that bloomfilter/summary ARE NOT regenerated
@@ -428,7 +428,7 @@ public class SSTableReaderTest
         summaryModified = Files.getLastModifiedTime(summaryPath).toMillis();
         summaryFile.delete();
 
-        Thread.sleep(TimeUnit.MILLISECONDS.toMillis(10)); // sleep to ensure modified time will be different
+        Thread.sleep(TimeUnit.MILLISECONDS.toMillis(1000)); // sleep to ensure modified time will be different
         bloomModified = Files.getLastModifiedTime(bloomPath).toMillis();
 
         target = SSTableReader.open(desc, components, store.metadata);
