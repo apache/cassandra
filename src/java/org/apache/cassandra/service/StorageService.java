e0524c099e0b59cccfd1a9cb9424147cfd001a32
Ariel Weisberg <aweisberg@apple.com>
2018-04-17 12:40:51 -0400

    nodetool listsnapshots is missing local system keyspace snapshots
    Patch by Ariel Weisberg; Reviewed by Jay Zhuang for CASSANDRA-14381

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -3484,9 +3484,6 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
         Map<String, TabularData> snapshotMap = new HashMap<>();
         for (Keyspace keyspace : Keyspace.all())
         {
-            if (SchemaConstants.isLocalSystemKeyspace(keyspace.getName()))
-                continue;
-
             for (ColumnFamilyStore cfStore : keyspace.getColumnFamilyStores())
             {
                 for (Map.Entry<String, Directories.SnapshotSizeDetails> snapshotDetail : cfStore.getSnapshotDetails().entrySet())
