dbbb8f3ca84dbb5a3f1a7d2dcb0fd2e435b37161
Michael Burman <yak@iki.fi>
2018-05-08 15:40:54 +0300

    Remove Netty timed batching and instead do the batch during next eventLoop invocation after a write has been enqueued. Old behavior can be enabled with setting native_transport_flush_in_batches_legacy
    patch by Michael Burman; reviewed by Benedict for CASSANDRA-13651

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -152,6 +152,7 @@ public class Config
     public int native_transport_max_frame_size_in_mb = 256;
     public volatile long native_transport_max_concurrent_connections = -1L;
     public volatile long native_transport_max_concurrent_connections_per_ip = -1L;
+    public boolean native_transport_flush_in_batches_legacy = false;
 
     /**
      * Max size of values in SSTables, in MegaBytes.
