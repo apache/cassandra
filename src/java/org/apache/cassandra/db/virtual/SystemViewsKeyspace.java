eebe79c6babdc1e1f5b706ff2f6df8205be2a23b
Chris Lohfink <clohfink@apple.com>
2018-06-25 12:08:40 -0500

    Add a virtual table to expose caches
    patch by Chris Lohfink; reviewed by Aleksey Yeschenko for
    CASSANDRA-14538

--- src/java/org/apache/cassandra/db/virtual/SystemViewsKeyspace.java
+++ src/java/org/apache/cassandra/db/virtual/SystemViewsKeyspace.java
@@ -27,6 +27,6 @@ public final class SystemViewsKeyspace extends VirtualKeyspace
 
     private SystemViewsKeyspace()
     {
-        super(NAME, ImmutableList.of(new SSTableTasksTable(NAME), new ClientsTable(NAME)));
+        super(NAME, ImmutableList.of(new SSTableTasksTable(NAME), new ClientsTable(NAME), new CachesTable(NAME)));
     }
 }
