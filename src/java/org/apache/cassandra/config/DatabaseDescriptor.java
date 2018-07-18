e9471e7a99f06aabcd69665920b952798f7d3897
sumanthpasupuleti <sumanth.pasupuleti.is@gmail.com>
2018-07-18 00:00:54 -0700

    Correcting braces styling and adding throws to method signature

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -2609,22 +2609,29 @@ public class DatabaseDescriptor
 
     public static int getDefaultKeyspaceRF() { return conf.default_keyspace_rf; }
 
-    public static void setDefaultKeyspaceRF(int value)
+    public static void setDefaultKeyspaceRF(int value) throws ConfigurationException
     {
         if (value < 1)
+        {
             throw new ConfigurationException("default_keyspace_rf cannot be less than 1");
+        }
+
         if (value < getMinimumKeyspaceRF())
+        {
             throw new ConfigurationException(String.format("default_keyspace_rf cannot be less than minimum_keyspace_rf (%d)", getMinimumKeyspaceRF()));
+        }
 
         conf.default_keyspace_rf = value;
     }
 
     public static int getMinimumKeyspaceRF() { return conf.minimum_keyspace_rf; }
 
-    public static void setMinimumKeyspaceRF(int value)
+    public static void setMinimumKeyspaceRF(int value) throws ConfigurationException
     {
         if (value < 0)
+        {
             throw new ConfigurationException("minimum_keyspace_rf cannot be negative");
+        }
 
         conf.minimum_keyspace_rf = value;
     }
