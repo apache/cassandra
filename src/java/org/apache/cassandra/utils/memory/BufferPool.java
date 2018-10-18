8819140b77c4d9a9f4d73b0c2c038f9628ad855c
Jon Meredith <jmeredithco@gmail.com>
2018-10-18 09:56:46 -0600

    In BufferPool, make allocating thread receive a Chunk.
    Prevents a condition where the thread allocating the macro chunk
    could have all of the chunks taken from the queue before it
    is able to use one.
    
    patch by Jon Meredith; reviewed by Benedict for CASSANDRA-14832

--- src/java/org/apache/cassandra/utils/memory/BufferPool.java
+++ src/java/org/apache/cassandra/utils/memory/BufferPool.java
@@ -241,23 +241,23 @@ public class BufferPool
         /** Return a chunk, the caller will take owership of the parent chunk. */
         public Chunk get()
         {
-            while (true)
-            {
-                Chunk chunk = chunks.poll();
-                if (chunk != null)
-                    return chunk;
+            Chunk chunk = chunks.poll();
+            if (chunk != null)
+                return chunk;
 
-                if (!allocateMoreChunks())
-                    // give it one last attempt, in case someone else allocated before us
-                    return chunks.poll();
-            }
+            chunk = allocateMoreChunks();
+            if (chunk != null)
+                return chunk;
+
+            // another thread may have just allocated last macro chunk, so make one final attempt before returning null
+            return chunks.poll();
         }
 
         /**
          * This method might be called by multiple threads and that's fine if we add more
          * than one chunk at the same time as long as we don't exceed the MEMORY_USAGE_THRESHOLD.
          */
-        private boolean allocateMoreChunks()
+        private Chunk allocateMoreChunks()
         {
             while (true)
             {
@@ -266,7 +266,7 @@ public class BufferPool
                 {
                     noSpamLogger.info("Maximum memory usage reached ({}), cannot allocate chunk of {}",
                                       MEMORY_USAGE_THRESHOLD, MACRO_CHUNK_SIZE);
-                    return false;
+                    return null;
                 }
                 if (memoryUsage.compareAndSet(cur, cur + MACRO_CHUNK_SIZE))
                     break;
@@ -284,20 +284,23 @@ public class BufferPool
                                    "Attempting to continue; buffers will be allocated in on-heap memory which can degrade performance. " +
                                    "Make sure direct memory size (-XX:MaxDirectMemorySize) is large enough to accommodate off-heap memtables and caches.",
                                    MACRO_CHUNK_SIZE, sizeInBytes(), oom.toString());
-                return false;
+                return null;
             }
 
             chunk.acquire(null);
             macroChunks.add(chunk);
-            for (int i = 0 ; i < MACRO_CHUNK_SIZE ; i += CHUNK_SIZE)
+
+            final Chunk callerChunk = new Chunk(chunk.get(CHUNK_SIZE));
+            if (DEBUG)
+                debug.register(callerChunk);
+            for (int i = CHUNK_SIZE ; i < MACRO_CHUNK_SIZE; i += CHUNK_SIZE)
             {
                 Chunk add = new Chunk(chunk.get(CHUNK_SIZE));
                 chunks.add(add);
                 if (DEBUG)
                     debug.register(add);
             }
-
-            return true;
+            return callerChunk;
         }
 
         public void recycle(Chunk chunk)
