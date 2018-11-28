1c8d0ad333c642405537150fed2cbb8623a8fe94
Ariel Weisberg <aweisberg@apple.com>
2018-11-28 18:44:06 -0500

    In mixed 3.x/4 version clusters write tracing and repair history information without new columns
    Patch by Ariel Weisberg; Reviewed by Jason Brown for CASSANDRA-14897

--- src/java/org/apache/cassandra/tracing/TraceKeyspace.java
+++ src/java/org/apache/cassandra/tracing/TraceKeyspace.java
@@ -26,6 +26,7 @@ import org.apache.cassandra.cql3.statements.schema.CreateTableStatement;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.partitions.PartitionUpdate;
 import org.apache.cassandra.db.rows.Row;
+import org.apache.cassandra.gms.Gossiper;
 import org.apache.cassandra.schema.KeyspaceMetadata;
 import org.apache.cassandra.schema.KeyspaceParams;
 import org.apache.cassandra.schema.SchemaConstants;
@@ -98,15 +99,16 @@ public final class TraceKeyspace
                                              int ttl)
     {
         PartitionUpdate.SimpleBuilder builder = PartitionUpdate.simpleBuilder(Sessions, sessionId);
-        builder.row()
-               .ttl(ttl)
-               .add("client", client)
-               .add("coordinator", FBUtilities.getBroadcastAddressAndPort().address)
-               .add("coordinator_port", FBUtilities.getBroadcastAddressAndPort().port)
-               .add("request", request)
-               .add("started_at", new Date(startedAt))
-               .add("command", command)
-               .appendAll("parameters", parameters);
+        Row.SimpleBuilder rb = builder.row();
+        rb.ttl(ttl)
+          .add("client", client)
+          .add("coordinator", FBUtilities.getBroadcastAddressAndPort().address);
+        if (!Gossiper.instance.haveMajorVersion3Nodes())
+            rb.add("coordinator_port", FBUtilities.getBroadcastAddressAndPort().port);
+        rb.add("request", request)
+          .add("started_at", new Date(startedAt))
+          .add("command", command)
+          .appendAll("parameters", parameters);
 
         return builder.buildAsMutation();
     }
@@ -127,9 +129,10 @@ public final class TraceKeyspace
                                               .ttl(ttl);
 
         rowBuilder.add("activity", message)
-                  .add("source", FBUtilities.getBroadcastAddressAndPort().address)
-                  .add("source_port", FBUtilities.getBroadcastAddressAndPort().port)
-                  .add("thread", threadName);
+                  .add("source", FBUtilities.getBroadcastAddressAndPort().address);
+        if (!Gossiper.instance.haveMajorVersion3Nodes())
+            rowBuilder.add("source_port", FBUtilities.getBroadcastAddressAndPort().port);
+        rowBuilder.add("thread", threadName);
 
         if (elapsed >= 0)
             rowBuilder.add("source_elapsed", elapsed);
