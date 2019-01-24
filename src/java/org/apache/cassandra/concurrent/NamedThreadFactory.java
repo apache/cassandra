ff6776f0c51de54b74d27aa8abae4767fc6fcbd2
Alex Petrov <oleksandr.petrov@gmail.com>
2019-01-24 13:44:55 +0100

    Remove unnecessary Runnable wrapping in NamedThreadFactory as is done by Netty.
    Patch by Alex Petrov; reviewed by Dinesh Joshi for CASSANDRA-15008

--- src/java/org/apache/cassandra/concurrent/NamedThreadFactory.java
+++ src/java/org/apache/cassandra/concurrent/NamedThreadFactory.java
@@ -70,26 +70,6 @@ public class NamedThreadFactory implements ThreadFactory
         return thread;
     }
 
-    /**
-     * Ensures that {@link FastThreadLocal#remove() FastThreadLocal.remove()} is called when the {@link Runnable#run()}
-     * method of the given {@link Runnable} instance completes to ensure cleanup of {@link FastThreadLocal} instances.
-     * This is especially important for direct byte buffers allocated locally for a thread.
-     */
-    public static Runnable threadLocalDeallocator(Runnable r)
-    {
-        return () ->
-        {
-            try
-            {
-                r.run();
-            }
-            finally
-            {
-                FastThreadLocal.removeAll();
-            }
-        };
-    }
-
     private static final AtomicInteger threadCounter = new AtomicInteger();
 
     @VisibleForTesting
@@ -116,7 +96,7 @@ public class NamedThreadFactory implements ThreadFactory
     public static Thread createThread(ThreadGroup threadGroup, Runnable runnable, String name, boolean daemon)
     {
         String prefix = globalPrefix;
-        Thread thread = new FastThreadLocalThread(threadGroup, threadLocalDeallocator(runnable), prefix != null ? prefix + name : name);
+        Thread thread = new FastThreadLocalThread(threadGroup, runnable, prefix != null ? prefix + name : name);
         thread.setDaemon(daemon);
         return thread;
     }
