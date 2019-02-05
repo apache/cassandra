e0c10fd76b38dea49ad3159fd4690e0ed4b55e1f
Alex Petrov <oleksandr.petrov@gmail.com>
2019-02-05 17:39:59 +0100

    Implement simple In-JVM tooling for testing distributed paging
    Patch by Alex Petrov; reviewed by Jon Meredith for CASSANDRA-15009

--- test/distributed/org/apache/cassandra/distributed/impl/Coordinator.java
+++ test/distributed/org/apache/cassandra/distributed/impl/Coordinator.java
@@ -20,15 +20,21 @@ package org.apache.cassandra.distributed.impl;
 
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.apache.cassandra.cql3.CQLStatement;
 import org.apache.cassandra.cql3.QueryOptions;
 import org.apache.cassandra.cql3.QueryProcessor;
+import org.apache.cassandra.cql3.UntypedResultSet;
+import org.apache.cassandra.cql3.statements.SelectStatement;
 import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.distributed.api.ICoordinator;
 import org.apache.cassandra.service.ClientState;
 import org.apache.cassandra.service.QueryState;
+import org.apache.cassandra.service.pager.Pageable;
+import org.apache.cassandra.service.pager.QueryPager;
+import org.apache.cassandra.service.pager.QueryPagers;
 import org.apache.cassandra.transport.Server;
 import org.apache.cassandra.transport.messages.ResultMessage;
 import org.apache.cassandra.utils.ByteBufferUtil;
@@ -55,13 +61,13 @@ public class Coordinator implements ICoordinator
 
             prepared.validate(QueryState.forInternalCalls().getClientState());
             ResultMessage res = prepared.execute(QueryState.forInternalCalls(),
-                    QueryOptions.create(consistencyLevel,
-                            boundBBValues,
-                            false,
-                            10,
-                            null,
-                            null,
-                            Server.CURRENT_VERSION));
+                                                 QueryOptions.create(consistencyLevel,
+                                                                     boundBBValues,
+                                                                     false,
+                                                                     Integer.MAX_VALUE,
+                                                                     null,
+                                                                     null,
+                                                                     Server.CURRENT_VERSION));
 
             if (res != null && res.kind == ResultMessage.Kind.ROWS)
             {
@@ -71,6 +77,58 @@ public class Coordinator implements ICoordinator
             {
                 return new Object[][]{};
             }
+        }).call();
+    }
+
+    @Override
+    public Iterator<Object[]> executeWithPaging(String query, Enum<?> consistencyLevelOrigin, int pageSize, Object... boundValues)
+    {
+        if (pageSize <= 0)
+            throw new IllegalArgumentException("Page size should be strictly positive but was " + pageSize);
+
+        return instance.sync(() -> {
+            ConsistencyLevel consistencyLevel = ConsistencyLevel.valueOf(consistencyLevelOrigin.name());
+            CQLStatement prepared = QueryProcessor.getStatement(query, ClientState.forInternalCalls()).statement;
+            List<ByteBuffer> boundBBValues = new ArrayList<>();
+            for (Object boundValue : boundValues)
+            {
+                boundBBValues.add(ByteBufferUtil.objectToBytes(boundValue));
+            }
+
+            prepared.validate(QueryState.forInternalCalls().getClientState());
+            assert prepared instanceof SelectStatement : "Only SELECT statements can be executed with paging";
+
+            ClientState clientState = QueryState.forInternalCalls().getClientState();
+            SelectStatement selectStatement = (SelectStatement) prepared;
+            QueryOptions queryOptions = QueryOptions.create(consistencyLevel,
+                                                            boundBBValues,
+                                                            false,
+                                                            pageSize,
+                                                            null,
+                                                            null,
+                                                            Server.CURRENT_VERSION);
+            Pageable pageable = selectStatement.getPageableCommand(queryOptions);
+
+            // Usually pager fetches a single page (see SelectStatement#execute). We need to iterate over all
+            // of the results lazily.
+            QueryPager pager = QueryPagers.pager(pageable, consistencyLevel, clientState, null);
+            Iterator<Object[]> iter = RowUtil.toObjects(selectStatement.getResultMetadata().names,
+                                                        UntypedResultSet.create(selectStatement,
+                                                                                pager,
+                                                                                pageSize).iterator());
+
+            // We have to make sure iterator is not running on main thread.
+            return new Iterator<Object[]>() {
+                public boolean hasNext()
+                {
+                    return instance.sync(() -> iter.hasNext()).call();
+                }
+
+                public Object[] next()
+                {
+                    return instance.sync(() -> iter.next()).call();
+                }
+            };
         }).call();
     }
 }
