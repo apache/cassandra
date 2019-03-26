ba72c7d16301aaf179506c8d6ebe92d1e91af06a
Blake Eggleston <bdeggleston@gmail.com>
2019-03-26 11:18:00 -0700

    Support building Cassandra with JDK 11
    Patch by Blake Eggleston; Reviewed by Dinesh Joshi and Sam Tunnicliffe for CASSANDRA-15108

--- src/java/org/apache/cassandra/metrics/StreamingMetrics.java
+++ src/java/org/apache/cassandra/metrics/StreamingMetrics.java
@@ -44,11 +44,25 @@ public class StreamingMetrics
 
     public static StreamingMetrics get(InetAddressAndPort ip)
     {
+       /*
+         computeIfAbsent doesn't work for this situation. Since JMX metrics register themselves in their ctor, we need
+         to create the metric exactly once, otherwise we'll get duplicate name exceptions. Although computeIfAbsent is
+         thread safe in the context of the map, it uses compare and swap to add the computed value to the map. This
+         means it eagerly allocates new metric instances, which can cause the jmx name collision we're trying to avoid
+         if multiple calls interleave. So here we use synchronized to ensure we only instantiate metrics exactly once.
+        */
        StreamingMetrics metrics = instances.get(ip);
        if (metrics == null)
        {
-           metrics = new StreamingMetrics(ip);
-           instances.put(ip, metrics);
+           synchronized (instances)
+           {
+               metrics = instances.get(ip);
+               if (metrics == null)
+               {
+                   metrics = new StreamingMetrics(ip);
+                   instances.put(ip, metrics);
+               }
+           }
        }
        return metrics;
     }
