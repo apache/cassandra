6f55d292afb189246b3bc9f9e8541a9a99b914a7
David Capwell <dcapwell@gmail.com>
2020-02-18 13:35:38 -0800

    Add the cause to the exception and allow to log when an invalid partitioner is detected.
    Patch by David Capwell, reviewed by brandonwilliams for CASSANDRA-15591

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1103,18 +1103,25 @@ public class DatabaseDescriptor
     // definitely not safe for tools + clients - implicitly instantiates schema
     public static void applyPartitioner()
     {
+        applyPartitioner(conf);
+    }
+
+    public static void applyPartitioner(Config conf)
+    {
         /* Hashing strategy */
         if (conf.partitioner == null)
         {
             throw new ConfigurationException("Missing directive: partitioner", false);
         }
+        String name = conf.partitioner;
         try
         {
-            partitioner = FBUtilities.newPartitioner(System.getProperty(Config.PROPERTY_PREFIX + "partitioner", conf.partitioner));
+            name = System.getProperty(Config.PROPERTY_PREFIX + "partitioner", conf.partitioner);
+            partitioner = FBUtilities.newPartitioner(name);
         }
         catch (Exception e)
         {
-            throw new ConfigurationException("Invalid partitioner class " + conf.partitioner, false);
+            throw new ConfigurationException("Invalid partitioner class " + name, e);
         }
 
         paritionerName = partitioner.getClass().getCanonicalName();
