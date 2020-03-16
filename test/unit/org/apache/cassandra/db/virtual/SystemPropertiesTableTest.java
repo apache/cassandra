bb6289272d1bd3636e159d7baa1f51424106e72a
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2020-03-16 09:46:01 -0400

    Add JAVA_HOME and java.rmi.server.randomID to the system_properties table
     patch by Ekaterina Dimitrova; reviewed by Mick Semb Wever for CASSANDRA-15643

--- test/unit/org/apache/cassandra/db/virtual/SystemPropertiesTableTest.java
+++ test/unit/org/apache/cassandra/db/virtual/SystemPropertiesTableTest.java
@@ -18,10 +18,13 @@
 
 package org.apache.cassandra.db.virtual;
 
+import java.lang.reflect.Field;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import com.google.common.collect.ImmutableList;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
@@ -31,9 +34,14 @@ import com.datastax.driver.core.ResultSet;
 import com.datastax.driver.core.Row;
 import org.apache.cassandra.cql3.CQLTester;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Maps;
+
 public class SystemPropertiesTableTest extends CQLTester
 {
     private static final String KS_NAME = "vts";
+    private static final Map<String, String> ORIGINAL_ENV_MAP = System.getenv();
+    private static final String TEST_PROP = "org.apache.cassandra.db.virtual.SystemPropertiesTableTest";
 
     private SystemPropertiesTable table;
 
@@ -56,22 +64,21 @@ public class SystemPropertiesTableTest extends CQLTester
         ResultSet result = executeNet("SELECT * FROM vts.system_properties");
 
         for (Row r : result)
-            Assert.assertEquals(System.getProperty(r.getString("name")), r.getString("value"));
+            Assert.assertEquals(System.getProperty(r.getString("name"), System.getenv(r.getString("name"))), r.getString("value"));
     }
 
     @Test
     public void testSelectPartition() throws Throwable
     {
-        List<String> properties = System.getProperties()
-                                        .stringPropertyNames()
-                                        .stream()
+        List<String> properties = Stream.concat(System.getProperties().stringPropertyNames().stream(),
+                                                System.getenv().keySet().stream())
                                         .filter(name -> SystemPropertiesTable.isCassandraRelevant(name))
                                         .collect(Collectors.toList());
 
         for (String property : properties)
         {
             String q = "SELECT * FROM vts.system_properties WHERE name = '" + property + '\'';
-            assertRowsNet(executeNet(q), new Object[] {property, System.getProperty(property)});
+            assertRowsNet(executeNet(q), new Object[] {property, System.getProperty(property, System.getenv(property))});
         }
     }
 
@@ -82,4 +89,100 @@ public class SystemPropertiesTableTest extends CQLTester
         assertRowsNet(executeNet(q));
     }
 
+    @Test
+    public void testSelectProperty() throws Throwable
+    {
+        try
+        {
+            String value = "test_value";
+            System.setProperty(TEST_PROP, value);
+            String q = String.format("SELECT * FROM vts.system_properties WHERE name = '%s'", TEST_PROP);
+            assertRowsNet(executeNet(q), new Object[] {TEST_PROP, value});
+        }
+        finally
+        {
+            System.clearProperty(TEST_PROP);
+        }
+    }
+
+    @Test
+    public void testSelectEnv() throws Throwable
+    {
+        try
+        {
+            String value = "test_value";
+            addEnv(TEST_PROP, value);
+            String q = String.format("SELECT * FROM vts.system_properties WHERE name = '%s'", TEST_PROP);
+            assertRowsNet(executeNet(q), new Object[] {TEST_PROP, value});
+        }
+        finally
+        {
+            resetEnv();
+        }
+    }
+
+    @Test
+    public void testSelectPropertyOverEnv() throws Throwable
+    {
+        try
+        {
+            String value = "test_value";
+            System.setProperty(TEST_PROP, value);
+            addEnv(TEST_PROP, "wrong_value");
+            String q = String.format("SELECT * FROM vts.system_properties WHERE name = '%s'", TEST_PROP);
+            assertRowsNet(executeNet(q), new Object[] {TEST_PROP, value});
+        }
+        finally
+        {
+            System.clearProperty(TEST_PROP);
+            resetEnv();
+        }
+    }
+
+    private static void addEnv(String env, String value) throws ReflectiveOperationException
+    {
+        Map<String, String> envMap = Maps.newConcurrentMap();
+        envMap.putAll(System.getenv());
+        envMap.put(env, value);
+        setEnv(envMap);
+    }
+
+    private static void resetEnv() throws ReflectiveOperationException
+    {
+        setEnv(ORIGINAL_ENV_MAP);
+    }
+
+    private static void setEnv(Map<String, String> newenv) throws ReflectiveOperationException
+    {
+        try
+        {
+            Class<?> cls = Class.forName("java.lang.ProcessEnvironment");
+            Field field = cls.getDeclaredField("theEnvironment");
+            field.setAccessible(true);
+            Map<String, String> envMap = (Map<String, String>) field.get(null);
+            envMap.clear();
+            envMap.putAll(newenv);
+            field = cls.getDeclaredField("theCaseInsensitiveEnvironment");
+            field.setAccessible(true);
+            envMap = (Map<String, String>) field.get(null);
+            envMap.clear();
+            envMap.putAll(newenv);
+        }
+        catch (NoSuchFieldException ignore)
+        {
+            Class[] classes = Collections.class.getDeclaredClasses();
+            Map<String, String> envMap = System.getenv();
+            for(Class cl : classes) {
+                if("java.util.Collections$UnmodifiableMap".equals(cl.getName()))
+                {
+                    Field field = cl.getDeclaredField("m");
+                    field.setAccessible(true);
+                    Object obj = field.get(envMap);
+                    envMap = (Map<String, String>) obj;
+                    envMap.clear();
+                    envMap.putAll(newenv);
+                }
+            }
+        }
+    }
 }
