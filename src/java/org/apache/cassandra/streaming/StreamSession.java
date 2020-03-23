31196730decc2d10fb08c3d9ca488acf3c186c45
Sankalp Kohli <sankalp@apple.com>
2020-03-23 10:59:27 +0100

    Expose repair streaming metrics
    Patch by Sankalp Kohli; reviewed by Ekaterina Dimitrova for CASSANDRA-15656

--- src/java/org/apache/cassandra/streaming/StreamSession.java
+++ src/java/org/apache/cassandra/streaming/StreamSession.java
@@ -510,8 +510,21 @@ public class StreamSession implements IEndpointStateChangeSubscriber
         state(State.PREPARING);
         PrepareSynMessage prepare = new PrepareSynMessage();
         prepare.requests.addAll(requests);
+        long totalBytesToStream = 0;
+        long totalSSTablesStreamed = 0;
         for (StreamTransferTask task : transfers.values())
+        {
+            totalBytesToStream += task.getTotalSize();
+            totalSSTablesStreamed += task.getTotalNumberOfFiles();
             prepare.summaries.add(task.getSummary());
+        }
+
+        if(StreamOperation.REPAIR == getStreamOperation())
+        {
+            StreamingMetrics.totalOutgoingRepairBytes.inc(totalBytesToStream);
+            StreamingMetrics.totalOutgoingRepairSSTables.inc(totalSSTablesStreamed);
+        }
+
         messageSender.sendMessage(prepare);
     }
 
