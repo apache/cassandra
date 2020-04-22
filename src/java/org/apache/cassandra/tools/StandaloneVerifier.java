2d27c769479f877eb3af308b49d73f8e124f4703
David Capwell <dcapwell@apple.com>
2020-04-22 11:13:24 -0700

    bin/sstableverify should support user provided token ranges
    Patch by David Capwell; reviewed by Dinesh Joshi for CASSANDRA-15753

--- src/java/org/apache/cassandra/tools/StandaloneVerifier.java
+++ src/java/org/apache/cassandra/tools/StandaloneVerifier.java
@@ -23,6 +23,9 @@ import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.Directories;
 import org.apache.cassandra.db.Keyspace;
 import org.apache.cassandra.db.compaction.*;
+import org.apache.cassandra.dht.Murmur3Partitioner;
+import org.apache.cassandra.dht.Range;
+import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.io.sstable.Component;
 import org.apache.cassandra.io.sstable.CorruptSSTableException;
 import org.apache.cassandra.io.sstable.Descriptor;
@@ -33,6 +36,8 @@ import org.apache.commons.cli.*;
 
 import java.util.*;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import static org.apache.cassandra.tools.BulkLoader.CmdLineOptions;
 
@@ -46,11 +51,13 @@ public class StandaloneVerifier
     private static final String CHECK_VERSION = "check_version";
     private static final String MUTATE_REPAIR_STATUS = "mutate_repair_status";
     private static final String QUICK = "quick";
+    private static final String TOKEN_RANGE = "token_range";
 
     public static void main(String args[])
     {
         Options options = Options.parseArgs(args);
         Util.initDatabaseDescriptor();
+        System.out.println("sstableverify using the following options: " + options);
 
         try
         {
@@ -97,8 +104,10 @@ public class StandaloneVerifier
                                                                .extendedVerification(options.extended)
                                                                .checkVersion(options.checkVersion)
                                                                .mutateRepairStatus(options.mutateRepairStatus)
-                                                               .checkOwnsTokens(false) // don't know the ranges when running offline
+                                                               .checkOwnsTokens(!options.tokens.isEmpty())
+                                                               .tokenLookup(ignore -> options.tokens)
                                                                .build();
+            handler.output("Running verifier with the following options: " + verifyOptions);
             for (SSTableReader sstable : sstables)
             {
                 try
@@ -145,6 +154,7 @@ public class StandaloneVerifier
         public boolean checkVersion;
         public boolean mutateRepairStatus;
         public boolean quick;
+        public Collection<Range<Token>> tokens;
 
         private Options(String keyspaceName, String cfName)
         {
@@ -187,6 +197,17 @@ public class StandaloneVerifier
                 opts.mutateRepairStatus = cmd.hasOption(MUTATE_REPAIR_STATUS);
                 opts.quick = cmd.hasOption(QUICK);
 
+                if (cmd.hasOption(TOKEN_RANGE))
+                {
+                    opts.tokens = Stream.of(cmd.getOptionValues(TOKEN_RANGE))
+                                        .map(StandaloneVerifier::parseTokenRange)
+                                        .collect(Collectors.toSet());
+                }
+                else
+                {
+                    opts.tokens = Collections.emptyList();
+                }
+
                 return opts;
             }
             catch (ParseException e)
@@ -194,6 +215,21 @@ public class StandaloneVerifier
                 errorMsg(e.getMessage(), options);
                 return null;
             }
+        }
+
+        public String toString()
+        {
+            return "Options{" +
+                   "keyspaceName='" + keyspaceName + '\'' +
+                   ", cfName='" + cfName + '\'' +
+                   ", debug=" + debug +
+                   ", verbose=" + verbose +
+                   ", extended=" + extended +
+                   ", checkVersion=" + checkVersion +
+                   ", mutateRepairStatus=" + mutateRepairStatus +
+                   ", quick=" + quick +
+                   ", tokens=" + tokens +
+                   '}';
         }
 
         private static void errorMsg(String msg, CmdLineOptions options)
@@ -213,6 +249,7 @@ public class StandaloneVerifier
             options.addOption("c",  CHECK_VERSION,         "make sure sstables are the latest version");
             options.addOption("r",  MUTATE_REPAIR_STATUS,  "don't mutate repair status");
             options.addOption("q",  QUICK,                 "do a quick check, don't read all data");
+            options.addOptionList("t", TOKEN_RANGE, "range", "long token range of the format left,right. This may be provided multiple times to define multiple different ranges");
             return options;
         }
 
@@ -226,5 +263,15 @@ public class StandaloneVerifier
             header.append("Options are:");
             new HelpFormatter().printHelp(usage, header.toString(), options, "");
         }
+    }
+
+    private static Range<Token> parseTokenRange(String line)
+    {
+        String[] split = line.split(",");
+        if (split.length != 2)
+            throw new IllegalArgumentException("Unable to parse token range from " + line + "; format is left,right but saw " + split.length + " parts");
+        long left = Long.parseLong(split[0]);
+        long right = Long.parseLong(split[1]);
+        return new Range<>(new Murmur3Partitioner.LongToken(left), new Murmur3Partitioner.LongToken(right));
     }
 }
