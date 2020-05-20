2d0b16804785660e8515aca9944784fb3733c619
Sylvain Lebresne <lebresne@gmail.com>
2020-05-20 14:56:02 +0200

    Fix serial read/non-applying CAS linearizability
    Before this patch, a SERIAL read or a non-applying CAS replay any
    in-progress commit by calling `beginAndRepairPaxos`, but only a quorum
    of nodes is contacted, so a minority of nodes could have an unfinished
    in-progress proposal in their Paxos state. If such in-progress proposal
    is not replayed by a SERIAL read/non-applying CAS, it should never be
    replayed by any following operation as that would break serializability,
    but nothing was done to avoid this.
    
    This patch ensures that both a SERIAL read or a non-applying CAS commit
    an empty update before succeeding. This ensures that no prior incomplete
    in-progress proposal can be replayed (such proposal will be discarded as
    older than the last committed ballot).
    
    As this fix has a performance impact on SERIAL reads, a flag is provided
    to disable the new code (even if this is discouraged by a warning).
    
    Patch by Sylvain Lebresne, reviewed by Benjamin Lerer for CASSANDRA-12126

--- src/java/org/apache/cassandra/service/paxos/PrepareCallback.java
+++ src/java/org/apache/cassandra/service/paxos/PrepareCallback.java
@@ -46,7 +46,6 @@ public class PrepareCallback extends AbstractPaxosCallback<PrepareResponse>
     public boolean promised = true;
     public Commit mostRecentCommit;
     public Commit mostRecentInProgressCommit;
-    public Commit mostRecentInProgressCommitWithUpdate;
 
     private final Map<InetAddress, Commit> commitsByReplica = new ConcurrentHashMap<InetAddress, Commit>();
 
@@ -56,7 +55,6 @@ public class PrepareCallback extends AbstractPaxosCallback<PrepareResponse>
         // need to inject the right key in the empty commit so comparing with empty commits in the reply works as expected
         mostRecentCommit = Commit.emptyCommit(key, metadata);
         mostRecentInProgressCommit = Commit.emptyCommit(key, metadata);
-        mostRecentInProgressCommitWithUpdate = Commit.emptyCommit(key, metadata);
     }
 
     public synchronized void response(MessageIn<PrepareResponse> message)
@@ -64,9 +62,8 @@ public class PrepareCallback extends AbstractPaxosCallback<PrepareResponse>
         PrepareResponse response = message.payload;
         logger.trace("Prepare response {} from {}", response, message.from);
 
-        // In case of clock skew, another node could be proposing with ballot that are quite a bit
-        // older than our own. In that case, we record the more recent commit we've received to make
-        // sure we re-prepare on an older ballot.
+        // We set the mostRecentInProgressCommit even if we're not promised as, in that case, the ballot of that commit
+        // will be used to avoid generating a ballot that has not chance to win on retry (think clock skew).
         if (response.inProgressCommit.isAfter(mostRecentInProgressCommit))
             mostRecentInProgressCommit = response.inProgressCommit;
 
@@ -81,11 +78,6 @@ public class PrepareCallback extends AbstractPaxosCallback<PrepareResponse>
         commitsByReplica.put(message.from, response.mostRecentCommit);
         if (response.mostRecentCommit.isAfter(mostRecentCommit))
             mostRecentCommit = response.mostRecentCommit;
-
-        // If some response has an update, then we should replay the update with the highest ballot. So find
-        // the the highest commit that actually have an update
-        if (response.inProgressCommit.isAfter(mostRecentInProgressCommitWithUpdate) && !response.inProgressCommit.update.isEmpty())
-            mostRecentInProgressCommitWithUpdate = response.inProgressCommit;
 
         latch.countDown();
     }
