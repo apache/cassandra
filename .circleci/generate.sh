7b23aab2c1bb0bcecc112641533fac8757ac8cf9
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2020-06-08 20:26:39 -0400

    Create config.yml.MIDRES
    Patch by Ekaterina Dimitrova, reviewed by brandonwilliams for CASSANDRA-15712

--- .circleci/generate.sh
+++ .circleci/generate.sh
@@ -6,4 +6,3 @@ circleci config process $BASEDIR/config-2_1.yml > $BASEDIR/config.yml.LOWRES
 patch -o $BASEDIR/config-2_1.yml.HIGHRES $BASEDIR/config-2_1.yml $BASEDIR/config-2_1.yml.high_res.patch
 circleci config process $BASEDIR/config-2_1.yml.HIGHRES > $BASEDIR/config.yml.HIGHRES
 rm $BASEDIR/config-2_1.yml.HIGHRES
-
