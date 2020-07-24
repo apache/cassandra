b619d21ed2b0de72da11daad7ce3938a6b348dd4
David Capwell <dcapwell@apple.com>
2020-07-23 17:41:20 -0700

    Circle CI approval jobs should not require build to run first
    patch by David Capwell; reviewed by Ekaterina Dimitrova for CASSANDRA-15966

--- .circleci/generate.sh
+++ .circleci/generate.sh
@@ -3,6 +3,13 @@
 BASEDIR=`dirname $0`
 
 circleci config process $BASEDIR/config-2_1.yml > $BASEDIR/config.yml.LOWRES
+
+# setup midres
+patch -o $BASEDIR/config-2_1.yml.MIDRES $BASEDIR/config-2_1.yml $BASEDIR/config-2_1.yml.mid_res.patch
+circleci config process $BASEDIR/config-2_1.yml.MIDRES > $BASEDIR/config.yml.MIDRES
+rm $BASEDIR/config-2_1.yml.MIDRES
+
+# setup higher
 patch -o $BASEDIR/config-2_1.yml.HIGHRES $BASEDIR/config-2_1.yml $BASEDIR/config-2_1.yml.high_res.patch
 circleci config process $BASEDIR/config-2_1.yml.HIGHRES > $BASEDIR/config.yml.HIGHRES
 rm $BASEDIR/config-2_1.yml.HIGHRES
