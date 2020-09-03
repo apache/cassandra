7305ebd664b8eb4a60eca25ed2624ef019221332
David Capwell <dcapwell@apache.org>
2020-09-03 09:51:15 -0700

    supporess resources

--- src/java/org/apache/cassandra/tools/JMXTool.java
+++ src/java/org/apache/cassandra/tools/JMXTool.java
@@ -118,6 +118,8 @@ public class JMXTool
             {
                 void dump(OutputStream output, Map<String, Info> map)
                 {
+                    @SuppressWarnings("resource")
+                    // output should be released by caller
                     PrintStream out = toPrintStream(output);
                     for (Map.Entry<String, Info> e : map.entrySet())
                     {
