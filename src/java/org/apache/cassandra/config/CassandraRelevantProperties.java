b3f2f5bd741ff58a51864e86628ccd1866dabb6b
Branimir Lambov <branimir.lambov@datastax.com>
2021-01-11 16:02:12 +0200

    Provides the Trie interface with MemtableTrie implementation
    also includes functionality to merge, intersect and iterate on tries.
    
    patch by Branimir Lambov
    reviewed by Jason Rutherglen and Jacek Lewandowski for DSE
    reviewed by Andres de la Peña and Calib Rackliffe for CASSANDRA-17240

--- src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
+++ src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
@@ -235,6 +235,7 @@ public enum CassandraRelevantProperties
 
     MEMTABLE_OVERHEAD_SIZE("cassandra.memtable.row_overhead_size", "-1"),
     MEMTABLE_OVERHEAD_COMPUTE_STEPS("cassandra.memtable_row_overhead_computation_step", "100000"),
+    MEMTABLE_TRIE_SIZE_LIMIT("cassandra.trie_size_limit_mb"),
     MIGRATION_DELAY("cassandra.migration_delay_ms", "60000"),
     /** Defines how often schema definitions are pulled from the other nodes */
     SCHEMA_PULL_INTERVAL_MS("cassandra.schema_pull_interval_ms", "60000"),
