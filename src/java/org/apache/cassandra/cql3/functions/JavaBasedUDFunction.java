a00eada87d2c95db60242971cb654ea58ba518cb
Alex Petrov <oleksandr.petrov@gmail.com>
2021-02-15 15:23:54 +0100

    Avoid using insecure class loader, while picking a right parent class loader for in-jvm dtests
    Patch by Alex Petrov; reviewed by Marcus Eriksson for CASSANDRA-16448.

--- src/java/org/apache/cassandra/cql3/functions/JavaBasedUDFunction.java
+++ src/java/org/apache/cassandra/cql3/functions/JavaBasedUDFunction.java
@@ -656,7 +656,7 @@ public final class JavaBasedUDFunction extends UDFunction
     {
         EcjTargetClassLoader()
         {
-            super(UDFClassLoader.insecureClassLoader);
+            super(UDFunction.udfClassLoader);
         }
 
         // This map is usually empty.
