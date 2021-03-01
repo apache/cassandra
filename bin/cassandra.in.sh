22fb77caa1e1a7970a7667eea3867866aca275cb
Jakub Zytka <jakub.zytka@datastax.com>
2021-03-01 18:10:08 +0100

    STAR-172: Update artifacts to be called dse-db rather than cassandra
    (cherry picked from commit d8708d5b62db0e8eceb4744d0948557932e6a8b4)
    (cherry picked from commit a98a1053bbbc988bd462c62b129659de0d230a87)
    (cherry picked from commit d5b41b45747bf622700442476045e14eb7a62e06)
    (cherry picked from commit b6910eea1d8fa1e9bc5432ab144a7c96d8ca8fbc)
    (cherry picked from commit 30ef052c152a6dc3ac6ff1e8fd82ac25ff2b94c7)
    (cherry picked from commit 972372dd20956c1d06f907a062eac74ab0189da3)

--- bin/cassandra.in.sh
+++ bin/cassandra.in.sh
@@ -30,7 +30,7 @@ CLASSPATH="$CASSANDRA_CONF"
 # compiled classes. NOTE: This isn't needed by the startup script,
 # it's just used here in constructing the classpath.
 if [ -d $CASSANDRA_HOME/build ] ; then
-    jars_cnt="`ls -1 $CASSANDRA_HOME/build/apache-cassandra*.jar | grep -v 'javadoc.jar' | grep -v 'sources.jar' | wc -l | xargs echo`"
+    jars_cnt="`ls -1 $CASSANDRA_HOME/build/dse-db*.jar | grep -v 'javadoc.jar' | grep -v 'sources.jar' | wc -l | xargs echo`"
     if [ "$jars_cnt" -gt 1 ]; then
         dir="`cd $CASSANDRA_HOME/build; pwd`"
         echo "There are JAR artifacts for multiple versions in the $dir directory. Please clean the project with 'ant realclean' and build it again." 1>&2
@@ -38,8 +38,8 @@ if [ -d $CASSANDRA_HOME/build ] ; then
     fi
 
     if [ "$jars_cnt" = "1" ]; then
-        cassandra_bin="`ls -1 $CASSANDRA_HOME/build/apache-cassandra*.jar | grep -v javadoc | grep -v sources`"
-        CLASSPATH="$CLASSPATH:$cassandra_bin"
+        dse_db_bin="`ls -1 $CASSANDRA_HOME/build/dse-db*.jar | grep -v javadoc | grep -v sources`"
+        CLASSPATH="$CLASSPATH:$dse_db_bin"
     fi
 fi
 
@@ -119,16 +119,16 @@ JAVA_VERSION=11
 if [ "$JVM_VERSION" = "1.8.0" ]  ; then
     JVM_PATCH_VERSION=${jvmver#*_}
     if [ "$JVM_VERSION" \< "1.8" ] || [ "$JVM_VERSION" \> "1.8.2" ] ; then
-        echo "Cassandra 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer). Java $JVM_VERSION is not supported."
+        echo "DSE DB 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer). Java $JVM_VERSION is not supported."
         exit 1;
     fi
     if [ "$JVM_PATCH_VERSION" -lt 151 ] ; then
-        echo "Cassandra 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer). Java 8 update $JVM_PATCH_VERSION is not supported."
+        echo "DSE DB 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer). Java 8 update $JVM_PATCH_VERSION is not supported."
         exit 1;
     fi
     JAVA_VERSION=8
 elif [ "$JVM_VERSION" \< "11" ] ; then
-    echo "Cassandra 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer)."
+    echo "DSE DB 4.0 requires either Java 8 (update 151 or newer) or Java 11 (or newer)."
     exit 1;
 fi
 
