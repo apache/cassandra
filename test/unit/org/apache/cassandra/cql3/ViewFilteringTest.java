c3826a739b0a3ed72fa9a41e09f82ced15a365d7
jacek-lewandowski <jacek.lewandowski@datastax.com>
2021-03-11 21:02:05 +0100

    Don't fail all view filtering tests because one method timed out
    Patch by Jacek Lewandowski; reviewed by brandonwilliams for
    CASSANDRA-16515

--- test/unit/org/apache/cassandra/cql3/ViewFilteringTest.java
+++ test/unit/org/apache/cassandra/cql3/ViewFilteringTest.java
@@ -30,6 +30,7 @@ import org.junit.Test;
 import com.datastax.driver.core.exceptions.InvalidQueryException;
 import org.junit.Assert;
 
+import com.datastax.driver.core.exceptions.OperationTimedOutException;
 import org.apache.cassandra.concurrent.SEPExecutor;
 import org.apache.cassandra.concurrent.Stage;
 import org.apache.cassandra.db.Keyspace;
@@ -70,10 +71,19 @@ public class ViewFilteringTest extends CQLTester
 
     private void createView(String name, String query) throws Throwable
     {
-        executeNet(protocolVersion, String.format(query, name));
-        // If exception is thrown, the view will not be added to the list; since it shouldn't have been created, this is
-        // the desired behavior
-        views.add(name);
+        try
+        {
+            executeNet(protocolVersion, String.format(query, name));
+            // If exception is thrown, the view will not be added to the list; since it shouldn't have been created, this is
+            // the desired behavior
+            views.add(name);
+        }
+        catch (OperationTimedOutException ex)
+        {
+            // ... except for timeout, when we actually do not know whether the view was created or not
+            views.add(name);
+            throw ex;
+        }
     }
 
     private void updateView(String query, Object... params) throws Throwable
