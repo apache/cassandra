07babf3cb10bc11efad48cc7865c7425d7c27ce4
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2021-03-11 12:12:12 -0500

    Ninja-fix: CASSANDRA-15897 broke one of the CompactStorageUpgrade tests; GOSSIP required

--- test/distributed/org/apache/cassandra/distributed/upgrade/CompactStorageUpgradeTest.java
+++ test/distributed/org/apache/cassandra/distributed/upgrade/CompactStorageUpgradeTest.java
@@ -24,7 +24,8 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import org.apache.cassandra.distributed.api.ConsistencyLevel;
-import org.apache.cassandra.distributed.api.Feature;
+import static org.apache.cassandra.distributed.api.Feature.GOSSIP;
+import static org.apache.cassandra.distributed.api.Feature.NETWORK;
 import org.apache.cassandra.distributed.shared.Versions;
 import static org.apache.cassandra.distributed.shared.AssertUtils.*;
 
@@ -145,6 +146,7 @@ public class CompactStorageUpgradeTest extends UpgradeTestBase
         .nodes(2)
         .nodesToUpgrade(1, 2)
         .upgrade(Versions.Major.v30, Versions.Major.v4)
+        .withConfig(config -> config.with(GOSSIP, NETWORK))
         .setup((cluster) -> {
             cluster.schemaChange("CREATE TABLE " + KEYSPACE + ".tbl (pk int, ck int, PRIMARY KEY (pk, ck)) WITH COMPACT STORAGE");
             cluster.coordinator(1).execute("INSERT INTO " + KEYSPACE + ".tbl (pk, ck) VALUES (1,1)", ConsistencyLevel.ALL);
