064d9eeb27c327b7b9a3ae55ac10137e3e30eca6
Mike Adamson <madamson@datastax.com>
2021-04-20 12:57:18 +0100

    STAR-343 Frozen Tuple, Frozen UDT & Boolean support

--- src/java/org/apache/cassandra/index/sai/disk/SSTableIndexWriter.java
+++ src/java/org/apache/cassandra/index/sai/disk/SSTableIndexWriter.java
@@ -54,7 +54,7 @@ public class SSTableIndexWriter implements ColumnIndexWriter
     private static final NoSpamLogger noSpamLogger = NoSpamLogger.getLogger(logger, 1, TimeUnit.MINUTES);
 
     public static final int MAX_STRING_TERM_SIZE = Integer.getInteger("cassandra.sai.max_string_term_size_kb", 1) * 1024;
-    public static final int MAX_FROZEN_COLLECTION_TERM_SIZE =Integer.getInteger("cassandra.sai.max_frozen_term_size_kb", 5) * 1024;
+    public static final int MAX_FROZEN_TERM_SIZE = Integer.getInteger("cassandra.sai.max_frozen_term_size_kb", 5) * 1024;
     public static final String TERM_OVERSIZE_MESSAGE =
             "Can't add term of column {} to index for key: {}, term size {} " +
                     "max allowed size {}, use analyzed = true (if not yet set) for that column.";
@@ -84,7 +84,7 @@ public class SSTableIndexWriter implements ColumnIndexWriter
         this.analyzer = columnContext.getAnalyzer();
         this.limiter = limiter;
         this.isIndexValid = isIndexValid;
-        this.maxTermSize = columnContext.isFrozenCollection() ? MAX_FROZEN_COLLECTION_TERM_SIZE : MAX_STRING_TERM_SIZE;
+        this.maxTermSize = columnContext.isFrozen() ? MAX_FROZEN_TERM_SIZE : MAX_STRING_TERM_SIZE;
 
     }
 
