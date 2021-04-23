bdec286a28f2826ae73ec6f66e3b1f4ce1a80bb1
Jacek Lewandowski <6516951+jacek-lewandowski@users.noreply.github.com>
2021-04-23 14:10:05 +0200

    STAR-493: Fix Scrubber (missing bad rows incrementation) (#127)
    (cherry picked from commit 18b2a69f1ab968007784e569b69d9898256b738d)
    (cherry picked from commit cf264ff0c378fbb77526b3b5a3c9e44684f5e765)
    (cherry picked from commit 60d2fbe4dd005009468654c4a276ca41a121d83e)
    (cherry picked from commit 7254d95ee7ef512b459643a06c098cd8c6a5654c)
    (cherry picked from commit d39f025e5e16cf97b6d3ac0e6042d5caa6ccbc24)

--- test/unit/org/apache/cassandra/db/ScrubTest.java
+++ test/unit/org/apache/cassandra/db/ScrubTest.java
@@ -92,6 +92,7 @@ import org.apache.cassandra.tools.StandaloneScrubber;
 import org.apache.cassandra.tools.ToolRunner;
 import org.apache.cassandra.tools.ToolRunner.ToolResult;
 import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.Throwables;
 import org.assertj.core.api.Assertions;
 
 import static org.apache.cassandra.SchemaLoader.counterCFMD;
@@ -207,7 +208,7 @@ public class ScrubTest
         }
         catch (IOError err)
         {
-            assertTrue(err.getCause() instanceof CorruptSSTableException);
+            assertTrue(Throwables.isCausedBy(err, CorruptSSTableException.class));
         }
 
         // with skipCorrupted == true, the corrupt rows will be skipped
