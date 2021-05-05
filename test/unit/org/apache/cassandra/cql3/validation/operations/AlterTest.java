e2af3b8ee8489664368f3d5544cc298881889e7a
Jakub Żytka <jakub.zytka@datastax.com>
2021-05-05 12:16:46 +0200

    STAR-396: Multi-buffer, sharded memtable trie (#131)
    * STAR-396: TrieMemtable sharding; Multi-buffer memtable trie
    
    Co-authored-by: Branimir Lambov <branimir.lambov@datastax.com>
    (cherry picked from commit 62ead9eb0fcf22ed39ad449398b56aed0a23505c)
    (cherry picked from commit 2eb26043abe8b882d9790abe187e9ad7019d21c8)
    (cherry picked from commit 00da732ee5b3507902e0a73a6f3f3ad7021be8b3)

--- test/unit/org/apache/cassandra/cql3/validation/operations/AlterTest.java
+++ test/unit/org/apache/cassandra/cql3/validation/operations/AlterTest.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.cql3.validation.operations;
 import java.util.List;
 import java.util.UUID;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -54,6 +55,15 @@ import static org.junit.Assert.fail;
 @RunWith(OrderedJUnit4ClassRunner.class)
 public class AlterTest extends CQLTester
 {
+    @BeforeClass
+    public static void setUpClass()
+    {
+        // AlterTest uses Murmur3 partitioner, but injects OrderPreservingPartitioner.StringToken
+        // into TokenMetadata; expect trouble
+        System.setProperty(TrieMemtable.SHARD_COUNT_PROPERTY, "1");
+        CQLTester.setUpClass();
+    }
+
     @Test
     public void testDropColumnAsPreparedStatement() throws Throwable
     {
