e97a1ae44714ccec754ffb8f067cc79d20e29314
jacek-lewandowski <jacek.lewandowski@datastax.com>
2021-05-06 13:05:09 +0200

    apply review comments

--- src/java/org/apache/cassandra/db/commitlog/CommitLogReplayer.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLogReplayer.java
@@ -250,7 +250,9 @@ public class CommitLogReplayer implements CommitLogReadHandler
         }
 
         if (!skippedSSTables.isEmpty()) {
-            logger.warn("Origin of {} sstables is unknown or doesn't match the local node; commitLogIntervals for them were ignored", skippedSSTables.size());
+            logger.warn("Origin of {} sstables is unknown or doesn't match the local node; commitLogIntervals for them were ignored. " +
+                        "This is a normal behaviour when there are sstables in older version or they were copied/streamed from other nodes.",
+                        skippedSSTables.size());
             logger.debug("Ignored commitLogIntervals from the following sstables: {}", skippedSSTables);
         }
 
