173993fc095ab99c70d62355561099cbcd3a1f22
dan jatnieks <jatnieks@pobox.com>
2021-06-08 16:56:03 -0700

    STAR-654 Fix permissions race in GuardrailDiskUsageTest.testWriteRequests (#183)
    (cherry picked from commit 019918125b1b5a2ba2eb7abb7c5d0c4f636061f5)
    (cherry picked from commit b708759218177d652a3cdb1535888123c96098dd)
    (cherry picked from commit 4c866c403755f3ae7e8797c09ead5caebac19e19)
    (cherry picked from commit 87dcb02e4ec9ee879d17fdc8efbce95d3c53fe49)

--- test/unit/org/apache/cassandra/guardrails/GuardrailTester.java
+++ test/unit/org/apache/cassandra/guardrails/GuardrailTester.java
@@ -21,6 +21,7 @@ package org.apache.cassandra.guardrails;
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.TimeUnit;
 import java.util.function.BiConsumer;
 import javax.annotation.Nullable;
 
@@ -30,6 +31,7 @@ import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 
+import com.datastax.driver.core.SimpleStatement;
 import com.datastax.driver.core.Statement;
 import com.datastax.driver.core.exceptions.InvalidQueryException;
 import org.apache.cassandra.auth.AuthenticatedUser;
@@ -39,6 +41,7 @@ import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.service.ClientState;
 import org.apache.cassandra.service.QueryState;
 import org.apache.cassandra.service.StorageService;
+import org.awaitility.Awaitility;
 
 import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
@@ -81,6 +84,13 @@ public abstract class GuardrailTester extends CQLTester
         useSuperUser();
         executeNet(format("CREATE USER IF NOT EXISTS %s WITH PASSWORD '%s'", USERNAME, PASSWORD));
         executeNet(format("GRANT ALL ON KEYSPACE %s TO %s", KEYSPACE, USERNAME));
+
+        // Make sure keyspace permissions have been applied
+        Awaitility.await()
+                  .atMost(10, TimeUnit.SECONDS)
+                  .with()
+                  .pollInterval(500, TimeUnit.MILLISECONDS)
+                  .until(() -> !executeNet(new SimpleStatement("LIST ALL OF " + USERNAME)).all().isEmpty());
 
         useUser(USERNAME, PASSWORD);
 
