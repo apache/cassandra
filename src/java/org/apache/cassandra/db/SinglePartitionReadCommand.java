ef6e4763370a8354369fb2235d0e93cae2307683
Benjamin Lerer <b.lerer@gmail.com>
2021-07-01 10:59:42 +0200

    Fix review comments

--- src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
+++ src/java/org/apache/cassandra/db/SinglePartitionReadCommand.java
@@ -578,9 +578,13 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
          *   2) If we have a name filter (so we query specific rows), we can make a bet: that all column for all queried row
          *      will have data in the most recent sstable(s), thus saving us from reading older ones. This does imply we
          *      have a way to guarantee we have all the data for what is queried, which is only possible for name queries
-         *      and if we have neither non-frozen collections/UDTs nor counters (indeed, for a non-frozen collection or UDT,
-         *      we can't guarantee an older sstable won't have some elements that weren't in the most recent sstables,
-         *      and counters are intrinsically a collection of shards and so have the same problem).
+         *      and if we have neither non-frozen collections/UDTs nor counters.
+         *      If a non-frozen collection or UDT is queried we can't guarantee that an older sstable won't have some
+         *      elements that weren't in the most recent sstables.
+         *      Counters are intrinsically a collection of shards and so have the same problem.
+         *      Counter tables are also special in the sense that their rows do not have primary key liveness
+         *      as INSERT statements are not supported on counter tables. Due to that even if only the primary key
+         *      columns where queried, querying SSTables in timestamp order will always be less efficient for counter tables.
          *      Also, if tracking repaired data then we skip this optimization so we can collate the repaired sstables
          *      and generate a digest over their merge, which procludes an early return.
          */
@@ -936,11 +940,11 @@ public class SinglePartitionReadCommand extends ReadCommand implements SinglePar
             return filter;
 
         // According to the CQL semantics a row exists if at least one of its columns is not null (including the primary key columns).
-        // Having the queried columns not null is unfortunately not enough to prove that a row exists has some column deletion
+        // Having the queried columns not null is unfortunately not enough to prove that a row exists as some column deletion
         // for the queried columns can exist on another node.
         // For CQL tables it is enough to have the primary key liveness and the queried columns as the primary key liveness prove that
         // the row exists even if all the other columns are deleted.
-        // COMPACT tables do not have primary key liveness by consequence we are forced to get  all the fetched columns to ensure that
+        // COMPACT tables do not have primary key liveness and by consequence we are forced to get  all the fetched columns to ensure that
         // we can return the correct result if the queried columns are deleted on another node but one of the non-queried columns is not.
         RegularAndStaticColumns columns = metadata().isCompactTable() ? columnFilter().fetchedColumns() : columnFilter().queriedColumns();
 
