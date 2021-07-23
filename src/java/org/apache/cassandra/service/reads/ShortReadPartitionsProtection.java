6e9c8b753ed550b0284779bf6c924ff621c8add9
Mike Adamson <madamson@datastax.com>
2021-07-23 13:05:19 +0100

    STAR-815: MultiRangeReadCommand always used by SAI and remove info log in query path (#222)
    (cherry picked from commit d40f9e30625e5c0a74bf3f519ff37fc3e97648f3)
    (cherry picked from commit 41b5b8f6d9bd87bead7deff1d31c2f691b4912d2)
    (cherry picked from commit d83a0ae3a13d782fa20c04a2702e11bd6a566ca9)
    (cherry picked from commit 9338023fae77910260d6579b5cb5f22ecbb9f621)
    (cherry picked from commit 9f8ad3687abd7348ba6e0fe794bbf2c638f5ccb1)
    (cherry picked from commit 6fe00460393379059cfc1dca4463834eac027471)

--- src/java/org/apache/cassandra/service/reads/ShortReadPartitionsProtection.java
+++ src/java/org/apache/cassandra/service/reads/ShortReadPartitionsProtection.java
@@ -18,6 +18,8 @@
 
 package org.apache.cassandra.service.reads;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.cassandra.locator.Endpoints;
 import org.apache.cassandra.locator.ReplicaPlan;
 import org.apache.cassandra.locator.ReplicaPlans;
@@ -46,10 +48,13 @@ import org.apache.cassandra.net.MessagingService;
 import org.apache.cassandra.service.reads.repair.NoopReadRepair;
 import org.apache.cassandra.service.StorageProxy;
 import org.apache.cassandra.tracing.Tracing;
+import org.apache.cassandra.utils.NoSpamLogger;
 
 public class ShortReadPartitionsProtection extends Transformation<UnfilteredRowIterator> implements MorePartitions<UnfilteredPartitionIterator>
 {
     private static final Logger logger = LoggerFactory.getLogger(ShortReadPartitionsProtection.class);
+    private static final NoSpamLogger oneMinuteLogger = NoSpamLogger.getLogger(logger, 1, TimeUnit.MINUTES);
+
     private final ReadCommand command;
     private final Replica source;
 
@@ -84,6 +89,7 @@ public class ShortReadPartitionsProtection extends Transformation<UnfilteredRowI
     public UnfilteredRowIterator applyToPartition(UnfilteredRowIterator partition)
     {
         partitionsFetched = true;
+        rangeFetched = true;
 
         lastPartitionKey = partition.partitionKey();
 
@@ -152,7 +158,9 @@ public class ShortReadPartitionsProtection extends Transformation<UnfilteredRowI
 
         ColumnFamilyStore.metricsFor(command.metadata().id).shortReadProtectionRequests.mark();
         Tracing.trace("Requesting {} extra rows from {} for short read protection", toQuery, source);
-        logger.info("Requesting {} extra rows from {} for short read protection", toQuery, source);
+        // This is a NoSpamLogger because, in the event of unrepaired data or missing data on nodes in
+        // a cluster, we can end up spamming the logs with this message
+        oneMinuteLogger.info("Requesting {} extra rows from {} for short read protection", toQuery, source);
 
         // If we've arrived here, all responses have been consumed, and we're about to request more.
         preFetchCallback.run();
