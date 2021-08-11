a30c6df8ba4a24cb264cfa00b5e6feeed2bf8122
David Capwell <dcapwell@apache.org>
2021-08-11 11:22:39 -0700

    CASSANDRA-16845 - ArrayClustering.unsharedHeapSize does not include the data so undercounts the heap size

--- src/java/org/apache/cassandra/db/ArrayClustering.java
+++ src/java/org/apache/cassandra/db/ArrayClustering.java
@@ -31,7 +31,11 @@ public class ArrayClustering extends AbstractArrayClusteringPrefix implements Cl
 
     public long unsharedHeapSize()
     {
-        return EMPTY_SIZE + ObjectSizes.sizeOfArray(values) + values.length;
+        long arrayRefSize = ObjectSizes.sizeOfArray(values);
+        long elementsSize = 0;
+        for (int i = 0; i < values.length; i++)
+            elementsSize += ObjectSizes.sizeOfArray(values[i]);
+        return EMPTY_SIZE + arrayRefSize + elementsSize;
     }
 
     public long unsharedHeapSizeExcludingData()
