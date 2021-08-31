17e2e16d3229d86fd2302056be79fc6da02beb6f
David Capwell <dcapwell@apache.org>
2021-08-31 12:33:51 -0700

    extra try

--- src/java/org/apache/cassandra/db/ReadCommandVerbHandler.java
+++ src/java/org/apache/cassandra/db/ReadCommandVerbHandler.java
@@ -57,13 +57,10 @@ public class ReadCommandVerbHandler implements IVerbHandler<ReadCommand>
             command.trackWarnings();
 
         ReadResponse response;
-        try
+        try (ReadExecutionController controller = command.executionController(message.trackRepairedData());
+             UnfilteredPartitionIterator iterator = command.executeLocally(controller))
         {
-            try (ReadExecutionController controller = command.executionController(message.trackRepairedData());
-                 UnfilteredPartitionIterator iterator = command.executeLocally(controller))
-            {
-                response = command.createResponse(iterator, controller.getRepairedDataInfo());
-            }
+            response = command.createResponse(iterator, controller.getRepairedDataInfo());
         }
         catch (RejectException e)
         {
