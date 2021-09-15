aae3cc53f994ccd6edf171c7be24c383110d4015
Anthony Grasso <anthony.grasso@gmail.com>
2021-09-15 18:18:26 +1000

    CASSANDRA-16763: Update doc tooling and generated files
    * Fixed AsciiDoc link errors
    * Deleted Docker files which ran Sphinx
    * Deleted Make files
    * Deleted Python scripts which generated RST files as they have been replaced with scripts in the 'scripts' directory that generate ADOC files
    * Removed configuration documentation file
        - doc/modules/cassandra/pages/configuration/cass_yaml_file.adoc
    * Removed all nodetool command (.txt) and documentation (*.adoc) files
        - doc/modules/cassandra/examples/TEXT/NODETOOL/*.txt
        - doc/modules/cassandra/pages/tools/nodetool/*.adoc
        - doc/modules/cassandra/pages/tools/nodetool/nodetool.txt
    * Removed legacy .rst -> .adoc conversion script
        - doc/modules/cassandra/pages/tools/nodetool/convert.sh
    
    Patch by Anthony Grasso; reviewed by Michael Semb Wever for CASSANDRA-16763
    
    Co-authored-by: polandll <lorina@datastax.com>

--- doc/modules/cassandra/pages/tools/nodetool/convert.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-for f in ~/CLONES/cassandra-examples/rst-to-asciidoc-tests/ASCIIDOC/modules/cassandra/pages/tools/nodetool/*.rst
-do
-    filename=$(basename "$f")
-    filename="${filename%.*}"
-    echo "Processing $filename"
-    pandoc "$f" -f rst -t asciidoc -s -o "$filename.adoc"
-done
