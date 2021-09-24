b1e77baa3d7cef7b417fa787e3005a999a440418
Jay Zhuang <jzhuang@apache.org>
2021-09-24 15:20:35 -0500

    Avoid token cache invalidation for removing a non-member node
    Patch by Jay Zhuang; reviewed by brandonwilliams and azotcsit for
    CASSANDRA-15290

--- src/java/org/apache/cassandra/locator/TokenMetadata.java
+++ src/java/org/apache/cassandra/locator/TokenMetadata.java
@@ -496,7 +496,7 @@ public class TokenMetadata
         try
         {
             bootstrapTokens.removeValue(endpoint);
-            tokenToEndpointMap.removeValue(endpoint);
+
             topology = topology.unbuild().removeEndpoint(endpoint).build();
             leavingEndpoints.remove(endpoint);
             if (replacementToOriginal.remove(endpoint) != null)
@@ -504,8 +504,12 @@ public class TokenMetadata
                 logger.debug("Node {} failed during replace.", endpoint);
             }
             endpointToHostIdMap.remove(endpoint);
-            sortedTokens = sortTokens();
-            invalidateCachedRingsUnsafe();
+            Collection<Token> removedTokens = tokenToEndpointMap.removeValue(endpoint);
+            if (removedTokens != null && !removedTokens.isEmpty())
+            {
+                sortedTokens = sortTokens();
+                invalidateCachedRingsUnsafe();
+            }
         }
         finally
         {
