89efa67632d5bc5dd7b3295e062f256c842a0d51
Caleb Rackliffe <calebrackliffe@gmail.com>
2021-10-07 13:34:26 -0500

    Skip remaining bytes in the Envelope buffer when a ProtocolException is thrown to avoid double decoding
    patch by Caleb Rackliffe; reviewed by Sam Tunnicliffe and Jon Meredith for CASSANDRA-17026

--- src/java/org/apache/cassandra/transport/Envelope.java
+++ src/java/org/apache/cassandra/transport/Envelope.java
@@ -353,7 +353,19 @@ public class Envelope
             int firstByte = buffer.getByte(idx++);
             Message.Direction direction = Message.Direction.extractFromVersion(firstByte);
             int versionNum = firstByte & PROTOCOL_VERSION_MASK;
-            ProtocolVersion version = ProtocolVersion.decode(versionNum, DatabaseDescriptor.getNativeTransportAllowOlderProtocols());
+
+            ProtocolVersion version;
+            
+            try
+            {
+                version = ProtocolVersion.decode(versionNum, DatabaseDescriptor.getNativeTransportAllowOlderProtocols());
+            }
+            catch (ProtocolException e)
+            {
+                // Skip the remaining useless bytes. Otherwise the channel closing logic may try to decode again. 
+                buffer.skipBytes(readableBytes);
+                throw e;
+            }
 
             // Wait until we have the complete header
             if (readableBytes < Header.LENGTH)
