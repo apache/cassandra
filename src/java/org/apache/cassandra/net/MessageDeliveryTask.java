d24cf70253e4564126e5742135e9d8009a891824
Aleksandr Sorokoumov <aleksandr.sorokoumov@gmail.com>
2021-10-26 18:14:35 +0100

    Fix failure handling in inter-node communication
    patch by Aleksandr Sorokoumov; reviewed by Andrés de la Peña and Paulo Motta for CASSANDRA-16334

--- src/java/org/apache/cassandra/net/MessageDeliveryTask.java
+++ src/java/org/apache/cassandra/net/MessageDeliveryTask.java
@@ -18,11 +18,14 @@
 package org.apache.cassandra.net;
 
 import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.EnumSet;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.filter.TombstoneOverwhelmingException;
 import org.apache.cassandra.gms.Gossiper;
 import org.apache.cassandra.index.IndexNotAvailableException;
@@ -31,12 +34,12 @@ public class MessageDeliveryTask implements Runnable
 {
     private static final Logger logger = LoggerFactory.getLogger(MessageDeliveryTask.class);
 
-    private final MessageIn message;
+    private final MessageIn<?> message;
     private final int id;
     private final long constructionTime;
     private final boolean isCrossNodeTimestamp;
 
-    public MessageDeliveryTask(MessageIn message, int id, long timestamp, boolean isCrossNodeTimestamp)
+    public MessageDeliveryTask(MessageIn<?> message, int id, long timestamp, boolean isCrossNodeTimestamp)
     {
         assert message != null;
         this.message = message;
@@ -90,9 +93,20 @@ public class MessageDeliveryTask implements Runnable
     {
         if (message.doCallbackOnFailure())
         {
-            MessageOut response = new MessageOut(MessagingService.Verb.INTERNAL_RESPONSE)
+            MessageOut<?> response = new MessageOut<>(MessagingService.Verb.INTERNAL_RESPONSE)
                                                 .withParameter(MessagingService.FAILURE_RESPONSE_PARAM, MessagingService.ONE_BYTE);
-            MessagingService.instance().sendReply(response, id, message.from);
+
+            InetAddress from;
+            byte[] fromBytes = message.parameters.get(Mutation.FORWARD_FROM);
+            try
+            {
+                from = fromBytes != null ? InetAddress.getByAddress(fromBytes) : message.from;
+            }
+            catch (UnknownHostException e)
+            {
+                throw new RuntimeException(e);
+            }
+            MessagingService.instance().sendReply(response, id, from);
         }
     }
 
--- src/java/org/apache/cassandra/service/StorageProxy.java
+++ src/java/org/apache/cassandra/service/StorageProxy.java
@@ -1445,7 +1445,7 @@ public class StorageProxy implements StorageProxyMBean
                 catch (Exception ex)
                 {
                     if (!(ex instanceof WriteTimeoutException))
-                        logger.error("Failed to apply mutation locally : {}", ex);
+                        logger.error("Failed to apply mutation locally : ", ex);
                     handler.onFailure(FBUtilities.getBroadcastAddress());
                 }
             }
