d658b08b72a6bd8f8c2925ff3c0031f66da68865
Bereng <berenguerblasi@gmail.com>
2021-10-29 08:26:34 +0200

    ViewComplexTest hardening
    patch by Andres de la Peña, Berenguer Blasi; reviewed by Andres de la Peña for CASSANDRA-17070
    
    Co-authored-by: Andres de la Peña <a.penya.garcia@gmail.com>
    Co-authored-by: Berenguer Blasi <berenguerblasi@gmail.com>

--- test/unit/org/apache/cassandra/cql3/ViewComplexLivenessTest.java
+++ test/unit/org/apache/cassandra/cql3/ViewComplexLivenessTest.java
@@ -18,26 +18,14 @@
 
 package org.apache.cassandra.cql3;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
 
-import org.junit.After;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.Keyspace;
-import org.apache.cassandra.transport.ProtocolVersion;
 import org.apache.cassandra.utils.FBUtilities;
 
-import static org.apache.cassandra.cql3.ViewComplexTest.createView;
-import static org.apache.cassandra.cql3.ViewComplexTest.updateView;
-import static org.apache.cassandra.cql3.ViewComplexTest.updateViewWithFlush;
 import static org.junit.Assert.assertEquals;
 
 /* ViewComplexTest class has been split into multiple ones because of timeout issues (CASSANDRA-16670)
@@ -48,38 +36,8 @@ import static org.junit.Assert.assertEquals;
  * - ViewComplexTest
  * - ViewComplexLivenessTest
  */
-@RunWith(Parameterized.class)
-public class ViewComplexLivenessTest extends CQLTester
+public class ViewComplexLivenessTest extends ViewComplexTester
 {
-    @Parameterized.Parameter
-    public ProtocolVersion version;
-
-    @Parameterized.Parameters()
-    public static Collection<Object[]> versions()
-    {
-        return ViewComplexTest.versions();
-    }
-
-    private final List<String> views = new ArrayList<>();
-
-    @BeforeClass
-    public static void startup()
-    {
-        ViewComplexTest.startup();
-    }
-
-    @Before
-    public void begin()
-    {
-        ViewComplexTest.beginSetup(views);
-    }
-
-    @After
-    public void end() throws Throwable
-    {
-        ViewComplexTest.endTearDown(views, version, this);
-    }
-
     @Test
     public void testUnselectedColumnWithExpiredLivenessInfo() throws Throwable
     {
@@ -90,48 +48,45 @@ public class ViewComplexLivenessTest extends CQLTester
         executeNet(version, "USE " + keyspace());
         Keyspace ks = Keyspace.open(keyspace());
 
-        createView("mv",
-                   "create materialized view %s as select k,c,b from %%s where c is not null and k is not null primary key (c, k);",
-                   version,
-                   this,
-                   views);
-        ks.getColumnFamilyStore("mv").disableAutoCompaction();
+        String name = createView("create materialized view %s as select k,c,b from %%s " +
+                                 "where c is not null and k is not null primary key (c, k)");
+        ks.getColumnFamilyStore(name).disableAutoCompaction();
 
         // sstable-1, Set initial values TS=1
-        updateViewWithFlush("UPDATE %s SET a = 1 WHERE k = 1 AND c = 1;", flush, version, this);
+        updateViewWithFlush("UPDATE %s SET a = 1 WHERE k = 1 AND c = 1;", flush);
 
         assertRowsIgnoringOrder(execute("SELECT * from %s WHERE k = 1 AND c = 1;"),
                                 row(1, 1, 1, null));
-        assertRowsIgnoringOrder(execute("SELECT k,c,b from mv WHERE k = 1 AND c = 1;"),
+        assertRowsIgnoringOrder(execute("SELECT k,c,b from " + name + " WHERE k = 1 AND c = 1;"),
                                 row(1, 1, null));
 
         // sstable-2
-        updateViewWithFlush("INSERT INTO %s(k,c) VALUES(1,1) USING TTL 5", flush, version, this);
+        updateViewWithFlush("INSERT INTO %s(k,c) VALUES(1,1) USING TTL 5", flush);
 
         assertRowsIgnoringOrder(execute("SELECT * from %s WHERE k = 1 AND c = 1;"),
                                 row(1, 1, 1, null));
-        assertRowsIgnoringOrder(execute("SELECT k,c,b from mv WHERE k = 1 AND c = 1;"),
+        assertRowsIgnoringOrder(execute("SELECT k,c,b from " + name + " WHERE k = 1 AND c = 1;"),
                                 row(1, 1, null));
 
         Thread.sleep(5001);
 
         assertRowsIgnoringOrder(execute("SELECT * from %s WHERE k = 1 AND c = 1;"),
                                 row(1, 1, 1, null));
-        assertRowsIgnoringOrder(execute("SELECT k,c,b from mv WHERE k = 1 AND c = 1;"),
+        assertRowsIgnoringOrder(execute("SELECT k,c,b from " + name + " WHERE k = 1 AND c = 1;"),
                                 row(1, 1, null));
 
         // sstable-3
-        updateViewWithFlush("Update %s set a = null where k = 1 AND c = 1;", flush, version, this);
+        updateViewWithFlush("Update %s set a = null where k = 1 AND c = 1;", flush);
 
         assertRowsIgnoringOrder(execute("SELECT * from %s WHERE k = 1 AND c = 1;"));
-        assertRowsIgnoringOrder(execute("SELECT k,c,b from mv WHERE k = 1 AND c = 1;"));
+        assertRowsIgnoringOrder(execute("SELECT k,c,b from " + name + " WHERE k = 1 AND c = 1;"));
 
         // sstable-4
-        updateViewWithFlush("Update %s USING TIMESTAMP 1 set b = 1 where k = 1 AND c = 1;", flush, version, this);
+        updateViewWithFlush("Update %s USING TIMESTAMP 1 set b = 1 where k = 1 AND c = 1;", flush);
 
         assertRowsIgnoringOrder(execute("SELECT * from %s WHERE k = 1 AND c = 1;"),
                                 row(1, 1, null, 1));
-        assertRowsIgnoringOrder(execute("SELECT k,c,b from mv WHERE k = 1 AND c = 1;"),
+        assertRowsIgnoringOrder(execute("SELECT k,c,b from " + name + " WHERE k = 1 AND c = 1;"),
                                 row(1, 1, 1));
     }
 
@@ -157,35 +112,29 @@ public class ViewComplexLivenessTest extends CQLTester
         executeNet(version, "USE " + keyspace());
         Keyspace ks = Keyspace.open(keyspace());
 
-        createView("mv1",
-                   "CREATE MATERIALIZED VIEW %s AS SELECT * FROM %%s WHERE k IS NOT NULL AND a IS NOT NULL PRIMARY KEY (k, a);",
-                   version,
-                   this,
-                   views);
-        createView("mv2",
-                   "CREATE MATERIALIZED VIEW %s AS SELECT * FROM %%s WHERE k IS NOT NULL AND a IS NOT NULL PRIMARY KEY (a, k);",
-                   version,
-                   this,
-                   views);
-        ks.getColumnFamilyStore("mv1").disableAutoCompaction();
-        ks.getColumnFamilyStore("mv2").disableAutoCompaction();
+        String mv1 = createView("CREATE MATERIALIZED VIEW %s AS SELECT * FROM %%s " +
+                                "WHERE k IS NOT NULL AND a IS NOT NULL PRIMARY KEY (k, a)");
+        String mv2 = createView("CREATE MATERIALIZED VIEW %s AS SELECT * FROM %%s " +
+                                "WHERE k IS NOT NULL AND a IS NOT NULL PRIMARY KEY (a, k)");
+        ks.getColumnFamilyStore(mv1).disableAutoCompaction();
+        ks.getColumnFamilyStore(mv2).disableAutoCompaction();
 
         for (int i = 1; i <= 100; i++)
-            updateView("INSERT INTO %s(k, a, b) VALUES (?, ?, ?);", version, this, i, i, i);
+            updateView("INSERT INTO %s(k, a, b) VALUES (?, ?, ?);", i, i, i);
         for (int i = 1; i <= 100; i++)
         {
             if (i % 50 == 0)
                 continue;
             // create expired liveness
-            updateView("DELETE a FROM %s WHERE k = ?;", version, this, i);
+            updateView("DELETE a FROM %s WHERE k = ?;", i);
         }
         if (flush)
         {
-            ks.getColumnFamilyStore("mv1").forceBlockingFlush();
-            ks.getColumnFamilyStore("mv2").forceBlockingFlush();
+            ks.getColumnFamilyStore(mv1).forceBlockingFlush();
+            ks.getColumnFamilyStore(mv2).forceBlockingFlush();
         }
 
-        for (String view : Arrays.asList("mv1", "mv2"))
+        for (String view : Arrays.asList(mv1, mv2))
         {
             // paging
             assertEquals(1, executeNetWithPaging(version, String.format("SELECT k,a,b FROM %s limit 1", view), 1).all().size());
@@ -211,21 +160,18 @@ public class ViewComplexLivenessTest extends CQLTester
         executeNet(version, "USE " + keyspace());
         Keyspace ks = Keyspace.open(keyspace());
 
-        createView("mv",
-                   "create materialized view %s as select * from %%s where p is not null and v1 is not null primary key (v1, p)"
-                           + " with gc_grace_seconds=5;",
-                   version,
-                   this,
-                   views);
-        ColumnFamilyStore cfs = ks.getColumnFamilyStore("mv");
+        String name = createView("create materialized view %s as select * from %%s " +
+                                 "where p is not null and v1 is not null primary key (v1, p) " +
+                                 "with gc_grace_seconds=5");
+        ColumnFamilyStore cfs = ks.getColumnFamilyStore(name);
         cfs.disableAutoCompaction();
 
-        updateView("Insert into %s (p, v1, v2) values (1, 1, 1) ;", version, this);
-        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from mv"), row(1, 1, 1));
+        updateView("Insert into %s (p, v1, v2) values (1, 1, 1)");
+        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from " + name), row(1, 1, 1));
 
-        updateView("Update %s set v1 = null WHERE p = 1", version, this);
+        updateView("Update %s set v1 = null WHERE p = 1");
         FBUtilities.waitOnFutures(ks.flush());
-        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from mv"));
+        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from " + name));
 
         cfs.forceMajorCompaction(); // before gc grace second, strict-liveness tombstoned dead row remains
         assertEquals(1, cfs.getLiveSSTables().size());
@@ -236,18 +182,18 @@ public class ViewComplexLivenessTest extends CQLTester
         cfs.forceMajorCompaction(); // after gc grace second, no data left
         assertEquals(0, cfs.getLiveSSTables().size());
 
-        updateView("Update %s using ttl 5 set v1 = 1 WHERE p = 1", version, this);
+        updateView("Update %s using ttl 5 set v1 = 1 WHERE p = 1");
         FBUtilities.waitOnFutures(ks.flush());
-        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from mv"), row(1, 1, 1));
+        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from " + name), row(1, 1, 1));
 
         cfs.forceMajorCompaction(); // before ttl+gc_grace_second, strict-liveness ttled dead row remains
         assertEquals(1, cfs.getLiveSSTables().size());
-        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from mv"), row(1, 1, 1));
+        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from " + name), row(1, 1, 1));
 
         Thread.sleep(5500); // after expired, before gc_grace_second
         cfs.forceMajorCompaction();// before ttl+gc_grace_second, strict-liveness ttled dead row remains
         assertEquals(1, cfs.getLiveSSTables().size());
-        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from mv"));
+        assertRowsIgnoringOrder(execute("SELECT p, v1, v2 from " + name));
 
         Thread.sleep(5500); // after expired + gc_grace_second
         assertEquals(1, cfs.getLiveSSTables().size()); // no auto compaction.
