79b85fc634cdf92ee8d79dc19b3364dfe5f5f90c
Jakub Zytka <jakub.zytka@datastax.com>
2021-12-14 16:39:40 +0100

    STAR-979: add cndd-related OperationTypes
    removed unused OperationType.DOWNGRADE_SSTABLES;
    changed OperationType.UNKNOWN to relevant ones
    reference cndb-only code in a test
    
    (cherry picked from commit 5b3b106257f2884ce457a1a3b7641af4a35270b7)
    (cherry picked from commit 343e1da916288130225929f869b1f399cfedad37)
    (cherry picked from commit 855ad55ae7c658536e5a7eee793ff9e90abd55ae)

--- src/java/org/apache/cassandra/db/lifecycle/Tracker.java
+++ src/java/org/apache/cassandra/db/lifecycle/Tracker.java
@@ -274,7 +274,7 @@ public class Tracker
 
     public Throwable dropSSTables(Throwable accumulate)
     {
-        return dropSSTables(Predicates.alwaysTrue(), OperationType.UNKNOWN, accumulate);
+        return dropSSTables(Predicates.alwaysTrue(), OperationType.DROP_TABLE, accumulate);
     }
 
     /**
@@ -341,7 +341,7 @@ public class Tracker
      */
     public void removeUnreadableSSTables(final File directory)
     {
-        maybeFail(dropSSTables(reader -> reader.descriptor.directory.equals(directory), OperationType.UNKNOWN, null));
+        maybeFail(dropSSTables(reader -> reader.descriptor.directory.equals(directory), OperationType.REMOVE_UNREADEABLE, null));
     }
 
 
