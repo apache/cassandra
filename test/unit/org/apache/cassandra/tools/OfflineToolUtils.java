057e1166b7b4d24e0b50671351b4ecbbda942b2f
Bereng <berenguerblasi@gmail.com>
2022-01-18 07:45:27 +0100

    Sstableverify unit test operate on SSTables
    patch by Brian Houser; reviewed by Andres de la Peña, Berenguer Blasi for CASSANDRA-17009
    
    Co-authored-by: Brian Houser <brianh@amazon.com>
    Co-authored-by: Berenguer Blasi <berenguerblasi@gmail.com>

--- test/unit/org/apache/cassandra/tools/OfflineToolUtils.java
+++ test/unit/org/apache/cassandra/tools/OfflineToolUtils.java
@@ -33,10 +33,7 @@ import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
 import org.apache.commons.io.FileUtils;
-
-import org.junit.AfterClass;
 import org.junit.BeforeClass;
-
 import org.slf4j.LoggerFactory;
 
 import static org.apache.cassandra.utils.FBUtilities.preventIllegalAccessWarnings;
