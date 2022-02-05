803e3add6eff2556dc573bc062740795210df12c
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2022-02-05 17:51:32 -0500

    Remove old Duration class in favor of DurationSpec class patch by Ekaterina Dimitrova; reviewed by Caleb Rackliffe, David Capwell, Michael Semb Wever and Benjamin Lerer for CASSANDRA-15234

--- src/java/org/apache/cassandra/tools/nodetool/Snapshot.java
+++ src/java/org/apache/cassandra/tools/nodetool/Snapshot.java
@@ -31,9 +31,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.cassandra.config.DurationSpec;
 import org.apache.cassandra.tools.NodeProbe;
 import org.apache.cassandra.tools.NodeTool.NodeToolCmd;
-import org.apache.cassandra.config.Duration;
 
 @Command(name = "snapshot", description = "Take a snapshot of specified keyspaces or a snapshot of the specified table")
 public class Snapshot extends NodeToolCmd
@@ -69,7 +69,7 @@ public class Snapshot extends NodeToolCmd
             Map<String, String> options = new HashMap<String,String>();
             options.put("skipFlush", Boolean.toString(skipFlush));
             if (null != ttl) {
-                Duration d = new Duration(ttl);
+                DurationSpec d = new DurationSpec(ttl);
                 options.put("ttl", d.toString());
             }
 
