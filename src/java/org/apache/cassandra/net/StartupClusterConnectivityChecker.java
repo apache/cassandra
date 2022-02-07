d8a23ceafe5d323f6051e7f8a21b595b93032273
Francisco Guerrero <frank.guerrero@gmail.com>
2022-02-07 10:11:22 -0800

    Remove unused import

--- src/java/org/apache/cassandra/net/StartupClusterConnectivityChecker.java
+++ src/java/org/apache/cassandra/net/StartupClusterConnectivityChecker.java
@@ -23,7 +23,6 @@ import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import org.apache.cassandra.utils.concurrent.CountDownLatch;
