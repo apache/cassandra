77c6cb1993c427c7f13fa0247a3f8535b61410bc
Jakub Żytka <jakub.zytka@datastax.com>
2022-02-14 20:52:54 +0100

    STAR-1132: dont fail listing on OBSOLETE files (#342)
    (cherry picked from commit dcb0231b72c1c5150ccf1d87f73dfa8dc53fb7fc)
    (cherry picked from commit a4668d9afd000268f9cf35bee442d5d0a12a713d)

--- src/java/org/apache/cassandra/db/Directories.java
+++ src/java/org/apache/cassandra/db/Directories.java
@@ -937,6 +937,7 @@ public class Directories
                 switch (type)
                 {
                     case TXN_LOG:
+                    case OBSOLETE:
                         return false;
                     case TEMPORARY:
                         if (skipTemporary)
