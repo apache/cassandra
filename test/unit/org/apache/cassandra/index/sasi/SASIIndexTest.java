7a4d479b09b0c71c7720c3b7377f251376ee0a31
Paulo Motta <paulo@apache.org>
2022-02-21 19:24:46 -0300

    Close read execution controller to prevent deadlocks

--- test/unit/org/apache/cassandra/index/sasi/SASIIndexTest.java
+++ test/unit/org/apache/cassandra/index/sasi/SASIIndexTest.java
@@ -2639,12 +2639,16 @@ public class SASIIndexTest
 
     private static Set<String> getIndexed(ColumnFamilyStore store, ColumnFilter columnFilter, int maxResults, Expression... expressions)
     {
-        return getKeys(getIndexed(store, columnFilter, null, maxResults, expressions));
+        ReadCommand command = getIndexReadCommand(store, columnFilter, null, maxResults, expressions);
+        try (ReadExecutionController controller = command.executionController();
+             UnfilteredPartitionIterator rows = command.executeLocally(controller))
+        {
+            return getKeys(rows);
+        }
     }
 
     private static Set<DecoratedKey> getPaged(ColumnFamilyStore store, int pageSize, Expression... expressions)
     {
-        UnfilteredPartitionIterator currentPage;
         Set<DecoratedKey> uniqueKeys = new TreeSet<>();
 
         DecoratedKey lastKey = null;
@@ -2653,28 +2657,32 @@ public class SASIIndexTest
         do
         {
             count = 0;
-            currentPage = getIndexed(store, ColumnFilter.all(store.metadata), lastKey, pageSize, expressions);
-            if (currentPage == null)
-                break;
+            ReadCommand command = getIndexReadCommand(store, ColumnFilter.all(store.metadata), lastKey, pageSize, expressions);
 
-            while (currentPage.hasNext())
+            try (ReadExecutionController controller = command.executionController();
+                 UnfilteredPartitionIterator currentPage = command.executeLocally(controller))
             {
-                try (UnfilteredRowIterator row = currentPage.next())
+                if (currentPage == null)
+                    break;
+
+                while (currentPage.hasNext())
                 {
-                    uniqueKeys.add(row.partitionKey());
-                    lastKey = row.partitionKey();
-                    count++;
+                    try (UnfilteredRowIterator row = currentPage.next())
+                    {
+                        uniqueKeys.add(row.partitionKey());
+                        lastKey = row.partitionKey();
+                        count++;
+                    }
                 }
             }
 
-            currentPage.close();
         }
         while (count == pageSize);
 
         return uniqueKeys;
     }
 
-    private static UnfilteredPartitionIterator getIndexed(ColumnFamilyStore store, ColumnFilter columnFilter, DecoratedKey startKey, int maxResults, Expression... expressions)
+    private static ReadCommand getIndexReadCommand(ColumnFamilyStore store, ColumnFilter columnFilter, DecoratedKey startKey, int maxResults, Expression[] expressions)
     {
         DataRange range = (startKey == null)
                             ? DataRange.allData(PARTITIONER)
@@ -2685,15 +2693,13 @@ public class SASIIndexTest
             filter.add(store.metadata.getColumnDefinition(e.name), e.op, e.value);
 
         ReadCommand command =
-            PartitionRangeReadCommand.create(false,
-                                             store.metadata,
+            PartitionRangeReadCommand.create(false, store.metadata,
                                              FBUtilities.nowInSeconds(),
                                              columnFilter,
                                              filter,
-                                             DataLimits.thriftLimits(maxResults, DataLimits.NO_LIMIT),
+                                             DataLimits.cqlLimits(maxResults),
                                              range);
-
-        return command.executeLocally(command.executionController());
+        return command;
     }
 
     private static Mutation newMutation(String key, String firstName, String lastName, int age, long timestamp)
