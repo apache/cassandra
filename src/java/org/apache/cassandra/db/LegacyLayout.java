dd51df6734d9b8d8dca9c2a22659e74f674060e2
Marten Kenbeek <marten@blueconic.com>
2022-03-09 16:29:56 +0100

    Only use statically defined subcolumns when determining column definition for supercolumn cell
    Patch by Marten Kenbeek; review by Benjamin Lerer and Brandon Williams for CASSANDRA-14113

--- src/java/org/apache/cassandra/db/LegacyLayout.java
+++ src/java/org/apache/cassandra/db/LegacyLayout.java
@@ -143,16 +143,27 @@ public abstract class LegacyLayout
 
     private static LegacyCellName decodeForSuperColumn(CFMetaData metadata, Clustering clustering, ByteBuffer subcol)
     {
-        ColumnDefinition def = metadata.getColumnDefinition(subcol);
+        ColumnDefinition def = getStaticallyDefinedSubColumn(metadata, subcol);
         if (def != null)
         {
-            // it's a statically defined subcolumn
+            // it's a statically defined subcolumn from column_metadata
             return new LegacyCellName(clustering, def, null);
         }
 
         def = metadata.compactValueColumn();
         assert def != null && def.type instanceof MapType;
         return new LegacyCellName(clustering, def, subcol);
+    }
+
+    private static ColumnDefinition getStaticallyDefinedSubColumn(CFMetaData metadata, ByteBuffer subcol) {
+        if (metadata.isDense())
+            return null;
+
+        ColumnDefinition def = metadata.getColumnDefinition(subcol);
+        if (def == null || !def.isRegular() || SuperColumnCompatibility.isSuperColumnMapColumn(def))
+            return null;
+
+        return def;
     }
 
     public static LegacyCellName decodeCellName(CFMetaData metadata, ByteBuffer cellname) throws UnknownColumnException
