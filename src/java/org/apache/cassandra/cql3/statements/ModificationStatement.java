f1c1694e4329a1f6dba8b978edaed4312e235116
Doug Rohrer <drohrer@apple.com>
2022-03-17 10:26:29 -0700

    Support DELETE in CQLSSTableWriter
    patch by Doug Rohrer, Eric Evans; reviewed by Sam Tunnicliffe, Yifan Cai for CASSANDRA-14797

--- src/java/org/apache/cassandra/cql3/statements/ModificationStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/ModificationStatement.java
@@ -790,7 +790,7 @@ public abstract class ModificationStatement implements CQLStatement.SingleKeyspa
         }
     }
 
-    Slices createSlices(QueryOptions options)
+    public Slices createSlices(QueryOptions options)
     {
         SortedSet<ClusteringBound<?>> startBounds = restrictions.getClusteringColumnsBounds(Bound.START, options);
         SortedSet<ClusteringBound<?>> endBounds = restrictions.getClusteringColumnsBounds(Bound.END, options);
