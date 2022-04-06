06f681cc2fd9ea4e85559f8759e8b67fe7993d45
David Capwell <dcapwell@apache.org>
2022-04-06 09:18:33 -0700

    feedback

--- test/unit/org/apache/cassandra/db/guardrails/GuardrailConsistencyLevelsTester.java
+++ test/unit/org/apache/cassandra/db/guardrails/GuardrailConsistencyLevelsTester.java
@@ -26,7 +26,6 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-import com.google.common.collect.ImmutableSet;
 import org.junit.Before;
 import org.junit.Test;
 
