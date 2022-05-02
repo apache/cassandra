c7f51978b452208e07f8b9a98ace5bbf0939f105
Josh McKenzie <jmckenzie@apache.org>
2022-05-02 13:18:51 -0400

    Rename truncate_drop guardrail to drop_truncate_table
    Patch by Josh McKenzie; reviewed by Andres de la Pena for CASSANDRA-17592

--- src/java/org/apache/cassandra/cql3/statements/TruncateStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/TruncateStatement.java
@@ -58,7 +58,7 @@ public class TruncateStatement extends QualifiedStatement implements CQLStatemen
     public void validate(ClientState state) throws InvalidRequestException
     {
         Schema.instance.validateTable(keyspace(), name());
-        Guardrails.truncateDropEnabled.ensureEnabled(state);
+        Guardrails.dropTruncateTableEnabled.ensureEnabled(state);
     }
 
     public ResultMessage execute(QueryState state, QueryOptions options, long queryStartNanoTime) throws InvalidRequestException, TruncateException
