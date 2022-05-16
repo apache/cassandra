2531dd1ebaeab4e71f970e27a5dd962ceb6b790b
Stefan Miklosovic <smiklosovic@apache.org>
2022-05-16 17:17:07 +0200

    remove ssl storage port from sstableloader
    patch by Stefan Miklosovic; reviewed by Brandon Williams for CASSANDRA-17602

--- src/java/org/apache/cassandra/tools/BulkLoadConnectionFactory.java
+++ src/java/org/apache/cassandra/tools/BulkLoadConnectionFactory.java
@@ -28,26 +28,24 @@ import org.apache.cassandra.streaming.StreamConnectionFactory;
 
 public class BulkLoadConnectionFactory extends DefaultConnectionFactory implements StreamConnectionFactory
 {
-    // TODO: what is this unused variable for?
-    private final boolean outboundBindAny;
-    private final int secureStoragePort;
+    private final int storagePort;
     private final EncryptionOptions.ServerEncryptionOptions encryptionOptions;
 
-    public BulkLoadConnectionFactory(int secureStoragePort, EncryptionOptions.ServerEncryptionOptions encryptionOptions, boolean outboundBindAny)
+    public BulkLoadConnectionFactory(EncryptionOptions.ServerEncryptionOptions encryptionOptions, int storagePort)
     {
-        this.secureStoragePort = secureStoragePort;
+        this.storagePort = storagePort;
         this.encryptionOptions = encryptionOptions;
-        this.outboundBindAny = outboundBindAny;
     }
 
     public Channel createConnection(OutboundConnectionSettings template, int messagingVersion) throws IOException
     {
-        // Connect to secure port for all peers if ServerEncryptionOptions is configured other than 'none'
-        // When 'all', 'dc' and 'rack', server nodes always have SSL port open, and since thin client like sstableloader
-        // does not know which node is in which dc/rack, connecting to SSL port is always the option.
+        // storage port can handle both encrypted and unencrypted traffic from 4.0
+        // so from sstableloader's point of view we can use just storage port for both cases
+
+        template = template.withConnectTo(template.to.withPort(storagePort));
 
         if (encryptionOptions != null && encryptionOptions.internode_encryption != EncryptionOptions.ServerEncryptionOptions.InternodeEncryption.none)
-            template = template.withConnectTo(template.to.withPort(secureStoragePort)).withEncryption(encryptionOptions);
+            template = template.withEncryption(encryptionOptions);
 
         return super.createConnection(template, messagingVersion);
     }
