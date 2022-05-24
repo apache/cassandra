132544e836a0573ff84a05102f0cf48546e4a3f6
Josh McKenzie <jmckenzie@apache.org>
2022-05-24 11:39:05 -0400

    rebase fix

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -834,8 +834,6 @@ public class Config
     public volatile boolean read_before_write_list_operations_enabled = true;
     public volatile boolean allow_filtering_enabled = true;
     public volatile boolean simplestrategy_enabled = true;
-    public volatile DataStorageSpec collection_size_warn_threshold = null;
-    public volatile DataStorageSpec collection_size_fail_threshold = null;
     public volatile DataStorageSpec.LongBytesBound collection_size_warn_threshold = null;
     public volatile DataStorageSpec.LongBytesBound collection_size_fail_threshold = null;
     public volatile int items_per_collection_warn_threshold = -1;
