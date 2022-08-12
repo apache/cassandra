aa6c57cc6251af3531b190cc3a0c00b01f658a41
David Capwell <dcapwell@apache.org>
2022-08-12 15:17:51 -0700

    feedback

--- src/java/org/apache/cassandra/io/util/DataOutputBuffer.java
+++ src/java/org/apache/cassandra/io/util/DataOutputBuffer.java
@@ -242,9 +242,9 @@ public class DataOutputBuffer extends BufferedDataOutputStreamPlus
 
     /**
      * Gets the underlying ByteBuffer and calls {@link ByteBuffer#flip()}.  This method is "unsafe" in the sense that
-     * it returns the underlying buffer, which may be modified by other methods after calling this method.  If the calling
-     * logic knows that no new calls to this object will happen after calling this method, then this method can avoid the
-     * copying done in {@link #getData()}, and {@link #asNewBuffer()}, and {@link #buffer()}.
+     * it returns the underlying buffer, which may be modified by other methods after calling this method (or cleared on
+     * {@link #close()}). If the calling logic knows that no new calls to this object will happen after calling this
+     * method, then this method can avoid the copying done in {@link #asNewBuffer()}, and {@link #buffer()}.
      */
     public ByteBuffer unsafeGetBufferAndFlip()
     {
