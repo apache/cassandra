b715ec2a50619936ee6d7c8958152abb516066bb
Josh McKenzie <jmckenzie@apache.org>
2022-09-08 13:09:57 -0400

    Remove usage of deprecated javax certificate class
    Patch by Doug Rohrer; reviewed by Jyothsna Konisa, Francisco Guerrero Hernandez, Josh McKenzie, and Caleb Rackliffe for CASSANDRA-17867
    
    Co-authored-by: Doug Rohrer <drohrer@apple.com>
    Co-authored-by: Josh McKenzie <jmckenzie@apache.org>

--- src/java/org/apache/cassandra/transport/ServerConnection.java
+++ src/java/org/apache/cassandra/transport/ServerConnection.java
@@ -17,8 +17,8 @@
  */
 package org.apache.cassandra.transport;
 
+import java.security.cert.Certificate;
 import javax.net.ssl.SSLPeerUnverifiedException;
-import javax.security.cert.X509Certificate;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -121,11 +121,11 @@ public class ServerConnection extends Connection
         return saslNegotiator;
     }
 
-    private X509Certificate[] certificates()
+    private Certificate[] certificates()
     {
         SslHandler sslHandler = (SslHandler) channel().pipeline()
                                                       .get("ssl");
-        X509Certificate[] certificates = null;
+        Certificate[] certificates = null;
 
         if (sslHandler != null)
         {
@@ -133,7 +133,7 @@ public class ServerConnection extends Connection
             {
                 certificates = sslHandler.engine()
                                          .getSession()
-                                         .getPeerCertificateChain();
+                                         .getPeerCertificates();
             }
             catch (SSLPeerUnverifiedException e)
             {
