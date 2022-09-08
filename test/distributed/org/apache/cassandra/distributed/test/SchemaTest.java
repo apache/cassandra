76be530a364b376c1d69d8447230ad5cf023be7f
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2022-09-08 16:02:50 +0200

    Fix schema reset functionality
    Patch by Jacek Lewandowski, reviewed by Andrés de la Peña and Ekaterina Dimitrova for CASSANDRA-17819

--- test/distributed/org/apache/cassandra/distributed/test/SchemaTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/SchemaTest.java
@@ -18,11 +18,23 @@
 
 package org.apache.cassandra.distributed.test;
 
+import java.util.concurrent.TimeUnit;
+
+import com.google.common.util.concurrent.Uninterruptibles;
 import org.junit.Test;
 
+import org.apache.cassandra.config.CassandraRelevantProperties;
 import org.apache.cassandra.distributed.Cluster;
 import org.apache.cassandra.distributed.api.ConsistencyLevel;
+import org.apache.cassandra.distributed.api.Feature;
+import org.apache.cassandra.gms.Gossiper;
+import org.apache.cassandra.schema.Schema;
+import org.apache.cassandra.service.StorageService;
+import org.apache.cassandra.utils.FBUtilities;
+import org.awaitility.Awaitility;
 
+import static java.time.Duration.ofMillis;
+import static java.time.Duration.ofSeconds;
 import static org.junit.Assert.assertTrue;
 
 public class SchemaTest extends TestBaseImpl
@@ -86,4 +98,59 @@ public class SchemaTest extends TestBaseImpl
             assertTrue(causeIsUnknownColumn);
         }
     }
+
+    @Test
+    public void schemaReset() throws Throwable
+    {
+        int delayUnit = 1000;
+        CassandraRelevantProperties.MIGRATION_DELAY.setInt(5 * delayUnit);
+        CassandraRelevantProperties.SCHEMA_PULL_INTERVAL_MS.setInt(5 * delayUnit);
+        CassandraRelevantProperties.SCHEMA_PULL_BACKOFF_DELAY_MS.setInt(delayUnit);
+
+        try (Cluster cluster = init(Cluster.build(2).withConfig(cfg -> cfg.with(Feature.GOSSIP, Feature.NETWORK)).start()))
+        {
+            cluster.schemaChange("CREATE TABLE " + KEYSPACE + ".tbl (pk INT PRIMARY KEY, v TEXT)");
+
+            assertTrue(cluster.get(1).callOnInstance(() -> Schema.instance.getTableMetadata(KEYSPACE, "tbl") != null));
+            assertTrue(cluster.get(2).callOnInstance(() -> Schema.instance.getTableMetadata(KEYSPACE, "tbl") != null));
+
+            cluster.get(2).shutdown().get();
+
+            Awaitility.await()
+                      .atMost(ofSeconds(30))
+                      .until(() -> cluster
+                                   .get(1)
+                                   .callOnInstance(() -> Gossiper.instance
+                                                         .getLiveMembers()
+                                                         .stream()
+                                                         .allMatch(addr -> addr.equals(FBUtilities.getBroadcastAddressAndPort()))));
+
+            // when schema is removed and there is no other node to fetch it from, node 1 should be left with clean schema
+            //noinspection Convert2MethodRef
+            cluster.get(1).runOnInstance(() -> StorageService.instance.resetLocalSchema());
+            assertTrue(cluster.get(1).callOnInstance(() -> Schema.instance.getTableMetadata(KEYSPACE, "tbl") == null));
+
+            // sleep slightly longer than the schema pull interval
+            Uninterruptibles.sleepUninterruptibly(6 * delayUnit, TimeUnit.MILLISECONDS);
+
+            // when the other node is started, schema should be back in sync - node 2 should send schema mutations to node 1
+            cluster.get(2).startup();
+
+            // sleep slightly longer than the schema pull interval
+            Uninterruptibles.sleepUninterruptibly(6 * delayUnit, TimeUnit.MILLISECONDS);
+
+            Awaitility.waitAtMost(ofMillis(6 * delayUnit))
+                      .pollDelay(ofSeconds(1))
+                      .until(() -> cluster.get(1).callOnInstance(() -> Schema.instance.getTableMetadata(KEYSPACE, "tbl") != null));
+
+            // when schema is removed and there is a node to fetch it from, node 1 should immediately restore the schema
+            //noinspection Convert2MethodRef
+            cluster.get(2).runOnInstance(() -> StorageService.instance.resetLocalSchema());
+
+            Awaitility.waitAtMost(ofMillis(6 * delayUnit))
+                      .pollDelay(ofSeconds(1))
+                      .until(() -> cluster.get(2).callOnInstance(() -> Schema.instance.getTableMetadata(KEYSPACE, "tbl") != null));
+        }
+    }
+
 }
