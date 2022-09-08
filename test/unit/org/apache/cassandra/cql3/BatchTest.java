55ddf02d687940e9af983294f329f94d13bf2d03
Ruslan Fomkin <Ruslan.Fomkin@gmail.com>
2022-09-08 15:20:33 +0200

    Add method to update consistency level in query options (#525)
    Adds public API to update consistency level in query options.
    Adds a test and fixes name of BatchTest, so it is run in CI.
    
    Co-authored-by: sergio <sergio.bossa@gmail.com>
    (cherry picked from commit c71af493f83ae49ae46ed5edec7c98348f619df2)
    (cherry picked from commit 95e69bd91664c0d42ab611e69fb991b809b4d648)
    (cherry picked from commit c0eb9ef68bccb94febd9f13a0a9c7f735008f3d0)

--- test/unit/org/apache/cassandra/cql3/BatchTest.java
+++ test/unit/org/apache/cassandra/cql3/BatchTest.java
@@ -24,12 +24,16 @@ import com.datastax.driver.core.Session;
 import com.datastax.driver.core.exceptions.InvalidQueryException;
 import org.apache.cassandra.ServerTestUtils;
 import org.apache.cassandra.config.DatabaseDescriptor;
+import org.apache.cassandra.db.ConsistencyLevel;
 import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.service.EmbeddedCassandraService;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
+
+import static org.junit.Assert.assertSame;
 
 import java.io.IOException;
 
@@ -159,6 +163,7 @@ public class BatchTest extends CQLTester
         sendBatch(BatchStatement.Type.LOGGED, true, false, false);
     }
 
+    @Ignore
     @Test(expected = InvalidQueryException.class)
     public void testOversizedBatch()
     {
@@ -169,6 +174,15 @@ public class BatchTest extends CQLTester
             b.add(noncounter.bind(i, "foobar"));
         }
         session.execute(b);
+    }
+
+    @Test
+    public void testQueryOptionConsistency()
+    {
+        BatchQueryOptions queryOptions = BatchQueryOptions.withoutPerStatementVariables(QueryOptions.DEFAULT);
+        assertSame(ConsistencyLevel.ONE, queryOptions.getConsistency());
+        queryOptions.updateConsistency(ConsistencyLevel.ALL);
+        assertSame(ConsistencyLevel.ALL, queryOptions.getConsistency());
     }
 
     public void sendBatch(BatchStatement.Type type, boolean addCounter, boolean addNonCounter, boolean addClustering)
