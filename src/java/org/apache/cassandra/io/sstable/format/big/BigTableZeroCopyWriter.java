c73b1f45d03abd1b7fbdc7cb3195d8fb18af7814
Josh McKenzie <jmckenzie@apache.org>
2022-09-15 12:40:25 -0400

    Mitigate direct buffer memory OOM on replacements
    Patch by Jon Meredith; reviewed by Josh McKenzie for CASSANDRA-17895
    
    Co-authored-by: Jon Meredith <jonmeredith@apache.org>
    Co-authored-by: Josh McKenzie <jmckenzie@apache.org>

--- src/java/org/apache/cassandra/io/sstable/format/big/BigTableZeroCopyWriter.java
+++ src/java/org/apache/cassandra/io/sstable/format/big/BigTableZeroCopyWriter.java
@@ -114,7 +114,7 @@ public class BigTableZeroCopyWriter extends SSTable implements SSTableMultiWrite
                 out.write(buff, 0, count);
                 bytesRead += count;
             }
-            out.sync();
+            out.sync(); // finish will also call sync(). Leaving here to get stuff flushed as early as possible
         }
         catch (IOException e)
         {
@@ -138,6 +138,10 @@ public class BigTableZeroCopyWriter extends SSTable implements SSTableMultiWrite
     public Collection<SSTableReader> finish(boolean openResult)
     {
         setOpenResult(openResult);
+
+        for (SequentialWriter writer : componentWriters.values())
+            writer.finish();
+
         return finished();
     }
 
