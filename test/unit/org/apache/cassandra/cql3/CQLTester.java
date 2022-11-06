bf1717d7ac864482c5a579f92d67c98aa47ad5cc
Maxim Chanturiay <maxim.chanturiay@ibm.com>
2022-11-06 09:54:32 +0200

    Fixed how we parse zero time duration written in ISO 86021 format. (#CASSANDRA-17919)

--- test/unit/org/apache/cassandra/cql3/CQLTester.java
+++ test/unit/org/apache/cassandra/cql3/CQLTester.java
@@ -867,7 +867,12 @@ public abstract class CQLTester
 
     protected String createTable(String keyspace, String query)
     {
-        String currentTable = createTableName();
+        return createTable(keyspace, query, null);
+    }
+
+    protected String createTable(String keyspace, String query, String tableName)
+    {
+        String currentTable = createTableName(tableName);
         String fullQuery = formatQuery(keyspace, query);
         logger.info(fullQuery);
         schemaChange(fullQuery);
@@ -876,7 +881,12 @@ public abstract class CQLTester
 
     protected String createTableName()
     {
-        String currentTable = String.format("table_%02d", seqNumber.getAndIncrement());
+        return createTableName(null);
+    }
+
+    protected String createTableName(String tableName)
+    {
+        String currentTable = tableName == null ? String.format("table_%02d", seqNumber.getAndIncrement()) : tableName;
         tables.add(currentTable);
         return currentTable;
     }
