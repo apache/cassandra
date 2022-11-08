b9da90a98e3896b48521319a2aa6121462c16b98
Mike Adamson <madamson@datastax.com>
2022-11-08 13:07:00 +0000

    STAR-1690 Stop message processing after MessagingService shutdown (#581)
    * STAR-1690 Stop message processing after MessagingService shutdown
      The RequestCallbacks class now stops adding callbacks after it
      is shutdown by the MessagingService when that is shutdown.
    
    * Make new log statements trace

--- src/java/org/apache/cassandra/net/RequestCallbacks.java
+++ src/java/org/apache/cassandra/net/RequestCallbacks.java
@@ -67,6 +67,8 @@ public class RequestCallbacks implements OutboundMessageCallbacks
     private final ScheduledExecutorService executor = new DebuggableScheduledThreadPoolExecutor("Callback-Map-Reaper");
     private final ConcurrentMap<CallbackKey, CallbackInfo> callbacks = new ConcurrentHashMap<>();
 
+    private volatile boolean shutdown;
+
     RequestCallbacks(MessagingService messagingService)
     {
         this.messagingService = messagingService;
@@ -100,6 +102,12 @@ public class RequestCallbacks implements OutboundMessageCallbacks
     {
         // mutations need to call the overload with a ConsistencyLevel
         assert message.verb() != Verb.MUTATION_REQ && message.verb() != Verb.COUNTER_MUTATION_REQ && message.verb() != Verb.PAXOS_COMMIT_REQ;
+        if (shutdown)
+        {
+            if (logger.isTraceEnabled())
+                logger.trace("Received request after messaging service shutdown so ignoring it");
+            return;
+        }
         CallbackInfo previous = callbacks.put(key(message.id(), to), new CallbackInfo(message, to, cb));
         assert previous == null : format("Callback already exists for id %d/%s! (%s)", message.id(), to, previous);
     }
@@ -112,6 +120,12 @@ public class RequestCallbacks implements OutboundMessageCallbacks
                                   boolean allowHints)
     {
         assert message.verb() == Verb.MUTATION_REQ || message.verb() == Verb.COUNTER_MUTATION_REQ || message.verb() == Verb.PAXOS_COMMIT_REQ;
+        if (shutdown)
+        {
+            if (logger.isTraceEnabled())
+                logger.trace("Received request after messaging service shutdown so ignoring it");
+            return;
+        }
         CallbackInfo previous = callbacks.put(key(message.id(), to.endpoint()), new WriteCallbackInfo(message, to, cb, consistencyLevel, allowHints));
         assert previous == null : format("Callback already exists for id %d/%s! (%s)", message.id(), to.endpoint(), previous);
     }
@@ -181,6 +195,7 @@ public class RequestCallbacks implements OutboundMessageCallbacks
 
     void shutdownNow(boolean expireCallbacks)
     {
+        shutdown = true;
         executor.shutdownNow();
         if (expireCallbacks)
             forceExpire();
@@ -188,6 +203,7 @@ public class RequestCallbacks implements OutboundMessageCallbacks
 
     void shutdownGracefully()
     {
+        shutdown = true;
         expire();
         if (!callbacks.isEmpty())
             executor.schedule(this::shutdownGracefully, 100L, MILLISECONDS);
