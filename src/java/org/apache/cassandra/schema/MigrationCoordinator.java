c84c95884689887b6e42cc8e0da5a3dd7b53b0ff
Stefan Miklosovic <smiklosovic@apache.org>
2022-12-06 15:32:24 +0100

    Do not spam the logs with MigrationCoordinator not able to pull schemas on bootstrap

--- src/java/org/apache/cassandra/schema/MigrationCoordinator.java
+++ src/java/org/apache/cassandra/schema/MigrationCoordinator.java
@@ -67,6 +67,7 @@ import org.apache.cassandra.net.RequestCallback;
 import org.apache.cassandra.net.Verb;
 import org.apache.cassandra.service.StorageService;
 import org.apache.cassandra.utils.FBUtilities;
+import org.apache.cassandra.utils.NoSpamLogger;
 import org.apache.cassandra.utils.Pair;
 import org.apache.cassandra.utils.Simulate;
 import org.apache.cassandra.utils.concurrent.Future;
@@ -97,6 +98,7 @@ import static org.apache.cassandra.utils.concurrent.WaitQueue.newWaitQueue;
 public class MigrationCoordinator
 {
     private static final Logger logger = LoggerFactory.getLogger(MigrationCoordinator.class);
+    private static final NoSpamLogger noSpamLogger = NoSpamLogger.getLogger(MigrationCoordinator.logger, 1, TimeUnit.MINUTES);
     private static final Future<Void> FINISHED_FUTURE = ImmediateFuture.success(null);
 
     private static LongSupplier getUptimeFn = () -> ManagementFactory.getRuntimeMXBean().getUptime();
@@ -647,7 +649,7 @@ public class MigrationCoordinator
     {
         if (!gossiper.isAlive(endpoint))
         {
-            logger.warn("Can't send schema pull request: node {} is down.", endpoint);
+            noSpamLogger.warn("Can't send schema pull request: node {} is down.", endpoint);
             callback.onFailure(endpoint, RequestFailureReason.UNKNOWN);
             return;
         }
