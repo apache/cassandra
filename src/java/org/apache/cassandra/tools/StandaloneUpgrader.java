f8379d59812496c2b6795076a797fda5272ff033
Claude Warren <claude.warren@aiven.io>
2023-01-11 10:20:57 +0000

    Fixes ordering in StandaloneUpgrader
    Fixed issue where sstables were not updated in order by sorting on SSTableId
    
    patch by Claude Warren; reviewed by <Reviewers> for CASSANDRA--18143

--- src/java/org/apache/cassandra/tools/StandaloneUpgrader.java
+++ src/java/org/apache/cassandra/tools/StandaloneUpgrader.java
@@ -76,8 +76,10 @@ public class StandaloneUpgrader
 
             Collection<SSTableReader> readers = new ArrayList<>();
 
-            // Upgrade sstables
-            for (Map.Entry<Descriptor, Set<Component>> entry : lister.list().entrySet())
+            // Upgrade sstables in SSTableId order
+            SortedMap<Descriptor, Set<Component>> selected = new TreeMap<>( (t1,t2) -> SSTableIdFactory.COMPARATOR.compare( t1.id, t2.id) );
+            selected.putAll(lister.list());
+            for (Map.Entry<Descriptor, Set<Component>> entry : selected.entrySet())
             {
                 Set<Component> components = entry.getValue();
                 if (!components.contains(Component.DATA) || !components.contains(Component.PRIMARY_INDEX))
