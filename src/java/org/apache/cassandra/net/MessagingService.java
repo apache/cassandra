4904d4f2fd199f18e63c39590481fe4123c9c3d9
Piotr Kołaczkowski <pkolaczk@gmail.com>
2023-01-26 23:18:05 +0100

    Fix compilation of MessagingService (#595)
    Fixes:
      [javac] cassandra/src/java/org/apache/cassandra/net/MessagingService.java:219: error: cannot find symbol
        [javac]     static Map<Integer, Integer> versionOrdinalMap = Arrays.stream(Version.values()).collect(Collectors.toMap(v -> v.value, v -> v.ordinal()));

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -25,6 +25,7 @@ import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
+import java.util.stream.Collectors;
 
 import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
