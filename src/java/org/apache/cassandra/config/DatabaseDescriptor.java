c65afc0bc886f2fa6df71661959a85699dbc51b5
Branimir Lambov <branimir.lambov@datastax.com>
2023-02-09 15:55:51 +0200

    Adds ability to change compaction default in YAML
    and switches to UCS for the trie unit test suite

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -4677,4 +4677,9 @@ public class DatabaseDescriptor
     {
         return Objects.requireNonNull(sstableFormatFactories, "Forgot to initialize DatabaseDescriptor?");
     }
+
+    public static ParameterizedClass getDefaultCompaction()
+    {
+        return conf != null ? conf.default_compaction : null;
+    }
 }
