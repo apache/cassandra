2e28f190d78da21a02bb75052b16b8e307e63d74
Claude Warren <claude.warren@aiven.io>
2023-03-02 11:20:49 +0000

    initial code

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -1092,6 +1092,9 @@ public class Config
     public volatile long min_tracked_partition_tombstone_count = 5000;
     public volatile boolean top_partitions_enabled = true;
 
+    @Nullable
+    public Map<String,String> sstable_compression;
+
     public static Supplier<Config> getOverrideLoadConfig()
     {
         return overrideLoadConfig;
