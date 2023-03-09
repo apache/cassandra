mahmudulhasancsedu5/cassandra-ucs

Rename increaseSlightly to nextValidToken

src/java/org/apache/cassandra/dht/Murmur3Partitioner.java
@@ -219,7 +219,7 @@ public class Murmur3Partitioner implements IPartitioner
         }
 
         @Override
-        public Token increaseSlightly()
+        public Token nextValidToken()
         {
             return new LongToken(token + 1);
         }

apache/cassandra

src/java/org/apache/cassandra/dht/Murmur3Partitioner.java
@@ -213,7 +213,7 @@ public class Murmur3Partitioner implements IPartitioner
         }
 
         @Override
-        public LongToken increaseSlightly()
+        public LongToken nextValidToken()
         {
             return new LongToken(token + 1);
         }