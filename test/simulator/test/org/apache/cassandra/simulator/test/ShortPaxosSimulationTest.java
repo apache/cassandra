ecb051bf59a741cf4c139187358aed240411412c
David Capwell <dcapwell@apache.org>
2023-03-13 12:25:14 -0700

    revert seed

--- test/simulator/test/org/apache/cassandra/simulator/test/ShortPaxosSimulationTest.java
+++ test/simulator/test/org/apache/cassandra/simulator/test/ShortPaxosSimulationTest.java
@@ -30,7 +30,7 @@ public class ShortPaxosSimulationTest
     @Test
     public void simulationTest() throws IOException
     {
-        PaxosSimulationRunner.main(new String[] { "run", "-n", "3..6", "-t", "1000", "-c", "2", "--cluster-action-limit", "2", "-s", "30", "--seed", "0xe0f54bdc2856e12f" });
+        PaxosSimulationRunner.main(new String[] { "run", "-n", "3..6", "-t", "1000", "-c", "2", "--cluster-action-limit", "2", "-s", "30" });
     }
 
     @Test
