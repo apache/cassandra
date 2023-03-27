85f3049e9002219f6f6be538d486398c7bc85ca2
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2023-03-27 10:55:45 +0200

    fixes

--- src/java/org/apache/cassandra/db/SystemKeyspace.java
+++ src/java/org/apache/cassandra/db/SystemKeyspace.java
@@ -36,6 +36,7 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import javax.management.openmbean.OpenDataException;
@@ -562,6 +563,12 @@ public final class SystemKeyspace
 
     public static void persistLocalMetadata()
     {
+        persistLocalMetadata(UUID::randomUUID);
+    }
+
+    @VisibleForTesting
+    public static void persistLocalMetadata(Supplier<UUID> nodeIdSupplier)
+    {
         String req = "INSERT INTO system.%s (" +
                      "key," +
                      "cluster_name," +
@@ -600,7 +607,7 @@ public final class SystemKeyspace
         // It is very unlikely that when upgrading the host id is not flushed to disk, but if that's the case, we limit
         // this change only to the new installations or the user should just flush system.local table.
         if (!CommitLog.instance.hasFilesToReplay())
-            SystemKeyspace.getOrInitializeLocalHostId();
+            SystemKeyspace.getOrInitializeLocalHostId(nodeIdSupplier);
     }
 
     public static void updateCompactionHistory(String ksname,
@@ -1227,12 +1234,17 @@ public final class SystemKeyspace
      */
     public static synchronized UUID getOrInitializeLocalHostId()
     {
+        return getOrInitializeLocalHostId(UUID::randomUUID);
+    }
+
+    private static synchronized UUID getOrInitializeLocalHostId(Supplier<UUID> nodeIdSupplier)
+    {
         UUID hostId = getLocalHostId();
         if (hostId != null)
             return hostId;
 
         // ID not found, generate a new one, persist, and then return it.
-        hostId = UUID.randomUUID();
+        hostId = nodeIdSupplier.get();
         logger.warn("No host ID found, created {} (Note: This should happen exactly once per node).", hostId);
         return setLocalHostId(hostId);
     }
