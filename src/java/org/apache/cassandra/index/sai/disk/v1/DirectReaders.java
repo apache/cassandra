133b3005a99a0aeb345752dfd79f1134b477a914
Jonathan Ellis <jbellis@datastax.com>
2023-04-13 21:43:57 -0500

    convert DirectReaders to little-endian

--- src/java/org/apache/cassandra/index/sai/disk/v1/DirectReaders.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/DirectReaders.java
@@ -87,7 +87,7 @@ public class DirectReaders
     private static final Reader READER_1 = (in, offset, index) -> {
         try
         {
-            int shift = 7 - (int) (index & 7);
+            int shift = (int) (index & 7);
             return (in.readByte(offset + (index >>> 3)) >>> shift) & 0x1;
         }
         catch (IOException e)
@@ -99,7 +99,7 @@ public class DirectReaders
     private static final Reader READER_2 = (in, offset, index) -> {
         try
         {
-            int shift = (3 - (int) (index & 3)) << 1;
+            int shift = (int) (index & 3) << 1;
             return (in.readByte(offset + (index >>> 2)) >>> shift) & 0x3;
         }
         catch (IOException e)
@@ -111,7 +111,7 @@ public class DirectReaders
     private static final Reader READER_4 = (in, offset, index) -> {
         try
         {
-            int shift = (int) ((index + 1) & 1) << 2;
+            int shift = (int) (index & 1) << 2;
             return (in.readByte(offset + (index >>> 1)) >>> shift) & 0xF;
         }
         catch (IOException e)
@@ -135,8 +135,10 @@ public class DirectReaders
         try
         {
             long o = (index * 12) >>> 3;
-            int shift = (int) ((index + 1) & 1) << 2;
-            return (in.readShort(offset + o) >>> shift) & 0xFFF;
+            int shift = (int) (index & 1) * 4;
+            int v = in.readShort(offset + o) & 0xFFFF;
+            v = (v >>> shift) | ((in.readByte(offset + o + 2) & 0xFF) << (16 - shift));
+            return v & 0xFFF;
         }
         catch (IOException e)
         {
@@ -147,7 +149,10 @@ public class DirectReaders
     private static final Reader READER_16 = (in, offset, index) -> {
         try
         {
-            return in.readShort(offset + (index << 1)) & 0xFFFF;
+            long position = offset + (index << 1);
+            int lowByte = in.readByte(position) & 0xFF;
+            int highByte = in.readByte(position + 1) & 0xFF;
+            return (highByte << 8) | lowByte;
         }
         catch (IOException e)
         {
@@ -159,9 +164,11 @@ public class DirectReaders
         try
         {
             long o = (index * 20) >>> 3;
-            int v = in.readInt(offset + o) >>> 8;
-            int shift = (int) ((index + 1) & 1) << 2;
-            return (v >>> shift) & 0xFFFFF;
+            int shift = (int) (index & 1) * 4;
+            int v1 = in.readShort(offset + o) & 0xFFFF;
+            int v2 = in.readByte(offset + o + 2) & 0xFF;
+            int v = (v1 >>> shift) | (v2 << (16 - shift));
+            return v & 0xFFFFF;
         }
         catch (IOException e)
         {
@@ -172,7 +179,10 @@ public class DirectReaders
     private static final Reader READER_24 = (in, offset, index) -> {
         try
         {
-            return in.readInt(offset + index * 3) >>> 8;
+            long o = offset + index * 3;
+            int v1 = in.readByte(o) & 0xFF;
+            int v2 = in.readShort(o + 1) & 0xFFFF;
+            return (v2 << 8) | v1;
         }
         catch (IOException e)
         {
@@ -184,8 +194,12 @@ public class DirectReaders
         try
         {
             long o = (index * 28) >>> 3;
-            int shift = (int) ((index + 1) & 1) << 2;
-            return (in.readInt(offset + o) >>> shift) & 0xFFFFFFFL;
+            int shift = (int) (index & 1) * 4;
+            int v1 = in.readByte(o) & 0xFF;
+            int v2 = in.readShort(o + 1) & 0xFFFF;
+            int v3 = in.readByte(o + 3) & 0xFF;
+            int v = (v3 << 24) | (v2 << 8) | v1;
+            return (v >>> shift) & 0xFFFFFFF;
         }
         catch (IOException e)
         {
@@ -196,7 +210,12 @@ public class DirectReaders
     private static final Reader READER_32 = (in, offset, index) -> {
         try
         {
-            return in.readInt(offset + (index << 2)) & 0xFFFFFFFFL;
+            long o = offset + (index << 2);
+            int v1 = in.readByte(o) & 0xFF;
+            int v2 = in.readByte(o + 1) & 0xFF;
+            int v3 = in.readByte(o + 2) & 0xFF;
+            int v4 = in.readByte(o + 3) & 0xFF;
+            return ((long) v4 << 24) | (v3 << 16) | (v2 << 8) | v1;
         }
         catch (IOException e)
         {
@@ -207,7 +226,10 @@ public class DirectReaders
     private static final Reader READER_40 = (in, offset, index) -> {
         try
         {
-            return in.readLong(offset + index * 5) >>> 24;
+            long o = offset + index * 5;
+            long v1 = in.readByte(o) & 0xFFL;
+            long v2 = in.readInt(o + 1) & 0xFFFFFFFFL;
+            return (v2 << 8) | v1;
         }
         catch (IOException e)
         {
@@ -218,7 +240,10 @@ public class DirectReaders
     private static final Reader READER_48 = (in, offset, index) -> {
         try
         {
-            return in.readLong(offset + index * 6) >>> 16;
+            long o = offset + index * 6;
+            long v1 = in.readShort(o) & 0xFFFFL;
+            long v2 = in.readInt(o + 2) & 0xFFFFFFFFL;
+            return (v2 << 16) | v1;
         }
         catch (IOException e)
         {
@@ -229,7 +254,11 @@ public class DirectReaders
     private static final Reader READER_56 = (in, offset, index) -> {
         try
         {
-            return in.readLong(offset + index * 7) >>> 8;
+            long o = offset + index * 7;
+            long v1 = in.readByte(o) & 0xFFL;
+            long v2 = in.readInt(o + 1) & 0xFFFFFFFFL;
+            long v3 = in.readShort(o + 5) & 0xFFFFL;
+            return (v3 << 40) | (v2 << 8) | v1;
         }
         catch (IOException e)
         {
@@ -240,7 +269,17 @@ public class DirectReaders
     private static final Reader READER_64 = (in, offset, index) -> {
         try
         {
-            return in.readLong(offset + (index << 3));
+            long o = offset + (index << 3);
+            long v1 = in.readByte(o) & 0xFFL;
+            long v2 = in.readByte(o + 1) & 0xFFL;
+            long v3 = in.readByte(o + 2) & 0xFFL;
+            long v4 = in.readByte(o + 3) & 0xFFL;
+            long v5 = in.readByte(o + 4) & 0xFFL;
+            long v6 = in.readByte(o + 5) & 0xFFL;
+            long v7 = in.readByte(o + 6) & 0xFFL;
+            long v8 = in.readByte(o + 7) & 0xFFL;
+            return (v8 << 56) | (v7 << 48) | (v6 << 40) | (v5 << 32) |
+                   (v4 << 24) | (v3 << 16) | (v2 << 8) | v1;
         }
         catch (IOException e)
         {
