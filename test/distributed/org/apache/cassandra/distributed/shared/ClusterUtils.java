821fa33f0bd9ef9661db1be786b7bb5cc866a6e8
David Capwell <dcapwell@apache.org>
2023-04-20 09:53:11 -0700

    imports

--- test/distributed/org/apache/cassandra/distributed/shared/ClusterUtils.java
+++ test/distributed/org/apache/cassandra/distributed/shared/ClusterUtils.java
@@ -24,7 +24,6 @@ import java.security.Permission;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
