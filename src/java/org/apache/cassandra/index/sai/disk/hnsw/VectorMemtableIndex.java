9223bc26b3e0e9acc1b65462af4e4f91b113f328
Jonathan Ellis <jbellis@datastax.com>
2023-04-24 14:43:40 -0500

    cleanup warnings

--- src/java/org/apache/cassandra/index/sai/disk/hnsw/VectorMemtableIndex.java
+++ src/java/org/apache/cassandra/index/sai/disk/hnsw/VectorMemtableIndex.java
@@ -62,7 +62,7 @@ public class VectorMemtableIndex implements MemtableIndex
     private final IndexContext indexContext;
     private final ByteBufferVectorValues vectorValues = new ByteBufferVectorValues();
     private final ArrayList<PrimaryKey> keys = new ArrayList<>();
-    private final HnswGraphBuilder builder;
+    private final HnswGraphBuilder<float[]> builder;
     private final LongAdder writeCount = new LongAdder();
 
     private final AtomicInteger cachedDimensions = new AtomicInteger();
@@ -252,7 +252,7 @@ public class VectorMemtableIndex implements MemtableIndex
 
         public long ramBytesUsed()
         {
-            return ObjectSizes.measure(buffers) + buffers.size() * (4 + 4 * dimension());
+            return ObjectSizes.measure(buffers) + buffers.size() * (4 + 4L * dimension());
         }
     }
 }
