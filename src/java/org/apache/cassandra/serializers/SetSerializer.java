e80fad431946b8b846a080d1e7b41c7a3d87801a
Jon Meredith <jonmeredith@apache.org>
2023-05-09 08:12:35 -0600

    switch to using readNonNullValue

--- src/java/org/apache/cassandra/serializers/SetSerializer.java
+++ src/java/org/apache/cassandra/serializers/SetSerializer.java
@@ -79,9 +79,7 @@ public class SetSerializer<T> extends CollectionSerializer<Set<T>>
             int offset = sizeOfCollectionSize(n, version);
             for (int i = 0; i < n; i++)
             {
-                V value = readValue(input, accessor, offset, version);
-                if (value == null)
-                    throw new MarshalException("Not enough bytes to read value in set");
+                V value = readNonNullValue(input, accessor, offset, version);
                 offset += sizeOfValue(value, accessor, version);
                 elements.validate(value, accessor);
             }
@@ -112,9 +110,7 @@ public class SetSerializer<T> extends CollectionSerializer<Set<T>>
 
             for (int i = 0; i < n; i++)
             {
-                V value = readValue(input, accessor, offset, version);
-                if (value == null)
-                    throw new MarshalException("Not enough bytes to read value in set");
+                V value = readNonNullValue(input, accessor, offset, version);
                 offset += sizeOfValue(value, accessor, version);
                 elements.validate(value, accessor);
                 l.add(elements.deserialize(value, accessor));
