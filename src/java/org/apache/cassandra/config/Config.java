c4b6a6bdaa0061bca7b104997e0d698f856addba
Stefan Miklosovic <smiklosovic@apache.org>
2023-05-31 23:04:16 +0200

    src

--- src/java/org/apache/cassandra/config/Config.java
+++ src/java/org/apache/cassandra/config/Config.java
@@ -1135,6 +1135,9 @@ public class Config
      */
     public ParameterizedClass default_compaction = null;
 
+    @Nullable
+    public ParameterizedClass sstable_compression;
+
     public static Supplier<Config> getOverrideLoadConfig()
     {
         return overrideLoadConfig;
