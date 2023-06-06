7869d7db45ea15cb238af5f69431fff1f2c3078a
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2023-06-06 10:31:08 +0200

    Refactor BatchlogManager to use bytes based pages

--- src/java/org/apache/cassandra/cql3/UntypedResultSet.java
+++ src/java/org/apache/cassandra/cql3/UntypedResultSet.java
@@ -28,6 +28,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.UUID;
+import javax.annotation.Nullable;
 
 import com.google.common.annotations.VisibleForTesting;
 
@@ -79,9 +80,9 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
         return new FromResultList(results);
     }
 
-    public static UntypedResultSet create(SelectStatement select, QueryPager pager, PageSize pageSize)
+    public static UntypedResultSet create(SelectStatement select, QueryPager pager, PageSize pageSize, @Nullable Runnable onNextPageListener)
     {
-        return new FromPager(select, pager, pageSize);
+        return new FromPager(select, pager, pageSize, onNextPageListener);
     }
 
     /**
@@ -93,9 +94,10 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
                                           ConsistencyLevel cl,
                                           ClientState clientState,
                                           QueryPager pager,
-                                          PageSize pageSize)
+                                          PageSize pageSize,
+                                          @Nullable Runnable onNextPageListener)
     {
-        return new FromDistributedPager(select, cl, clientState, pager, pageSize);
+        return new FromDistributedPager(select, cl, clientState, pager, pageSize, onNextPageListener);
     }
 
     public boolean isEmpty()
@@ -199,13 +201,15 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
         private final QueryPager pager;
         private final PageSize pageSize;
         private final List<ColumnSpecification> metadata;
+        private final Runnable onNextPageListener;
 
-        private FromPager(SelectStatement select, QueryPager pager, PageSize pageSize)
+        private FromPager(SelectStatement select, QueryPager pager, PageSize pageSize, Runnable onNextPageListener)
         {
             this.select = select;
             this.pager = pager;
             this.pageSize = pageSize;
             this.metadata = select.getResultMetadata().requestNames();
+            this.onNextPageListener = onNextPageListener;
         }
 
         public int size()
@@ -235,6 +239,8 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
                         try (ReadExecutionController executionController = pager.executionController();
                              PartitionIterator iter = pager.fetchPageInternal(pageSize, executionController))
                         {
+                            if (onNextPageListener != null)
+                                onNextPageListener.run();
                             currentPage = select.process(iter, nowInSec, true).rows.iterator();
                         }
                     }
@@ -260,12 +266,14 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
         private final QueryPager pager;
         private final PageSize pageSize;
         private final List<ColumnSpecification> metadata;
+        private final Runnable onNextPageListener;
 
         private FromDistributedPager(SelectStatement select,
                                      ConsistencyLevel cl,
                                      ClientState clientState,
                                      QueryPager pager,
-                                     PageSize pageSize)
+                                     PageSize pageSize,
+                                     Runnable onNextPageListener)
         {
             this.select = select;
             this.cl = cl;
@@ -273,6 +281,7 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
             this.pager = pager;
             this.pageSize = pageSize;
             this.metadata = select.getResultMetadata().requestNames();
+            this.onNextPageListener = onNextPageListener;
         }
 
         public int size()
@@ -301,6 +310,8 @@ public abstract class UntypedResultSet implements Iterable<UntypedResultSet.Row>
 
                         try (PartitionIterator iter = pager.fetchPage(pageSize, cl, clientState, nanoTime()))
                         {
+                            if (onNextPageListener != null)
+                                onNextPageListener.run();
                             currentPage = select.process(iter, nowInSec, true).rows.iterator();
                         }
                     }
--- src/java/org/apache/cassandra/repair/consistent/LocalSessions.java
+++ src/java/org/apache/cassandra/repair/consistent/LocalSessions.java
@@ -352,7 +352,7 @@ public class LocalSessions
     {
         Preconditions.checkArgument(!started, "LocalSessions.start can only be called once");
         Preconditions.checkArgument(sessions.isEmpty(), "No sessions should be added before start");
-        UntypedResultSet rows = QueryProcessor.executeInternalWithPaging(String.format("SELECT * FROM %s.%s", keyspace, table), PageSize.inRows(1000));
+        UntypedResultSet rows = QueryProcessor.executeInternalWithPaging(String.format("SELECT * FROM %s.%s", keyspace, table), PageSize.inRows(1000), null);
         Map<TimeUUID, LocalSession> loadedSessions = new HashMap<>();
         Map<TableId, List<RepairedState.Level>> initialLevels = new HashMap<>();
         for (UntypedResultSet.Row row : rows)
