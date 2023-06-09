e919331901c9be8c9fcf0c604b6ab34bb7209562
ningzi.zhan <ningzi.zhan@datastax.com>
2023-06-09 11:19:08 -0700

    simplify the test add column name to error message

--- test/unit/org/apache/cassandra/schema/CreateTableValidationTest.java
+++ test/unit/org/apache/cassandra/schema/CreateTableValidationTest.java
@@ -32,7 +32,6 @@ import org.apache.cassandra.transport.messages.QueryMessage;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
