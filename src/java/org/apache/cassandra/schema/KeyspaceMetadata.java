33fd8d8a4cd58b3acd4bf594edab3da752844061
Maciej Sokol <maciej.sokol@ericsson.com>
2023-06-14 08:10:50 +0200

    Fix assertion error when describing mv as table

--- src/java/org/apache/cassandra/schema/KeyspaceMetadata.java
+++ src/java/org/apache/cassandra/schema/KeyspaceMetadata.java
@@ -158,6 +158,12 @@ public final class KeyspaceMetadata implements SchemaElement
              : view.metadata;
     }
 
+    @Nullable
+    public TableMetadata getTableNullable(String tableName)
+    {
+        return tables.getNullable(tableName);
+    }
+
     public boolean hasTable(String tableName)
     {
         return tables.get(tableName).isPresent();
