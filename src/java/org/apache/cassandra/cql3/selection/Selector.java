7aca77c070c5ab5dc3d489706df3dd8677e82784
David Capwell <dcapwell@apache.org>
2023-06-15 14:27:05 -0700

    remove todo

--- src/java/org/apache/cassandra/cql3/selection/Selector.java
+++ src/java/org/apache/cassandra/cql3/selection/Selector.java
@@ -92,7 +92,6 @@ public abstract class Selector
         AGGREGATE_FUNCTION_SELECTOR(AggregateFunctionSelector.deserializer),
         ELEMENT_SELECTOR(ElementsSelector.ElementSelector.deserializer),
         SLICE_SELECTOR(ElementsSelector.SliceSelector.deserializer),
-        // TODO : does this require we bump messaging version?  I think we are going to for TCM, but good to know ahead of time...
         VECTOR_SELECTOR(VectorSelector.deserializer);
 
         private final SelectorDeserializer deserializer;
