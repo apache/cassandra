824ac9895bb40208d916ee67ec02f25d89384604
David Capwell <dcapwell@apache.org>
2023-06-15 13:39:50 -0700

    imports

--- src/java/org/apache/cassandra/schema/MemtableParams.java
+++ src/java/org/apache/cassandra/schema/MemtableParams.java
@@ -20,7 +20,6 @@ package org.apache.cassandra.schema;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.Map;
