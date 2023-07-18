66e15cf5dabcca2ab31e5ca871f443d0dc9ab9f2
Andrés de la Peña <adelapena@users.noreply.github.com>
2023-07-18 15:23:30 +0100

    VECTOR-68 Vector dimensions are not being validated correctly (#679)

--- test/unit/org/apache/cassandra/cql3/CQLTester.java
+++ test/unit/org/apache/cassandra/cql3/CQLTester.java
@@ -18,7 +18,6 @@
 package org.apache.cassandra.cql3;
 
 import java.io.IOException;
-import java.lang.reflect.Array;
 import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.net.InetAddress;
@@ -2119,7 +2118,8 @@ public abstract class CQLTester
         return Arrays.asList(values);
     }
 
-    protected Vector<Float> vector(Float... values)
+    @SafeVarargs
+    protected final <T> Vector<T> vector(T... values)
     {
         return new Vector<>(values);
     }
