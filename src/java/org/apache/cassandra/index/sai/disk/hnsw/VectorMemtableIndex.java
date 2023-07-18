eec8b7b387cd041fbd54aef8f0090773962a9e49
Piotr Kołaczkowski <pkolaczk@gmail.com>
2023-07-18 17:36:43 +0200

    VECTOR-69: Fix LWT test failure caused by null key bounds (#686)
    * VECTOR-69: Fix LWT test failure caused by null key bounds
    
    ---------
    
    Co-authored-by: Jakub Zytka <jakub.zytka@datastax.com>
    Co-authored-by: Jonathan Ellis <jbellis@datastax.com>

--- src/java/org/apache/cassandra/index/sai/disk/hnsw/VectorMemtableIndex.java
+++ src/java/org/apache/cassandra/index/sai/disk/hnsw/VectorMemtableIndex.java
@@ -93,14 +93,7 @@ public class VectorMemtableIndex implements MemtableIndex
         if (value == null || value.remaining() == 0)
             return 0;
 
-        if (minimumKey == null)
-            minimumKey = primaryKey;
-        else if (primaryKey.compareTo(minimumKey) < 0)
-            minimumKey = primaryKey;
-        if (maximumKey == null)
-            maximumKey = primaryKey;
-        else if (primaryKey.compareTo(maximumKey) > 0)
-            maximumKey = primaryKey;
+        updateKeyBounds(primaryKey);
 
         writeCount.increment();
         primaryKeys.add(primaryKey);
@@ -129,6 +122,10 @@ public class VectorMemtableIndex implements MemtableIndex
         if (different)
         {
             var primaryKey = indexContext.keyFactory().create(key, clustering);
+            // update bounds because only rows with vectors are included in the key bounds,
+            // so if the vector was null before, we won't have included it
+            updateKeyBounds(primaryKey);
+
             // make the changes in this order so we don't have a window where the row is not in the index at all
             if (newRemaining > 0)
                 graph.add(newValue, primaryKey, FAIL);
@@ -139,6 +136,17 @@ public class VectorMemtableIndex implements MemtableIndex
             if (newRemaining <= 0 && oldRemaining > 0)
                 primaryKeys.remove(primaryKey);
         }
+    }
+
+    private void updateKeyBounds(PrimaryKey primaryKey) {
+        if (minimumKey == null)
+            minimumKey = primaryKey;
+        else if (primaryKey.compareTo(minimumKey) < 0)
+            minimumKey = primaryKey;
+        if (maximumKey == null)
+            maximumKey = primaryKey;
+        else if (primaryKey.compareTo(maximumKey) > 0)
+            maximumKey = primaryKey;
     }
 
     @Override
