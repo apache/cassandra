mahmudulhasancsedu5/cassandra-ucs

Test and fix shard number calculation for multiple nodes/disks

src/java/org/apache/cassandra/db/compaction/unified/Controller.java
@@ -402,15 +402,21 @@ public abstract class Controller
      * Additionally, if a minimum sstable size is set, we can go below the baseShardCount when that would result in
      * sstables smaller than that minimum. Note that in the case of a non-power-of-two base count this will cause
      * smaller sstables to not be aligned with the ones whose size is enough for the base count.
+     * <p>
+     * Note that to get the sstables resulting from this splitting within the bounds, the density argument must be
+     * normalized to the span that is being split. In other words, if no disks are defined, the density should be
+     * scaled by the token coverage of the locally-owned ranges. If multiple data directories are defined, the density
+     * should be scaled by the token coverage of the respective data directory. That is, localDensity = size / span,
+     * where the span is normalized so that span = 1 when the data covers the range that is being split.
      */
-    public int getNumShards(double density)
+    public int getNumShards(double localDensity)
     {
         int shards;
         // Check the minimum size first.
         long minSize = getMinSstableSizeBytes();
         if (minSize > 0)
         {
-            double count = density / minSize;
+            double count = localDensity / minSize;
             // Minimum size only applies if it is smaller than the base count.
             // Note: the minimum size cannot be larger than the target size's minimum.
             if (count < baseShardCount)
@@ -421,8 +427,8 @@ public abstract class Controller
                 if (logger.isDebugEnabled())
                     logger.debug("Shard count {} for density {}, {} times min size {}",
                                  shards,
-                                 FBUtilities.prettyPrintBinary(density, "B", " "),
-                                 density / minSize,
+                                 FBUtilities.prettyPrintBinary(localDensity, "B", " "),
+                                 localDensity / minSize,
                                  FBUtilities.prettyPrintBinary(minSize, "B", " "));
 
                 return shards;
@@ -434,7 +440,7 @@ public abstract class Controller
             shards = baseShardCount;
             logger.debug("Shard count {} for density {} in fixed shards mode",
                          shards,
-                         FBUtilities.prettyPrintBinary(density, "B", " "));
+                         FBUtilities.prettyPrintBinary(localDensity, "B", " "));
             return shards;
         }
         else if (sstableGrowthModifier == 0)
@@ -442,7 +448,7 @@ public abstract class Controller
             // How many we would have to aim for the target size. Divided by the base shard count, so that we can ensure
             // the result is a multiple of it by multiplying back below. Adjusted by sqrt(0.5) to calculate the split
             // points needed for the minimum size.
-            double count = density / (targetSSTableSize * INVERSE_SQRT_2 * baseShardCount);
+            double count = localDensity / (targetSSTableSize * INVERSE_SQRT_2 * baseShardCount);
             if (count > MAX_SHARD_SPLIT)
                 count = MAX_SHARD_SPLIT;
             assert !(count < 0);    // Must be positive, 0 or NaN, which should translate to baseShardCount
@@ -456,8 +462,8 @@ public abstract class Controller
             if (logger.isDebugEnabled())
                 logger.debug("Shard count {} for density {}, {} times target {}",
                              shards,
-                             FBUtilities.prettyPrintBinary(density, "B", " "),
-                             density / targetSSTableSize,
+                             FBUtilities.prettyPrintBinary(localDensity, "B", " "),
+                             localDensity / targetSSTableSize,
                              FBUtilities.prettyPrintBinary(targetSSTableSize, "B", " "));
             return shards;
         }
@@ -465,7 +471,7 @@ public abstract class Controller
         {
             // How many we would have to aim for the target size. Divided by the base shard count, so that we can ensure
             // the result is a multiple of it by multiplying back below.
-            double count = density / (targetSSTableSize * baseShardCount);
+            double count = localDensity / (targetSSTableSize * baseShardCount);
             // Take a logarithm of the count (in base 2), and adjust it by the given growth modifier.
             // Adjust by 0.5 to round the exponent so that the result falls between targetSSTableSize * sqrt(0.5) and
             // targetSSTableSize * sqrt(2). Finally, make sure the exponent is at least 0 and not greater than the
@@ -488,8 +494,8 @@ public abstract class Controller
                 long targetSize = (long) (targetSSTableSize * Math.exp(countLog * sstableGrowthModifier));
                 logger.debug("Shard count {} for density {}, {} times target {}",
                              shards,
-                             FBUtilities.prettyPrintBinary(density, "B", " "),
-                             density / targetSize,
+                             FBUtilities.prettyPrintBinary(localDensity, "B", " "),
+                             localDensity / targetSize,
                              FBUtilities.prettyPrintBinary(targetSize, "B", " "));
             }
             return shards;

apache/cassandra

src/java/org/apache/cassandra/db/compaction/unified/Controller.java
@@ -195,12 +195,18 @@ public class Controller
      * This is calculated as a power-of-two multiple of baseShardCount, so that the expected size of resulting sstables
      * is between targetSSTableSizeMin and 2*targetSSTableSizeMin (in other words, sqrt(0.5) * targetSSTableSize and
      * sqrt(2) * targetSSTableSize), with a minimum of baseShardCount shards for smaller sstables.
+     *
+     * Note that to get the sstables resulting from this splitting within the bounds, the density argument must be
+     * normalized to the span that is being split. In other words, if no disks are defined, the density should be
+     * scaled by the token coverage of the locally-owned ranges. If multiple data directories are defined, the density
+     * should be scaled by the token coverage of the respective data directory. That is localDensity = size / span,
+     * where the span is normalized so that span = 1 when the data covers the range that is being split.
      */
-    public int getNumShards(double density)
+    public int getNumShards(double localDensity)
     {
         // How many we would have to aim for the target size. Divided by the base shard count, so that we can ensure
         // the result is a multiple of it by multiplying back below.
-        double count = density / (targetSSTableSizeMin * baseShardCount);
+        double count = localDensity / (targetSSTableSizeMin * baseShardCount);
         if (count > MAX_SHARD_SPLIT)
             count = MAX_SHARD_SPLIT;
         assert !(count < 0);    // Must be positive, 0 or NaN, which should translate to baseShardCount
@@ -212,8 +218,8 @@ public class Controller
         int shards = baseShardCount * Integer.highestOneBit((int) count | 1);
         logger.debug("Shard count {} for density {}, {} times target {}",
                      shards,
-                     FBUtilities.prettyPrintBinary(density, "B", " "),
-                     density / targetSSTableSizeMin,
+                     FBUtilities.prettyPrintBinary(localDensity, "B", " "),
+                     localDensity / targetSSTableSizeMin,
                      FBUtilities.prettyPrintBinary(targetSSTableSizeMin, "B", " "));
         return shards;
     }