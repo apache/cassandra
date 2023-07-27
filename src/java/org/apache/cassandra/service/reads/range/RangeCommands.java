261929869acb0a85bcaf241e43588663fa09aa21
Stefan Miklosovic <smiklosovic@apache.org>
2023-07-27 13:17:45 +0200

    CASSANDRA-18700 - remove AssertJ from the production code

--- src/java/org/apache/cassandra/service/reads/range/RangeCommands.java
+++ src/java/org/apache/cassandra/service/reads/range/RangeCommands.java
@@ -18,6 +18,7 @@
 
 package org.apache.cassandra.service.reads.range;
 
+import com.google.common.annotations.VisibleForTesting;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -30,7 +31,6 @@ import org.apache.cassandra.db.partitions.PartitionIterator;
 import org.apache.cassandra.index.Index;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.FBUtilities;
-import org.assertj.core.util.VisibleForTesting;
 
 public class RangeCommands
 {
