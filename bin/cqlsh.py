721a1d41649cf0e1191ff0a80df0304de5af0b61
Brandon Williams <brandonwilliams@apache.org>
2023-08-17 13:23:47 -0500

    Warn when cqlsh does not match the server version it was built with
    Patch by brandonwilliams; reviewed by bereng for CASSANDRA-18745

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -171,6 +171,7 @@ from cqlshlib.formatting import (DEFAULT_DATE_FORMAT, DEFAULT_NANOTIME_FORMAT,
                                  formatter_for)
 from cqlshlib.tracing import print_trace, print_trace_session
 from cqlshlib.util import get_file_encoding_bomsize, trim_if_present
+from cqlshlib.serverversion import version as build_version
 
 DEFAULT_HOST = '127.0.0.1'
 DEFAULT_PORT = 9042
@@ -797,6 +798,8 @@ class Shell(cmd.Cmd):
         if stdin is None:
             stdin = sys.stdin
 
+        self.check_build_versions()
+
         if tty:
             self.reset_prompt()
             self.report_connection()
@@ -815,6 +818,14 @@ class Shell(cmd.Cmd):
     @property
     def batch_mode(self):
         return not self.tty
+
+    def check_build_versions(self):
+        baseversion = self.connection_versions['build']
+        extra = baseversion.rfind('-')
+        if extra:
+            baseversion = baseversion[0:extra]
+        if baseversion != build_version:
+            print("WARNING: cqlsh was built against {}, but this server is {}.  All features may not work!".format(baseversion, build_version))
 
     @property
     def is_using_utf8(self):
