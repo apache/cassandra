897005a792a699c1f0e77309f6abd55c8b777490
Stefan Miklosovic <smiklosovic@apache.org>
2023-08-28 20:12:16 +0200

    Refactor validation logic in StorageService.rebuild
    patch by Stefan Miklosovic; reviewed by Ariel Weisberg for CASSANDRA-18803

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -1308,35 +1308,27 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
 
     public void rebuild(String sourceDc)
     {
-        try
+        if (sourceDc != null)
         {
-            // check ongoing rebuild
-            if (!isRebuilding.compareAndSet(false, true))
+            TokenMetadata.Topology topology = getTokenMetadata().cloneOnlyTokenMap().getTopology();
+            Set<String> availableDCs = topology.getDatacenterEndpoints().keySet();
+            if (!availableDCs.contains(sourceDc))
             {
-                throw new IllegalStateException("Node is still rebuilding. Check nodetool netstats.");
-            }
-
-            if (sourceDc != null)
-            {
-                TokenMetadata.Topology topology = getTokenMetadata().cloneOnlyTokenMap().getTopology();
-                Set<String> availableDCs = topology.getDatacenterEndpoints().keySet();
-                if (!availableDCs.contains(sourceDc))
-                {
-                    throw new IllegalArgumentException(String.format("Provided datacenter '%s' is not a valid datacenter, available datacenters are: %s",
-                                                                     sourceDc, String.join(",", availableDCs)));
-                }
+                throw new IllegalArgumentException(String.format("Provided datacenter '%s' is not a valid datacenter, available datacenters are: %s",
+                                                                 sourceDc, String.join(",", availableDCs)));
             }
         }
-        catch (Throwable ex)
-        {
-            isRebuilding.set(false);
-            throw ex;
-        }
 
-        logger.info("rebuild from dc: {}", sourceDc == null ? "(any dc)" : sourceDc);
+        // check ongoing rebuild
+        if (!isRebuilding.compareAndSet(false, true))
+        {
+            throw new IllegalStateException("Node is still rebuilding. Check nodetool netstats.");
+        }
 
         try
         {
+            logger.info("rebuild from dc: {}", sourceDc == null ? "(any dc)" : sourceDc);
+
             RangeStreamer streamer = new RangeStreamer(tokenMetadata,
                                                        null,
                                                        FBUtilities.getBroadcastAddress(),
