b3935c4a913aa914dffd74103d947d1a08d31d7f
Jon Meredith <jonmeredith@apache.org>
2023-09-05 15:39:34 -0600

    Internode legacy SSL storage port certificate is not hot reloaded on update
    patch by Jon Meredith; reviewed by Dinesh Joshi for CASSANDRA-18681

--- test/unit/org/apache/cassandra/security/SSLFactoryTest.java
+++ test/unit/org/apache/cassandra/security/SSLFactoryTest.java
@@ -19,6 +19,7 @@
 package org.apache.cassandra.security;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
@@ -76,6 +77,7 @@ public class SSLFactoryTest
     @Before
     public void setup()
     {
+        SSLFactory.clearSslContextCache();
         encryptionOptions = new ServerEncryptionOptions()
                             .withTrustStore("test/conf/cassandra_ssl_test.truststore")
                             .withTrustStorePassword("cassandra")
@@ -112,20 +114,24 @@ public class SSLFactoryTest
         {
             ServerEncryptionOptions options = addKeystoreOptions(encryptionOptions)
                                               .withInternodeEncryption(ServerEncryptionOptions.InternodeEncryption.all);
+            // emulate InboundSockets and share the cert but with different options, no extra hot reloading init
+            ServerEncryptionOptions legacyOptions = options.withOptional(false).withInternodeEncryption(ServerEncryptionOptions.InternodeEncryption.all);
+            SSLFactory.initHotReloading(options, options, true); // deliberately not initializing with legacyOptions to match InboundSockets.addBindings
 
-            SSLFactory.initHotReloading(options, options, true);
-
-            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
+            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SslContext oldLegacyCtx = SSLFactory.getOrCreateSslContext(legacyOptions, true, ISslContextFactory.SocketType.CLIENT, "test legacy");
             File keystoreFile = new File(options.keystore);
 
-            SSLFactory.checkCertFilesForHotReloading(options, options);
+            SSLFactory.checkCertFilesForHotReloading();
 
             keystoreFile.trySetLastModified(System.currentTimeMillis() + 15000);
 
-            SSLFactory.checkCertFilesForHotReloading(options, options);
-            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
+            SSLFactory.checkCertFilesForHotReloading();
+            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SslContext newLegacyCtx = SSLFactory.getOrCreateSslContext(legacyOptions, true, ISslContextFactory.SocketType.CLIENT, "test legacy");
 
             Assert.assertNotSame(oldCtx, newCtx);
+            Assert.assertNotSame(oldLegacyCtx, newLegacyCtx);
         }
         catch (Exception e)
         {
@@ -177,21 +183,26 @@ public class SSLFactoryTest
         try
         {
             ServerEncryptionOptions options = addPEMKeystoreOptions(encryptionOptions)
-                                              .withInternodeEncryption(ServerEncryptionOptions.InternodeEncryption.all);
+                                              .withInternodeEncryption(ServerEncryptionOptions.InternodeEncryption.dc);
+            // emulate InboundSockets and share the cert but with different options, no extra hot reloading init
+            ServerEncryptionOptions legacyOptions = options.withOptional(false).withInternodeEncryption(ServerEncryptionOptions.InternodeEncryption.all);
 
-            SSLFactory.initHotReloading(options, options, true);
+            SSLFactory.initHotReloading(options, options, true); // deliberately not initializing with legacyOptions to match InboundSockets.addBindings
 
-            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
+            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SslContext oldLegacyCtx = SSLFactory.getOrCreateSslContext(legacyOptions, true, ISslContextFactory.SocketType.CLIENT, "test legacy");
             File keystoreFile = new File(options.keystore);
 
-            SSLFactory.checkCertFilesForHotReloading(options, options);
+            SSLFactory.checkCertFilesForHotReloading();
 
             keystoreFile.trySetLastModified(System.currentTimeMillis() + 15000);
 
-            SSLFactory.checkCertFilesForHotReloading(options, options);
-            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
+            SSLFactory.checkCertFilesForHotReloading();
+            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SslContext newLegacyCtx = SSLFactory.getOrCreateSslContext(legacyOptions, true, ISslContextFactory.SocketType.CLIENT, "test legacy");
 
             Assert.assertNotSame(oldCtx, newCtx);
+            Assert.assertNotSame(oldLegacyCtx, newLegacyCtx);
         }
         catch (Exception e)
         {
@@ -219,20 +230,26 @@ public class SSLFactoryTest
         try
         {
             ServerEncryptionOptions options = addKeystoreOptions(encryptionOptions);
+            // emulate InboundSockets and share the cert but with different options, no extra hot reloading init
+            ServerEncryptionOptions legacyOptions = options.withOptional(false).withInternodeEncryption(ServerEncryptionOptions.InternodeEncryption.all);
 
-            SSLFactory.initHotReloading(options, options, true);
-            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
-            File keystoreFile = new File(options.keystore);
+            File testKeystoreFile = new File(options.keystore + ".test");
+            FileUtils.copyFile(new File(options.keystore).toJavaIOFile(), testKeystoreFile.toJavaIOFile());
+            options = options.withKeyStore(testKeystoreFile.path());
 
-            SSLFactory.checkCertFilesForHotReloading(options, options);
-            keystoreFile.trySetLastModified(System.currentTimeMillis() + 5000);
+            SSLFactory.initHotReloading(options, options, true);  // deliberately not initializing with legacyOptions to match InboundSockets.addBindings
 
-            ServerEncryptionOptions modOptions = new ServerEncryptionOptions(options)
-                                                 .withKeyStorePassword("bad password");
-            SSLFactory.checkCertFilesForHotReloading(modOptions, modOptions);
-            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
+            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SslContext oldLegacyCtx = SSLFactory.getOrCreateSslContext(legacyOptions, true, ISslContextFactory.SocketType.CLIENT, "test legacy");
+
+            changeKeystorePassword(options.keystore, options.keystore_password, "bad password");
+
+            SSLFactory.checkCertFilesForHotReloading();
+            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SslContext newLegacyCtx = SSLFactory.getOrCreateSslContext(legacyOptions, true, ISslContextFactory.SocketType.CLIENT, "test legacy");
 
             Assert.assertSame(oldCtx, newCtx);
+            Assert.assertSame(oldLegacyCtx, newLegacyCtx);
         }
         finally
         {
@@ -253,14 +270,14 @@ public class SSLFactoryTest
 
 
             SSLFactory.initHotReloading(options, options, true);
-            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
-            SSLFactory.checkCertFilesForHotReloading(options, options);
+            SslContext oldCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
+            SSLFactory.checkCertFilesForHotReloading();
 
             testKeystoreFile.trySetLastModified(System.currentTimeMillis() + 15000);
             FileUtils.forceDelete(testKeystoreFile.toJavaIOFile());
 
-            SSLFactory.checkCertFilesForHotReloading(options, options);
-            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT);
+            SSLFactory.checkCertFilesForHotReloading();
+            SslContext newCtx = SSLFactory.getOrCreateSslContext(options, true, ISslContextFactory.SocketType.CLIENT, "test");
 
             Assert.assertSame(oldCtx, newCtx);
         }
@@ -282,7 +299,7 @@ public class SSLFactoryTest
                                     .withCipherSuites("TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256");
 
         SslContext ctx1 = SSLFactory.getOrCreateSslContext(options, true,
-                                                           ISslContextFactory.SocketType.SERVER);
+                                                           ISslContextFactory.SocketType.SERVER, "test");
 
         Assert.assertTrue(ctx1.isServer());
         Assert.assertEquals(ctx1.cipherSuites(), options.cipher_suites);
@@ -290,7 +307,7 @@ public class SSLFactoryTest
         options = options.withCipherSuites("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256");
 
         SslContext ctx2 = SSLFactory.getOrCreateSslContext(options, true,
-                                                           ISslContextFactory.SocketType.CLIENT);
+                                                           ISslContextFactory.SocketType.CLIENT, "test");
 
         Assert.assertTrue(ctx2.isClient());
         Assert.assertEquals(ctx2.cipherSuites(), options.cipher_suites);
@@ -309,7 +326,7 @@ public class SSLFactoryTest
         .withRequireClientAuth(true)
         .withRequireEndpointVerification(false);
 
-        SSLFactory.CacheKey cacheKey1 = new SSLFactory.CacheKey(encryptionOptions1, ISslContextFactory.SocketType.SERVER
+        SSLFactory.CacheKey cacheKey1 = new SSLFactory.CacheKey(encryptionOptions1, ISslContextFactory.SocketType.SERVER, "test"
         );
 
         Map<String,String> parameters2 = new HashMap<>();
@@ -322,7 +339,7 @@ public class SSLFactoryTest
         .withRequireClientAuth(true)
         .withRequireEndpointVerification(false);
 
-        SSLFactory.CacheKey cacheKey2 = new SSLFactory.CacheKey(encryptionOptions2, ISslContextFactory.SocketType.SERVER
+        SSLFactory.CacheKey cacheKey2 = new SSLFactory.CacheKey(encryptionOptions2, ISslContextFactory.SocketType.SERVER, "test"
         );
 
         Assert.assertEquals(cacheKey1, cacheKey2);
@@ -339,7 +356,7 @@ public class SSLFactoryTest
         .withSslContextFactory(new ParameterizedClass(DummySslContextFactoryImpl.class.getName(), parameters1))
         .withProtocol("TLSv1.1");
 
-        SSLFactory.CacheKey cacheKey1 = new SSLFactory.CacheKey(encryptionOptions1, ISslContextFactory.SocketType.SERVER
+        SSLFactory.CacheKey cacheKey1 = new SSLFactory.CacheKey(encryptionOptions1, ISslContextFactory.SocketType.SERVER, "test"
         );
 
         Map<String,String> parameters2 = new HashMap<>();
@@ -350,7 +367,7 @@ public class SSLFactoryTest
         .withSslContextFactory(new ParameterizedClass(DummySslContextFactoryImpl.class.getName(), parameters2))
         .withProtocol("TLSv1.1");
 
-        SSLFactory.CacheKey cacheKey2 = new SSLFactory.CacheKey(encryptionOptions2, ISslContextFactory.SocketType.SERVER
+        SSLFactory.CacheKey cacheKey2 = new SSLFactory.CacheKey(encryptionOptions2, ISslContextFactory.SocketType.SERVER, "test"
         );
 
         Assert.assertNotEquals(cacheKey1, cacheKey2);
@@ -386,5 +403,27 @@ public class SSLFactoryTest
         X509KeyManager keyManager1 = (X509KeyManager) keyManager.invoke(provider);
         final Certificate[] certificates = keyManager1.getCertificateChain("cassandra_ssl_test");
         return certificates[0];
+    }
+
+    void changeKeystorePassword(String filename, String currentPassword, String newPassword)
+    {
+        try
+        {
+            KeyStore keystore = KeyStore.getInstance(KeyStore.getDefaultType());
+            char[] loadPasswd = currentPassword.toCharArray();
+            char[] storePasswd = newPassword.toCharArray();
+            try (FileInputStream is = new FileInputStream(filename))
+            {
+                keystore.load(is, loadPasswd);
+            }
+            try (FileOutputStream os = new FileOutputStream(filename))
+            {
+                keystore.store(os, storePasswd);
+            }
+        }
+        catch (Throwable tr)
+        {
+            throw new RuntimeException(tr);
+        }
     }
 }
