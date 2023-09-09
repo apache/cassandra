48e1b46991d999c8588b42bc43fb3f0300fee117
Mick Semb Wever <mck@apache.org>
2023-09-09 21:14:10 +0200

    Fixes for in-tree scripts: offline mode, maybe-build for fqltool-test, jvm-dtest-upgrade
     - jvm-dtest-upgrade script running in ci-cassandra.a.o (needs default bash)
     - add offline mode (to avoid `git fetch` before building dtest-jars on other branches),
     - avoid rebuilding in fqltool-test target, as is standard for test targets.
     - python detection and override
     - FileTest cleanup (tmp directory was left unreadable and undeletable)
     - reuse ~/.ccm (like normal)
     - remove support for python 3.6
     - remove docker volumes in image (they are added at run time)
     - remove superfluousjava version debug
    
     patch by Mick Semb Wever; reviewed by Brandon Williams, Ekaterina Dimitrova, Josh McKenzie, Maxim Muzafarov for CASSANDRA-18665

--- .build/docker/run-tests.sh
+++ .build/docker/run-tests.sh
@@ -69,8 +69,11 @@ if [[ ! "${java_version}" =~ $regx_java_version ]]; then
     exit 1
 fi
 
-python_version="3.6"
-command -v python >/dev/null 2>&1 && python_version="$(python -V | awk '{print $2}' | awk -F'.' '{print $1"."$2}')"
+# allow python version override, otherwise default to current python version or 3.7
+if [ "x" == "x${python_version}" ] ; then
+    command -v python >/dev/null 2>&1 && python_version="$(python -V 2>&1 | awk '{print $2}' | awk -F'.' '{print $1"."$2}')"
+    python_version="${python_version:-3.7}"
+fi
 
 # print debug information on versions
 docker --version
@@ -150,6 +153,7 @@ elif [[ "${target}" =~ dtest* ]] ; then
 else
     docker_flags="--cpus=${docker_cpus} -m 5g --memory-swap 5g"
 fi
+
 docker_flags="${docker_flags} --env-file build/env.list -d --rm"
 
 # make sure build_dir is good
