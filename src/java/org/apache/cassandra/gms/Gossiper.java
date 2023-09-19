2ddab34eaed4e344645263d9766fb14f399db194
Cameron Zemek <cameron@instaclustr.com>
2023-09-19 12:54:15 +1000

    Wait for live endpoints as part of waiting for gossip to settle

--- src/java/org/apache/cassandra/gms/Gossiper.java
+++ src/java/org/apache/cassandra/gms/Gossiper.java
@@ -2291,12 +2291,14 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
         int totalPolls = 0;
         int numOkay = 0;
         int epSize = Gossiper.instance.getEndpointCount();
+        int liveSize = Gossiper.instance.getLiveMembers().size();
         while (numOkay < GOSSIP_SETTLE_POLL_SUCCESSES_REQUIRED)
         {
             Uninterruptibles.sleepUninterruptibly(GOSSIP_SETTLE_POLL_INTERVAL_MS, TimeUnit.MILLISECONDS);
             int currentSize = Gossiper.instance.getEndpointCount();
+            int currentLive = Gossiper.instance.getLiveMembers().size();
             totalPolls++;
-            if (currentSize == epSize)
+            if (currentSize == epSize && currentLive == liveSize && (epSize == liveSize || liveSize > 1))
             {
                 logger.debug("Gossip looks settled.");
                 numOkay++;
@@ -2307,6 +2309,7 @@ public class Gossiper implements IFailureDetectionEventListener, GossiperMBean
                 numOkay = 0;
             }
             epSize = currentSize;
+            liveSize = currentLive;
             if (forceAfter > 0 && totalPolls > forceAfter)
             {
                 logger.warn("Gossip not settled but startup forced by cassandra.skip_wait_for_gossip_to_settle. Gossip total polls: {}",
