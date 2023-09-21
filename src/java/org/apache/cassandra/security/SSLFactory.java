2ef82f57346b276802eb2250eeb3ba1cb935e73b
Jon Meredith <jonmeredith@apache.org>
2023-09-21 15:25:43 -0600

    unused imports

--- src/java/org/apache/cassandra/security/SSLFactory.java
+++ src/java/org/apache/cassandra/security/SSLFactory.java
@@ -22,7 +22,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
