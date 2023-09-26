3c5af8ff6b02fd061ec31b1c0a9b2cd439e2806b
David Capwell <dcapwell@apache.org>
2023-09-26 15:18:44 -0700

    import order

--- src/java/org/apache/cassandra/db/repair/CassandraTableRepairManager.java
+++ src/java/org/apache/cassandra/db/repair/CassandraTableRepairManager.java
@@ -25,7 +25,6 @@ import java.util.concurrent.Future;
 
 import com.google.common.base.Predicate;
 
-import org.apache.cassandra.repair.SharedContext;
 import org.apache.cassandra.db.ColumnFamilyStore;
 import org.apache.cassandra.db.compaction.CompactionManager;
 import org.apache.cassandra.dht.Bounds;
@@ -33,6 +32,7 @@ import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
 import org.apache.cassandra.io.sstable.format.SSTableReader;
 import org.apache.cassandra.metrics.TopPartitionTracker;
+import org.apache.cassandra.repair.SharedContext;
 import org.apache.cassandra.repair.TableRepairManager;
 import org.apache.cassandra.repair.ValidationPartitionIterator;
 import org.apache.cassandra.repair.NoSuchRepairSessionException;
