79132c378d145938f1cc5498c39c4389fbfb5609
Stefan Miklosovic <smiklosovic@apache.org>
2023-10-03 16:22:14 +0200

    refactoring

--- src/java/org/apache/cassandra/service/CassandraDaemon.java
+++ src/java/org/apache/cassandra/service/CassandraDaemon.java
@@ -780,9 +780,9 @@ public class CassandraDaemon
             start();
 
             logger.info("Startup complete");
-            if (SystemD.isAvailable()) {
-                SystemD.get().notifyReady();
-            }
+
+            if (Systemd.isAvailable())
+                Systemd.get().notifyReady();
         }
         catch (Throwable e)
         {
