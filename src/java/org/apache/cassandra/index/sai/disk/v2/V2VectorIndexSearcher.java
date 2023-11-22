2bb973c1c8e0fc80b812f3b0c959375f573ae5f1
Jonathan Ellis <jbellis@datastax.com>
2023-11-22 08:45:22 -0600

    fix updateExpectedNodes to use the observed row count instead of segment id (!)

--- src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
+++ src/java/org/apache/cassandra/index/sai/disk/v2/V2VectorIndexSearcher.java
@@ -354,7 +354,6 @@ public class V2VectorIndexSearcher extends IndexSearcher implements SegmentOrder
         // path to take until we have an accurate key count.
         SparseFixedBitSet bits = bitSetForSearch();
         IntArrayList rowIds = new IntArrayList();
-        var maxSegmentRowId = metadata.toSegmentRowId(metadata.maxSSTableRowId);
         try (var primaryKeyMap = primaryKeyMapFactory.newPerSSTablePrimaryKeyMap();
              var ordinalsView = graph.getOrdinalsView())
         {
@@ -396,13 +395,13 @@ public class V2VectorIndexSearcher extends IndexSearcher implements SegmentOrder
         // else ask the index to perform a search limited to the bits we created
         float[] queryVector = exp.lower.value.vector;
         var results = graph.search(queryVector, topK, limit, bits, context);
-        updateExpectedNodes(results.getVisitedCount(), getRawExpectedNodes(topK, maxSegmentRowId));
+        updateExpectedNodes(results.getVisitedCount(), getRawExpectedNodes(topK, numRows));
         return toPrimaryKeyIterator(results, context);
     }
 
-    private int getRawExpectedNodes(int topK, int maxSegmentRowId)
+    private int getRawExpectedNodes(int topK, int nPermittedOrdinals)
     {
-        return VectorMemtableIndex.expectedNodesVisited(topK, maxSegmentRowId, graph.size());
+        return VectorMemtableIndex.expectedNodesVisited(topK, nPermittedOrdinals, graph.size());
     }
 
     private void logAndTrace(String message, Object... args)
