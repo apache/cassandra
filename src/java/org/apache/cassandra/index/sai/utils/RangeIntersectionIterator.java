3e85201fb11f06e9618431fc5fd1f39b89d747b5
Jonathan Ellis <jbellis@datastax.com>
2023-12-01 08:05:32 -0600

    remove unnecessary compareTo calls

--- src/java/org/apache/cassandra/index/sai/utils/RangeIntersectionIterator.java
+++ src/java/org/apache/cassandra/index/sai/utils/RangeIntersectionIterator.java
@@ -77,7 +77,7 @@ public class RangeIntersectionIterator extends RangeIterator
         rangeStats[0]++;
 
         outer:
-        while (highestKey != null && highestKey.compareTo(getMaximum()) <= 0)
+        while (highestKey != null)
         {
             // Try advance all iterators to the highest key seen so far.
             // Once this inner loop finishes normally, all iterators are guaranteed to be at the same value.
