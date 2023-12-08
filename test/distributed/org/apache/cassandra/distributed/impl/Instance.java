00cbf92200c50ae8f3b1128d0b95d3717816c8ce
Ariel Weisberg <aweisberg@apple.com>
2023-12-08 14:02:41 -0500

    Instance.receiveMessage should use sync
    We mistakenly set this to async thinking async delivery was needed, but whether it is async or not is based on the boolean paramter passed to it.
    
    Right now SchemaTest relies on it running sync, deserializaing in the other class loader and generating an error, and then handling that error from inside C*. This isn't how it actually works on failed deserialize (it's just a timeout), but the test relies on it so for now not changing it.

--- test/distributed/org/apache/cassandra/distributed/impl/Instance.java
+++ test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -504,7 +504,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
     @Override
     public void receiveMessage(IMessage message)
     {
-        async(receiveMessageRunnable(message)).apply(false);
+        sync(receiveMessageRunnable(message)).accept(false);
     }
 
     @Override
