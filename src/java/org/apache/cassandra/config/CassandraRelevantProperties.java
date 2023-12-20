da1bcc5be5766de9a050f81995150de4aea219be
Jacek Lewandowski <lewandowski.jacek@gmail.com>
2023-12-20 12:11:52 +0100

    apply review changes

--- src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
+++ src/java/org/apache/cassandra/config/CassandraRelevantProperties.java
@@ -619,7 +619,7 @@ public enum CassandraRelevantProperties
     USE_NIX_RECURSIVE_DELETE("cassandra.use_nix_recursive_delete"),
     /** Gossiper compute expiration timeout. Default value 3 days. */
     VERY_LONG_TIME_MS("cassandra.very_long_time_ms", "259200000"),
-    WAIT_FOR_TRACING_EVENTS_TIMEOUT_SECS("cassandra.wait_for_tracing_events_timeout_secs", "0")
+    WAIT_FOR_TRACING_EVENTS_TIMEOUT_SECS("cassandra.wait_for_tracing_events_timeout_secs", "0"),
     ;
 
     static
