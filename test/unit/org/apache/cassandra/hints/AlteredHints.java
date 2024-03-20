d216ee17bc73b41619e7277a67fa30f664eaafbb
Stefan Miklosovic <smiklosovic@apache.org>
2024-03-20 12:24:37 +0100

    Do not go to disk for reading hints file sizes
    patch by Stefan Miklosovic; reviewed by Aleksey Yeschenko, Chris Lohfink for CASSANDRA-19477

--- test/unit/org/apache/cassandra/hints/AlteredHints.java
+++ test/unit/org/apache/cassandra/hints/AlteredHints.java
@@ -37,6 +37,7 @@ import org.apache.cassandra.schema.TableMetadata;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.RowUpdateBuilder;
 import org.apache.cassandra.schema.KeyspaceParams;
+import org.hamcrest.Matchers;
 
 import static org.apache.cassandra.utils.ByteBufferUtil.bytes;
 
@@ -100,6 +101,8 @@ public abstract class AlteredHints
                     hintNum++;
                 }
             }
+
+            Assert.assertThat(descriptor.hintsFileSize(dir), Matchers.greaterThan(0L));
         }
 
         try (HintsReader reader = HintsReader.open(descriptor.file(dir)))
