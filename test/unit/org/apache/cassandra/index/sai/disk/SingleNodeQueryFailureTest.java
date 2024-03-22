36e2667288635545ab1378ae3b5ce4a1e04a9b17
Ekaterina Dimitrova <ekaterina.dimitrova@datastax.com>
2024-03-22 09:54:40 -0400

    Change SingleNodeQueryFailureTest

--- test/unit/org/apache/cassandra/index/sai/disk/SingleNodeQueryFailureTest.java
+++ test/unit/org/apache/cassandra/index/sai/disk/SingleNodeQueryFailureTest.java
@@ -37,8 +37,8 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class SingleNodeQueryFailureTest extends SAITester
 {
-    private static final String CREATE_TABLE_TEMPLATE = "CREATE TABLE %s (id text PRIMARY KEY, v1 text) WITH " +
-                                                        "compaction = {'class' : 'SizeTieredCompactionStrategy', 'enabled' : false }";
+    private static final String CREATE_TABLE_TEMPLATE = "CREATE TABLE %s (id text PRIMARY KEY, v1 int, v2 text) WITH " +
+            "compaction = {'class' : 'SizeTieredCompactionStrategy', 'enabled' : false }";
 
     @Before
     public void setup()
@@ -67,31 +67,32 @@ public class SingleNodeQueryFailureTest extends SAITester
     private void testFailedMultiIndexesQuery(String name, Class<?> targetClass, String targetMethod) throws Throwable
     {
         Injection injection = Injections.newCustom(name)
-                                        .add(newInvokePoint().onClass(targetClass).onMethod(targetMethod))
-                                        .add(newActionBuilder().actions().doThrow(RuntimeException.class, quote("Injected failure!")))
-                                        .build();
+                .add(newInvokePoint().onClass(targetClass).onMethod(targetMethod))
+                .add(newActionBuilder().actions().doThrow(RuntimeException.class, quote("Injected failure!")))
+                .build();
 
         createTable(CREATE_TABLE_TEMPLATE);
         createIndex(String.format(CREATE_INDEX_TEMPLATE, "v1"));
+        createIndex(String.format(CREATE_INDEX_TEMPLATE, "v2"));
 
-        execute("INSERT INTO %s (id, v1) VALUES ('1', '0')");
+        execute("INSERT INTO %s (id, v1, v2) VALUES ('1', 0, '0')");
         flush();
-        execute("INSERT INTO %s (id, v1) VALUES ('2', '1')");
+        execute("INSERT INTO %s (id, v1, v2) VALUES ('2', 1, '1')");
         flush();
-        execute("INSERT INTO %s (id, v1) VALUES ('3', '2')");
+        execute("INSERT INTO %s (id, v1, v2) VALUES ('3', 2, '2')");
         flush();
 
         try
         {
             Injections.inject(injection);
 
-            assertThatThrownBy(() -> executeNet("SELECT id FROM %s WHERE v1 = '0'"))
+            assertThatThrownBy(() -> executeNet("SELECT id FROM %s WHERE v1 < 1 and v2 = '0'"))
                     .isInstanceOf(ReadFailureException.class);
 
-            assertThatThrownBy(() -> executeNet("SELECT id FROM %s WHERE v1 = '1'"))
+            assertThatThrownBy(() -> executeNet("SELECT id FROM %s WHERE v1 >= 1 and v2 = '1'"))
                     .isInstanceOf(ReadFailureException.class);
 
-            assertThatThrownBy(() -> executeNet("SELECT id FROM %s WHERE v1 = '2'"))
+            assertThatThrownBy(() -> executeNet("SELECT id FROM %s WHERE v1 >= 2 and v2 = '2'"))
                     .isInstanceOf(ReadFailureException.class);
         }
         catch (Exception e)
@@ -103,8 +104,9 @@ public class SingleNodeQueryFailureTest extends SAITester
             injection.disable();
         }
 
-        Assert.assertEquals(1, executeNet("SELECT id FROM %s WHERE v1 = '0'").all().size());
-        Assert.assertEquals(1, executeNet("SELECT id FROM %s WHERE v1 = '1'").all().size());
-        Assert.assertEquals(1, executeNet("SELECT id FROM %s WHERE v1 = '2'").all().size());
+        Assert.assertEquals(3, executeNet("SELECT id FROM %s WHERE v1 >= 0").all().size());
+        Assert.assertEquals(1, executeNet("SELECT id FROM %s WHERE v2 = '0'").all().size());
+        Assert.assertEquals(1, executeNet("SELECT id FROM %s WHERE v2 = '1'").all().size());
+        Assert.assertEquals(1, executeNet("SELECT id FROM %s WHERE v2 = '2'").all().size());
     }
 }
