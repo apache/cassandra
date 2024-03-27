26e42ecd87b2a930c6a1e4e7b8b15c8798cb1c2f
Blake Eggleston <blake@ultrablake.com>
2024-03-26 17:23:46 -0700

    rebase fixes

--- src/java/org/apache/cassandra/metrics/CacheMetrics.java
+++ src/java/org/apache/cassandra/metrics/CacheMetrics.java
@@ -27,6 +27,7 @@ import static org.apache.cassandra.metrics.CassandraMetricsRegistry.Metrics;
  */
 public class CacheMetrics extends AbstractCacheMetrics
 {
+    public static final String CACHE = "Cache";
     /** Cache capacity in bytes */
     public final Gauge<Long> capacity;
 
@@ -44,7 +45,7 @@ public class CacheMetrics extends AbstractCacheMetrics
      */
     public CacheMetrics(String type, CacheSize cache)
     {
-        super(new DefaultNameFactory("Cache", type), type);
+        super(new DefaultNameFactory(CACHE, type), type);
         capacity = Metrics.register(factory.createMetricName("Capacity"), cache::capacity);
         size = Metrics.register(factory.createMetricName("Size"), cache::weightedSize);
         entries = Metrics.register(factory.createMetricName("Entries"), cache::size);
