a1e294a54d6bf8968e6db7e8e635a72b3397e59a
Tibor Répási <rtib@users.noreply.github.com>
2024-04-02 20:45:34 +0200

    deprecate the usage of apt-key and adopt distribution name

--- doc/modules/cassandra/examples/BASH/get_deb_package.sh
+++ doc/modules/cassandra/examples/BASH/get_deb_package.sh
@@ -1,2 +1,2 @@
-$ echo "deb https://debian.cassandra.apache.org 40x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
-deb https://debian.cassandra.apache.org 40x main
+$ echo "deb [signed-by=/etc/apt/keyrings/apache-cassandra.asc] https://debian.cassandra.apache.org 41x main" | sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list
+deb https://debian.cassandra.apache.org 41x main
