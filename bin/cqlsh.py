95c05b8081d5bf0c31a81a2ee89602c8cc1423cd
Мещеряков Вячеслав Юрьевич <vmeshcheriakov@stsoft.ru>
2024-04-04 15:41:53 +0300

    Support legacy plain_text_auth section in credentials file removed unintentionally
    patch by Мещеряков Вячеслав Юрьевич; reviewed by Stefan Miklosovic and Brad Schoening for CASSANDRA-19498

--- bin/cqlsh.py
+++ bin/cqlsh.py
@@ -2183,7 +2183,7 @@ def read_options(cmdlineargs, environment):
         credentials.read(options.credentials)
 
         # use the username from credentials file but fallback to cqlshrc if username is absent from the command line parameters
-        options.username = username_from_cqlshrc
+        options.username = option_with_default(credentials.get, 'plain_text_auth', 'username', username_from_cqlshrc)
 
     if not options.password:
         rawcredentials = configparser.RawConfigParser()
@@ -2191,7 +2191,6 @@ def read_options(cmdlineargs, environment):
 
         # handling password in the same way as username, priority cli > credentials > cqlshrc
         options.password = option_with_default(rawcredentials.get, 'plain_text_auth', 'password', password_from_cqlshrc)
-        options.password = password_from_cqlshrc
     elif not options.insecure_password_without_warning:
         print("\nWarning: Using a password on the command line interface can be insecure."
               "\nRecommendation: use the credentials file to securely provide the password.\n", file=sys.stderr)
