c8e5ff23f927900be2f8d9890976177d737b156e
maxwellguo <cclive1601@gmail.com>
2024-04-06 11:47:27 +0800

    fix some review comments

--- src/java/org/apache/cassandra/db/commitlog/CommitLogMBean.java
+++ src/java/org/apache/cassandra/db/commitlog/CommitLogMBean.java
@@ -44,8 +44,8 @@ public interface CommitLogMBean
      * There are only three different formats to express three time precisions:
      * Seconds, Milliseconds, and Microseconds.
      * Seconds format: yyyy:MM:dd HH:mm:ss (2012:04:31 20:43:12)
-     * Milliseconds format: yyyy:MM:dd HH:mm:ss[.[SSS]] (2012:04:31 20:43:12.633)
-     * Microseconds format: yyyy:MM:dd HH:mm:ss[.[SSSSSS]] (2012:04:31 20:43:12.633222)
+     * Milliseconds format: yyyy:MM:dd HH:mm:ss.SSS (2012:04:31 20:43:12.633)
+     * Microseconds format: yyyy:MM:dd HH:mm:ss.SSSSSS (2012:04:31 20:43:12.633222)
      *
      * Recovery will continue through the segment when the first client-supplied
      * timestamp greater than this time is encountered, but only mutations less than
