3d93942897eb0eda53a75dc29747d822fe0f1555
Blake Eggleston <blake@ultrablake.com>
2024-06-17 16:28:05 -0700

    Don't run incremental repairs for consensus only repairs
    Patch by Blake Eggleston; reviewed by David Capwell for CASSANDRA-19717

--- src/java/org/apache/cassandra/repair/messages/RepairOption.java
+++ src/java/org/apache/cassandra/repair/messages/RepairOption.java
@@ -463,6 +463,11 @@ public class RepairOption
         return accordOnly;
     }
 
+    public boolean isConsensusOnly()
+    {
+        return paxosOnly() || accordOnly();
+    }
+
     public boolean isConsensusMigration()
     {
         return isConsensusMigration;
