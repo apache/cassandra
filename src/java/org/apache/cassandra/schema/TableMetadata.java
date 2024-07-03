a5767a58343f7c954e2a5bdb36811f909483dd97
Stefan Miklosovic <smiklosovic@apache.org>
2024-07-03 13:49:42 +0200

    Fix schema.cql created by a snapshot after dropping more than one column
    patch by Stefan Miklosovic; reviewed Benjamin Lerer, Francisco Guerrero for CASSANDRA-19747

--- src/java/org/apache/cassandra/schema/TableMetadata.java
+++ src/java/org/apache/cassandra/schema/TableMetadata.java
@@ -1237,7 +1237,7 @@ public class TableMetadata implements SchemaElement
                 DroppedColumn dropped = iterDropped.next();
                 dropped.column.appendCqlTo(builder);
 
-                if (!hasSingleColumnPrimaryKey || iter.hasNext())
+                if (!hasSingleColumnPrimaryKey || iterDropped.hasNext())
                     builder.append(',');
 
                 builder.newLine();
