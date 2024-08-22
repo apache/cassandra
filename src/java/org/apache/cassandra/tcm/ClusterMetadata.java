bf8bd5c8ca273df8a808f319429c4118d4f60ec0
Marcus Eriksson <marcuse@apache.org>
2024-08-22 17:01:43 +0200

    grab lowest node id to execute the snapshot

--- src/java/org/apache/cassandra/tcm/ClusterMetadata.java
+++ src/java/org/apache/cassandra/tcm/ClusterMetadata.java
@@ -74,6 +74,7 @@ import org.apache.cassandra.tcm.serialization.Version;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.cassandra.utils.Pair;
 
+import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static org.apache.cassandra.config.CassandraRelevantProperties.LINE_SEPARATOR;
 import static org.apache.cassandra.db.TypeSizes.sizeof;
 
@@ -98,6 +99,7 @@ public class ClusterMetadata
     // These two fields are lazy but only for the test purposes, since their computation requires initialization of the log ks
     private EndpointsForRange fullCMSReplicas;
     private Set<InetAddressAndPort> fullCMSEndpoints;
+    private Set<NodeId> fullCMSIds;
 
     public ClusterMetadata(IPartitioner partitioner)
     {
@@ -179,6 +181,13 @@ public class ClusterMetadata
         if (fullCMSEndpoints == null)
             this.fullCMSEndpoints = ImmutableSet.copyOf(placements.get(ReplicationParams.meta(this)).reads.byEndpoint().keySet());
         return fullCMSEndpoints;
+    }
+
+    public Set<NodeId> fullCMSMemberIds()
+    {
+        if (fullCMSIds == null)
+            this.fullCMSIds = placements.get(ReplicationParams.meta(this)).reads.byEndpoint().keySet().stream().map(directory::peerId).collect(toImmutableSet());
+        return fullCMSIds;
     }
 
     public EndpointsForRange fullCMSMembersAsReplicas()
