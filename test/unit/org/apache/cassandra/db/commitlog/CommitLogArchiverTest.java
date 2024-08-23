631314159a53a8505369573292d7ef52d9cafcf0
Maxwell Guo <cclive1601@gmail.com>
2024-08-23 22:59:41 +0800

    wait for all archive task finish

--- test/unit/org/apache/cassandra/db/commitlog/CommitLogArchiverTest.java
+++ test/unit/org/apache/cassandra/db/commitlog/CommitLogArchiverTest.java
@@ -67,7 +67,6 @@ public class CommitLogArchiverTest extends CQLTester
     {
         File dir = new File(dirName);
         dir.deleteRecursive();
-        CommitLog.instance.setCommitlogArchiver(archiver);
     }
 
     @Test
