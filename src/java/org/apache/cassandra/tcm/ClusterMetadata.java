2e7b98bad08890cec84b736ca068ac604873e73a
David Capwell <dcapwell@apache.org>
2024-08-27 11:31:39 -0700

    Revert "patch by marcus to fix tcm issues found"
    This reverts commit 4bab6145505d1edec796a1632d178b4ff9d56c10.

--- src/java/org/apache/cassandra/tcm/ClusterMetadata.java
+++ src/java/org/apache/cassandra/tcm/ClusterMetadata.java
@@ -74,7 +74,6 @@ import org.apache.cassandra.tcm.serialization.Version;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.cassandra.utils.Pair;
 
-import static com.google.common.collect.ImmutableSet.toImmutableSet;
 import static org.apache.cassandra.config.CassandraRelevantProperties.LINE_SEPARATOR;
 import static org.apache.cassandra.db.TypeSizes.sizeof;
 
@@ -99,7 +98,6 @@ public class ClusterMetadata
     // These fields are lazy but only for the test purposes, since their computation requires initialization of the log ks
     private EndpointsForRange fullCMSReplicas;
     private Set<InetAddressAndPort> fullCMSEndpoints;
-    private Set<NodeId> fullCMSIds;
 
     public ClusterMetadata(IPartitioner partitioner)
     {
@@ -181,13 +179,6 @@ public class ClusterMetadata
         if (fullCMSEndpoints == null)
             this.fullCMSEndpoints = ImmutableSet.copyOf(placements.get(ReplicationParams.meta(this)).reads.byEndpoint().keySet());
         return fullCMSEndpoints;
-    }
-
-    public Set<NodeId> fullCMSMemberIds()
-    {
-        if (fullCMSIds == null)
-            this.fullCMSIds = placements.get(ReplicationParams.meta(this)).reads.byEndpoint().keySet().stream().map(directory::peerId).collect(toImmutableSet());
-        return fullCMSIds;
     }
 
     public EndpointsForRange fullCMSMembersAsReplicas()
