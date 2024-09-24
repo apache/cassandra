9761d95c448cb214d92134d576d12e92fcf5f4bb
Blake Eggleston <blake@ultrablake.com>
2024-09-24 10:59:46 -0700

    ninja: remove another unused import

--- test/distributed/org/apache/cassandra/distributed/test/accord/AccordProgressLogTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/accord/AccordProgressLogTest.java
@@ -27,7 +27,6 @@ import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.distributed.Cluster;
 import org.apache.cassandra.distributed.api.ConsistencyLevel;
 import org.apache.cassandra.distributed.api.Feature;
