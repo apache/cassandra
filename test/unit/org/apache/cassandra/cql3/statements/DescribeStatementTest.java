c6811577a7d7b5dbe4519cb3c1691ab33f6c208a
jaydeepkumar1984 <chovatia.jaydeep@gmail.com>
2024-09-30 11:59:25 -0700

    Fix unit test cases DescribeStatementTest & SchemaCQLHelperTest

--- test/unit/org/apache/cassandra/cql3/statements/DescribeStatementTest.java
+++ test/unit/org/apache/cassandra/cql3/statements/DescribeStatementTest.java
@@ -1004,7 +1004,9 @@ public class DescribeStatementTest extends CQLTester
                "    AND memtable_flush_period_in_ms = 0\n" +
                "    AND min_index_interval = 128\n" +
                "    AND read_repair = 'BLOCKING'\n" +
-               "    AND speculative_retry = '99p';";
+               "    AND speculative_retry = '99p'\n" +
+               "    AND automated_repair_full = {'enabled': 'true'}\n" +
+               "    AND automated_repair_incremental = {'enabled': 'true'};";
     }
 
     private static String mvParametersCql()
@@ -1024,7 +1026,9 @@ public class DescribeStatementTest extends CQLTester
                "    AND memtable_flush_period_in_ms = 0\n" +
                "    AND min_index_interval = 128\n" +
                "    AND read_repair = 'BLOCKING'\n" +
-               "    AND speculative_retry = '99p';";
+               "    AND speculative_retry = '99p'\n" +
+               "    AND automated_repair_full = {'enabled': 'true'}\n" +
+               "    AND automated_repair_incremental = {'enabled': 'true'};";
     }
 
     private static String keyspaceOutput()
