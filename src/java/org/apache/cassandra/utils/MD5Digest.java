edc224cd563ea73014b36e6611afbd41a2a26181
Bereng <berenguerblasi@gmail.com>
2024-10-29 09:47:27 +0100

    Prepared statements cache computation improvements

--- src/java/org/apache/cassandra/utils/MD5Digest.java
+++ src/java/org/apache/cassandra/utils/MD5Digest.java
@@ -113,4 +113,10 @@ public class MD5Digest
     {
         return localMD5Digest.get();
     }
+
+    public int size()
+    {
+        return bytes.length + //bytes
+                4; // int hashCode
+    }
 }
