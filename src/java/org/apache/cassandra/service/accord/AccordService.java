ce7c64ccd6083c2f01d8bc0fead10846444be943
David Capwell <dcapwell@apache.org>
2024-11-06 09:58:08 -0800

    import

--- src/java/org/apache/cassandra/service/accord/AccordService.java
+++ src/java/org/apache/cassandra/service/accord/AccordService.java
@@ -30,7 +30,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutionException;
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.BiFunction;
