10ecb9a4f8982eeddb55b1ad95abd14f90ff797c
Kristijonas Zalys <kzalys@uber.com>
2024-12-03 23:31:03 -0800

    Fix preview repair options for system_schema ks

--- src/java/org/apache/cassandra/schema/SchemaKeyspace.java
+++ src/java/org/apache/cassandra/schema/SchemaKeyspace.java
@@ -1084,7 +1084,7 @@ public final class SchemaKeyspace
                                                  .readRepair(getReadRepairStrategy(row))
                                                  .automatedRepairFull(AutoRepairParams.fromMap(AutoRepairConfig.RepairType.full, row.getFrozenTextMap("repair_full")))
                                                  .automatedRepairIncremental(AutoRepairParams.fromMap(AutoRepairConfig.RepairType.incremental, row.getFrozenTextMap("repair_incremental")))
-                                                 .automatedRepairIncremental(AutoRepairParams.fromMap(AutoRepairConfig.RepairType.preview_repaired, row.getFrozenTextMap("repair_preview_repaired")));
+                                                 .automatedRepairPreviewRepaired(AutoRepairParams.fromMap(AutoRepairConfig.RepairType.preview_repaired, row.getFrozenTextMap("repair_preview_repaired")));
 
         // allow_auto_snapshot column was introduced in 4.2
         if (row.has("allow_auto_snapshot"))
