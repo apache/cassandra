d6e82880396a1a3634e80efd0321138a3b86f64d
Bereng <berenguerblasi@gmail.com>
2025-02-11 09:30:45 +0100

    Long running repairs autofail prematurely

--- src/java/org/apache/cassandra/repair/consistent/LocalSessions.java
+++ src/java/org/apache/cassandra/repair/consistent/LocalSessions.java
@@ -1027,6 +1027,7 @@ public class LocalSessions
         }
         else
         {
+            session.setLastUpdate();
             logger.debug("Received StatusResponse for repair session {} with state {}, which is not actionable. Doing nothing.", sessionID, response.state);
         }
     }
