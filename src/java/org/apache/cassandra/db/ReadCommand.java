caafc6627fc39f512ac4acb6e9d0030e2aa2e345
Blake Eggleston <blake@ultrablake.com>
2025-02-18 16:02:14 -0800

    rebase fixes

--- src/java/org/apache/cassandra/db/ReadCommand.java
+++ src/java/org/apache/cassandra/db/ReadCommand.java
@@ -168,7 +168,7 @@ public abstract class ReadCommand extends AbstractReadQuery
 
         public static ResponseType fromMetadata(TableMetadata metadata)
         {
-            ReplicationType replicationType = metadata.replicationType();
+            ReplicationType replicationType = Schema.instance.getKeyspaceMetadata(metadata.keyspace).params.replicationType;
             switch (replicationType)
             {
                 case legacy:
