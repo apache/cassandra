c9e71864e9826166fc793b2538da77f7d77b2aa8
Tibor Répási <rtib@users.noreply.github.com>
2025-02-19 14:08:54 +0100

    allow grant permission on virtual keyspaces

--- test/unit/org/apache/cassandra/auth/GrantAndRevokeTest.java
+++ test/unit/org/apache/cassandra/auth/GrantAndRevokeTest.java
@@ -36,6 +36,7 @@ import org.apache.cassandra.db.SystemKeyspace;
 import org.apache.cassandra.schema.Schema;
 import org.apache.cassandra.schema.SchemaConstants;
 import org.apache.cassandra.schema.TableMetadata;
+import org.apache.cassandra.service.CassandraDaemon;
 import org.apache.cassandra.transport.ProtocolVersion;
 
 import static java.lang.String.format;
@@ -59,6 +60,7 @@ public class GrantAndRevokeTest extends CQLTester
         DatabaseDescriptor.setRolesValidity(0);
         requireAuthentication();
         requireNetwork();
+        CassandraDaemon.getInstanceForTesting().setupVirtualKeyspaces();
     }
 
     @After
@@ -561,6 +563,25 @@ public class GrantAndRevokeTest extends CQLTester
         maybeReadSystemTables(true);
         // and also write to them, though this is still strongly discouraged
         executeNet(ProtocolVersion.CURRENT, "INSERT INTO system.peers_v2(peer, peer_port, data_center) VALUES ('127.0.100.100', 7012, 'invalid_dc')");
+    }
+
+    @Test
+    public void testGrantOnVirtualKeyspaces() throws Throwable
+    {
+        useSuperUser();
+        executeNet(String.format("CREATE ROLE %s WITH LOGIN = TRUE AND password='%s'", user, pass));
+        
+        useUser(user, pass);
+        assertUnauthorizedQuery("User user has no SELECT permission on <table system_views.settings> or any of its parents",
+                                "SELECT * FROM system_views.settings LIMIT 1");
+        
+        useSuperUser();
+        executeNet(ProtocolVersion.CURRENT, format("GRANT SELECT PERMISSION ON KEYSPACE system_virtual_schema TO %s", user));
+        executeNet(ProtocolVersion.CURRENT, format("GRANT SELECT PERMISSION ON KEYSPACE system_views TO %s", user));
+        
+        useUser(user, pass);
+        executeNet(ProtocolVersion.CURRENT, "SELECT * FROM system_views.settings LIMIT 1");
+        executeNet(ProtocolVersion.CURRENT, "SELECT * FROM system_virtual_schema.tables LIMIT 1");
     }
 
     private void maybeReadSystemTables(boolean superuser) throws Throwable
