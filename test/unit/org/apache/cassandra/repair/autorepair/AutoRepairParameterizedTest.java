f6dbf1c7f949060c084f5811c08181f1aa498b78
Kristijonas Zalys <kzalys@uber.com>
2025-02-24 13:46:54 -0800

    Address comments

--- test/unit/org/apache/cassandra/repair/autorepair/AutoRepairParameterizedTest.java
+++ test/unit/org/apache/cassandra/repair/autorepair/AutoRepairParameterizedTest.java
@@ -20,6 +20,7 @@ package org.apache.cassandra.repair.autorepair;
 
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
@@ -830,8 +831,8 @@ public class AutoRepairParameterizedTest extends CQLTester
     public void testSchedulerIgnoresErrorsFromUnrelatedRepairRunables()
     {
         RepairOption options = new RepairOption(RepairParallelism.PARALLEL, true, repairType == AutoRepairConfig.RepairType.INCREMENTAL, false,
-                                                AutoRepairService.instance.getAutoRepairConfig().getRepairThreads(repairType), Set.of(),
-                                                false, false, PreviewKind.NONE, false, true, false, false, false, false);
+                                                AutoRepairService.instance.getAutoRepairConfig().getRepairThreads(repairType), Collections.emptySet(),
+                                                 false, false, PreviewKind.NONE, false, true, false, false, false, false);
         AutoRepairState repairState = AutoRepair.instance.repairStates.get(repairType);
         AutoRepairState spyState = spy(repairState);
         AtomicReference<RepairCoordinator> failingRepair = new AtomicReference<>(new RepairCoordinator(StorageService.instance, StorageService.nextRepairCommand.incrementAndGet(), options, keyspace.getName()));
