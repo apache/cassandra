53d6d5c58f91221df3ace9693fe052b5f79530a9
Blake Eggleston <blake@ultrablake.com>
2025-02-28 09:38:51 -0800

    skipping more tests

--- test/distributed/org/apache/cassandra/distributed/test/ReadRepairSliceQueriesTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/ReadRepairSliceQueriesTest.java
@@ -64,8 +64,7 @@ public class ReadRepairSliceQueriesTest extends ReadRepairQueryTester
     @Test
     public void testSliceQueryWithFilter()
     {
-        if (coordinator == 2)
-            MutationTrackingUtils.fixmeSkipIfLogged(replicationType, "Depends on ReplicaFilteringProtection");
+        MutationTrackingUtils.fixmeSkipIfLogged(replicationType, "Depends on short read protection and ReplicaFilteringProtection");
         tester("WHERE k=0 AND a>10 AND a<40 ALLOW FILTERING")
         .createTable("CREATE TABLE %s (k int, c int, a int, b int, PRIMARY KEY(k, c))")
         .mutate("INSERT INTO %s (k, c, a, b) VALUES (0, 1, 10, 100)",
@@ -95,8 +94,7 @@ public class ReadRepairSliceQueriesTest extends ReadRepairQueryTester
     @Test
     public void testSliceQueryWithLimit()
     {
-        if (coordinator == 2)
-            MutationTrackingUtils.fixmeSkipIfLogged(replicationType, "Depends on short read protection");
+        MutationTrackingUtils.fixmeSkipIfLogged(replicationType, "Depends on short read protection");
         tester("WHERE k=0 AND c>1 LIMIT 2")
         .createTable("CREATE TABLE %s (k int, c int, a int, b int, PRIMARY KEY(k, c))")
         .mutate("INSERT INTO %s (k, c, a, b) VALUES (0, 1, 10, 100)",
