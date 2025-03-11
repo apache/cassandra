193c0a08cc48f1ef4975aa2184146e2e0f151fef
Aleksey Yeschenko <aleksey@apache.org>
2025-03-11 11:10:31 +0000

    Make checkstyle happy

--- src/java/org/apache/cassandra/db/CassandraWriteContext.java
+++ src/java/org/apache/cassandra/db/CassandraWriteContext.java
@@ -21,7 +21,6 @@ package org.apache.cassandra.db;
 import com.google.common.base.Preconditions;
 
 import org.apache.cassandra.db.commitlog.CommitLogPosition;
-import org.apache.cassandra.replication.MutationTracker;
 import org.apache.cassandra.replication.PendingWrite;
 import org.apache.cassandra.utils.concurrent.OpOrder;
 
