814d92bd77215bf7f6e25b222d7db285c3cc81df
Blake Eggleston <blake@ultrablake.com>
2025-03-12 09:42:07 -0700

    fold pending writes back into mutation tracker

--- src/java/org/apache/cassandra/replication/MutationTracker.java
+++ src/java/org/apache/cassandra/replication/MutationTracker.java
@@ -18,16 +18,16 @@
 
 package org.apache.cassandra.replication;
 
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
 
 import com.google.common.base.Preconditions;
+import com.google.common.collect.Sets;
 import org.apache.cassandra.db.DecoratedKey;
 import org.apache.cassandra.db.Mutation;
 import org.apache.cassandra.db.PartitionPosition;
 import org.apache.cassandra.db.ReadCommand;
+import org.apache.cassandra.db.partitions.UnfilteredPartitionIterator;
 import org.apache.cassandra.dht.AbstractBounds;
 import org.apache.cassandra.dht.Range;
 import org.apache.cassandra.dht.Token;
@@ -38,7 +38,95 @@ import org.apache.cassandra.tcm.ClusterMetadata;
 
 public class MutationTracker
 {
-    private final PendingWrites pendingWrites = new PendingWrites();
+    public interface PendingRead extends AutoCloseable
+    {
+        static PendingRead NOOP = new PendingRead()
+        {
+            @Override
+            public void close()
+            {
+            }
+
+            @Override
+            public UnfilteredPartitionIterator augmentResponseWithPendingWrites(UnfilteredPartitionIterator iterator, MutationSummary summary)
+            {
+                return iterator;
+            }
+        };
+
+        @Override
+        void close();
+
+        /**
+         * Returns mutations contained in the mutation summary that may not have been
+         * applied to the memtable when it was read.
+         *
+         * @param summary
+         * @return
+         */
+        UnfilteredPartitionIterator augmentResponseWithPendingWrites(UnfilteredPartitionIterator iterator, MutationSummary summary);
+    }
+
+    public class ListeningPendingRead implements PendingRead
+    {
+        private final ReadCommand command;
+        private final Map<MutationId, Mutation> pendingWrites = new ConcurrentHashMap<>();
+
+        public ListeningPendingRead(ReadCommand command)
+        {
+            this.command = command;
+        }
+
+        public void onNewWrite(Mutation mutation)
+        {
+            if (command.readsMutationContents(mutation))
+                pendingWrites.put(mutation.id(), mutation);
+        }
+
+        @Override
+        public void close()
+        {
+            pendingReads.remove(this);
+        }
+
+        public Set<MutationId> mutationIds()
+        {
+            return pendingWrites.keySet();
+        }
+
+        @Override
+        public UnfilteredPartitionIterator augmentResponseWithPendingWrites(UnfilteredPartitionIterator iterator, MutationSummary summary)
+        {
+            if (pendingWrites.isEmpty() || summary.isEmpty())
+                return iterator;
+
+            List<Mutation> augmentingMutations = new ArrayList<>(pendingWrites.size());
+            for (Mutation mutation : pendingWrites.values())
+            {
+                if (summary.contains(mutation.id()))
+                    augmentingMutations.add(mutation);
+            }
+            return command.augmentResultWithMutations(iterator, augmentingMutations);
+        }
+    }
+
+
+    public interface PendingWrite extends AutoCloseable
+    {
+        static PendingWrite NOOP = new PendingWrite()
+        {
+            @Override
+            public void close()
+            {
+            }
+        };
+
+        @Override
+        void close();
+    }
+
+    private final Map<MutationId, Mutation> pendingMutations = new ConcurrentHashMap<MutationId, Mutation>();
+    private final Set<ListeningPendingRead> pendingReads = Sets.newConcurrentHashSet();
 
     public void start()
     {
@@ -46,14 +134,35 @@ public class MutationTracker
         Shards.instance.load(ClusterMetadata.current());
     }
 
+
     public PendingWrite startWrite(Mutation mutation)
     {
-        return pendingWrites.startWrite(mutation);
+        if (mutation.id().isNone())
+            return PendingWrite.NOOP;
+
+        pendingMutations.put(mutation.id(), mutation);
+        pendingReads.forEach(read -> read.onNewWrite(mutation));
+
+        return new PendingWrite()
+        {
+            @Override
+            public void close()
+            {
+                pendingMutations.remove(mutation.id());
+            }
+        };
     }
 
     public PendingRead startRead(ReadCommand command)
     {
-        return pendingWrites.startRead(command);
+        if (!Schema.instance.getKeyspaceMetadata(command.metadata().keyspace).useMutationTracking())
+            return PendingRead.NOOP;
+
+        ListeningPendingRead pendingRead = new ListeningPendingRead(command);
+        pendingReads.add(pendingRead);
+        pendingMutations.values().forEach(pendingRead::onNewWrite);
+
+        return pendingRead;
     }
 
     // TODO: ditch?
