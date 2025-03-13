1dd72ea6173032aa7579bf2c93b9dcde12d54e17
Stefan Miklosovic <smiklosovic@apache.org>
2025-03-13 09:47:58 +0100

    Un-ignore HintsUpgradeTest
    patch by Stefan Miklosovic; reviewed by Alex Petrov for CASSANDRA-20435

--- test/unit/org/apache/cassandra/hints/HintsUpgradeTest.java
+++ test/unit/org/apache/cassandra/hints/HintsUpgradeTest.java
@@ -26,7 +26,6 @@ import java.util.function.Consumer;
 import com.google.common.collect.ImmutableMap;
 import org.junit.After;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import org.apache.cassandra.SchemaLoader;
@@ -50,7 +49,6 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-@Ignore("TODO: TCM")
 public class HintsUpgradeTest
 {
     static
