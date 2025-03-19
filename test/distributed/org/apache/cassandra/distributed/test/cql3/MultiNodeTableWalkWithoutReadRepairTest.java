2ba8dd045eed6021653f330fa047d7832d04ee71
David Capwell <dcapwell@apache.org>
2025-03-19 14:26:07 -0700

    revert

--- test/distributed/org/apache/cassandra/distributed/test/cql3/MultiNodeTableWalkWithoutReadRepairTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/cql3/MultiNodeTableWalkWithoutReadRepairTest.java
@@ -35,6 +35,6 @@ public class MultiNodeTableWalkWithoutReadRepairTest extends MultiNodeTableWalkB
         // if a failing seed is detected, populate here
         // Example: builder.withSeed(42L);
         // CQL operations may have opertors such as +, -, and / (example 4 + 4), to "apply" them to get a constant value
-         CQL_DEBUG_APPLY_OPERATOR = true;
+//         CQL_DEBUG_APPLY_OPERATOR = true;
     }
 }
