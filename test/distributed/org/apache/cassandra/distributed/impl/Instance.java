c123691b483a2211ca95ae87b9b9c2aea2600586
Blake Eggleston <blake@ultrablake.com>
2025-03-24 16:34:49 -0700

    shutdown mutation journal in dtests

--- test/distributed/org/apache/cassandra/distributed/impl/Instance.java
+++ test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -978,6 +978,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
             // CommitLog must shut down after Stage, or threads from the latter may attempt to use the former.
             // (ex. A Mutation stage thread may attempt to add a mutation to the CommitLog.)
             error = parallelRun(error, executor, CommitLog.instance::shutdownBlocking);
+            error = parallelRun(error, executor, MutationJournal.instance::shutdownBlocking);
             error = parallelRun(error, executor, MutationTrackingService.instance::shutdownBlocking);
             error = parallelRun(error, executor,
                                 () -> shutdownAndWait(Collections.singletonList(JMXBroadcastExecutor.executor))
