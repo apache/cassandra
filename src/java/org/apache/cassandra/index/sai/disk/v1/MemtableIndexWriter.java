355edebc327368f6a4f374bb464ae97165926fb0
David Capwell <dcapwell@apache.org>
2025-04-23 14:43:34 -0700

    if the intersect(memtable, rowMapping) is empty, dont bother to try to flush

--- src/java/org/apache/cassandra/index/sai/disk/v1/MemtableIndexWriter.java
+++ src/java/org/apache/cassandra/index/sai/disk/v1/MemtableIndexWriter.java
@@ -146,12 +146,15 @@ public class MemtableIndexWriter implements PerColumnIndexWriter
             {
                 final Iterator<Pair<ByteComparable, LongArrayList>> iterator = rowMapping.merge(memtable);
 
-                try (MemtableTermsIterator terms = new MemtableTermsIterator(memtable.getMinTerm(), memtable.getMaxTerm(), iterator))
+                long cellCount = 0;
+                if (iterator.hasNext())
                 {
-                    long cellCount = flush(terms);
-
-                    completeIndexFlush(cellCount, start, stopwatch);
+                    try (MemtableTermsIterator terms = new MemtableTermsIterator(memtable.getMinTerm(), memtable.getMaxTerm(), iterator))
+                    {
+                        cellCount = flush(terms);
+                    }
                 }
+                completeIndexFlush(cellCount, start, stopwatch);
             }
         }
         catch (Throwable t)
