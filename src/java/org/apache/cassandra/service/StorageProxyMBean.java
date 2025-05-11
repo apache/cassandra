c51da7a61348b9183a68443fd7d1deb550d56450
Andy Tolbert <6889771+tolbertam@users.noreply.github.com>
2025-05-11 18:35:42 -0500

    Add NativeTransportMaxConcurrentConnectionsPerIp to StorageProxyMBean
    Adds setNativeTransportMaxConcurrentConnectionsPerIp and
    getNativeTransportMaxConcurrentConnectionsPerIp to StorageProxyMBean so
    these methods can be used from JMX like the
    *NativeTransportMaxConcurrentConnections methods.
    
    patch by Andy Tolbert; reviewed by Chris Lohfink for CASSANDRA-20642

--- src/java/org/apache/cassandra/service/StorageProxyMBean.java
+++ src/java/org/apache/cassandra/service/StorageProxyMBean.java
@@ -54,6 +54,9 @@ public interface StorageProxyMBean
     public void setNativeTransportMaxConcurrentConnections(Long nativeTransportMaxConcurrentConnections);
     public Long getNativeTransportMaxConcurrentConnections();
 
+    public void setNativeTransportMaxConcurrentConnectionsPerIp(Long nativeTransportMaxConcurrentConnections);
+    public Long getNativeTransportMaxConcurrentConnectionsPerIp();
+
     public void reloadTriggerClasses();
 
     public long getReadRepairAttempted();
