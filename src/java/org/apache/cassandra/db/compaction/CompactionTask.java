3915dd12a6a265458ee8352d17617b72cc2023d5
Stefan Miklosovic <smiklosovic@apache.org>
2025-05-28 13:40:00 +0200

    obsolete expired SSTables before compaction starts

--- src/java/org/apache/cassandra/db/compaction/CompactionTask.java
+++ src/java/org/apache/cassandra/db/compaction/CompactionTask.java
@@ -186,6 +186,7 @@ public class CompactionTask extends AbstractCompactionTask
             if (!fullyExpiredSSTables.isEmpty())
             {
                 logger.debug("Compaction {} dropping expired sstables: {}", transaction.opIdString(), fullyExpiredSSTables);
+                fullyExpiredSSTables.forEach(transaction::obsolete);
                 actuallyCompact.removeAll(fullyExpiredSSTables);
             }
 
