f07e8c05b8cae70cdf34dc451f9e608414fea95a
maoling <maoling@apache.org>
2025-08-11 23:25:00 +0800

    ninja: remove unused imports in MessagingService to pass checkstyle

--- src/java/org/apache/cassandra/net/MessagingService.java
+++ src/java/org/apache/cassandra/net/MessagingService.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
