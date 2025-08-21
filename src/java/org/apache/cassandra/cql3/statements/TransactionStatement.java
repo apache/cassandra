f85a66941e258e8ff38da7a3a9878aed80840e1e
David Capwell <dcapwell@apache.org>
2025-08-21 15:45:33 -0700

    try to free the borrowed memory

--- src/java/org/apache/cassandra/cql3/statements/TransactionStatement.java
+++ src/java/org/apache/cassandra/cql3/statements/TransactionStatement.java
@@ -481,6 +481,7 @@ public class TransactionStatement implements CQLStatement.CompositeCQLStatement,
                 // ModificationStatement yield no Mutation (DELETE WHERE pk=0 AND c < 0 AND c > 0 -- matches no keys; so has no mutation)
 
                 // recreate the reads to avoid autoReads adding reads
+                keyCollector.build(); // just to discared the borrowed memory
                 keyCollector = new TableMetadatasAndKeys.KeyCollector(tables);
                 reads = createNamedReads(options, null, keyCollector);
                 if (reads.isEmpty())
