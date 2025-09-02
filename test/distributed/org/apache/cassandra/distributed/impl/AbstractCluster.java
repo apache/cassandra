8755520b75ec4cfba8ff9c5c8bcb3620e4000d40
Doug Rohrer <doug@therohrers.org>
2025-09-02 16:12:34 -0400

    CASSANDRA-20884 - Move JMX classes to the in-jvm-dtest API project

--- test/distributed/org/apache/cassandra/distributed/impl/AbstractCluster.java
+++ test/distributed/org/apache/cassandra/distributed/impl/AbstractCluster.java
@@ -202,12 +202,6 @@ public abstract class AbstractCluster<I extends IInstance> implements ICluster<I
             withSharedClasses(SHARED_PREDICATE);
         }
 
-        @SuppressWarnings("unchecked")
-        private B self()
-        {
-            return (B) this;
-        }
-
         public B withNodeProvisionStrategy(INodeProvisionStrategy.Strategy nodeProvisionStrategy)
         {
             this.nodeProvisionStrategy = nodeProvisionStrategy;
@@ -585,7 +579,7 @@ public abstract class AbstractCluster<I extends IInstance> implements ICluster<I
     }
 
     @VisibleForTesting
-    InstanceConfig createInstanceConfig(int nodeNum)
+    public InstanceConfig createInstanceConfig(int nodeNum)
     {
         INodeProvisionStrategy provisionStrategy = nodeProvisionStrategy.create(subnet, portMap);
         Collection<String> tokens = tokenSupplier.tokens(nodeNum);
