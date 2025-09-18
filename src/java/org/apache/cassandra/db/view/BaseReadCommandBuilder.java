b77d7e9094e449f2fb25a5fb75c624a0ab440d3e
Yuqi Yan <yuqiy@uber.com>
2025-09-18 11:55:13 -0700

    BaseRead generator for MV key rebuild
    Summary:
    `BaseReadCommandBuilder` will generate the corresponding read command to base table.
    
    - The `BaseReadCommandBuilder` class was introduced to facilitate the generation of base read commands for MV key rebuild operations.
    - Key functionalities included building partition and clustering key data maps and translating view data into base table queries.
    
    Test Plan:
    Added UT
    
    - A new test class `BaseReadCommandBuilderTest` was added to create unit tests for the `BaseReadCommandBuilder`.
    - The tests covered different configurations of partition and clustering keys in both base tables and MV
    
    Reviewers: O2017 Cassandra Service, #ldap_cassandra-super-users, ureview, runtian
    
    Reviewed By: O2017 Cassandra Service, #ldap_cassandra-super-users, runtian
    
    Subscribers: runtian
    
    Tags: #has_java
    
    JIRA Issues: SO-51432
    
    Differential Revision: https://code.uberinternal.com/D19271849

---
+++ src/java/org/apache/cassandra/db/view/BaseReadCommandBuilder.java
@@ -0,0 +1,135 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.db.view;
+
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.db.DecoratedKey;
+import org.apache.cassandra.db.SinglePartitionReadCommand;
+import org.apache.cassandra.db.filter.ClusteringIndexNamesFilter;
+import org.apache.cassandra.db.filter.ColumnFilter;
+import org.apache.cassandra.db.filter.DataLimits;
+import org.apache.cassandra.db.filter.RowFilter;
+import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.db.marshal.ByteBufferAccessor;
+import org.apache.cassandra.db.marshal.CompositeType;
+import org.apache.cassandra.schema.ColumnMetadata;
+import org.apache.cassandra.schema.TableMetadata;
+import org.apache.cassandra.utils.FBUtilities;
+
+public class BaseReadCommandBuilder
+{
+    private final TableMetadata baseMetadata;
+    private final TableMetadata viewMetadata;
+    private final View view;
+
+    public BaseReadCommandBuilder(View view)
+    {
+        this.view = view;
+        this.baseMetadata = view.getDefinition().baseTableMetadata();
+        this.viewMetadata = view.getDefinition().metadata;
+    }
+
+    DecoratedKey buildBaseTablePartitionKey(Map<ColumnMetadata, ByteBuffer> viewRawDataMap)
+    {
+        List<ColumnMetadata> basePks = baseMetadata.partitionKeyColumns();
+        ByteBuffer[] pkValues = new ByteBuffer[basePks.size()];
+        for (int i = 0; i < basePks.size(); i++)
+        {
+            ColumnMetadata baseColumn = basePks.get(i);
+            ColumnMetadata viewColumn = view.getViewColumn(baseColumn);
+            ByteBuffer value = viewRawDataMap.get(viewColumn);
+            if (value == null)
+                throw new IllegalArgumentException("Missing value for base table partition key column: " + baseColumn.name);
+            pkValues[i] = value;
+        }
+
+        ByteBuffer partitionKey;
+        if (basePks.size() == 1)
+            partitionKey = pkValues[0];
+        else
+        {
+            AbstractType<?> partitionKeyType = baseMetadata.partitionKeyType;
+            if (partitionKeyType instanceof CompositeType)
+                partitionKey = CompositeType.build(ByteBufferAccessor.instance, pkValues);
+            else
+                throw new IllegalStateException("Expected CompositeType for multi-column partition key");
+        }
+        return baseMetadata.partitioner.decorateKey(partitionKey);
+    }
+
+    Clustering<?> buildBaseClusteringKey(Map<ColumnMetadata, ByteBuffer> viewRawDataMap)
+    {
+        List<ColumnMetadata> baseCks = baseMetadata.clusteringColumns();
+        if (baseCks.isEmpty())
+            return Clustering.EMPTY;
+
+        ByteBuffer[] ckValues = new ByteBuffer[baseCks.size()];
+        for (int i = 0; i < baseCks.size(); i++)
+        {
+            ColumnMetadata baseColumn = baseCks.get(i);
+            ColumnMetadata viewColumn = view.getViewColumn(baseColumn);
+            ByteBuffer value = viewRawDataMap.get(viewColumn);
+            if (value == null)
+                throw new IllegalArgumentException("Missing value for base cluster key column: " + baseColumn.name);
+            ckValues[i] = value;
+        }
+        return Clustering.make(ckValues);
+    }
+
+    Map<ColumnMetadata, ByteBuffer> getViewRawDataMap(ByteBuffer viewPartitionKey, Clustering<?> viewClustering)
+    {
+        Map<ColumnMetadata, ByteBuffer> map = new HashMap<>(viewMetadata.partitionKeyColumns().size() + viewMetadata.clusteringColumns().size());
+        List<ColumnMetadata> partitionKeyColumns = viewMetadata.partitionKeyColumns();
+
+        if (viewMetadata.partitionKeyType instanceof CompositeType)
+        {
+            CompositeType compositeType = (CompositeType) viewMetadata.partitionKeyType;
+            ByteBuffer[] components = compositeType.split(viewPartitionKey);
+            for (ColumnMetadata column : viewMetadata.partitionKeyColumns())
+                map.put(column, components[column.position()]);
+        }
+        else
+            map.put(partitionKeyColumns.get(0), viewPartitionKey);
+
+        ByteBuffer[] clusteringComponents = viewClustering.getBufferArray();
+        for (ColumnMetadata column : viewMetadata.clusteringColumns())
+            map.put(column, clusteringComponents[column.position()]);
+        return map;
+    }
+
+    public SinglePartitionReadCommand buildBaseTableReadCommand(ByteBuffer viewPartitionKey, Clustering<?> viewClustering, int nowInSec)
+    {
+        Map<ColumnMetadata, ByteBuffer> viewRawDataMap = getViewRawDataMap(viewPartitionKey, viewClustering);
+        DecoratedKey basePK = buildBaseTablePartitionKey(viewRawDataMap);
+        Clustering<?> baseClustering = buildBaseClusteringKey(viewRawDataMap);
+        ClusteringIndexNamesFilter baseClusteringFilter = new ClusteringIndexNamesFilter(FBUtilities.singleton(baseClustering, baseMetadata.comparator), false);
+        return SinglePartitionReadCommand.create(baseMetadata,
+                                                 nowInSec,
+                                                 ColumnFilter.all(baseMetadata),
+                                                 RowFilter.NONE,
+                                                 DataLimits.NONE,
+                                                 basePK,
+                                                 baseClusteringFilter);
+    }
+}
---
+++ test/unit/org/apache/cassandra/db/view/BaseReadCommandBuilderTest.java
@@ -0,0 +1,303 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.db.view;
+
+import java.nio.ByteBuffer;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+import com.google.common.collect.Iterables;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import org.apache.cassandra.Util;
+import org.apache.cassandra.cql3.CQLStatement;
+import org.apache.cassandra.cql3.CQLTester;
+import org.apache.cassandra.cql3.ColumnIdentifier;
+import org.apache.cassandra.cql3.QueryOptions;
+import org.apache.cassandra.cql3.statements.ModificationStatement;
+import org.apache.cassandra.cql3.statements.SelectStatement;
+import org.apache.cassandra.db.Clustering;
+import org.apache.cassandra.db.ConsistencyLevel;
+import org.apache.cassandra.db.Keyspace;
+import org.apache.cassandra.db.ReadQuery;
+import org.apache.cassandra.db.SinglePartitionReadCommand;
+import org.apache.cassandra.db.marshal.CompositeType;
+import org.apache.cassandra.db.partitions.PartitionIterator;
+import org.apache.cassandra.db.rows.Row;
+import org.apache.cassandra.db.rows.RowIterator;
+import org.apache.cassandra.schema.ColumnMetadata;
+import org.apache.cassandra.schema.Schema;
+import org.apache.cassandra.schema.TableMetadata;
+import org.apache.cassandra.service.ClientState;
+import org.apache.cassandra.transport.Dispatcher;
+import org.apache.cassandra.utils.ByteBufferUtil;
+import org.apache.cassandra.utils.FBUtilities;
+
+import static org.apache.cassandra.cql3.QueryProcessor.parseStatement;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
+public class BaseReadCommandBuilderTest extends CQLTester
+{
+    QueryOptions options = QueryOptions.forInternalCalls(null);
+    ClientState state = ClientState.forInternalCalls();
+
+    @BeforeClass
+    public static void init()
+    {
+        CQLTester.prepareServer();
+        CQLTester.requireNetwork();
+    }
+
+    @Test
+    public void testSimplePartitionKey()
+    {
+        String tbl = createTable("CREATE TABLE %s (" +
+                                 "k int, " +
+                                 "c bigint, " +
+                                 "intval smallint, " +
+                                 "random text, " +
+                                 "PRIMARY KEY (k, c))");
+        String viewName = createView("CREATE MATERIALIZED VIEW %s AS SELECT * FROM %s WHERE k IS NOT NULL AND c IS NOT NULL AND intval IS NOT NULL PRIMARY KEY (intval, c, k)");
+        TableMetadata baseTableMetadata = Schema.instance.getTableMetadata(keyspace(), tbl);
+        assertNotNull(baseTableMetadata);
+        TableMetadata viewMetadata = Keyspace.open(keyspace()).getMetadata().getTableOrViewNullable(viewName);
+        assertNotNull(viewMetadata);
+        View view = Keyspace.open(keyspace()).viewManager.getByName(viewName);
+        assertNotNull(view);
+
+        CQLStatement stmt = parseStatement(String.format("DELETE FROM %s.%s WHERE intval = 1 AND c = 2 AND k = 3", viewMetadata.keyspace, viewMetadata.name), state);
+        assertTrue(stmt instanceof ModificationStatement);
+        ModificationStatement modStmt = (ModificationStatement) stmt;
+
+        CQLStatement baseStmt = parseStatement(String.format("SELECT * FROM %s.%s WHERE k = 3 AND c = 2", baseTableMetadata.keyspace, baseTableMetadata.name), state);
+        assertTrue(baseStmt instanceof SelectStatement);
+        SelectStatement selectStmt = (SelectStatement) baseStmt;
+
+        verifyRawDataMap(modStmt, selectStmt, view, baseTableMetadata, viewMetadata);
+        verifyReadCommand(modStmt, selectStmt, view);
+    }
+
+    @Test
+    public void testCompositePartitionKeyInMV()
+    {
+        String tbl = createTable("CREATE TABLE %s (" +
+                                 "k int, " +
+                                 "c bigint, " +
+                                 "intval smallint, " +
+                                 "random text, " +
+                                 "PRIMARY KEY (k, c))");
+        String viewName = createView("CREATE MATERIALIZED VIEW %s AS SELECT * FROM %s WHERE k IS NOT NULL AND c IS NOT NULL AND intval IS NOT NULL PRIMARY KEY ((intval, c), k)");
+        TableMetadata baseTableMetadata = Schema.instance.getTableMetadata(keyspace(), tbl);
+        assertNotNull(baseTableMetadata);
+        TableMetadata viewMetadata = Keyspace.open(keyspace()).getMetadata().getTableOrViewNullable(viewName);
+        assertNotNull(viewMetadata);
+        View view = Keyspace.open(keyspace()).viewManager.getByName(viewName);
+        assertNotNull(view);
+
+        CQLStatement stmt = parseStatement(String.format("DELETE FROM %s.%s WHERE intval = 1 AND c = 2 AND k = 3", viewMetadata.keyspace, viewMetadata.name), state);
+        assertTrue(stmt instanceof ModificationStatement);
+        ModificationStatement modStmt = (ModificationStatement) stmt;
+
+        CQLStatement baseStmt = parseStatement(String.format("SELECT * FROM %s.%s WHERE k = 3 AND c = 2", baseTableMetadata.keyspace, baseTableMetadata.name), state);
+        assertTrue(baseStmt instanceof SelectStatement);
+        SelectStatement selectStmt = (SelectStatement) baseStmt;
+
+        verifyRawDataMap(modStmt, selectStmt, view, baseTableMetadata, viewMetadata);
+        verifyReadCommand(modStmt, selectStmt, view);
+    }
+
+    @Test
+    public void testCompositePartitionKeyInBase()
+    {
+        String tbl = createTable("CREATE TABLE %s (" +
+                                 "k int, " +
+                                 "c bigint, " +
+                                 "intval smallint, " +
+                                 "random text, " +
+                                 "PRIMARY KEY ((k, c), intval))");
+        String viewName = createView("CREATE MATERIALIZED VIEW %s AS SELECT * FROM %s WHERE k IS NOT NULL AND c IS NOT NULL AND intval IS NOT NULL PRIMARY KEY (k, intval, c)");
+        TableMetadata baseTableMetadata = Schema.instance.getTableMetadata(keyspace(), tbl);
+        assertNotNull(baseTableMetadata);
+        TableMetadata viewMetadata = Keyspace.open(keyspace()).getMetadata().getTableOrViewNullable(viewName);
+        assertNotNull(viewMetadata);
+        View view = Keyspace.open(keyspace()).viewManager.getByName(viewName);
+        assertNotNull(view);
+
+        CQLStatement stmt = parseStatement(String.format("DELETE FROM %s.%s WHERE intval = 1 AND c = 2 AND k = 3", viewMetadata.keyspace, viewMetadata.name), state);
+        assertTrue(stmt instanceof ModificationStatement);
+        ModificationStatement modStmt = (ModificationStatement) stmt;
+
+        CQLStatement baseStmt = parseStatement(String.format("SELECT * FROM %s.%s WHERE k = 3 AND c = 2 AND intval = 1", baseTableMetadata.keyspace, baseTableMetadata.name), state);
+        assertTrue(baseStmt instanceof SelectStatement);
+        SelectStatement selectStmt = (SelectStatement) baseStmt;
+
+        verifyRawDataMap(modStmt, selectStmt, view, baseTableMetadata, viewMetadata);
+        verifyReadCommand(modStmt, selectStmt, view);
+    }
+
+    @Test
+    public void testParitialSelectionInMV()
+    {
+        String tbl = createTable("CREATE TABLE %s (" +
+                                 "k int, " +
+                                 "c bigint, " +
+                                 "intval smallint, " +
+                                 "random text, " +
+                                 "PRIMARY KEY ((k, c), intval))");
+        String viewName = createView("CREATE MATERIALIZED VIEW %s AS SELECT k, c, intval FROM %s WHERE k IS NOT NULL AND c IS NOT NULL AND intval IS NOT NULL PRIMARY KEY (k, intval, c)");
+        TableMetadata baseTableMetadata = Schema.instance.getTableMetadata(keyspace(), tbl);
+        assertNotNull(baseTableMetadata);
+        TableMetadata viewMetadata = Keyspace.open(keyspace()).getMetadata().getTableOrViewNullable(viewName);
+        assertNotNull(viewMetadata);
+        View view = Keyspace.open(keyspace()).viewManager.getByName(viewName);
+        assertNotNull(view);
+
+        CQLStatement stmt = parseStatement(String.format("DELETE FROM %s.%s WHERE intval = 1 AND c = 2 AND k = 3", viewMetadata.keyspace, viewMetadata.name), state);
+        assertTrue(stmt instanceof ModificationStatement);
+        ModificationStatement modStmt = (ModificationStatement) stmt;
+
+        CQLStatement baseStmt = parseStatement(String.format("SELECT * FROM %s.%s WHERE k = 3 AND c = 2 AND intval = 1", baseTableMetadata.keyspace, baseTableMetadata.name), state);
+        assertTrue(baseStmt instanceof SelectStatement);
+        SelectStatement selectStmt = (SelectStatement) baseStmt;
+
+        verifyRawDataMap(modStmt, selectStmt, view, baseTableMetadata, viewMetadata);
+        verifyReadCommand(modStmt, selectStmt, view);
+    }
+
+    @Test
+    public void testReadResult() throws Throwable
+    {
+        String tbl = createTable("CREATE TABLE %s (" +
+                                 "k int, " +
+                                 "c bigint, " +
+                                 "intval smallint, " +
+                                 "random text, " +
+                                 "PRIMARY KEY (k, c))");
+        String viewName = createView("CREATE MATERIALIZED VIEW %s AS SELECT k, c, random FROM %s WHERE k IS NOT NULL AND c IS NOT NULL PRIMARY KEY (c, k)");
+        execute("INSERT INTO %s (k, c, intval, random) VALUES (3, 2, 1, 'test')");
+        execute("INSERT INTO %s (k, c, intval, random) VALUES (0, 0, 0, 'test0')");
+
+        TableMetadata baseTableMetadata = Schema.instance.getTableMetadata(keyspace(), tbl);
+        assertNotNull(baseTableMetadata);
+        TableMetadata viewMetadata = Keyspace.open(keyspace()).getMetadata().getTableOrViewNullable(viewName);
+        assertNotNull(viewMetadata);
+        View view = Keyspace.open(keyspace()).viewManager.getByName(viewName);
+        assertNotNull(view);
+
+        CQLStatement stmt = parseStatement(String.format("DELETE FROM %s.%s WHERE c = 2 AND k = 3", viewMetadata.keyspace, viewMetadata.name), state);
+        assertTrue(stmt instanceof ModificationStatement);
+        ModificationStatement modStmt = (ModificationStatement) stmt;
+
+        BaseReadCommandBuilder builder = new BaseReadCommandBuilder(view);
+        // build the read command
+        int nowInSec = FBUtilities.nowInSeconds();
+        SinglePartitionReadCommand cmd = builder.buildBaseTableReadCommand(modStmt.buildPartitionKeyNames(options, state).get(0),
+                                                                           Iterables.getOnlyElement(modStmt.createClustering(options, state)),
+                                                                           nowInSec);
+
+        try (PartitionIterator iter = cmd.execute(ConsistencyLevel.ONE, state, Dispatcher.RequestTime.forImmediateExecution()))
+        {
+            assertTrue(iter.hasNext());
+            RowIterator rowIter = iter.next();
+            // verify the partition key is the same as the one we inserted
+            assertEquals(ByteBufferUtil.bytes(3), rowIter.partitionKey().getKey());
+            assertTrue(rowIter.hasNext());
+            Row row = rowIter.next();
+            // verify the clustering is the same as the one we inserted
+            assertEquals(Util.clustering(baseTableMetadata.comparator, 2L), row.clustering());
+            // verify the row is the same as the one we inserted
+            assertEquals(ByteBufferUtil.bytes((short) 1), row.getCell(baseTableMetadata.getColumn(ByteBufferUtil.bytes("intval"))).buffer());
+            assertEquals(ByteBufferUtil.bytes("test"), row.getCell(baseTableMetadata.getColumn(ByteBufferUtil.bytes("random"))).buffer());
+            // verify 2 columns are selected (intval, random)
+            assertEquals(2, row.columnCount());
+            // only 1 row
+            assertFalse(rowIter.hasNext());
+            // only 1 partition
+            assertFalse(iter.hasNext());
+        }
+    }
+
+    private void verifyReadCommand(ModificationStatement modStmt,
+                                   SelectStatement selectStmt,
+                                   View view)
+    {
+        BaseReadCommandBuilder builder = new BaseReadCommandBuilder(view);
+        // build the read command
+        int nowInSec = FBUtilities.nowInSeconds();
+        SinglePartitionReadCommand cmd = builder.buildBaseTableReadCommand(modStmt.buildPartitionKeyNames(options, state).get(0),
+                                                                           Iterables.getOnlyElement(modStmt.createClustering(options, state)),
+                                                                           nowInSec);
+        ReadQuery expected = selectStmt.getQuery(options, nowInSec);
+        SinglePartitionReadCommand.Group group = (SinglePartitionReadCommand.Group) expected;
+        assertEquals(1, group.queries.size());
+        assertEquals(group.queries.get(0).toString(), cmd.toString());
+    }
+
+    private void verifyRawDataMap(ModificationStatement modStmt,
+                                  SelectStatement selectStmt,
+                                  View view,
+                                  TableMetadata baseTableMetadata,
+                                  TableMetadata viewMetadata)
+    {
+        BaseReadCommandBuilder builder = new BaseReadCommandBuilder(view);
+        // raw data for base read
+        ByteBuffer basePartitionKey = selectStmt.getRestrictions().getPartitionKeys(options, state).get(0);
+        Clustering<?> baseClusteringKey = selectStmt.getRestrictions().getClusteringColumns(options, state).iterator().next();
+
+        // build the primary key map
+        Map<ColumnMetadata, ByteBuffer> primaryKey = builder.getViewRawDataMap(modStmt.buildPartitionKeyNames(options, state).get(0),
+                                                                               Iterables.getOnlyElement(modStmt.createClustering(options, state)));
+        assertEquals(viewMetadata.partitionKeyColumns().size() + viewMetadata.clusteringColumns().size(), primaryKey.size());
+
+        // re-index the primaryKey by column name (ColumnIdentifier)
+        Map<ColumnIdentifier, ByteBuffer> viewPK = primaryKey.entrySet()
+                                                             .stream()
+                                                             .collect(Collectors.toMap(e -> e.getKey().name, Map.Entry::getValue));
+
+        // verify the partition key for base table is found in the view's primary key map, and the values match
+        if (baseTableMetadata.partitionKeyType instanceof CompositeType)
+        {
+            CompositeType compositeType = (CompositeType) baseTableMetadata.partitionKeyType;
+            ByteBuffer[] components = compositeType.split(basePartitionKey);
+
+            List<ColumnMetadata> pkCols = baseTableMetadata.partitionKeyColumns();
+            for (int i = 0; i < pkCols.size(); i++)
+            {
+                ColumnMetadata col = pkCols.get(i);
+                assertEquals(components[i], viewPK.get(col.name));
+            }
+        }
+        else
+            assertEquals(basePartitionKey, viewPK.get(baseTableMetadata.partitionKeyColumns().get(0).name));
+
+
+        // verify the clustering key for base table is found in the view's primary key map, and the values match
+        List<ColumnMetadata> ckCols = baseTableMetadata.clusteringColumns();
+        for (int i = 0; i < ckCols.size(); i++)
+        {
+            ColumnMetadata col = ckCols.get(i);
+            assertEquals(baseClusteringKey.bufferAt(col.position()), viewPK.get(col.name));
+        }
+    }
+}
