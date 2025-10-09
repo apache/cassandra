2c049a3129352d84497ded6efeaf0d52859b67e3
Pranav Shenoy <pranavshenoy06@gmail.com>
2025-10-08 23:05:57 -0700

    add SSTableFlushObserver#onSSTableWriterSwitched to flush SAI segment builder for written shards without waiting for entire transaction to complete. This reduces memory usage during sharded compaction.
        Flush segment builder when sstable writer is switched to free memory
        without waiting full compaction to complete
    
    Patch by Pranav Shenoy and reviewed by Caleb Rackliffe and Zhao Yang

--- src/java/org/apache/cassandra/io/sstable/SSTableRewriter.java
+++ src/java/org/apache/cassandra/io/sstable/SSTableRewriter.java
@@ -282,6 +282,7 @@ public class SSTableRewriter extends Transactional.AbstractTransactional impleme
 
         currentlyOpenedEarlyAt = 0;
         bytesWritten += writer.getFilePointer();
+        writer.onSSTableWriterSwitched();
         writer = newWriter;
     }
 
