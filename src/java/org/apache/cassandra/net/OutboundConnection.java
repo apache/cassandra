8eae9d30da6afb90fbdb3c795d8b54270660c5d3
vivekkoya <13vivekkoya@gmail.com>
2025-11-02 23:31:44 -0800

    Remove instanceof for subtype of Success (MessagingSuccess)

--- src/java/org/apache/cassandra/net/OutboundConnection.java
+++ src/java/org/apache/cassandra/net/OutboundConnection.java
@@ -52,6 +52,7 @@ import io.netty.util.concurrent.SucceededFuture;
 import org.apache.cassandra.config.DatabaseDescriptor;
 import org.apache.cassandra.io.util.DataOutputBufferFixed;
 import org.apache.cassandra.net.OutboundConnectionInitiator.Result.MessagingSuccess;
+import org.apache.cassandra.net.OutboundConnectionInitiator.Result.Success;
 import org.apache.cassandra.tracing.Tracing;
 import org.apache.cassandra.utils.FBUtilities;
 import org.apache.cassandra.utils.JVMStabilityInspector;
@@ -1118,7 +1119,7 @@ public class OutboundConnection
                     case SUCCESS:
                         // it is expected that close, if successful, has already cancelled us; so we do not need to worry about leaking connections
                         assert !state.isClosed();
-                        if(result.success() instanceof MessagingSuccess) {
+                        if(result.success() != null) {
                             MessagingSuccess success = (MessagingSuccess) result.success();
                             debug.onConnect(success.messagingVersion, settings);
                             state.disconnected().maintenance.cancel(false);
