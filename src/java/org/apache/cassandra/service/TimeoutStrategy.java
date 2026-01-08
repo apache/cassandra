386183fce11707f176b736d92e50cb75ad0680b8
Nivy Kani <nivy@apple.com>
2026-01-08 14:27:36 -0800

    Fix off-by-one bug in exponential backoff for repair retry config
    patch by Nivy Kani; reviewed by David Capwell, Jyothsna Konisa for CASSANDRA-21102

--- src/java/org/apache/cassandra/service/TimeoutStrategy.java
+++ src/java/org/apache/cassandra/service/TimeoutStrategy.java
@@ -98,7 +98,8 @@ public class TimeoutStrategy implements WaitStrategy
         default LatencyModifier identity() { return (l, a) -> l; }
         default LatencyModifier multiply(double constant) { return (l, a) -> saturatedCast(l * constant); }
         default LatencyModifier multiplyByAttempts(double multiply) { return (l, a) -> saturatedCast(l * multiply * a); }
-        default LatencyModifier multiplyByAttemptsExp(double base) { return (l, a) -> saturatedCast(l * pow(base, a)); }
+        // Ensure attempts is non-negative before subtracting 1.
+        default LatencyModifier multiplyByAttemptsExp(double base) { return (l, a) -> saturatedCast(l * pow(base, max(0, (max(a, 0) - 1)))); }
     }
 
     public interface Wait
