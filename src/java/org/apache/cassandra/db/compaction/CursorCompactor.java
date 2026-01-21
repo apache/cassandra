f2633cebbc3cf1852227fde283b37072b8f68af3
Stefan Miklosovic <smiklosovic@apache.org>
2026-01-21 21:15:29 +0100

    Do not serialize dropped columns to SSTable header after compaction
    patch by Stefan Miklosovic; reviewed by TBD for CASSANDRA-21000

--- src/java/org/apache/cassandra/db/compaction/CursorCompactor.java
+++ src/java/org/apache/cassandra/db/compaction/CursorCompactor.java
@@ -636,6 +636,16 @@ public class CursorCompactor extends CompactionInfo.Holder
         ReusableLivenessInfo cellLiveness = cellCursor.cellLiveness;
         DataOutputBuffer tempCellBuffer = null;
 
+        if (!metadata().regularAndStaticColumns().contains(cellCursor.cellColumn))
+        {
+            for (int i = 0; i < cellMergeLimit; i++)
+            {
+                if (sstableCursors[i].state() == CELL_VALUE_START)
+                    sstableCursors[i].skipCellValue();
+            }
+            return isRowDropped;
+        }
+
         if (cellCursor.cellColumn.isComplex())
             throw new UnsupportedOperationException("TODO: Not ready for complex cells.");
         if (cellCursor.cellColumn.isCounterColumn())
