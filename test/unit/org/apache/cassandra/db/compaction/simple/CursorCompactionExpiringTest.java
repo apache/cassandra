35045f66a6f3b5863bd42fe286d8d4074709e629
arvindksi274-ksolves <arvind.kandpal@ksolves.com>
2026-02-12 22:23:09 +0530

    CASSANDRA-21152 Fix zombie expired rows in CursorCompaction and disable MV support

---
+++ test/unit/org/apache/cassandra/db/compaction/simple/CursorCompactionExpiringTest.java
@@ -0,0 +1,71 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cassandra.db.compaction.simple;
+
+import org.junit.After;
+import org.junit.Assume;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.apache.cassandra.cql3.CQLTester;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.Keyspace;
+
+import static org.junit.Assert.assertEquals;
+
+public class CursorCompactionExpiringTest extends CQLTester
+{
+    private String originalCursorCompactionProperty;
+
+    @Before
+    public void setup()
+    {
+        originalCursorCompactionProperty = System.getProperty("cassandra.cursor_compaction_enabled");
+        System.setProperty("cassandra.cursor_compaction_enabled", "true");
+    }
+
+    @After
+    public void teardown()
+    {
+        if (originalCursorCompactionProperty != null)
+            System.setProperty("cassandra.cursor_compaction_enabled", originalCursorCompactionProperty);
+        else
+            System.clearProperty("cassandra.cursor_compaction_enabled");
+    }
+
+    @Test
+    public void testExpiredRowPurge() throws Throwable
+    {
+        Assume.assumeTrue("Skipping test because Cursor Compaction is disabled.", Boolean.getBoolean("cassandra.cursor_compaction_enabled"));
+
+        createTable("CREATE TABLE %s (k int PRIMARY KEY, v int) WITH gc_grace_seconds=10000");
+        execute("INSERT INTO %s (k) VALUES (1) USING TTL 1");
+
+        flush();
+
+        ColumnFamilyStore cfs = Keyspace.open(KEYSPACE).getColumnFamilyStore(currentTable());
+        assertEquals("Expected 1 SSTable after flush", 1, cfs.getLiveSSTables().size());
+
+        Thread.sleep(2000);
+        compact();
+
+        assertEquals("Expected 0 SSTables after compacting fully expired table, but found zombie data.", 
+                     0, cfs.getLiveSSTables().size());
+    }
+}
