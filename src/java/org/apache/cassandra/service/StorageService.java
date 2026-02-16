3353bc9c4bb3f24e2941ede5f5121b858a42454a
Caleb Rackliffe <calebrackliffe@gmail.com>
2026-02-16 17:39:37 -0600

    Optionally force IndexStatusManager to use the optimized index status format
    patch by Caleb Rackliffe; reviewed by Jon Meredith for CASSANDRA-21132

--- src/java/org/apache/cassandra/service/StorageService.java
+++ src/java/org/apache/cassandra/service/StorageService.java
@@ -5719,6 +5719,18 @@ public class StorageService extends NotificationBroadcasterSupport implements IE
     }
 
     @Override
+    public boolean getForceOptimizedIndexStatusFormat()
+    {
+        return DatabaseDescriptor.getForceOptimizedIndexStatusFormat();
+    }
+
+    @Override
+    public void setForceOptimizedIndexStatusFormat(boolean value)
+    {
+        DatabaseDescriptor.setForceOptimizedIndexStatusFormat(value);
+    }
+
+    @Override
     public void setPaxosRepairRaceWait(boolean paxosRepairRaceWait)
     {
         DatabaseDescriptor.setPaxosRepairRaceWait(paxosRepairRaceWait);
