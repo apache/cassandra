29ec0f0453ff895dbf894ab370075fc5f158412c
Blake Eggleston <blake@ultrablake.com>
2026-02-17 13:48:28 -0800

    ninja - fix missing import

--- src/java/org/apache/cassandra/db/streaming/CassandraStreamReceiver.java
+++ src/java/org/apache/cassandra/db/streaming/CassandraStreamReceiver.java
@@ -27,6 +27,7 @@ import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterables;
 
+import org.apache.cassandra.streaming.StreamOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
