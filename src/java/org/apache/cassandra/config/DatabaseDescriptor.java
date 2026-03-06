dfdb7bebb08a9a161be0bd6e80127c1108dc8f94
Tina Haiser <tinahaiser@gmail.com>
2026-03-06 13:50:35 +0100

    CASSANDRA-21196: Include tools in applyCompatibilityMode check
    uses correct compound method

--- src/java/org/apache/cassandra/config/DatabaseDescriptor.java
+++ src/java/org/apache/cassandra/config/DatabaseDescriptor.java
@@ -1599,7 +1599,7 @@ public class DatabaseDescriptor
 
     private static void applyCompatibilityMode()
     {
-        if (isClientInitialized())
+        if (isClientOrToolInitialized())
             // tools or clients should not limit the sstable formats they support
             storageCompatibilityMode = StorageCompatibilityMode.NONE;
         else if (conf != null && conf.storage_compatibility_mode != null)
