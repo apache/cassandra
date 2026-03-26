aff2f725ba4246ee067c483359b772c0480f7e18
Joel Shepherd <shepherd@amazon.com>
2026-03-26 00:32:06 +0000

    Missed from previous commit: d-test AuthTest calls CassandraDaemon to set up auth

--- test/distributed/org/apache/cassandra/distributed/test/AuthTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/AuthTest.java
@@ -37,7 +37,7 @@ import org.apache.cassandra.distributed.api.IMessageFilters.Filter;
 import org.apache.cassandra.distributed.api.TokenSupplier;
 import org.apache.cassandra.locator.SimpleSeedProvider;
 import org.apache.cassandra.net.Verb;
-import org.apache.cassandra.service.StorageService;
+import org.apache.cassandra.service.CassandraDaemon;
 
 import static java.util.concurrent.TimeUnit.SECONDS;
 import static org.apache.cassandra.distributed.api.Feature.GOSSIP;
@@ -64,7 +64,7 @@ public class AuthTest extends TestBaseImpl
             await().pollDelay(1, SECONDS)
                    .pollInterval(1, SECONDS)
                    .atMost(10, SECONDS)
-                   .until(() -> instance.callOnInstance(() -> StorageService.instance.authSetupCalled()));
+                   .until(() -> instance.callOnInstance(() -> CassandraDaemon.authSetupCalled()));
         }
     }
 
