1a9ab95c203c511ba13c209768f08d887602ed2e
Dmitry Konstantinov <netudima@gmail.com>
2026-03-29 17:30:53 +0100

    Fix a removed TTLed row re-appearance in a materialized view after a cursor compaction
    Add an implementation of isExpired to ReusableLivenessInfo, before the change the method implementation was interited from an interface and always returned false, so livenessInfo().supersedes(...) did not work properly.
    
    patch by Dmitry Konstantinov; reviewed by Branimir Lambov, Nitsan Wakart for CASSANDRA-21152

---
+++ test/unit/org/apache/cassandra/cql3/ViewCompactionTest.java
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.cql3;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+import org.apache.cassandra.Util;
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.db.Keyspace;
+
+public class ViewCompactionTest extends ViewAbstractTest
+{
+
+    @Test
+    public void testCompactionOfDeletedRowWithTtl() throws Throwable
+    {
+        createTable("CREATE TABLE %s (k int, a int, b int, c int, primary key(k, a)) with default_time_to_live=6000");
+        createView("CREATE MATERIALIZED VIEW %s AS SELECT k,a,b FROM %s WHERE k IS NOT NULL AND a IS NOT NULL PRIMARY KEY (a, k)");
+
+        execute("UPDATE %s SET c=2 WHERE k=1 AND a=1");
+        flushView();
+        assertRows(execute("SELECT k,a,b,c FROM %s"), row(1, 1, null, 2));
+        assertRows(executeView("SELECT k,a,b FROM %s"), row(1, 1, null));
+
+        compact(keyspace(), currentView());
+
+        assertRows(execute("SELECT k,a,b,c FROM %s"), row(1, 1, null, 2));
+        assertRows(executeView("SELECT k,a,b FROM %s"), row(1, 1, null));
+
+        execute("DELETE c FROM %s WHERE k=1 AND a=1");
+        flushView();
+
+        assertRows(execute("SELECT k,a,b,c FROM %s"), Collections.emptyList());
+        assertRows(executeView("SELECT k,a,b FROM %s"), Collections.emptyList());
+
+        compact(keyspace(), currentView());
+
+        assertRows(execute("SELECT k,a,b,c FROM %s"), Collections.emptyList());
+        assertRows(executeView("SELECT k,a,b FROM %s"), Collections.emptyList());
+    }
+
+    private void flushView()
+    {
+        ColumnFamilyStore cfs = Keyspace.open(keyspace()).getColumnFamilyStore(currentView());
+        Util.flush(cfs);
+    }
+}
