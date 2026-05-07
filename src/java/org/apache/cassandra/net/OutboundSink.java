mahmudulhasancsedu5/cassandra-ucs

CASSANDRA-17024: Artificial Latency Injection

src/java/org/apache/cassandra/net/OutboundSink.java
@@ -38,12 +38,17 @@ public class OutboundSink
         void accept(Message<?> message, InetAddressAndPort to, ConnectionType connectionType);
     }
 
+    public interface Filter
+    {
+        public boolean test(Message<?> message, InetAddressAndPort to, ConnectionType type);
+    }
+
     private static class Filtered implements Sink
     {
-        final BiPredicate<Message<?>, InetAddressAndPort> condition;
+        final Filter condition;
         final Sink next;
 
-        private Filtered(BiPredicate<Message<?>, InetAddressAndPort> condition, Sink next)
+        private Filtered(Filter condition, Sink next)
         {
             this.condition = condition;
             this.next = next;
@@ -51,7 +56,7 @@ public class OutboundSink
 
         public void accept(Message<?> message, InetAddressAndPort to, ConnectionType connectionType)
         {
-            if (condition.test(message, to))
+            if (condition.test(message, to, connectionType))
                 next.accept(message, to, connectionType);
         }
     }
@@ -70,12 +75,12 @@ public class OutboundSink
         sink.accept(message, to, connectionType);
     }
 
-    public void add(BiPredicate<Message<?>, InetAddressAndPort> allow)
+    public void push(Filter allow)
     {
         sinkUpdater.updateAndGet(this, sink -> new Filtered(allow, sink));
     }
 
-    public void remove(BiPredicate<Message<?>, InetAddressAndPort> allow)
+    public void remove(Filter allow)
     {
         sinkUpdater.updateAndGet(this, sink -> without(sink, allow));
     }
@@ -92,7 +97,7 @@ public class OutboundSink
         return sink;
     }
 
-    private static Sink without(Sink sink, BiPredicate<Message<?>, InetAddressAndPort> condition)
+    private static Sink without(Sink sink, Filter condition)
     {
         if (!(sink instanceof Filtered))
             return sink;

test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -100,6 +100,7 @@ import org.apache.cassandra.io.util.FileUtils;
 import org.apache.cassandra.locator.InetAddressAndPort;
 import org.apache.cassandra.metrics.CassandraMetricsRegistry;
 import org.apache.cassandra.metrics.Sampler;
+import org.apache.cassandra.net.ArtificialLatency;
 import org.apache.cassandra.net.Message;
 import org.apache.cassandra.net.MessagingService;
 import org.apache.cassandra.net.NoPayload;
@@ -312,8 +313,11 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
 
     private void registerMockMessaging(ICluster<?> cluster)
     {
-        MessagingService.instance().outboundSink.add((message, to) -> {
-            cluster.deliverMessage(to, serializeMessage(message.from(), to, message));
+        MessagingService.instance().outboundSink.push((message, to, type) -> {
+            InetSocketAddress toAddr = fromCassandraInetAddressAndPort(to);
+            IInstance toInstance = cluster.get(toAddr);
+            if (toInstance != null)
+                toInstance.receiveMessage(serializeMessage(message.from(), to, message));
             return false;
         });
     }
@@ -337,7 +341,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
 
     private void registerOutboundFilter(ICluster cluster)
     {
-        MessagingService.instance().outboundSink.add((message, to) -> {
+        MessagingService.instance().outboundSink.push((message, to, type) -> {
             if (isShutdown())
                 return false;
             IMessage serialzied = serializeMessage(message.from(), to, message);
@@ -605,6 +609,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
 //                    -- not sure what that means?  SocketFactory.instance.getClass();
                     registerMockMessaging(cluster);
                 }
+                ArtificialLatency.touch();
                 registerInboundFilter(cluster);
                 registerOutboundFilter(cluster);
 

test/unit/org/apache/cassandra/db/compaction/CompactionIteratorTest.java
@@ -472,7 +472,7 @@ public class CompactionIteratorTest extends CQLTester
         TableMetadata metadata = getCurrentColumnFamilyStore().metadata();
 
         final HashMap<InetAddressAndPort, Message<?>> sentMessages = new HashMap<>();
-        MessagingService.instance().outboundSink.add((message, to) -> { sentMessages.put(to, message); return false;});
+        MessagingService.instance().outboundSink.push((message, to, type) -> { sentMessages.put(to, message); return false;});
 
         // no duplicates
         sentMessages.clear();

test/unit/org/apache/cassandra/repair/RepairJobTest.java
@@ -844,7 +844,7 @@ public class RepairJobTest
                                          List<Message<?>> messageCapture)
     {
         MessagingService.instance().inboundSink.add(message -> message.verb().isResponse());
-        MessagingService.instance().outboundSink.add((message, to) -> {
+        MessagingService.instance().outboundSink.push((message, to, type) -> {
             if (message == null || !(message.payload instanceof RepairMessage))
                 return false;
 

apache/cassandra

src/java/org/apache/cassandra/net/OutboundSink.java
@@ -38,12 +38,17 @@ public class OutboundSink
         void accept(Message<?> message, InetAddressAndPort to, ConnectionType connectionType);
     }
 
+    public interface Filter
+    {
+        public boolean test(Message<?> message, InetAddressAndPort to, ConnectionType type);
+    }
+
     private static class Filtered implements Sink
     {
-        final BiPredicate<Message<?>, InetAddressAndPort> condition;
+        final Filter condition;
         final Sink next;
 
-        private Filtered(BiPredicate<Message<?>, InetAddressAndPort> condition, Sink next)
+        private Filtered(Filter condition, Sink next)
         {
             this.condition = condition;
             this.next = next;
@@ -51,7 +56,7 @@ public class OutboundSink
 
         public void accept(Message<?> message, InetAddressAndPort to, ConnectionType connectionType)
         {
-            if (condition.test(message, to))
+            if (condition.test(message, to, connectionType))
                 next.accept(message, to, connectionType);
         }
     }
@@ -70,12 +75,12 @@ public class OutboundSink
         sink.accept(message, to, connectionType);
     }
 
-    public void add(BiPredicate<Message<?>, InetAddressAndPort> allow)
+    public void add(Filter allow)
     {
         sinkUpdater.updateAndGet(this, sink -> new Filtered(allow, sink));
     }
 
-    public void remove(BiPredicate<Message<?>, InetAddressAndPort> allow)
+    public void remove(Filter allow)
     {
         sinkUpdater.updateAndGet(this, sink -> without(sink, allow));
     }
@@ -92,7 +97,7 @@ public class OutboundSink
         return sink;
     }
 
-    private static Sink without(Sink sink, BiPredicate<Message<?>, InetAddressAndPort> condition)
+    private static Sink without(Sink sink, Filter condition)
     {
         if (!(sink instanceof Filtered))
             return sink;

test/distributed/org/apache/cassandra/distributed/impl/Instance.java
@@ -121,6 +121,7 @@ import org.apache.cassandra.locator.InetAddressAndPort;
 import org.apache.cassandra.metrics.CassandraMetricsRegistry;
 import org.apache.cassandra.metrics.Sampler;
 import org.apache.cassandra.metrics.ThreadLocalMetrics;
+import org.apache.cassandra.net.ArtificialLatency;
 import org.apache.cassandra.net.Message;
 import org.apache.cassandra.net.MessagingService;
 import org.apache.cassandra.net.NoPayload;
@@ -374,7 +375,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
 
     protected void registerMockMessaging(ICluster<?> cluster)
     {
-        MessagingService.instance().outboundSink.add((message, to) -> {
+        MessagingService.instance().outboundSink.add((message, to, type) -> {
             if (!internodeMessagingStarted)
             {
                 inInstancelogger.debug("Dropping outbound message {} to {} as internode messaging has not been started yet",
@@ -405,7 +406,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
 
     protected void registerOutboundFilter(ICluster cluster)
     {
-        MessagingService.instance().outboundSink.add((message, to) -> {
+        MessagingService.instance().outboundSink.add((message, to, type) -> {
             if (isShutdown())
                 return false; // TODO: Simulator needs this to trigger a failure
             int fromNum = config.num(); // since this instance is sending the message, from will always be this instance
@@ -678,6 +679,7 @@ public class Instance extends IsolatedExecutor implements IInvokableInstance
                 {
                     partialStartup(cluster);
                 }
+                ArtificialLatency.touch();
             }
             catch (Throwable t)
             {

test/unit/org/apache/cassandra/db/compaction/CompactionIteratorTest.java
@@ -505,7 +505,7 @@ public class CompactionIteratorTest extends CQLTester
         TableMetadata metadata = getCurrentColumnFamilyStore().metadata();
 
         final HashMap<InetAddressAndPort, Message<?>> sentMessages = new HashMap<>();
-        MessagingService.instance().outboundSink.add((message, to) -> { sentMessages.put(to, message); return false;});
+        MessagingService.instance().outboundSink.add((message, to, type) -> { sentMessages.put(to, message); return false;});
 
         // no duplicates
         sentMessages.clear();

test/unit/org/apache/cassandra/repair/RepairJobTest.java
@@ -880,7 +880,7 @@ public class RepairJobTest
                                          List<Message<?>> messageCapture)
     {
         MessagingService.instance().inboundSink.add(message -> message.verb().isResponse());
-        MessagingService.instance().outboundSink.add((message, to) -> {
+        MessagingService.instance().outboundSink.add((message, to, type) -> {
                 if (message == null || !(message.payload instanceof RepairMessage))
                     return false;
 