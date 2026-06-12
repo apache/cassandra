f2dd66968910d846aca321c1c3644b895414882b
Jon Meredith <jonmeredith@apache.org>
2026-06-12 14:26:26 +0100

    Add policy for selecting CMS host when submitting commit request
    Patch by Jon Meredith; reviewed by Sam Tunnicliffe for CASSANDRA-21456

--- test/unit/org/apache/cassandra/config/DatabaseDescriptorRefTest.java
+++ test/unit/org/apache/cassandra/config/DatabaseDescriptorRefTest.java
@@ -96,6 +96,7 @@ public class DatabaseDescriptorRefTest
     "org.apache.cassandra.config.CassandraRelevantProperties$PropertyConverter",
     "org.apache.cassandra.config.Config",
     "org.apache.cassandra.config.Config$1",
+    "org.apache.cassandra.config.Config$CMSCommitMemberPreferencePolicy",
     "org.apache.cassandra.config.Config$CommitFailurePolicy",
     "org.apache.cassandra.config.Config$CQLStartTime",
     "org.apache.cassandra.config.Config$CommitLogSync",
