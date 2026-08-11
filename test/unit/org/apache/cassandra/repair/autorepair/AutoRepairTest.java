1b50c70deb3d3ac94233379e5f0c7c3fcf6c385c
tommy stendahl <tommy.stendahl@ericsson.com>
2026-08-11 13:15:34 +0200

    Restore old value for repairTaskMinDuration

--- test/unit/org/apache/cassandra/repair/autorepair/AutoRepairTest.java
+++ test/unit/org/apache/cassandra/repair/autorepair/AutoRepairTest.java
@@ -272,6 +272,7 @@ public class AutoRepairTest extends CQLTester
         UUID myId = StorageService.instance.getHostIdForEndpoint(FBUtilities.getBroadcastAddressAndPort());
         long now = System.currentTimeMillis();
 
+        DurationSpec.LongSecondsBound repairTaskMinDuration = DatabaseDescriptor.getAutoRepairConfig().getRepairTaskMinDuration();
         // Ensure repair tasks don't artificially sleep
         DatabaseDescriptor.getAutoRepairConfig().setRepairTaskMinDuration("0s");
 
@@ -297,6 +298,9 @@ public class AutoRepairTest extends CQLTester
         assertTrue("repair_finish_ts should advance after force repair runs, but was "
                    + finishTimeBefore + " -> " + finishTimeAfter,
                    finishTimeAfter > finishTimeBefore);
+
+        // Restore original value
+        DatabaseDescriptor.getAutoRepairConfig().setRepairTaskMinDuration(repairTaskMinDuration.toString());
     }
 
     @Test
