3d431b9ba4c49de182142e3900b96c2d41bc6da8
Marcus Eriksson <marcuse@apache.org>
2026-08-19 08:45:55 +0200

    Avoid "During operation execution,... at ONE the ring has changed from epoch1 to epoch2 error" with counter writes

--- test/distributed/org/apache/cassandra/distributed/test/tcm/FailureDetectorRecomputeTest.java
+++ test/distributed/org/apache/cassandra/distributed/test/tcm/FailureDetectorRecomputeTest.java
@@ -31,6 +31,7 @@ import org.junit.Test;
 
 import org.apache.cassandra.distributed.Cluster;
 import org.apache.cassandra.distributed.api.ConsistencyLevel;
+import org.apache.cassandra.distributed.api.Feature;
 import org.apache.cassandra.distributed.test.TestBaseImpl;
 import org.apache.cassandra.gms.FailureDetector;
 import org.apache.cassandra.locator.InetAddressAndPort;
@@ -69,6 +70,21 @@ public class FailureDetectorRecomputeTest extends TestBaseImpl
             cluster.get(1).runOnInstance(() -> BB.enabled.set(true));
             for (int i = 0; i < 10; i++)
                 cluster.coordinator(1).execute(withKeyspace("insert into %s.tbl (id) values (?)"), ConsistencyLevel.QUORUM, i);
+        }
+    }
+
+    @Test
+    public void counterWriteOneTest() throws IOException
+    {
+        try (Cluster cluster = init(Cluster.build(3)
+                                           .withInstanceInitializer(BB::install)
+                                           .withConfig(c -> c.with(Feature.values()))
+                                           .start()))
+        {
+            cluster.schemaChange(withKeyspace("create table %s.tbl (id int primary key, c counter)"));
+            cluster.get(1).runOnInstance(() -> BB.enabled.set(true));
+            for (int i = 0; i < 10; i++)
+                cluster.coordinator(1).execute(withKeyspace("update %s.tbl set c = c + 1 where id = ?"), ConsistencyLevel.ONE, i);
         }
     }
 
