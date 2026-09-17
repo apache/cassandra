bdfa317387850342260103259dcfd334c45254e4
Stefan Miklosovic <smiklosovic@apache.org>
2026-09-17 19:01:55 +0200

    Implement Zstd compression level guardrail
    patch by Stefan Miklosovic; reviewed by Yifan Cai for CASSANDRA-21021

--- src/java/org/apache/cassandra/db/guardrails/GuardrailsMBean.java
+++ src/java/org/apache/cassandra/db/guardrails/GuardrailsMBean.java
@@ -1249,4 +1249,20 @@ public interface GuardrailsMBean
      * @param value JSON representation of a map of driver name to minimum version string.
      */
     void setMinimumClientDriverVersionsDisallowed(String value);
+
+    /**
+     * @return The threshold to warn when creating a table with a Zstd compression level above it.
+     */
+    int getZstdCompressionLevelWarnThreshold();
+
+    /**
+     * @return The threshold to fail when creating a table with a Zstd compression level above it.
+     */
+    int getZstdCompressionLevelFailThreshold();
+
+    /**
+     * @param warn The threshold to warn when creating a table with a Zstd compression level above it. -1 means disabled.
+     * @param fail The threshold to prevent creating a table with a Zstd compression level above it. -1 means disabled.
+     */
+    void setZstdCompressionLevelThreshold(int warn, int fail);
 }
